ed26ee7c2eeaa8b1cdaab44ec99f2c0d7af02b3b
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2005-05-05 08:15:06 +0000

    Tidying interfaces
    git-svn-id: http://subversion.assembla.com/svn/Debrief@13 cb33b658-6c9e-41a7-9690-cba343611204

---
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/DataTypes/Temporal/ControllableTime.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.mwc.cmap.core.DataTypes.Temporal;
+
+import MWC.GenericData.HiResDate;
+
+/**
+ * Interface for objects for whom their time attribute may be 
+ * externally controlled.
+ * @author ian.mayo
+ *
+ */
+public interface ControllableTime {
+	
+	/** set the new time
+	 * 
+	 * @param origin - an identifier for the object sending the update,
+	 * provided largely so that triggering object can ignore time 
+	 * changes it originally created
+	 * @param newDate - the new DTG to use
+	 */
+	public void setTime(Object origin, HiResDate newDate);
+	
+}
---
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/DataTypes/Temporal/TimeManager.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package org.mwc.cmap.core.DataTypes.Temporal;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+import MWC.GenericData.HiResDate;
+import MWC.GenericData.TimePeriod;
+
+/**
+ * @author ian.mayo
+ */
+public class TimeManager implements ControllableTime, TimeProvider
+{
+
+	/** manage all of the listeners, etc.
+	 * 
+	 */
+	private PropertyChangeSupport _pSupport;
+	
+	/** the current time
+	 * 
+	 */
+	private HiResDate _currentTime;
+	
+	/** the time period covered by the data
+	 * 
+	 */
+	private TimePeriod _timePeriod;
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.mwc.cmap.core.DataTypes.Temporal.ControllableTime#setTime(java.lang.Object,
+	 *      MWC.GenericData.HiResDate)
+	 */
+	public void setTime(Object origin, HiResDate newDate)
+	{
+		// are we managing our own time period?
+		if(_timePeriod != null)
+		{
+			if(!_timePeriod.contains(newDate))
+			{
+				// bugger. it's outside the time period
+				// throw a wobbly
+				throw new RuntimeException("Invalid time provided");
+			}
+		}
+		
+		// ok. remember the old time (if we have one)
+		HiResDate oldTime = null;
+		if(_currentTime != null)
+			oldTime = new HiResDate(_currentTime);
+		
+		// store the new time
+		_currentTime = newDate;
+		
+		// do we have any listeners?
+		if(_pSupport != null)
+		{
+			_pSupport.firePropertyChange(TIME_CHANGED_PROPERTY_NAME, oldTime, _currentTime);
+		}
+		
+		// done.
+	}
+
+	
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.mwc.cmap.core.DataTypes.Temporal.TimeProvider#getPeriod()
+	 */
+	public TimePeriod getPeriod()
+	{
+		return _timePeriod;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.mwc.cmap.core.DataTypes.Temporal.TimeProvider#getTime()
+	 */
+	public HiResDate getTime()
+	{
+		return _currentTime;
+	}
+
+	/** let somebody start listening to our changes
+	 * 
+	 * @param listener the new listener
+	 * @param propertyType the (optional) property to listen to. Use null if you don't mind
+	 */
+	public void addListener(PropertyChangeListener listener, String propertyType)
+	{
+		if(_pSupport == null)
+			_pSupport = new PropertyChangeSupport(this);
+		
+		_pSupport.addPropertyChangeListener(propertyType, listener);
+	}
+
+	/** let somebody stop listening to our changes
+	 * 
+	 * @param listener the old listener
+	 * @param propertyType the (optional) property to stop listening to. Use null if you don't mind
+	 */
+	public void removeListener(PropertyChangeListener listener, String propertyType)
+	{
+		_pSupport.removePropertyChangeListener(propertyType, listener);
+	}
+
+
+
+	/** let somebody specify the time period we're managing.
+	 * @param origin - whoever is setting the time period (so that they can optionally ignore changes they triggered)
+	 * @param period - the new time period
+	 */
+	public void setPeriod(Object origin, TimePeriod period)
+	{
+		// ok. remember the old period
+		TimePeriod oldTime = new TimePeriod.BaseTimePeriod(_timePeriod.getStartDTG(), _timePeriod.getEndDTG());
+		
+		// store the new time
+		_timePeriod = period;
+		
+		// do we have any listeners?
+		if(_pSupport != null)
+		{
+			_pSupport.firePropertyChange(PERIOD_CHANGED_PROPERTY_NAME, oldTime, _currentTime);
+		}
+		
+	}
+
+}
