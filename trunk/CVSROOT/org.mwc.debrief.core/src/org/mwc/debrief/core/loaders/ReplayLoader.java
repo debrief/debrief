33d039bb63c173da7f709f30a359d2ed40e9b6d8
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2005-05-11 13:52:46 +0000

    Start fiddling with legacy importers
    git-svn-id: http://subversion.assembla.com/svn/Debrief@41 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.debrief.core/src/org/mwc/debrief/core/loaders/ReplayLoader.java
+++ trunk/CVSROOT/org.mwc.debrief.core/src/org/mwc/debrief/core/loaders/ReplayLoader.java
@@ -3,9 +3,17 @@
  */
 package org.mwc.debrief.core.loaders;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.mwc.cmap.plotViewer.editors.PlotEditor;
 import org.mwc.debrief.core.interfaces.IPlotLoader;
+
+import MWC.GUI.Layers;
 
 /**
  * @author ian.mayo
@@ -20,6 +28,12 @@ public class ReplayLoader extends IPlotLoader.BaseLoader
 	public void loadFile(PlotEditor thePlot, IEditorInput input)
 	{
 		String source = super.getFileName(input);
+
+//		IFileEditorInput ife = null;
+		final Layers theLayers = (Layers) thePlot.getAdapter(Layers.class);
+
+		Object importer = new Debrief.ReaderWriter.Replay.ImportReplay();		
+
 		
 		// ok, load the data...
 		System.out.println(getName() + " LOADER: loading data");
