126bd864cb422f6d5d92007c8ec4057c7c804d81
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2005-06-15 14:22:52 +0000

    Add WMF export functionality
    git-svn-id: http://subversion.assembla.com/svn/Debrief@225 cb33b658-6c9e-41a7-9690-cba343611204

---
+++ trunk/CVSROOT/org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/actions/ExportWMF.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.mwc.cmap.plotViewer.actions;
+
+import org.eclipse.core.runtime.Status;
+import org.mwc.cmap.core.CorePlugin;
+import org.mwc.cmap.plotViewer.editors.chart.SWTCanvas;
+
+import MWC.Algorithms.PlainProjection;
+import MWC.GUI.*;
+import MWC.GUI.Canvas.MetafileCanvas;
+import MWC.GUI.Tools.Chart.WriteMetafile;
+
+/**
+ * @author ian.mayo
+ *
+ */
+public class ExportWMF extends CoreEditorAction
+{
+	public static ToolParent _theParent = null;
+
+	/** ok, store who the parent is for the operation
+	 * 
+	 * @param theParent
+	 */
+	public static void init(ToolParent theParent)
+	{
+		_theParent = theParent;
+	}
+	
+	
+	/** and execute..
+	 * 
+	 *
+	 */
+	protected void run()
+	{
+		final PlainChart theChart = getChart();
+		
+		if(_theParent == null)
+		{
+			CorePlugin.logError(Status.ERROR, "Tool parent missing for Write Metafile", null);
+			return;
+		}
+		
+		WriteMetafile write = new WriteMetafile(_theParent, theChart, theChart.getLayers())
+		{
+
+			/**
+			 * @param mf
+			 */
+			protected void paintToMetafile(MetafileCanvas mf)
+			{
+				SWTCanvas sc = (SWTCanvas) theChart.getCanvas();
+		    sc.paintPlot(mf);
+		  }
+			
+		};
+		write.execute();
+	}
+	
+}
