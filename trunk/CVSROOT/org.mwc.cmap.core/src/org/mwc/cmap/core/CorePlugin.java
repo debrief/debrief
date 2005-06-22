c91b7b5d4f39a7264a8ec43754b8ad70a0a7c4c3
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2005-06-22 13:19:50 +0000

    Put the clipboard management bits here so that everybody can see them
    git-svn-id: http://subversion.assembla.com/svn/Debrief@239 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/CorePlugin.java
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/CorePlugin.java
@@ -16,6 +16,7 @@ import org.osgi.framework.BundleContext;
 import Debrief.Tools.Tote.Calculations.rangeCalc;
 import MWC.GUI.ToolParent;
 import MWC.GUI.Tools.Palette.*;
+import MWC.GenericData.WorldLocation;
 
 /**
  * The main plugin class to be used in the desktop.
@@ -48,6 +49,11 @@ public class CorePlugin extends AbstractUIPlugin
 	 * 
 	 */
 	private Clipboard _myClipboard;
+
+	/** fixed string used to indicate a string is in our location format
+	 * 
+	 */
+	public static final String LOCATION_STRING_IDENTIFIER = "LOC:";
 
 	/**
 	 * The constructor.
@@ -150,6 +156,47 @@ public class CorePlugin extends AbstractUIPlugin
 			resourceBundle = null;
 		}
 		return resourceBundle;
+	}
+
+	/** convenience method to assist in extracting a location from the clipboard
+	 * 
+	 * @param txt
+	 * @return
+	 */
+	public static WorldLocation fromClipboard(String txt)
+	{		
+		// get rid of the title
+		String dataPart = txt.substring(LOCATION_STRING_IDENTIFIER.length(), txt.length());
+	  StringTokenizer st = new StringTokenizer(dataPart);
+	  String latP = st.nextToken(",");
+	  String longP = st.nextToken(",");
+	  String depthP = st.nextToken();
+	  Double _lat = new Double(latP);
+	  Double _long = new Double(longP);
+	  Double _depth = new Double(depthP);
+	  WorldLocation res = new WorldLocation(_lat.doubleValue(), _long.doubleValue(), _depth.doubleValue());
+		return res;
+	}
+
+	/** convenience method to check if a string is in our format
+	 * 
+	 * @param txt
+	 * @return
+	 */
+	public static boolean isLocation(String txt)
+	{
+		return txt.startsWith(LOCATION_STRING_IDENTIFIER);
+	}
+
+	/** convenience method to assist placing locations on the clipboard
+	 * 
+	 * @param loc
+	 * @return
+	 */
+	public static String toClipboard(WorldLocation loc)
+	{
+		String res = LOCATION_STRING_IDENTIFIER + loc.getLat() + "," + loc.getLong() + "," + loc.getDepth();
+		return res;
 	}
 
 	/**
