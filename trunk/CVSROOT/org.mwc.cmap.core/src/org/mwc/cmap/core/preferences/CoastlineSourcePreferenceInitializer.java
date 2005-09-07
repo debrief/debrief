3a4c26664a3fbb2b54c1e97a8368900a6b2796f3
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2005-09-07 15:01:51 +0000

    First versions
    git-svn-id: http://subversion.assembla.com/svn/Debrief@300 cb33b658-6c9e-41a7-9690-cba343611204

---
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/preferences/CoastlineSourcePreferenceInitializer.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.mwc.cmap.core.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.mwc.cmap.core.CorePlugin;
+
+public class CoastlineSourcePreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = CorePlugin.getDefault()
+				.getPreferenceStore();
+	}
+
+}
