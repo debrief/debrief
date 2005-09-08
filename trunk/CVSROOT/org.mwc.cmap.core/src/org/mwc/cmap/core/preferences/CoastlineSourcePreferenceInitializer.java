10aad359be60d58587f9a42cd2fce63c96ba2cc6
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2005-09-08 08:33:33 +0000

    Sort out initial location of world data-file
    git-svn-id: http://subversion.assembla.com/svn/Debrief@306 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/preferences/CoastlineSourcePreferenceInitializer.java
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/preferences/CoastlineSourcePreferenceInitializer.java
@@ -3,6 +3,10 @@
  */
 package org.mwc.cmap.core.preferences;
 
+import java.io.*;
+import java.net.URL;
+
+import org.eclipse.core.runtime.*;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.mwc.cmap.core.CorePlugin;
@@ -15,8 +19,26 @@ public class CoastlineSourcePreferenceInitializer extends AbstractPreferenceInit
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
 	 */
 	public void initializeDefaultPreferences() {
-		IPreferenceStore store = CorePlugin.getDefault()
-				.getPreferenceStore();
+		
+		// hmm, set the default location
+		Path coastPath = new Path("data/world.dat");
+		
+		// and get the relative path compared to the Core Plugin
+		URL fileURL = Platform.find(CorePlugin.getDefault().getBundle(), coastPath);
+		
+		// right, that's the relative location, switch to absolute path
+		try
+		{
+			fileURL = Platform.asLocalURL(fileURL);
+		}
+		catch (IOException e)
+		{
+			CorePlugin.logError(Status.ERROR, "Unable to find coastline data-file", e);
+		}
+		
+		// and store the default location
+		IPreferenceStore store = CorePlugin.getDefault().getPreferenceStore();
+		store.setDefault(CoastlineSourcePrefsPage.PreferenceConstants.COASTLINE_FILE,fileURL.getFile());
 	}
 
 }
