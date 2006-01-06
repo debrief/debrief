651decc2309169766e1b2ff3033d61d2fe390ad7
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-01-06 15:43:14 +0000

    Include our auto-ranging of segments
    git-svn-id: http://subversion.assembla.com/svn/Debrief@519 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.TimeController/src/com/borlander/rac353542/bislider/cdata/CalendarDateSuite.java
+++ trunk/CVSROOT/org.mwc.cmap.TimeController/src/com/borlander/rac353542/bislider/cdata/CalendarDateSuite.java
@@ -1,8 +1,8 @@
 package com.borlander.rac353542.bislider.cdata;
 
+import java.io.Serializable;
 import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 import com.borlander.rac353542.bislider.*;
 
@@ -41,7 +41,9 @@ public class CalendarDateSuite {
     }
 
     public static class CalendarDateModel extends DataObjectDataModel {
-        public CalendarDateModel() {
+        private Label_Limit[] _limits;
+
+				public CalendarDateModel() {
             super(CALENDAR_DATE);
         }
 
@@ -71,7 +73,82 @@ public class CalendarDateSuite {
 
         public void setTotalRange(Date minimum, Date maximum) {
             setTotalObjectRange(minimum, maximum);
+            rescaleSegments(maximum.getTime() - minimum.getTime());
         }
+        
+
+        // ok, sort out a nice, tidy segment size to use for our dates
+        private void rescaleSegments(long timeRange)
+        {
+        	long scaleStep;
+
+          // check we have our set of data
+          if (_limits == null)
+            initialiseLimits();
+
+          // find the range we are working in
+          int counter = 0;
+          while ((counter < _limits.length) &&
+            (timeRange > _limits[counter].upper_limit))
+          {
+            counter++;
+          }    	
+          
+          scaleStep = _limits[counter].increment;
+          
+          setSegmentLength(scaleStep);
+        }
+        
+        /**
+         * setup the list
+         */
+        private void initialiseLimits()
+        {
+        	long ONE_SECOND = 1000;
+        	long ONE_MINUTE = ONE_SECOND * 60;
+        	long ONE_HOUR = ONE_MINUTE * 60;
+        	long ONE_DAY = ONE_HOUR * 24;    	
+        	
+          // create the array of limits values in a tmp parameter
+        	// the structure of the data is as follows:
+        	// for an overall time period of under 7 hours, 30 minute steps are used,
+        	// for 7 or more hours, it switches to 1 hour time steps.  that's all.
+          Label_Limit[] tmp = {
+              new Label_Limit(ONE_SECOND * 7,            ONE_SECOND),         // 0
+              new Label_Limit(ONE_SECOND * 35,            ONE_SECOND * 30),   // 1
+              new Label_Limit(ONE_MINUTE * 12,            ONE_MINUTE),         // 2
+              new Label_Limit(ONE_MINUTE * 120,            ONE_MINUTE * 15),   // 3
+              new Label_Limit(ONE_HOUR * 3,            ONE_MINUTE * 30),   // 4
+              new Label_Limit(ONE_HOUR * 8,            ONE_HOUR),         // 5
+              new Label_Limit(ONE_HOUR * 13,            ONE_HOUR * 2),         // 6
+              new Label_Limit(ONE_HOUR * 72,            ONE_HOUR * 12),       // 7
+              new Label_Limit(ONE_DAY * 15,            ONE_DAY)               // 8
+          		};
+
+          // and now store the array in our local variable
+          _limits = tmp;
+        }   
+        
+        /////////////////////////////////////////////////////////////
+        // scale limits and labels from a data range
+        ////////////////////////////////////////////////////////////
+        private static final class Label_Limit implements Serializable
+        {
+          /**
+      		 * 
+      		 */
+      		private static final long serialVersionUID = 1L;
+      		
+      		long upper_limit;
+          long increment;
+
+          Label_Limit(long limit, long inc)
+          {
+            upper_limit = limit;
+            increment = inc;
+          }
+        }            
+        
     }
 
     /**
@@ -134,4 +211,5 @@ public class CalendarDateSuite {
 //            return 1000 * 60 * 60 * 24;
 //        }
     };
+    
 }
