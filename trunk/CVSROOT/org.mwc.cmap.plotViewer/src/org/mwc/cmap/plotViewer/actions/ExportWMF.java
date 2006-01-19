779f876ffce0c39bca477e6c519ee13dd8661049
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-01-19 13:02:17 +0000

    Add "copy to clipboard" functionality
    git-svn-id: http://subversion.assembla.com/svn/Debrief@556 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/actions/ExportWMF.java
+++ trunk/CVSROOT/org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/actions/ExportWMF.java
@@ -3,23 +3,27 @@
  */
 package org.mwc.cmap.plotViewer.actions;
 
+import java.awt.Dimension;
+
 import org.eclipse.core.runtime.Status;
 import org.mwc.cmap.core.CorePlugin;
 import org.mwc.cmap.plotViewer.editors.chart.SWTCanvas;
 
+import com.pietjonas.wmfwriter2d.ClipboardCopy;
+
 import MWC.GUI.*;
-import MWC.GUI.Canvas.MetafileCanvas;
+import MWC.GUI.Canvas.*;
 import MWC.GUI.Tools.Chart.WriteMetafile;
 
 /**
  * @author ian.mayo
- *
  */
 public class ExportWMF extends CoreEditorAction
 {
 	public static ToolParent _theParent = null;
 
-	/** ok, store who the parent is for the operation
+	/**
+	 * ok, store who the parent is for the operation
 	 * 
 	 * @param theParent
 	 */
@@ -27,22 +31,20 @@ public class ExportWMF extends CoreEditorAction
 	{
 		_theParent = theParent;
 	}
-	
-	
-	/** and execute..
-	 * 
-	 *
+
+	/**
+	 * and execute..
 	 */
 	protected void run()
 	{
 		final PlainChart theChart = getChart();
-		
-		if(_theParent == null)
+
+		if (_theParent == null)
 		{
 			CorePlugin.logError(Status.ERROR, "Tool parent missing for Write Metafile", null);
 			return;
 		}
-		
+
 		WriteMetafile write = new WriteMetafile(_theParent, theChart, theChart.getLayers())
 		{
 
@@ -52,11 +54,38 @@ public class ExportWMF extends CoreEditorAction
 			protected void paintToMetafile(MetafileCanvas mf)
 			{
 				SWTCanvas sc = (SWTCanvas) theChart.getCanvas();
-		    sc.paintPlot(mf);
-		  }
-			
+				sc.paintPlot(mf);
+			}
+
 		};
 		write.execute();
+
+		// try to get the filename
+		String fName = MetafileCanvas.getLastFileName();
+
+		if (fName != null)
+		{
+			// create the clipboard
+
+			// try to copy the wmf to the clipboard
+			try
+			{
+				// get the dimensions
+				Dimension dim = MetafileCanvas.getLastScreenSize();
+				
+				ClipboardCopy cc = new ClipboardCopy();
+				 cc.copyWithPixelSize(fName, dim.width, dim.height, false);
+				// cc.copyWithPixelSize(fName, 6000, 4000, false);
+
+			}
+			catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else
+			System.err.println("Target filename missing");
 	}
-	
+
 }
