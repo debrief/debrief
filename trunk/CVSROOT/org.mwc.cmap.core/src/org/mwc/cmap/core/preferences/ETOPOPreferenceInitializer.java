3690594790375925ab4f6242af741b1ead4d7143
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-01-31 14:55:44 +0000

    Initialise where the ETOPO bits are
    git-svn-id: http://subversion.assembla.com/svn/Debrief@573 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/preferences/ETOPOPreferenceInitializer.java
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/preferences/ETOPOPreferenceInitializer.java
@@ -3,7 +3,13 @@
  */
 package org.mwc.cmap.core.preferences;
 
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.*;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.mwc.cmap.core.CorePlugin;
 
 public class ETOPOPreferenceInitializer extends AbstractPreferenceInitializer {
 
@@ -13,6 +19,27 @@ public class ETOPOPreferenceInitializer extends AbstractPreferenceInitializer {
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
 	 */
 	public void initializeDefaultPreferences() {
+		
+		// hmm, set the default location
+		Path coastPath = new Path("data/etopo.raw");
+		
+		// and get the relative path compared to the Core Plugin
+		URL fileURL = Platform.find(CorePlugin.getDefault().getBundle(), coastPath);
+		
+		// right, that's the relative location, switch to absolute path
+		try
+		{
+			fileURL = Platform.asLocalURL(fileURL);
+		}
+		catch (IOException e)
+		{
+			CorePlugin.logError(Status.ERROR, "Unable to find ETOPO data-file", e);
+		}
+		
+		// and store the default location
+		IPreferenceStore store = CorePlugin.getDefault().getPreferenceStore();
+		store.setDefault(ETOPOPrefsPage.PreferenceConstants.ETOPO_FILE,fileURL.getFile());
+		
 	}
 
 }
