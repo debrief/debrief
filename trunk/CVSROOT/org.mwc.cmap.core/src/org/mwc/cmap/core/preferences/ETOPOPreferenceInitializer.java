da1dde39d714914d4225ec7d38d0f742d1c1dfbe
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-02-01 15:56:58 +0000

    Better initialisers (fault-tolerant)
    git-svn-id: http://subversion.assembla.com/svn/Debrief@589 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/preferences/ETOPOPreferenceInitializer.java
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/preferences/ETOPOPreferenceInitializer.java
@@ -11,35 +11,40 @@ import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.mwc.cmap.core.CorePlugin;
 
-public class ETOPOPreferenceInitializer extends AbstractPreferenceInitializer {
+public class ETOPOPreferenceInitializer extends AbstractPreferenceInitializer
+{
 
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
 	 */
-	public void initializeDefaultPreferences() {
-		
+	public void initializeDefaultPreferences()
+	{
+
 		// hmm, set the default location
-		Path coastPath = new Path("data/etopo.raw");
-		
+		Path coastPath = new Path("data/ETOPO2.raw");
+
 		// and get the relative path compared to the Core Plugin
 		URL fileURL = Platform.find(CorePlugin.getDefault().getBundle(), coastPath);
-		
-		// right, that's the relative location, switch to absolute path
-		try
+
+		if (fileURL != null)
 		{
-			fileURL = Platform.asLocalURL(fileURL);
+			// right, that's the relative location, switch to absolute path
+			try
+			{
+				fileURL = Platform.asLocalURL(fileURL);
+			}
+			catch (IOException e)
+			{
+				CorePlugin.logError(Status.ERROR, "Unable to find ETOPO data-file", e);
+			}
+
+			// and store the default location
+			IPreferenceStore store = CorePlugin.getDefault().getPreferenceStore();
+			store.setDefault(ETOPOPrefsPage.PreferenceConstants.ETOPO_FILE, fileURL.getFile());
+
 		}
-		catch (IOException e)
-		{
-			CorePlugin.logError(Status.ERROR, "Unable to find ETOPO data-file", e);
-		}
-		
-		// and store the default location
-		IPreferenceStore store = CorePlugin.getDefault().getPreferenceStore();
-		store.setDefault(ETOPOPrefsPage.PreferenceConstants.ETOPO_FILE,fileURL.getFile());
-		
 	}
 
 }
