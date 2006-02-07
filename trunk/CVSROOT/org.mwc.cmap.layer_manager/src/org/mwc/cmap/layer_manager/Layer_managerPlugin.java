087508a4e842da964b26b96901d9de6d57e41ed1
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-02-07 09:21:18 +0000

    Lots of tidying, wrap declaration of xy-plot
    git-svn-id: http://subversion.assembla.com/svn/Debrief@596 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.layer_manager/src/org/mwc/cmap/layer_manager/Layer_managerPlugin.java
+++ trunk/CVSROOT/org.mwc.cmap.layer_manager/src/org/mwc/cmap/layer_manager/Layer_managerPlugin.java
@@ -1,25 +1,28 @@
 package org.mwc.cmap.layer_manager;
 
-import org.eclipse.ui.plugin.*;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.mwc.cmap.core.property_support.RightClickSupport;
-import org.mwc.cmap.xyplot.XYPlotGeneratorButtons;
-import org.osgi.framework.BundleContext;
 import java.util.*;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.mwc.cmap.core.property_support.RightClickSupport;
+import org.osgi.framework.BundleContext;
 
 /**
  * The main plugin class to be used in the desktop.
  */
-public class Layer_managerPlugin extends AbstractUIPlugin {
-	//The shared instance.
+public class Layer_managerPlugin extends AbstractUIPlugin
+{
+	// The shared instance.
 	private static Layer_managerPlugin plugin;
-	//Resource bundle.
+
+	// Resource bundle.
 	private ResourceBundle resourceBundle;
-	
+
 	/**
 	 * The constructor.
 	 */
-	public Layer_managerPlugin() {
+	public Layer_managerPlugin()
+	{
 		super();
 		plugin = this;
 	}
@@ -27,19 +30,27 @@ public class Layer_managerPlugin extends AbstractUIPlugin {
 	/**
 	 * This method is called upon plug-in activation
 	 */
-	public void start(BundleContext context) throws Exception {
+	public void start(BundleContext context) throws Exception
+	{
 		super.start(context);
-		
-		
-		// also fire up the xy-plotter item
-		RightClickSupport.addRightClickGenerator(new XYPlotGeneratorButtons());		
-		
+
+		try
+		{
+			// also fire up the xy-plotter item
+			RightClickSupport.addRightClickGenerator(new org.mwc.cmap.xyplot.XYPlotGeneratorButtons());
+		}
+		catch (Exception e)
+		{
+			System.out.println("still can't find xy-plot");
+		}
+
 	}
 
 	/**
 	 * This method is called when the plug-in is stopped
 	 */
-	public void stop(BundleContext context) throws Exception {
+	public void stop(BundleContext context) throws Exception
+	{
 		super.stop(context);
 		plugin = null;
 		resourceBundle = null;
@@ -48,19 +59,24 @@ public class Layer_managerPlugin extends AbstractUIPlugin {
 	/**
 	 * Returns the shared instance.
 	 */
-	public static Layer_managerPlugin getDefault() {
+	public static Layer_managerPlugin getDefault()
+	{
 		return plugin;
 	}
 
 	/**
-	 * Returns the string from the plugin's resource bundle,
-	 * or 'key' if not found.
+	 * Returns the string from the plugin's resource bundle, or 'key' if not
+	 * found.
 	 */
-	public static String getResourceString(String key) {
+	public static String getResourceString(String key)
+	{
 		ResourceBundle bundle = Layer_managerPlugin.getDefault().getResourceBundle();
-		try {
+		try
+		{
 			return (bundle != null) ? bundle.getString(key) : key;
-		} catch (MissingResourceException e) {
+		}
+		catch (MissingResourceException e)
+		{
 			return key;
 		}
 	}
@@ -68,24 +84,31 @@ public class Layer_managerPlugin extends AbstractUIPlugin {
 	/**
 	 * Returns the plugin's resource bundle,
 	 */
-	public ResourceBundle getResourceBundle() {
-		try {
+	public ResourceBundle getResourceBundle()
+	{
+		try
+		{
 			if (resourceBundle == null)
-				resourceBundle = ResourceBundle.getBundle("org.mwc.cmap.layer_manager.Layer_managerPluginResources");
-		} catch (MissingResourceException x) {
+				resourceBundle = ResourceBundle
+						.getBundle("org.mwc.cmap.layer_manager.Layer_managerPluginResources");
+		}
+		catch (MissingResourceException x)
+		{
 			resourceBundle = null;
 		}
 		return resourceBundle;
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path.
+	 * 
+	 * @param path
+	 *          the path
 	 * @return the image descriptor
 	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
+	public static ImageDescriptor getImageDescriptor(String path)
+	{
 		return AbstractUIPlugin.imageDescriptorFromPlugin("org.mwc.cmap.layer_manager", path);
 	}
 }
