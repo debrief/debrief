4f29d728e1e2bd9e65b6c44bf204ddd3de7e1823
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-02-23 11:35:22 +0000

    Add double-helper
    git-svn-id: http://subversion.assembla.com/svn/Debrief@650 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/property_support/DebriefProperty.java
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/property_support/DebriefProperty.java
@@ -155,6 +155,7 @@ public class DebriefProperty implements IPropertyDescriptor
 			_myHelperList.add(new WorldAccelerationHelper());
 			_myHelperList.add(new WorldDistanceHelper());
 			_myHelperList.add(new DurationHelper());
+			_myHelperList.add(new DoubleHelper());
 
 		}
 	}
