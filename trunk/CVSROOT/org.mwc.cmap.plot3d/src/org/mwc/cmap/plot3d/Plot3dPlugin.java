09a1068a8ffa9a9e7a3ce0a96c8942b66f6f72f4
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-03-08 14:02:31 +0000

    First versions imported
    git-svn-id: http://subversion.assembla.com/svn/Debrief@716 cb33b658-6c9e-41a7-9690-cba343611204

---
+++ trunk/CVSROOT/org.mwc.cmap.plot3d/src/org/mwc/cmap/plot3d/Plot3dPlugin.java
@@ -0,0 +1,56 @@
+package org.mwc.cmap.plot3d;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class Plot3dPlugin extends AbstractUIPlugin {
+
+	//The shared instance.
+	private static Plot3dPlugin plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public Plot3dPlugin() {
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);	
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 *
+	 * @return the shared instance.
+	 */
+	public static Plot3dPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin("org.mwc.cmap.plot3d", path);
+	}
+}
