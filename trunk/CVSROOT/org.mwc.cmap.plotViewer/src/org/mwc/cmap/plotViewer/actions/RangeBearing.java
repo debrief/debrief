799ec23d2a1e77be13f5105678b34795310e75e3
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-03-22 15:32:00 +0000

    Minor white-space tidying
    git-svn-id: http://subversion.assembla.com/svn/Debrief@776 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/actions/RangeBearing.java
+++ trunk/CVSROOT/org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/actions/RangeBearing.java
@@ -20,57 +20,61 @@ import MWC.GenericData.*;
 final public class RangeBearing extends CoreDragAction
 {
 
-	/** embedded class that handles the range/bearing measurement
+	/**
+	 * embedded class that handles the range/bearing measurement
 	 * 
 	 * @author Ian
-	 *
 	 */
 	final public static class RangeBearingMode extends SWTChart.PlotMouseDragger
 	{
-		/** the start point, in world coordinates (so we don't have to calculate it as often)
-		 * 
+		/**
+		 * the start point, in world coordinates (so we don't have to calculate it
+		 * as often)
 		 */
 		WorldLocation _startLocation;
-		
-		/** the start point, in screen coordinates - where we started our drag
-		 * 
+
+		/**
+		 * the start point, in screen coordinates - where we started our drag
 		 */
 		Point _startPoint;
-		
-		/** the last rectangle drawn, so we can erase it on the next update
-		 * 
+
+		/**
+		 * the last rectangle drawn, so we can erase it on the next update
 		 */
 		Rectangle _lastRect;
-		
-		/** the canvas we're updating..
-		 * 
+
+		/**
+		 * the canvas we're updating..
 		 */
 		SWTCanvas _myCanvas;
 
 		final public void doMouseMove(Point pt, int JITTER, Layers theLayers)
 		{
-			 if (_startPoint != null) {
-		     GC gc = new GC(_myCanvas.getCanvas());
-		 
-		     // This is the same as a !XOR
-		     gc.setXORMode(true);
-		     gc.setForeground(gc.getBackground());
-		 
-		     // Erase existing rectangle
-		     if (_lastRect != null)
-		      plotUpdate(gc);
-		 
-		     int dx = pt.x - _startPoint.x;
-		     int dy = pt.y - _startPoint.y;
-		 
-		     // Draw selection rectangle
-		     _lastRect = new Rectangle(_startPoint.x, _startPoint.y, dx, dy);
-		     plotUpdate(gc);
-		 
-		    } else {
-		     // System.out.println("no point.");
-		    }
-			
+			if (_startPoint != null)
+			{
+				GC gc = new GC(_myCanvas.getCanvas());
+
+				// This is the same as a !XOR
+				gc.setXORMode(true);
+				gc.setForeground(gc.getBackground());
+
+				// Erase existing rectangle
+				if (_lastRect != null)
+					plotUpdate(gc);
+
+				int dx = pt.x - _startPoint.x;
+				int dy = pt.y - _startPoint.y;
+
+				// Draw selection rectangle
+				_lastRect = new Rectangle(_startPoint.x, _startPoint.y, dx, dy);
+				plotUpdate(gc);
+
+			}
+			else
+			{
+				// System.out.println("no point.");
+			}
+
 		}
 
 		final public void doMouseUp(Point point, int keyState)
@@ -106,9 +110,10 @@ final public class RangeBearing extends CoreDragAction
 
 		final private void plotUpdate(GC dest)
 		{
-			
-			java.awt.Point endPoint = new java.awt.Point(_lastRect.x + _lastRect.width, _lastRect.y + _lastRect.height);
-			
+
+			java.awt.Point endPoint = new java.awt.Point(_lastRect.x + _lastRect.width,
+					_lastRect.y + _lastRect.height);
+
 			dest.setForeground(new Color(Display.getDefault(), 111, 111, 111));
 			dest.setLineWidth(2);
 			dest.drawLine(_lastRect.x, _lastRect.y, _lastRect.x + _lastRect.width, _lastRect.y
@@ -117,17 +122,18 @@ final public class RangeBearing extends CoreDragAction
 			// also put in a text-label
 			WorldLocation endLocation = _myCanvas.getProjection().toWorld(endPoint);
 			WorldVector sep = endLocation.subtract(_startLocation);
-			
-			String myUnits = CorePlugin.getToolParent().getProperty(MWC.GUI.Properties.UnitsPropertyEditor.UNITS_PROPERTY);
-			
+
+			String myUnits = CorePlugin.getToolParent().getProperty(
+					MWC.GUI.Properties.UnitsPropertyEditor.UNITS_PROPERTY);
+
 			double rng = rangeCalc.convertRange(sep.getRange(), myUnits);
 			double brg = sep.getBearing();
 			brg = brg * 180 / Math.PI;
 			String txt = "[" + (int) rng + myUnits + " " + (int) brg + "\u00b0]";
 
 			// decide the mid-point
-			java.awt.Point loc = new java.awt.Point(_lastRect.x + _lastRect.width / 2, _lastRect.y
-					+ _lastRect.height / 2);
+			java.awt.Point loc = new java.awt.Point(_lastRect.x + _lastRect.width / 2,
+					_lastRect.y + _lastRect.height / 2);
 
 			// find out how big the text is
 			FontMetrics fm = dest.getFontMetrics();
@@ -135,8 +141,8 @@ final public class RangeBearing extends CoreDragAction
 			loc.translate(0, fm.getHeight() / 2);
 			loc.translate(-txt.length() / 2 * fm.getAverageCharWidth(), 0);
 
-	  	dest.setForeground(new Color(Display.getDefault(), 200, 200, 200));
-//			dest.setForeground(dest.getBackground());
+			dest.setForeground(new Color(Display.getDefault(), 200, 200, 200));
+			// dest.setForeground(dest.getBackground());
 
 			dest.drawText(txt, loc.x, loc.y, SWT.DRAW_TRANSPARENT);
 
