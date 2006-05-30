54924021b09f00e5f54e475a8bf6d2c87248f0fd
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-05-30 08:48:29 +0000

    Rebuilding
    git-svn-id: http://subversion.assembla.com/svn/Debrief@915 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/editor_views/PolygonEditorControl.java
+++ trunk/CVSROOT/org.mwc.cmap.core/src/org/mwc/cmap/core/editor_views/PolygonEditorControl.java
@@ -20,15 +20,13 @@ import org.eclipse.swt.widgets.*;
 */
 public class PolygonEditorControl extends org.eclipse.swt.widgets.Composite {
 	private Composite topHolder;
+	private Label introLabel;
 	private Composite btnHolder;
-	private Button addBtn;
+	private Button moveDownBtn;
 	private Button deleteBtn;
-	private Label tmpLabel;
-	private Composite pointEditorHolder;
-	private ListViewer pointList;
-	private Button downBtn;
-	private Button moveUp;
-	private Label instructions;
+	private List thePoints;
+	private Button newPtBtn;
+	private Button moveUpBtn;
 
 	/**
 	* Auto-generated main method to display this 
@@ -77,40 +75,52 @@ public class PolygonEditorControl extends org.eclipse.swt.widgets.Composite {
 			{
 				topHolder = new Composite(this, SWT.NONE);
 				FillLayout topHolderLayout = new FillLayout(org.eclipse.swt.SWT.HORIZONTAL);
-				topHolder.setLayout(topHolderLayout);
 				GridData topHolderLData = new GridData();
-				topHolderLData.horizontalAlignment = GridData.FILL;
-				topHolderLData.verticalAlignment = GridData.FILL;
 				topHolderLData.grabExcessHorizontalSpace = true;
+				topHolderLData.horizontalAlignment = GridData.FILL;
+				topHolderLData.verticalAlignment = GridData.BEGINNING;
 				topHolder.setLayoutData(topHolderLData);
+				topHolder.setLayout(topHolderLayout);
 				{
-					instructions = new Label(topHolder, SWT.WRAP | SWT.BORDER);
-					instructions.setText("here are the instructions on how to edit a polygon");
-					RowData instructionsLData = new RowData();
-					instructions.setLayoutData(instructionsLData);
+					introLabel = new Label(topHolder, SWT.WRAP);
+					GridData introLabelLData = new GridData();
+					introLabelLData.horizontalAlignment = GridData.FILL;
+					introLabelLData.grabExcessHorizontalSpace = true;
+					introLabel.setLayoutData(introLabelLData);
+					introLabel.setText("how to use this control...");
 				}
 				{
-					btnHolder = new Composite(topHolder, SWT.NONE);
+					btnHolder = new Composite(topHolder, SWT.BORDER);
 					GridLayout btnHolderLayout = new GridLayout();
+					btnHolderLayout.makeColumnsEqualWidth = true;
 					btnHolderLayout.numColumns = 2;
-					btnHolder.setLayout(btnHolderLayout);
-					RowData btnHolderLData = new RowData();
+					GridData btnHolderLData = new GridData();
+					btnHolderLData.horizontalAlignment = GridData.END;
 					btnHolder.setLayoutData(btnHolderLData);
+					btnHolder.setLayout(btnHolderLayout);
 					{
-						moveUp = new Button(btnHolder, SWT.PUSH | SWT.CENTER);
-						GridData moveUpLData = new GridData();
-						moveUpLData.verticalAlignment = GridData.FILL;
-						moveUpLData.horizontalAlignment = GridData.FILL;
-						moveUp.setLayoutData(moveUpLData);
-						moveUp.setText("Up");
+						moveUpBtn = new Button(btnHolder, SWT.PUSH | SWT.CENTER);
+						GridData moveUpBtnLData = new GridData();
+						moveUpBtnLData.horizontalAlignment = GridData.FILL;
+						moveUpBtnLData.verticalAlignment = GridData.FILL;
+						moveUpBtn.setLayoutData(moveUpBtnLData);
+						moveUpBtn.setText("Up");
 					}
 					{
-						addBtn = new Button(btnHolder, SWT.PUSH | SWT.CENTER);
-						GridData addBtnLData = new GridData();
-						addBtnLData.verticalAlignment = GridData.FILL;
-						addBtnLData.horizontalAlignment = GridData.FILL;
-						addBtn.setLayoutData(addBtnLData);
-						addBtn.setText("Add");
+						moveDownBtn = new Button(btnHolder, SWT.PUSH | SWT.CENTER);
+						GridData moveDownBtnLData = new GridData();
+						moveDownBtnLData.horizontalAlignment = GridData.FILL;
+						moveDownBtnLData.verticalAlignment = GridData.FILL;
+						moveDownBtn.setLayoutData(moveDownBtnLData);
+						moveDownBtn.setText("Down");
+					}
+					{
+						newPtBtn = new Button(btnHolder, SWT.PUSH | SWT.CENTER);
+						GridData newPtBtnLData = new GridData();
+						newPtBtnLData.horizontalAlignment = GridData.FILL;
+						newPtBtnLData.verticalAlignment = GridData.FILL;
+						newPtBtn.setLayoutData(newPtBtnLData);
+						newPtBtn.setText("New");
 					}
 					{
 						deleteBtn = new Button(btnHolder, SWT.PUSH | SWT.CENTER);
@@ -120,41 +130,14 @@ public class PolygonEditorControl extends org.eclipse.swt.widgets.Composite {
 						deleteBtn.setLayoutData(deleteBtnLData);
 						deleteBtn.setText("Delete");
 					}
-					{
-						downBtn = new Button(btnHolder, SWT.PUSH | SWT.CENTER);
-						GridData downBtnLData = new GridData();
-						downBtnLData.verticalAlignment = GridData.FILL;
-						downBtnLData.horizontalAlignment = GridData.FILL;
-						downBtn.setLayoutData(downBtnLData);
-						downBtn.setText("Down");
-					}
 				}
 			}
 			{
-				pointList = new ListViewer(this, SWT.NONE);
-				GridData pointListLData = new GridData();
-				pointListLData.horizontalAlignment = GridData.FILL;
-				pointListLData.verticalAlignment = GridData.FILL;
-				pointListLData.verticalSpan = 2;
-				pointListLData.grabExcessHorizontalSpace = true;
-				pointListLData.grabExcessVerticalSpace = true;
-				pointList.getControl().setLayoutData(pointListLData);
-			}
-			{
-				pointEditorHolder = new Composite(this, SWT.NONE);
-				FillLayout pointEditorHolderLayout = new FillLayout(
-					org.eclipse.swt.SWT.HORIZONTAL);
-				pointEditorHolder.setLayout(pointEditorHolderLayout);
-				GridData pointEditorHolderLData = new GridData();
-				pointEditorHolderLData.horizontalAlignment = GridData.FILL;
-				pointEditorHolderLData.verticalAlignment = GridData.FILL;
-				pointEditorHolder.setLayoutData(pointEditorHolderLData);
-				{
-					tmpLabel = new Label(pointEditorHolder, SWT.SHADOW_OUT | SWT.BORDER);
-					tmpLabel.setText("Point editor goes here");
-					tmpLabel.setBounds(10, 209, 91, 28);
-					tmpLabel.setAlignment(SWT.CENTER);
-				}
+				GridData thePointsLData = new GridData();
+				thePointsLData.horizontalAlignment = GridData.FILL;
+				thePointsLData.grabExcessHorizontalSpace = true;
+				thePoints = new List(this, SWT.NONE);
+				thePoints.setLayoutData(thePointsLData);
 			}
 			this.layout();
 		} catch (Exception e) {
