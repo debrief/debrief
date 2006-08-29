6dc9a6bbf45d3d4ecc636ded5ccb5f19a1d9eca6
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-08-29 13:13:42 +0000

    Provide list of sensors - so new ASSET can view them
    git-svn-id: http://subversion.assembla.com/svn/Debrief@1106 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.asset.legacy/src/ASSET/GUI/Workbench/Plotters/SensorsPlottable.java
+++ trunk/CVSROOT/org.mwc.asset.legacy/src/ASSET/GUI/Workbench/Plotters/SensorsPlottable.java
@@ -1,13 +1,34 @@
 package ASSET.GUI.Workbench.Plotters;
 
+import java.util.Enumeration;
+
 import ASSET.Models.Sensor.SensorList;
+import MWC.GUI.Plottables;
 
 public class SensorsPlottable extends BasePlottable
 {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public SensorsPlottable(SensorList sensorFit)
 	{
 		super(sensorFit);
 	}
 
+	public Enumeration elements()
+	{
+		Enumeration res = null;
+
+		// hmm, do we have child behaviours?
+		if (getModel() instanceof SensorList)
+		{
+			SensorList bl = (SensorList) getModel();
+			res = new Plottables.IteratorWrapper(bl.getSensors().iterator());
+		}
+
+		return res;
+	}
 }
