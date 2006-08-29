6b71111648c774536da913467142f5b8af9a5257
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-08-29 13:16:09 +0000

    Provide extensible image factory support
    git-svn-id: http://subversion.assembla.com/svn/Debrief@1110 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.cmap.layer_manager/src/org/mwc/cmap/layer_manager/views/support/ViewLabelProvider.java
+++ trunk/CVSROOT/org.mwc.cmap.layer_manager/src/org/mwc/cmap/layer_manager/views/support/ViewLabelProvider.java
@@ -3,6 +3,9 @@
  */
 package org.mwc.cmap.layer_manager.views.support;
 
+import java.util.*;
+
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.*;
@@ -17,21 +20,23 @@ import MWC.GUI.VPF.FeaturePainter;
 public class ViewLabelProvider extends LabelProvider implements ITableLabelProvider
 {
 
-	/** image to indicate that item is visible
-	 * 
+	static Vector imageHelpers = null;
+
+	/**
+	 * image to indicate that item is visible
 	 */
 	Image visImage = null;
-	
-	/** image to indicate that item is hidden (would you believe...)
-	 * 
+
+	/**
+	 * image to indicate that item is hidden (would you believe...)
 	 */
 	Image hiddenImage = null;
 
-	/** image to indicate that item isn't plottable
-	 * 
+	/**
+	 * image to indicate that item isn't plottable
 	 */
 	Image nonVisibleImage = null;
-	
+
 	/**
 	 * 
 	 */
@@ -40,54 +45,90 @@ public class ViewLabelProvider extends LabelProvider implements ITableLabelProvi
 	 */
 	public ViewLabelProvider()
 	{
-			// ok, retrieve the images from our own registry
-			visImage = CorePlugin.getImageFromRegistry("check2.png");
-			hiddenImage = CorePlugin.getImageFromRegistry("blank_check.png");
-			nonVisibleImage = CorePlugin.getImageFromRegistry("desktop.png");
+		// ok, retrieve the images from our own registry
+		visImage = CorePlugin.getImageFromRegistry("check2.png");
+		hiddenImage = CorePlugin.getImageFromRegistry("blank_check.png");
+		nonVisibleImage = CorePlugin.getImageFromRegistry("desktop.png");
 	}
 
+	public static void addImageHelper(ViewLabelImageHelper helper)
+	{
+		if (imageHelpers == null)
+			imageHelpers = new Vector(1, 1);
+		imageHelpers.add(helper);
+	}
+
+	public static void removeImageHelper(ViewLabelImageHelper helper)
+	{
+		imageHelpers.remove(helper);
+	}
+
+	
 	public String getText(Object obj)
 	{
 		EditableWrapper pw = (EditableWrapper) obj;
 		return pw.getEditable().toString();
 	}
 
-
-	public Image getImage(Object item)
+	public Image getImage(Object subject)
 	{
-		String imageKey = ISharedImages.IMG_OBJ_ELEMENT;
+		
+		EditableWrapper item = (EditableWrapper) subject;
+		Editable editable = item.getEditable();
 
-		EditableWrapper pw = (EditableWrapper) item;
-		Editable obj = pw.getEditable();
+		Image res = null;
 
-		if (obj instanceof TrackWrapper)
-			imageKey = "track.gif";
-		else if (obj instanceof Layer)
-			imageKey = "layer.gif";
-		else if (obj instanceof FixWrapper)
-			imageKey = "fix.gif";
-		else if (obj instanceof ShapeWrapper)
-			imageKey = "shape.gif";
-		else if (obj instanceof GridPainter)
-			imageKey = "grid.gif";
-		else if (obj instanceof ScalePainter)
-			imageKey = "scale.gif";
-		else if (obj instanceof CoastPainter)
-			imageKey = "coast.gif";
-		else if (obj instanceof FeaturePainter)
-			imageKey = "vpf.gif";
-
-		Image theImage = PlatformUI.getWorkbench().getSharedImages().getImage(imageKey);
-
-		if (theImage == null)
+		// try our helpers first
+		ImageDescriptor thirdPartyImageDescriptor = null;
+		if (imageHelpers != null)
 		{
-			// ok, try to get the image from our own registry
-			theImage = CorePlugin.getImageFromRegistry(imageKey);
+			for (Iterator iter = imageHelpers.iterator(); iter.hasNext();)
+			{
+				ViewLabelImageHelper helper = (ViewLabelImageHelper) iter.next();
+				thirdPartyImageDescriptor = helper.getImageFor(editable);
+				if (thirdPartyImageDescriptor != null)
+				{
+					break;
+				}
+			}
 		}
 
-		// if (obj instanceof TreeParent)
-		// imageKey = ISharedImages.IMG_OBJ_FOLDER;
-		return theImage;
+		if (thirdPartyImageDescriptor != null)
+		{
+			// cool go for it
+			res = thirdPartyImageDescriptor.createImage();
+		}
+		else
+		{
+
+			String imageKey = ISharedImages.IMG_OBJ_ELEMENT;
+
+			if (editable instanceof TrackWrapper)
+				imageKey = "track.gif";
+			else if (editable instanceof Layer)
+				imageKey = "layer.gif";
+			else if (editable instanceof FixWrapper)
+				imageKey = "fix.gif";
+			else if (editable instanceof ShapeWrapper)
+				imageKey = "shape.gif";
+			else if (editable instanceof GridPainter)
+				imageKey = "grid.gif";
+			else if (editable instanceof ScalePainter)
+				imageKey = "scale.gif";
+			else if (editable instanceof CoastPainter)
+				imageKey = "coast.gif";
+			else if (editable instanceof FeaturePainter)
+				imageKey = "vpf.gif";
+
+			res = PlatformUI.getWorkbench().getSharedImages().getImage(imageKey);
+
+			if (res == null)
+			{
+				// ok, try to get the image from our own registry
+				res = CorePlugin.getImageFromRegistry(imageKey);
+			}
+		}
+		return res;
 	}
 
 	public Image getColumnImage(Object element, int columnIndex)
@@ -95,17 +136,17 @@ public class ViewLabelProvider extends LabelProvider implements ITableLabelProvi
 		Image res = null;
 		if (columnIndex == 0)
 			res = getImage(element);
-		else if(columnIndex == 1)
+		else if (columnIndex == 1)
 		{
 			// hey - don't bother with this bit - just use the text-marker
-			
+
 			// sort out the visibility
 			EditableWrapper pw = (EditableWrapper) element;
 			Editable ed = pw.getEditable();
 			if (ed instanceof Plottable)
 			{
 				Plottable pl = (Plottable) ed;
-				
+
 				if (pl.getVisible())
 					res = visImage;
 				else
@@ -125,20 +166,20 @@ public class ViewLabelProvider extends LabelProvider implements ITableLabelProvi
 		String res = null;
 		if (columnIndex == 0)
 			res = getText(element);
-//		else
-//		{
-//			// sort out the visibility
-//			EditableWrapper pw = (EditableWrapper) element;
-//			Editable ed = pw.getEditable();
-//			if (ed instanceof Editable)
-//			{
-//				Editable pl = (Editable) ed;
-//				if (pl.getVisible())
-//					res = "Y";
-//				else
-//					res = "N";
-//			}
-//		}
+		// else
+		// {
+		// // sort out the visibility
+		// EditableWrapper pw = (EditableWrapper) element;
+		// Editable ed = pw.getEditable();
+		// if (ed instanceof Editable)
+		// {
+		// Editable pl = (Editable) ed;
+		// if (pl.getVisible())
+		// res = "Y";
+		// else
+		// res = "N";
+		// }
+		// }
 
 		return res;
 	}
@@ -148,6 +189,17 @@ public class ViewLabelProvider extends LabelProvider implements ITableLabelProvi
 		boolean res = true;
 
 		return res;
+	}
+
+	public static interface ViewLabelImageHelper
+	{
+		/**
+		 * produce an image icon for this object
+		 * 
+		 * @param subject
+		 * @return
+		 */
+		public ImageDescriptor getImageFor(Object subject);
 	}
 
 }
