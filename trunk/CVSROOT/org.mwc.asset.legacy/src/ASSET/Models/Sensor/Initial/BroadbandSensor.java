bec1242dfa6e6a88f4ec8878369088bcdc2bb0fb
Ian.Mayo <Ian.Mayo@cb33b658-6c9e-41a7-9690-cba343611204>
2006-11-06 16:08:51 +0000

    Hard-code detection range so we get immediate contact
    git-svn-id: http://subversion.assembla.com/svn/Debrief@1270 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/CVSROOT/org.mwc.asset.legacy/src/ASSET/Models/Sensor/Initial/BroadbandSensor.java
+++ trunk/CVSROOT/org.mwc.asset.legacy/src/ASSET/Models/Sensor/Initial/BroadbandSensor.java
@@ -135,6 +135,8 @@ public class BroadbandSensor extends InitialSensor
     // use the environment to determine the loss
     res = environment.getLossBetween(EnvironmentType.BROADBAND_PASSIVE, target, host);
 
+    res = -200;
+    
     return res;
   }
 
@@ -189,7 +191,10 @@ public class BroadbandSensor extends InitialSensor
    * get the version details for this model.
    * <pre>
    * $Log$
-   * Revision 1.2  2006-09-21 12:20:41  Ian.Mayo
+   * Revision 1.3  2006-11-06 16:08:51  Ian.Mayo
+   * Hard-code detection range so we get immediate contact
+   *
+   * Revision 1.2  2006/09/21 12:20:41  Ian.Mayo
    * Reflect introduction of default names
    *
    * Revision 1.1  2006/08/08 14:21:54  Ian.Mayo
