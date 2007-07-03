552d2b182ae798174179404a52686cfa5d2b8c8b
ian <ian@cb33b658-6c9e-41a7-9690-cba343611204>
2007-07-03 12:27:11 +0000

    Refactor, so that user can select target layer
    git-svn-id: http://subversion.assembla.com/svn/Debrief@1439 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/creators/shapes/InsertLabel.java
+++ trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/creators/shapes/InsertLabel.java
@@ -5,97 +5,51 @@ package org.mwc.debrief.core.creators.shapes;
 
 import java.awt.Color;
 
-import org.mwc.cmap.core.CorePlugin;
-import org.mwc.cmap.core.operations.DebriefActionWrapper;
-import org.mwc.cmap.plotViewer.actions.CoreEditorAction;
-
-import Debrief.Tools.Palette.CreateLabel;
 import Debrief.Wrappers.LabelWrapper;
 import MWC.GUI.*;
-import MWC.GUI.Tools.Action;
+import MWC.GUI.Shapes.PlainShape;
 import MWC.GenericData.*;
 
 /**
  * @author ian.mayo
  */
-public class InsertLabel extends CoreEditorAction
+public class InsertLabel extends CoreInsertShape
 {
-	public static ToolParent _theParent = null;
 
-	/**
-	 * ok, store who the parent is for the operation
+	/** get a plottable object
 	 * 
-	 * @param theParent
+	 * @param centre
+	 * @param theChart
+	 * @return
 	 */
-	public static void init(ToolParent theParent)
+	protected Plottable getPlottable(PlainChart theChart)
 	{
-		_theParent = theParent;
-	}
-
-	/**
-	 * and execute..
-	 */
-	protected void execute()
-	{
-		final PlainChart theChart = getChart();
-
-		Action res = getData(theChart);
 		
-		// ok, now wrap the action
-		DebriefActionWrapper daw = new DebriefActionWrapper(res);
-		
-		// and add it to our buffer (which will execute it anyway)
-		CorePlugin.run(daw);
-
-		// res.execute();
-		
-		
-
-	}
-
-	public final Action getData(PlainChart theChart)
-	{
-		Action res = null;
+		// right, what's the area we're looking at
 		WorldArea wa = theChart.getDataArea();
+		
+		// get centre of area (at zero depth)
+		WorldLocation centre = wa.getCentreAtSurface();
 
-		// see if we have an area defined
-		if (wa != null)
-		{
+		// and now wrap the shape
+		LabelWrapper theWrapper = new LabelWrapper("Blank label", centre,
+				Color.red);
+		
+		return theWrapper;
 
-			// get centre of area (at zero depth)
-			WorldLocation centre = wa.getCentreAtSurface();
-
-			// and now wrap the shape
-			LabelWrapper theWrapper = new LabelWrapper("Blank label", centre,
-					Color.red);
-
-			// lastly, get the data
-			Layers theData = theChart.getLayers();
-
-			// aah, and the misc layer, in which we will store the shape
-			Layer theLayer = theData.findLayer("Misc");
-
-			// did we find it?
-			if (theLayer == null)
-			{
-				// nope, better create it.
-				theLayer = new BaseLayer();
-				theLayer.setName("Misc");
-				theData.addThisLayer(theLayer);
-			}
-
-			// and put it into an action (so we can undo it)
-			res = new CreateLabel.CreateLabelAction(null, theLayer, theWrapper,
-					theChart.getLayers());
-		}
-		else
-		{
-			// we haven't got an area, inform the user
-			CorePlugin.showMessage("Create Feature",
-							"Sorry, we can't create a label until the area is defined.  Try adding a coastline first");
-		}
-
-		return res;
 	}
 
+	@Override
+	protected PlainShape getShape(WorldLocation centre)
+	{
+		// don't bother, we're not generating shapes this way...
+		return null;
+	}
+
+	@Override
+	protected String getShapeName()
+	{
+		// don't bother, we're not generating shapes this way...
+		return null;
+	}
 }
