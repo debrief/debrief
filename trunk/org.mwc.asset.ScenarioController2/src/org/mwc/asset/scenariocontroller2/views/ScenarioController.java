6637f03bdbf95c706315031ad53ed038e1c293f1
ian <ian@cb33b658-6c9e-41a7-9690-cba343611204>
2009-03-19 11:44:15 +0000

    Part way through laying out single scenario tab
    git-svn-id: http://subversion.assembla.com/svn/Debrief@1918 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.asset.ScenarioController2/src/org/mwc/asset/scenariocontroller2/views/ScenarioController.java
+++ trunk/org.mwc.asset.ScenarioController2/src/org/mwc/asset/scenariocontroller2/views/ScenarioController.java
@@ -1,39 +1,33 @@
 package org.mwc.asset.scenariocontroller2.views;
 
-
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.part.*;
-import org.eclipse.jface.viewers.*;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.SWT;
-
-
-/**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
- */
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
 
 public class ScenarioController extends ViewPart {
 	private Action action1;
 	private Action action2;
+	private TabFolder _myTabs;
+	private TabItem _singleScenTab;
+	private TabItem _multiScenTab;
+
 	/**
 	 * The constructor.
 	 */
@@ -41,18 +35,100 @@ public class ScenarioController extends ViewPart {
 	}
 
 	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
+	 * This is a callback that will allow us to create the viewer and initialize
+	 * it.
 	 */
 	public void createPartControl(Composite parent) {
-		
+		// create the two tabs
+		_myTabs = new TabFolder(parent, SWT.None);
+		createMultipleTabs();
 
-		// Create the help context id for the viewer's control
-//		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "org.mwc.asset.ScenarioController2.viewer");
+		// populate the single scenario tab
+		populateSingleScenario(_myTabs, _singleScenTab);
+
 		makeActions();
 		contributeToActionBars();
 	}
 
+	private void populateSingleScenario(TabFolder tabs, TabItem myTab) {
+		Composite sC = new Composite(tabs, SWT.None);
+		myTab.setControl(sC);
+		sC.setLayout(new FormLayout());
+
+		// first the labels
+		Label scenLbl = new Label(sC, SWT.RIGHT);
+		scenLbl.setText("Scenario:");
+		FormData fd = new FormData();
+		fd.top = new FormAttachment(0, 10);
+		fd.left = new FormAttachment(0, 5);
+		fd.bottom = new FormAttachment(10, 0);
+		fd.right = new FormAttachment(40, 0);
+		scenLbl.setLayoutData(fd);
+
+		Label contLbl = new Label(sC, SWT.RIGHT);
+		contLbl.setText("Controller:");
+		fd = new FormData();
+		fd.top = new FormAttachment(scenLbl, 10);
+		fd.left = new FormAttachment(0, 5);
+		fd.bottom = new FormAttachment(20, 0);
+		fd.right = new FormAttachment(40, 0);
+		contLbl.setLayoutData(fd);
+
+		// now the text boxes
+		Label scenVal = new Label(sC, SWT.BORDER | SWT.SINGLE);
+		scenVal.setText("[pending]          ");		
+		fd = new FormData();
+		fd.top = new FormAttachment(scenLbl, 0, SWT.TOP);
+		fd.left = new FormAttachment(scenLbl, 10);
+		scenVal.setLayoutData(fd);
+
+		Label contVal = new Label(sC, SWT.BORDER | SWT.SINGLE);
+		contVal.setText("[pending]          ");
+		fd = new FormData();
+		fd.top = new FormAttachment(contLbl, 0, SWT.TOP);
+		fd.left = new FormAttachment(contLbl, 10);
+		contVal.setLayoutData(fd);
+
+		// now the file selector buttons
+		Button scenBtn = new Button(sC, SWT.NONE);
+		scenBtn.setText("...");
+		fd = new FormData();
+		fd.top = new FormAttachment(scenVal, 0, SWT.TOP);
+		fd.left = new FormAttachment(scenVal, 10);
+		scenBtn.setLayoutData(fd);
+
+		Button contBtn = new Button(sC, SWT.NONE);
+		contBtn.setText("...");
+		fd = new FormData();
+		fd.top = new FormAttachment(contVal, 0, SWT.TOP);
+		fd.left = new FormAttachment(contVal, 10);
+		contBtn.setLayoutData(fd);
+		
+		// and the load button
+		Button loadBtn = new Button(sC, SWT.RIGHT);
+		loadBtn.setText("Load");
+		fd = new FormData();
+		fd.top = new FormAttachment(contLbl, 10);
+//		fd.left = new FormAttachment(0, 5);
+//		fd.bottom = new FormAttachment(30, 0);
+		fd.right = new FormAttachment(40, 0);
+		loadBtn.setLayoutData(fd);
+	}
+
+	private void createMultipleTabs() {
+		_singleScenTab = new TabItem(_myTabs, SWT.NONE);
+		_singleScenTab.setText("Single Scenario");
+		_multiScenTab = new TabItem(_myTabs, SWT.NONE);
+		_multiScenTab.setText("Multi Scenario");
+	}
+
+	private void selectTab(boolean isSingle) {
+		if (isSingle)
+			_myTabs.setSelection(_singleScenTab);
+		else
+			_myTabs.setSelection(_multiScenTab);
+
+	}
 
 	private void contributeToActionBars() {
 		IActionBars bars = getViewSite().getActionBars();
@@ -79,9 +155,9 @@ public class ScenarioController extends ViewPart {
 		};
 		action1.setText("Action 1");
 		action1.setToolTipText("Action 1 tooltip");
-		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
+		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+
 		action2 = new Action() {
 			public void run() {
 				showMessage("Action 2 executed");
@@ -89,21 +165,19 @@ public class ScenarioController extends ViewPart {
 		};
 		action2.setText("Action 2");
 		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
 	}
 
 	private void showMessage(String message) {
-		MessageDialog.openInformation(
-			Display.getCurrent().getActiveShell(),
-			"Scenario controller",
-			message);
+		MessageDialog.openInformation(Display.getCurrent().getActiveShell(),
+				"Scenario controller", message);
 	}
 
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
 	public void setFocus() {
-//		viewer.getControl().setFocus();
+		// viewer.getControl().setFocus();
 	}
 }
