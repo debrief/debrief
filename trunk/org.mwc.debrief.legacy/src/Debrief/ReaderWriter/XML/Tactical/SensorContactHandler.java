054b4c6ac0396e4d65886e8aae264f2005cd9f06
ian <ian@cb33b658-6c9e-41a7-9690-cba343611204>
2009-11-17 09:25:39 +0000

    make (slightly) more versatile - able to export named sensor data items
    git-svn-id: http://subversion.assembla.com/svn/Debrief@2541 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.debrief.legacy/src/Debrief/ReaderWriter/XML/Tactical/SensorContactHandler.java
+++ trunk/org.mwc.debrief.legacy/src/Debrief/ReaderWriter/XML/Tactical/SensorContactHandler.java
@@ -16,8 +16,8 @@ import MWC.GenericData.WorldDistance;
 import MWC.Utilities.ReaderWriter.XML.Util.*;
 
 abstract public class SensorContactHandler extends
-		MWC.Utilities.ReaderWriter.XML.MWCXMLReader {
-
+		MWC.Utilities.ReaderWriter.XML.MWCXMLReader
+{
 
 	private static final String SENSOR_CONTACT = "sensor_contact";
 
@@ -46,7 +46,6 @@ abstract public class SensorContactHandler extends
 	private static final String HAS_FREQUENCY = "HasFrequency";
 
 	private static final String RANGE = "Range";
-	
 
 	Debrief.Wrappers.SensorContactWrapper _theContact;
 
@@ -66,103 +65,143 @@ abstract public class SensorContactHandler extends
 	static final MWC.GUI.Properties.LineStylePropertyEditor ls = new MWC.GUI.Properties.LineStylePropertyEditor();
 
 	private WorldDistance _myRange = null;
-	
-	public SensorContactHandler() {
-		// inform our parent what type of class we are
-		super(SENSOR_CONTACT);
 
-		addAttributeHandler(new HandleAttribute(LABEL) {
-			public void setValue(String name, String value) {
+	/** default constructor - using fixed name
+	 * 
+	 */
+	public SensorContactHandler()
+	{
+		// inform our parent what type of class we are
+		this(SENSOR_CONTACT);
+	}
+
+	/** versatile constructor that allows any item name to be specified
+	 * 
+	 * @param typeName the element name that we're looking for
+	 */
+	public SensorContactHandler(final String typeName)
+	{
+		// inform our parent what type of class we are
+		super(typeName);
+
+		addAttributeHandler(new HandleAttribute(LABEL)
+		{
+			public void setValue(String name, String value)
+			{
 				_theContact.setLabel(fromXML(value));
 			}
 		});
 
-		addAttributeHandler(new HandleDoubleAttribute(RANGE) {
-			public void setValue(String name, double value) {
-					_theContact.setRange(new WorldDistance(
-							value, WorldDistance.YARDS));
+		addAttributeHandler(new HandleDoubleAttribute(RANGE)
+		{
+			public void setValue(String name, double value)
+			{
+				_theContact.setRange(new WorldDistance(value, WorldDistance.YARDS));
 			}
 		});
 
-		addAttributeHandler(new HandleDoubleAttribute(BEARING) {
-			public void setValue(String name, double value) {
-					_theContact.setBearing(value);
+		addAttributeHandler(new HandleDoubleAttribute(BEARING)
+		{
+			public void setValue(String name, double value)
+			{
+				_theContact.setBearing(value);
 			}
 		});
 
-		
-		addAttributeHandler(new HandleDoubleAttribute(AMBIGUOUS_BEARING) {
-			public void setValue(String name, double value) {
-					_theContact.setAmbiguousBearing(value);
+		addAttributeHandler(new HandleDoubleAttribute(AMBIGUOUS_BEARING)
+		{
+			public void setValue(String name, double value)
+			{
+				_theContact.setAmbiguousBearing(value);
 			}
 		});
-		addAttributeHandler(new HandleBooleanAttribute(HAS_AMBIGUOUS_BEARING) {
-			public void setValue(String name, boolean value) {
-					_theContact.setHasAmbiguousBearing(value);
+		addAttributeHandler(new HandleBooleanAttribute(HAS_AMBIGUOUS_BEARING)
+		{
+			public void setValue(String name, boolean value)
+			{
+				_theContact.setHasAmbiguousBearing(value);
 			}
 		});
-		
-		addAttributeHandler(new HandleDoubleAttribute(FREQUENCY) {
-			public void setValue(String name, double value) {
-					_theContact.setFrequency(value);
-			}
-		});
-		addAttributeHandler(new HandleBooleanAttribute(HAS_FREQUENCY) {
-			public void setValue(String name, boolean value) {
-					_theContact.setHasFrequency(value);
-			}
-		});
-		
-		
 
-		addAttributeHandler(new HandleAttribute(DTG) {
-			public void setValue(String name, String value) {
+		addAttributeHandler(new HandleDoubleAttribute(FREQUENCY)
+		{
+			public void setValue(String name, double value)
+			{
+				_theContact.setFrequency(value);
+			}
+		});
+		addAttributeHandler(new HandleBooleanAttribute(HAS_FREQUENCY)
+		{
+			public void setValue(String name, boolean value)
+			{
+				_theContact.setHasFrequency(value);
+			}
+		});
+
+		addAttributeHandler(new HandleAttribute(DTG)
+		{
+			public void setValue(String name, String value)
+			{
 				_theContact.setDTG(parseThisDate(value));
 			}
 		});
 
-		addHandler(new LocationHandler(CENTRE) {
-			public void setLocation(MWC.GenericData.WorldLocation res) {
+		addHandler(new LocationHandler(CENTRE)
+		{
+			public void setLocation(MWC.GenericData.WorldLocation res)
+			{
 				_theContact.setOrigin(res);
 			}
 		});
 
-		addHandler(new ColourHandler() {
-			public void setColour(java.awt.Color theVal) {
+		addHandler(new ColourHandler()
+		{
+			public void setColour(java.awt.Color theVal)
+			{
 				_theContact.setColor(theVal);
 			}
 		});
 
-		addAttributeHandler(new HandleBooleanAttribute(LABEL_SHOWING) {
-			public void setValue(String name, boolean value) {
+		addAttributeHandler(new HandleBooleanAttribute(LABEL_SHOWING)
+		{
+			public void setValue(String name, boolean value)
+			{
 				_theContact.setLabelVisible(value);
 			}
 		});
 
-		addAttributeHandler(new HandleBooleanAttribute(VISIBLE) {
-			public void setValue(String name, boolean value) {
+		addAttributeHandler(new HandleBooleanAttribute(VISIBLE)
+		{
+			public void setValue(String name, boolean value)
+			{
 				_theContact.setVisible(value);
 			}
 		});
 
-		addAttributeHandler(new HandleAttribute(LABEL_LOCATION) {
-			public void setValue(String name, String val) {
+		addAttributeHandler(new HandleAttribute(LABEL_LOCATION)
+		{
+			public void setValue(String name, String val)
+			{
 				lp.setAsText(val);
 				Integer res = (Integer) lp.getValue();
 				if (res != null)
 					_theContact.setLabelLocation(res);
 			}
 		});
-		addAttributeHandler(new HandleAttribute(PUT_LABEL_AT) {
-			public void setValue(String name, String val) {
+		addAttributeHandler(new HandleAttribute(PUT_LABEL_AT)
+		{
+			public void setValue(String name, String val)
+			{
 				ll.setAsText(val);
 				Integer res = (Integer) ll.getValue();
 				if (res != null)
 					_theContact.setPutLabelAt(res);
 			}
 		});
-		addAttributeHandler(new HandleAttribute(LINE_STYLE) {
-			public void setValue(String name, String val) {
+		addAttributeHandler(new HandleAttribute(LINE_STYLE)
+		{
+			public void setValue(String name, String val)
+			{
 				ls.setAsText(val.replace('_', ' '));
 				Integer res = (Integer) ls.getValue();
 				if (res != null)
@@ -170,17 +209,20 @@ abstract public class SensorContactHandler extends
 			}
 		});
 
-		addHandler(new WorldDistanceHandler(RANGE) {
-			public void setWorldDistance(WorldDistance value) {
+		addHandler(new WorldDistanceHandler(RANGE)
+		{
+			public void setWorldDistance(WorldDistance value)
+			{
 				_myRange = value;
 			}
 		});
-		}
+	}
 
-	public final void handleOurselves(String name, Attributes atts) {
+	public final void handleOurselves(String name, Attributes atts)
+	{
 		// create the new items
 		_theContact = new Debrief.Wrappers.SensorContactWrapper();
-	
+
 		lp.setValue(null);
 		ls.setValue(null);
 		ll.setValue(null);
@@ -188,12 +230,13 @@ abstract public class SensorContactHandler extends
 		super.handleOurselves(name, atts);
 	}
 
-	public final void elementClosed() {
+	public final void elementClosed()
+	{
 		// do we have a range?
-		if(_myRange != null)
+		if (_myRange != null)
 		{
 			_theContact.setRange(_myRange);
-			
+
 			// and clear it
 			_myRange = null;
 		}
@@ -201,7 +244,6 @@ abstract public class SensorContactHandler extends
 		// and store it
 		addContact(_theContact);
 
-		
 		// reset our variables
 		_theContact = null;
 		_myRange = null;
@@ -209,49 +251,74 @@ abstract public class SensorContactHandler extends
 
 	abstract public void addContact(MWC.GUI.Plottable plottable);
 
+
+	/** export this item using the default xml element name
+	 * 
+	 * @param contact the item to export
+	 * @param parent the xml parent element
+	 * @param doc the document we're being written to
+	 */
 	public static void exportFix(Debrief.Wrappers.SensorContactWrapper contact,
-			org.w3c.dom.Element parent, org.w3c.dom.Document doc) {
+			org.w3c.dom.Element parent, org.w3c.dom.Document doc)
+	{
+		exportFix(SENSOR_CONTACT, contact, parent, doc);
+	}
+	
+	
+	/** export this item using the specified element name
+	 * 
+	 * @param typeName the xml element name to use
+	 * @param contact the item to export
+	 * @param parent the xml parent element
+	 * @param doc the document we're being written to
+	 */
+	public static void exportFix(final String typeName, Debrief.Wrappers.SensorContactWrapper contact,
+			org.w3c.dom.Element parent, org.w3c.dom.Document doc)
+	{
 		/*
 		 * 
-		 * Dtg CDATA #REQUIRED Track CDATA #REQUIRED Bearing CDATA #REQUIRED
-		 * Range CDATA #REQUIRED Visible (TRUE|FALSE) "TRUE" Label CDATA
-		 * #REQUIRED LabelShowing (TRUE|FALSE) "TRUE" LabelLocation
+		 * Dtg CDATA #REQUIRED Track CDATA #REQUIRED Bearing CDATA #REQUIRED Range
+		 * CDATA #REQUIRED Visible (TRUE|FALSE) "TRUE" Label CDATA #REQUIRED
+		 * LabelShowing (TRUE|FALSE) "TRUE" LabelLocation
 		 * (Top|Left|Bottom|Centre|Right) "Left" LineStyle
 		 * (SOLID|DOTTED|DOT_DASH|SHORT_DASHES|LONG_DASHES|UNCONNECTED) "SOLID"
 		 */
-		Element eFix = doc.createElement(SENSOR_CONTACT);
+		Element eFix = doc.createElement(typeName);
 
 		eFix.setAttribute(DTG, writeThis(contact.getDTG()));
 
 		eFix.setAttribute(VISIBLE, writeThis(contact.getVisible()));
-		eFix.setAttribute(BEARING, writeThis(contact.getBearing()));		
+		eFix.setAttribute(BEARING, writeThis(contact.getBearing()));
 		eFix.setAttribute(LABEL_SHOWING, writeThis(contact.getLabelVisible()));
 		eFix.setAttribute(LABEL, toXML(contact.getLabel()));
-		// we no longer export range as an attribute, but as an element-so we can store units
-		//	eFix.setAttribute("Range", writeThis(contact.getRange().getValueIn(
-		//	WorldDistance.YARDS)));
+		// we no longer export range as an attribute, but as an element-so we can
+		// store units
+		// eFix.setAttribute("Range", writeThis(contact.getRange().getValueIn(
+		// WorldDistance.YARDS)));
 
 		// do we have ambiguous data?
-		eFix.setAttribute(AMBIGUOUS_BEARING, writeThis(contact.getAmbiguousBearing()));
-		eFix.setAttribute(HAS_AMBIGUOUS_BEARING, writeThis(contact.getHasAmbiguousBearing()));
+		eFix.setAttribute(AMBIGUOUS_BEARING, writeThis(contact
+				.getAmbiguousBearing()));
+		eFix.setAttribute(HAS_AMBIGUOUS_BEARING, writeThis(contact
+				.getHasAmbiguousBearing()));
 
 		// do we have frequency data?
 		eFix.setAttribute(FREQUENCY, writeThis(contact.getFrequency()));
 		eFix.setAttribute(HAS_FREQUENCY, writeThis(contact.getHasFrequency()));
-			
+
 		// sort out the range
 		// do we have range?
-		if(contact.getRange() != null)
-  		WorldDistanceHandler.exportDistance(RANGE, contact.getRange(), eFix, doc);
-		
+		if (contact.getRange() != null)
+			WorldDistanceHandler.exportDistance(RANGE, contact.getRange(), eFix, doc);
+
 		// sort out the line style
 		ls.setValue(contact.getLineStyle());
 		eFix.setAttribute(LINE_STYLE, ls.getAsText().replace(' ', '_')); // note,
-																			// we
-																			// swap
-																			// spaces
-																			// for
-																			// underscores
+		// we
+		// swap
+		// spaces
+		// for
+		// underscores
 
 		// and the line label location
 		ll.setValue(contact.getPutLabelAt());
@@ -266,8 +333,8 @@ abstract public class SensorContactHandler extends
 		// normal getColor method which may return the track colour
 		java.awt.Color fCol = contact.getActualColor();
 		if (fCol != null)
-			MWC.Utilities.ReaderWriter.XML.Util.ColourHandler.exportColour(
-					fCol, eFix, doc);
+			MWC.Utilities.ReaderWriter.XML.Util.ColourHandler.exportColour(fCol,
+					eFix, doc);
 
 		// and now the centre item,
 		MWC.GenericData.WorldLocation origin = contact.getOrigin();
