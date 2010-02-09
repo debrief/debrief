03e93a559f1da4a458da96e000f0e8f0d7775db5
ian <ian@cb33b658-6c9e-41a7-9690-cba343611204>
2010-02-09 20:42:19 +0000

    steady progress
    git-svn-id: http://subversion.assembla.com/svn/Debrief@2790 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.cmap.legacy/src/MWC/Utilities/ReaderWriter/FlatFile/FlatFileExporter.java
+++ trunk/org.mwc.cmap.legacy/src/MWC/Utilities/ReaderWriter/FlatFile/FlatFileExporter.java
@@ -1,17 +1,82 @@
 package MWC.Utilities.ReaderWriter.FlatFile;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FlatFileExporter
 {
+	private String createTabs(int num)
+	{
+		StringBuffer res = new StringBuffer();
+		for (int i = 0; i < num; i++)
+		{
+			res.append("\t");
+		}
+		return res.toString();
+	}
+
+	
+	private final String BRK = "" + (char)13 + (char)10;
+
 	public String exportThis()
 	{
-		String res = "na";
-		return res;
+		String header = "STRAND Scenario Report 1.00"
+				+ createTabs(33)
+				+ BRK
+				+ "MISSION_NAME"
+				+ createTabs(33)
+				+ BRK
+				+ "Vessel"
+				+ createTabs(33)
+				+ BRK
+				+ "OS track 0100-0330"
+				+ createTabs(33)
+				+ BRK
+				+ "GapsFatBowBTH_5-4-04"
+				+ createTabs(33)
+				+ BRK
+				+ "tla"
+				+ createTabs(33)
+				+ BRK
+				+ "tla"
+				+ createTabs(33)
+				+ BRK
+				+ "01:45:00	22/12/2002"
+				+ createTabs(32)
+				+ BRK
+				+ "02:40:00	22/12/2002"
+				+ createTabs(32)
+				+ BRK
+				+ "0"
+				+ createTabs(33)
+				+ BRK
+				+ "0"
+				+ createTabs(33)
+				+ BRK
+				+ "0"
+				+ createTabs(33)
+				+ BRK
+				+ "5"
+				+ createTabs(33)
+				+ BRK
+				+ "-1.23E+04	-654321"
+				+ createTabs(32)
+				+ BRK
+				+ "Time	OS_Status	OS_X	OS_Y	OS_Speed	OS_Heading	Sensor_Status	Sensor_X	Sensor_Y	Sensor_Brg	Sensor_Bacc	Sensor_Freq	Sensor_Facc	Sensor_Speed	Sensor_Heading	Sensor_Type	Msd_Status	Msd_X	Msd_Y	Msd_Speed	Msd_Heading	Prd_Status	Prd_X	Prd_Y	Prd_Brg	Prd_Brg_Acc	Prd_Range	Prd_Range_Acc	Prd_Course	Prd_Cacc	Prd_Speed	Prd_Sacc	Prd_Freq	Prd_Freq_Acc" + BRK;
+		;
+		String body = "0	7	6.32332	-5555.55	2.7	200.1	0	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9"+ BRK
+				+ "1	7	6.32332	-5555.551	2.7	200	0	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9"+ BRK
+				+ "2	7	6.32332	-5555.55	2.7	200	0	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9"+ BRK
+				+ "3	7	6.32332	-5521.2	4.6	200	0	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9"+ BRK
+				+ "4	7	6.32332	-5555.32	4.7	200	0	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9"+ BRK
+				+ "5	7	6.32332	-5543.73	4.8	200.1	0	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9";
+
+		return header + body;
 	}
-	
-	
+
 	static public final class testMe extends junit.framework.TestCase
 	{
 
@@ -21,33 +86,48 @@ public class FlatFileExporter
 		{
 			super(val);
 		}
-		
-		
-		private File getDataFile()
+
+		private String readFileAsString(String filePath) throws java.io.IOException
 		{
-			File res = new File("src/MWC/Utilities/ReaderWriter/FlatFile/fakedata.txt");
-			return res;
+			StringBuffer fileData = new StringBuffer(1000);
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));
+			char[] buf = new char[1024];
+			int numRead = 0;
+			while ((numRead = reader.read(buf)) != -1)
+			{
+				String readData = String.valueOf(buf, 0, numRead);
+				fileData.append(readData);
+				buf = new char[1024];
+			}
+			reader.close();
+			return fileData.toString();
 		}
-		
+
 		private String getTestData()
 		{
-			String res = "";
+			String res = null;
+			try
+			{
+				res = readFileAsString("src/MWC/Utilities/ReaderWriter/FlatFile/fakedata.txt");
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
 			return res;
 		}
-		
-		public void testExport()
+
+		public void testExport() throws IOException
 		{
+			final String TARGET_STR = getTestData();
+			assertNotNull("test data found", TARGET_STR);
+			assertEquals("has data", 2157, TARGET_STR.length());
+
 			FlatFileExporter fa = new FlatFileExporter();
-			String res = fa.exportThis();
-			
-			assertEquals("correct string", "na", res);
-			
-			File iFile = getDataFile();
-			assertTrue("file exists", iFile.exists());
-			
-			assertEquals("has data", 2367, iFile.length());
+			String res = fa.exportThis();			
+			assertEquals("correct string", TARGET_STR, res);
+
 		}
 	}
 
-	
 }
