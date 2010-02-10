8d854f44f1e85bdea6ea0893a58254fbe1ea81d9
ian <ian@cb33b658-6c9e-41a7-9690-cba343611204>
2010-02-10 21:16:33 +0000

    more sensor data in there
    git-svn-id: http://subversion.assembla.com/svn/Debrief@2806 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.cmap.legacy/src/MWC/Utilities/ReaderWriter/FlatFile/FlatFileExporter.java
+++ trunk/org.mwc.cmap.legacy/src/MWC/Utilities/ReaderWriter/FlatFile/FlatFileExporter.java
@@ -92,24 +92,27 @@ public class FlatFileExporter
 				+ HEADER_LINE
 				+ BRK;
 		;
-		String body = collateLine(0, 7, 6.32332, -5555.55) + BRK
-		+  collateLine(1, 7, 6.32332,-5555.551 ) + BRK
-		+  collateLine(2, 7, 6.32332, -5555.55) + BRK
-		+  collateLine(3, 7, 6.32332, -5521.2) + BRK
-		+  collateLine(4, 7, 6.32332, -5555.32) + BRK
-		+  collateLine(5, 7, 6.32332, -5543.73);
+		String body = collateLine(0, 7, 6.32332, -5555.55, 2.7, 200.1, 0, -999, -999, -999.9, -999.9) + BRK
+		+  collateLine(1, 7, 6.32332,-5555.551 , 2.7, 200, 0, -999, -999, -999.9, -999.9) + BRK
+		+  collateLine(2, 7, 6.32332, -5555.55, 2.7, 200, 0, -999, -999, -999.9, -999.9) + BRK
+		+  collateLine(3, 7, 6.32332, -5521.2, 4.6, 200, 0, -999, -999, -999.9, -999.9) + BRK
+		+  collateLine(4, 7, 6.32332, -5555.32, 4.7, 200, 0, -999, -999, -999.9, -999.9) + BRK
+		+  collateLine(5, 7, 6.32332, -5543.73, 4.8, 200.1, 0, -999, -999, -999.9, -999.9);
 		
 		return header + body;
 	}
 
-	private String collateLine(int secs, int osStat, double osX, double osY)
+	private String collateLine(int secs, int osStat, double osX, double osY, double spdKts, double headDegs, int sensorStat, int sensorX, int sensorY,
+			double sensorBrg, double sensorBacc)
 	{
 		final String tab = "\t";
 		String res = null;
 		switch(secs)
 		{
 		case 0:
-			res = secs + tab + osStat + tab + osX + tab + osY + tab +"2.7	200.1	0	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9";
+			res = secs + tab + osStat + tab + osX + tab + osY + tab + spdKts + tab + headDegs + tab + 
+					sensorStat + tab + sensorX + tab + sensorY + tab + sensorBrg + tab + sensorBacc + tab + 
+					"-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9";
 			break;
 		case 1:
 			res = secs + "	7	6.32332	-5555.551	2.7	200	0	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9	-999	6	-999.9	-999.9	1.1	11.12	0	-999.9	-999.9	-999.9	-999.9	-999	-999	-999.9	-999.9	-999.9	-999.9	-999.9	-999.9";
