a86aa73117b64d65edc60cd6c26503230dae6997
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2010-11-01 16:22:06 +0000

    only tidy-reformatting
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3106 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.cmap.grideditor/src/org/mwc/cmap/grideditor/chart/DataPointsDragTracker.java
+++ trunk/org.mwc.cmap.grideditor/src/org/mwc/cmap/grideditor/chart/DataPointsDragTracker.java
@@ -19,7 +19,9 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.RectangleEdge;
 
-public abstract class DataPointsDragTracker implements ChartMouseListenerExtension {
+public abstract class DataPointsDragTracker implements
+		ChartMouseListenerExtension
+{
 
 	private final JFreeChartComposite myChartPanel;
 
@@ -27,88 +29,116 @@ public abstract class DataPointsDragTracker implements ChartMouseListenerExtensi
 
 	private final boolean myAllowVerticalMovesOnly;
 
-	protected abstract void dragCompleted(BackedChartItem item, double finalX, double finalY);
+	protected abstract void dragCompleted(BackedChartItem item, double finalX,
+			double finalY);
 
-	public DataPointsDragTracker(JFreeChartComposite chartPanel, boolean allowYOnly) {
+	public DataPointsDragTracker(JFreeChartComposite chartPanel,
+			boolean allowYOnly)
+	{
 		myChartPanel = chartPanel;
 		myAllowVerticalMovesOnly = allowYOnly;
 		myDragSubject = new DragSubject();
 	}
 
 	@Override
-	public void chartMouseClicked(ChartMouseEvent event) {
+	public void chartMouseClicked(ChartMouseEvent event)
+	{
 		myDragSubject.setSubject(event.getEntity());
 		myChartPanel.redrawCanvas();
 	}
 
 	@Override
-	public void chartMouseMoved(ChartMouseEvent event) {
-		if (!myDragSubject.isEmpty()) {
+	public void chartMouseMoved(ChartMouseEvent event)
+	{
+		if (!myDragSubject.isEmpty())
+		{
 			myChartPanel.forgetZoomPoints();
-			
-//			Rectangle clientArea = myChartPanel.getClientArea();
-//			int screenX = event.getTrigger().getX() - clientArea.x;
-//			int screenY = event.getTrigger().getY() - clientArea.y;
-			
+
+			// Rectangle clientArea = myChartPanel.getClientArea();
+			// int screenX = event.getTrigger().getX() - clientArea.x;
+			// int screenY = event.getTrigger().getY() - clientArea.y;
+
 			// [IM] don't bother with sorting out the client area offset
-			//  - we've stopped using it in the FixedChartComposite calling method
+			// - we've stopped using it in the FixedChartComposite calling method
 			int screenX = event.getTrigger().getX();
 			int screenY = event.getTrigger().getY();
 
-			
-			// deliberately switch axes for following line, now that we've switched the axes to put time
+			// deliberately switch axes for following line, now that we've switched
+			// the axes to put time
 			// down the LH side.
 			Point2D point2d = new Point2D.Double(screenY, screenX);
 			XYPlot xyplot = myChartPanel.getChart().getXYPlot();
 			ChartRenderingInfo renderingInfo = myChartPanel.getChartRenderingInfo();
 			Rectangle2D dataArea = renderingInfo.getPlotInfo().getDataArea();
-			
-			// WORKAROUND:  when the grid graph gets really wide, the labels on the y-axis get stretched.
-			//              but, the dataArea value doesn't reflect this.
-			//              So, get the width values from the getScreenDataArea method - which
-			//              does reflect the scaling applied to the y axis.
-			//              - and all works well now.
+
+			// WORKAROUND: when the grid graph gets really wide, the labels on the
+			// y-axis get stretched.
+			// but, the dataArea value doesn't reflect this.
+			// So, get the width values from the getScreenDataArea method - which
+			// does reflect the scaling applied to the y axis.
+			// - and all works well now.
 			Rectangle dataArea2 = myChartPanel.getScreenDataArea();
-			dataArea = new Rectangle2D.Double(dataArea2.x, dataArea.getY(), dataArea2.width, dataArea.getHeight());
-			
-			
+			dataArea = new Rectangle2D.Double(dataArea2.x, dataArea.getY(),
+					dataArea2.width, dataArea.getHeight());
+
 			ValueAxis domainAxis = xyplot.getDomainAxis();
 			RectangleEdge domainEdge = xyplot.getDomainAxisEdge();
 			ValueAxis valueAxis = xyplot.getRangeAxis();
 			RectangleEdge valueEdge = xyplot.getRangeAxisEdge();
-			double domainX = domainAxis.java2DToValue(point2d.getX(), dataArea, domainEdge);
-			double domainY = valueAxis.java2DToValue(point2d.getY(), dataArea, valueEdge);
-			
-			if (myAllowVerticalMovesOnly) {
+			double domainX = domainAxis.java2DToValue(point2d.getX(), dataArea,
+					domainEdge);
+			double domainY = valueAxis.java2DToValue(point2d.getY(), dataArea,
+					valueEdge);
+
+			if (myAllowVerticalMovesOnly)
+			{
 				domainX = myDragSubject.getDraggedItem().getXValue();
 			}
 
-			myDragSubject.setProposedValues(domainX, domainY);
+			if (!myDragSubject.isEmpty())
+				myDragSubject.setProposedValues(domainX, domainY);
 			myChartPanel.redrawCanvas();
 		}
 	}
 
 	@Override
-	public void chartMouseReleased(ChartMouseEvent event) {
-		if (!myDragSubject.isEmpty() && myDragSubject.getLastDomainPoint() != null) {
-			Point2D finalPoint = myDragSubject.getLastDomainPoint();
-			dragCompleted(myDragSubject.getDraggedItem(), finalPoint.getX(), finalPoint.getY());
+	public void chartMouseReleased(ChartMouseEvent event)
+	{
+		if (!myDragSubject.isEmpty() && myDragSubject.getLastDomainPoint() != null)
+		{
+			try
+			{
+				Point2D finalPoint = myDragSubject.getLastDomainPoint();
+				dragCompleted(myDragSubject.getDraggedItem(), finalPoint.getX(),
+						finalPoint.getY());
+			}
+			catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		if (!myDragSubject.isEmpty()) {
+		if (!myDragSubject.isEmpty())
+		{
 			myDragSubject.setSubject(null);
 			myChartPanel.redrawCanvas();
 		}
 	}
 
-	private RendererWithDynamicFeedback getFeedbackRenderer() {
-		if (myChartPanel.getChart() == null) {
+	private RendererWithDynamicFeedback getFeedbackRenderer()
+	{
+		if (myChartPanel.getChart() == null)
+		{
 			return null;
 		}
-		XYItemRenderer renderer = myChartPanel.getChart().getXYPlot().getRenderer(0);
-		return renderer instanceof RendererWithDynamicFeedback ? (RendererWithDynamicFeedback) renderer : null;
+		XYItemRenderer renderer = myChartPanel.getChart().getXYPlot()
+				.getRenderer(0);
+		return renderer instanceof RendererWithDynamicFeedback ? (RendererWithDynamicFeedback) renderer
+				: null;
 	}
 
-	private class DragSubject {
+	private class DragSubject
+	{
 
 		private BackedChartItem myDraggedItem;
 
@@ -116,75 +146,97 @@ public abstract class DataPointsDragTracker implements ChartMouseListenerExtensi
 
 		private Point2D.Double myLastDomainPoint;
 
-		public void setSubject(ChartEntity chartEntity) {
+		public void setSubject(ChartEntity chartEntity)
+		{
 			clear();
-			if (chartEntity instanceof XYItemEntity) {
+			if (chartEntity instanceof XYItemEntity)
+			{
 				myDraggedEntity = (XYItemEntity) chartEntity;
 				myDraggedItem = extractBackedChartItem(myDraggedEntity);
-				if (myDraggedItem == null) {
+				if (myDraggedItem == null)
+				{
 					clear();
 				}
 			}
 
 			RendererWithDynamicFeedback renderer = getFeedbackRenderer();
-			if (renderer != null) {
+			if (renderer != null)
+			{
 				renderer.setFeedbackSubject(myDraggedEntity);
 				renderer.setFeedBackValue(null);
 			}
 		}
 
-		public void setProposedValues(double x, double y) {
-			if (isEmpty()) {
+		public void setProposedValues(double x, double y)
+		{
+			if (isEmpty())
+			{
 				return;
 			}
 
 			RendererWithDynamicFeedback renderer = getFeedbackRenderer();
-			if (renderer == null) {
+			if (renderer == null)
+			{
 				return;
 			}
-			if (myLastDomainPoint == null) {
+			if (myLastDomainPoint == null)
+			{
 				myLastDomainPoint = new Point2D.Double();
 			}
 			myLastDomainPoint.setLocation(x, y);
 			renderer.setFeedBackValue((Point2D.Double) myLastDomainPoint.clone());
 		}
 
-		public BackedChartItem getDraggedItem() {
+		public BackedChartItem getDraggedItem()
+		{
 			return myDraggedItem;
 		}
 
 		@SuppressWarnings("unused")
-		public XYItemEntity getDraggedEntity() {
+		public XYItemEntity getDraggedEntity()
+		{
 			return myDraggedEntity;
 		}
 
-		public boolean isEmpty() {
+		public boolean isEmpty()
+		{
 			return myDraggedEntity == null;
 		}
 
-		private void clear() {
+		private void clear()
+		{
 			myDraggedEntity = null;
 			myDraggedItem = null;
 			myLastDomainPoint = null;
 		}
 
-		public Point2D.Double getLastDomainPoint() {
+		public Point2D.Double getLastDomainPoint()
+		{
 			return myLastDomainPoint;
 		}
 
-		private BackedChartItem extractBackedChartItem(XYItemEntity xyEntity) {
+		private BackedChartItem extractBackedChartItem(XYItemEntity xyEntity)
+		{
 			@SuppressWarnings("unchecked")
-			Comparable seriesKey = xyEntity.getDataset().getSeriesKey(xyEntity.getSeriesIndex());
-			if (xyEntity.getDataset() instanceof XYSeriesCollection) {
-				XYSeries series = ((XYSeriesCollection) xyEntity.getDataset()).getSeries(seriesKey);
+			Comparable seriesKey = xyEntity.getDataset().getSeriesKey(
+					xyEntity.getSeriesIndex());
+			if (xyEntity.getDataset() instanceof XYSeriesCollection)
+			{
+				XYSeries series = ((XYSeriesCollection) xyEntity.getDataset())
+						.getSeries(seriesKey);
 				XYDataItem dataItem = series.getDataItem(xyEntity.getItem());
-				if (dataItem instanceof BackedChartItem) {
+				if (dataItem instanceof BackedChartItem)
+				{
 					return (BackedChartItem) dataItem;
 				}
-			} else if (xyEntity.getDataset() instanceof TimeSeriesCollection) {
-				TimeSeries series = ((TimeSeriesCollection) xyEntity.getDataset()).getSeries(seriesKey);
+			}
+			else if (xyEntity.getDataset() instanceof TimeSeriesCollection)
+			{
+				TimeSeries series = ((TimeSeriesCollection) xyEntity.getDataset())
+						.getSeries(seriesKey);
 				TimeSeriesDataItem dataItem = series.getDataItem(xyEntity.getItem());
-				if (dataItem instanceof BackedChartItem) {
+				if (dataItem instanceof BackedChartItem)
+				{
 					return (BackedChartItem) dataItem;
 				}
 			}
