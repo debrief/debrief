e947246377c71436daa0059c981de85b08d0902d
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2010-11-16 10:47:57 +0000

    introduce equality test for category objects
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3202 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.asset.legacy/src/ASSET/Participants/Category.java
+++ trunk/org.mwc.asset.legacy/src/ASSET/Participants/Category.java
@@ -2,12 +2,11 @@ package ASSET.Participants;
 
 import java.awt.*;
 
+import junit.framework.TestCase;
+
 /**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- *
+ * Title: Description: Copyright: Copyright (c) 2001 Company:
+ * 
  * @author
  * @version 1.0
  */
@@ -15,267 +14,303 @@ import java.awt.*;
 public class Category implements java.io.Serializable
 {
 
-  /**
-   * the serializable id of this type
-   */
-  private static final long serialVersionUID = 33;
+	/**
+	 * the serializable id of this type
+	 */
+	private static final long serialVersionUID = 33;
 
-  public interface Force
-  {
-    /**
-     * force
-     */
-    static public final String RED = "RED";
-    static public final String BLUE = "BLUE";
-    public static final String GREEN = "GREEN";
-  }
+	@Override
+	public boolean equals(Object arg0)
+	{
+		return this.hashCode() == arg0.hashCode();
+	}
 
-  public interface Environment
-  {
-    /**
-     * environment
-     */
-    static public final String SUBSURFACE = "SUBSURFACE";
-    static public final String SURFACE = "SURFACE";
-    public static final String AIRBORNE = "AIRBORNE";
-    public static final String CROSS = "CROSS";
-  }
+	@Override
+	public int hashCode()
+	{
+		return environment.hashCode() * force.hashCode() * type.hashCode();
+	}
 
-  public interface Type
-  {
+	public interface Force
+	{
+		/**
+		 * force
+		 */
+		static public final String RED = "RED";
+		static public final String BLUE = "BLUE";
+		public static final String GREEN = "GREEN";
+	}
 
-    /**
-     * submarine types
-     */
-    static public final String SUBMARINE = "SUBMARINE";
-    public static final String MINISUB = "MINISUB";
-    public static final String TORPEDO = "TORPEDO";
-    public static final String SONAR_BUOY = "SONAR_BUOY";
+	public interface Environment
+	{
+		/**
+		 * environment
+		 */
+		static public final String SUBSURFACE = "SUBSURFACE";
+		static public final String SURFACE = "SURFACE";
+		public static final String AIRBORNE = "AIRBORNE";
+		public static final String CROSS = "CROSS";
+	}
 
-    /**
-     * surface types
-     */
-    static public final String CARRIER = "CARRIER";
-    static public final String FRIGATE = "FRIGATE";
-    static public final String DESTROYER = "DESTROYER";
-    public static final String TROOP_CARRIER = "TROOP_CARRIER";
-    public static final String OILER = "OILER";
-    public static final String FISHING_VESSEL = "FISHING_VESSEL";
+	public interface Type
+	{
 
+		/**
+		 * submarine types
+		 */
+		static public final String SUBMARINE = "SUBMARINE";
+		public static final String MINISUB = "MINISUB";
+		public static final String TORPEDO = "TORPEDO";
+		public static final String SONAR_BUOY = "SONAR_BUOY";
 
-    /**
-     * airborne types
-     */
-    public static final String HELO = "HELICOPTER";
-    public static final String MPA = "MPA";
-    static public final String AV_MISSILE = "AV_MISSILE";
+		/**
+		 * surface types
+		 */
+		static public final String CARRIER = "CARRIER";
+		static public final String FRIGATE = "FRIGATE";
+		static public final String DESTROYER = "DESTROYER";
+		public static final String TROOP_CARRIER = "TROOP_CARRIER";
+		public static final String OILER = "OILER";
+		public static final String FISHING_VESSEL = "FISHING_VESSEL";
 
-  }
+		/**
+		 * airborne types
+		 */
+		public static final String HELO = "HELICOPTER";
+		public static final String MPA = "MPA";
+		static public final String AV_MISSILE = "AV_MISSILE";
 
-  /**
-   * the lists of objects
-   */
-  static private java.util.Vector<String> _forces;
-  static private java.util.Vector<String> _environments;
-  static private java.util.Vector<String> _types;
+	}
 
-  /**
-   * our local objects
-   */
-  private String type;
-  private String environment;
-  private String force;
+	/**
+	 * the lists of objects
+	 */
+	static private java.util.Vector<String> _forces;
+	static private java.util.Vector<String> _environments;
+	static private java.util.Vector<String> _types;
 
-  //////////////////////////////////////////////////////////////////////
-  // constructors
-  //////////////////////////////////////////////////////////////////////
-  public Category(final String force, final String environment, final String type)
-  {
-    setForce(force);
-    setType(type);
-    setEnvironment(environment);
-  }
+	/**
+	 * our local objects
+	 */
+	private String type;
+	private String environment;
+	private String force;
 
-  public Category()
-  {
-  }
-  //////////////////////////////////////////////////////////////////////
-  // property getter/setters
-  //////////////////////////////////////////////////////////////////////
+	// ////////////////////////////////////////////////////////////////////
+	// constructors
+	// ////////////////////////////////////////////////////////////////////
+	public Category(final String force, final String environment,
+			final String type)
+	{
+		setForce(force);
+		setType(type);
+		setEnvironment(environment);
+	}
 
-  public String getType()
-  {
-    return type;
-  }
+	public Category()
+	{
+	}
 
-  public void setType(final String newType)
-  {
-    type = newType;
-  }
+	// ////////////////////////////////////////////////////////////////////
+	// property getter/setters
+	// ////////////////////////////////////////////////////////////////////
 
-  public void setEnvironment(final String newEnvironment)
-  {
-    environment = newEnvironment;
-  }
+	public String getType()
+	{
+		return type;
+	}
 
-  public String getEnvironment()
-  {
-    return environment;
-  }
+	public void setType(final String newType)
+	{
+		type = newType;
+	}
 
-  public void setForce(final String newForce)
-  {
-    force = newForce;
-  }
+	public void setEnvironment(final String newEnvironment)
+	{
+		environment = newEnvironment;
+	}
 
-  public String getForce()
-  {
-    return force;
-  }
+	public String getEnvironment()
+	{
+		return environment;
+	}
 
-  public boolean isA(final String type1)
-  {
-    final boolean res;
+	public void setForce(final String newForce)
+	{
+		force = newForce;
+	}
 
-    if ((getType() == type1) ||
-      (getForce() == type1) ||
-      (getEnvironment() == type1))
-      res = true;
-    else
-      res = false;
+	public String getForce()
+	{
+		return force;
+	}
 
-    return res;
-  }
+	public boolean isA(final String type1)
+	{
+		final boolean res;
 
-  public String toString()
-  {
-    return "Category force:" + getForce() + " type:" + getType() + " env:" + getEnvironment();
-  }
+		if ((getType() == type1) || (getForce() == type1)
+				|| (getEnvironment() == type1))
+			res = true;
+		else
+			res = false;
 
-  public String toShortString()
-  {
-    return getForce() + " " + getType().substring(0, 4) + " " + getEnvironment().substring(0, 3);
-  }
+		return res;
+	}
 
-  static public java.util.Vector<String> getTypes()
-  {
-    checkLists();
-    return _types;
-  }
+	public String toString()
+	{
+		return "Category force:" + getForce() + " type:" + getType() + " env:"
+				+ getEnvironment();
+	}
 
-  static public java.util.Vector<String> getForces()
-  {
-    checkLists();
-    return _forces;
-  }
+	public String toShortString()
+	{
+		return getForce() + " " + getType().substring(0, 4) + " "
+				+ getEnvironment().substring(0, 3);
+	}
 
-  static public java.util.Vector<String> getEnvironments()
-  {
-    checkLists();
-    return _environments;
-  }
+	static public java.util.Vector<String> getTypes()
+	{
+		checkLists();
+		return _types;
+	}
 
-  static private void checkLists()
-  {
-    if (_types == null)
-    {
-      _forces = new java.util.Vector<String>(3, 1);
-      _forces.addElement(Force.RED);
-      _forces.addElement(Force.GREEN);
-      _forces.addElement(Force.BLUE);
+	static public java.util.Vector<String> getForces()
+	{
+		checkLists();
+		return _forces;
+	}
 
-      _environments = new java.util.Vector<String>(4, 1);
-      _environments.addElement(Environment.SUBSURFACE);
-      _environments.addElement(Environment.SURFACE);
-      _environments.addElement(Environment.AIRBORNE);
-      _environments.addElement(Environment.CROSS);
+	static public java.util.Vector<String> getEnvironments()
+	{
+		checkLists();
+		return _environments;
+	}
 
-      _types = new java.util.Vector<String>(6, 1);
-      _types.addElement(Type.SUBMARINE);
-      _types.addElement(Type.MINISUB);
-      _types.addElement(Type.TORPEDO);
-      _types.addElement(Type.SONAR_BUOY);
-      _types.addElement(Type.CARRIER);
-      _types.addElement(Type.FRIGATE);
-      _types.addElement(Type.DESTROYER);
-      _types.addElement(Type.TROOP_CARRIER);
-      _types.addElement(Type.OILER);
-      _types.addElement(Type.FISHING_VESSEL);
-      _types.addElement(Type.HELO);
-      _types.addElement(Type.MPA);
-      _types.addElement(Type.AV_MISSILE);
-    }
-  }
+	static private void checkLists()
+	{
+		if (_types == null)
+		{
+			_forces = new java.util.Vector<String>(3, 1);
+			_forces.addElement(Force.RED);
+			_forces.addElement(Force.GREEN);
+			_forces.addElement(Force.BLUE);
 
+			_environments = new java.util.Vector<String>(4, 1);
+			_environments.addElement(Environment.SUBSURFACE);
+			_environments.addElement(Environment.SURFACE);
+			_environments.addElement(Environment.AIRBORNE);
+			_environments.addElement(Environment.CROSS);
 
-  static public String checkEnv(final String val)
-  {
-    String res = null;
+			_types = new java.util.Vector<String>(6, 1);
+			_types.addElement(Type.SUBMARINE);
+			_types.addElement(Type.MINISUB);
+			_types.addElement(Type.TORPEDO);
+			_types.addElement(Type.SONAR_BUOY);
+			_types.addElement(Type.CARRIER);
+			_types.addElement(Type.FRIGATE);
+			_types.addElement(Type.DESTROYER);
+			_types.addElement(Type.TROOP_CARRIER);
+			_types.addElement(Type.OILER);
+			_types.addElement(Type.FISHING_VESSEL);
+			_types.addElement(Type.HELO);
+			_types.addElement(Type.MPA);
+			_types.addElement(Type.AV_MISSILE);
+		}
+	}
 
-    final java.util.Iterator<String> it = getEnvironments().iterator();
-    while (it.hasNext())
-    {
-      final String thisC = (String) it.next();
-      if (thisC.equals(val))
-      {
-        res = thisC;
-        break;
-      }
+	static public String checkEnv(final String val)
+	{
+		String res = null;
 
-    }
-    return res;
-  }
+		final java.util.Iterator<String> it = getEnvironments().iterator();
+		while (it.hasNext())
+		{
+			final String thisC = (String) it.next();
+			if (thisC.equals(val))
+			{
+				res = thisC;
+				break;
+			}
 
-  static public String checkForce(final String val)
-  {
-    String res = null;
+		}
+		return res;
+	}
 
-    final java.util.Iterator<String> it = getForces().iterator();
-    while (it.hasNext())
-    {
-      final String thisC = (String) it.next();
-      if (thisC.equals(val))
-      {
-        res = thisC;
-        break;
-      }
+	static public String checkForce(final String val)
+	{
+		String res = null;
 
-    }
+		final java.util.Iterator<String> it = getForces().iterator();
+		while (it.hasNext())
+		{
+			final String thisC = (String) it.next();
+			if (thisC.equals(val))
+			{
+				res = thisC;
+				break;
+			}
 
-    return res;
-  }
+		}
 
-  static public String checkType(final String val)
-  {
-    String res = null;
+		return res;
+	}
 
-    final java.util.Iterator<String> it = getTypes().iterator();
-    while (it.hasNext())
-    {
-      final String thisC = (String) it.next();
-      if (thisC.equals(val))
-      {
-        res = thisC;
-        break;
-      }
+	static public String checkType(final String val)
+	{
+		String res = null;
 
-    }
-    return res;
-  }
+		final java.util.Iterator<String> it = getTypes().iterator();
+		while (it.hasNext())
+		{
+			final String thisC = (String) it.next();
+			if (thisC.equals(val))
+			{
+				res = thisC;
+				break;
+			}
 
-  private static Color getColorFor(final String category)
-  {
-    if (category.equals(ASSET.Participants.Category.Force.RED))
-      return java.awt.Color.red;
-    else if (category.equals(ASSET.Participants.Category.Force.BLUE))
-      return java.awt.Color.blue;
-    else
-      return java.awt.Color.green;
-  }
+		}
+		return res;
+	}
 
-  static public Color getColorFor(final Category theCategory)
-  {
-    return getColorFor(theCategory.getForce());
-  }
+	private static Color getColorFor(final String category)
+	{
+		if (category.equals(ASSET.Participants.Category.Force.RED))
+			return java.awt.Color.red;
+		else if (category.equals(ASSET.Participants.Category.Force.BLUE))
+			return java.awt.Color.blue;
+		else
+			return java.awt.Color.green;
+	}
 
+	static public Color getColorFor(final Category theCategory)
+	{
+		return getColorFor(theCategory.getForce());
+	}
+
+	/**
+	 * and here's how to test it
+	 * 
+	 * @author ianmayo
+	 * 
+	 */
+	public static class CommsTest extends TestCase
+	{
+		public void testEquals()
+		{
+			Category catOne = new Category(Force.BLUE, Environment.SURFACE,
+					Type.FRIGATE);
+			Category catTwo = new Category(Force.RED, Environment.SURFACE,
+					Type.FRIGATE);
+			Category catThree = new Category(Force.BLUE, Environment.SURFACE,
+					Type.FRIGATE);
+
+			assertNotSame("not equal", catOne, catTwo);
+			assertTrue(" equal", catOne.equals(catThree));
+			assertTrue(" equal", catThree.equals(catOne));
+			assertFalse(" equal", catOne.equals(catTwo));
+		}
+	}
 }
