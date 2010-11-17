2164c928adba242af705f3390dd739d94f46cfc9
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2010-11-17 18:57:14 +0000

    more UI in there (dem states)
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3220 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.asset.NetAsset/src/org/mwc/asset/netasset/view/HolderPane.java
+++ trunk/org.mwc.asset.NetAsset/src/org/mwc/asset/netasset/view/HolderPane.java
@@ -3,9 +3,15 @@ package org.mwc.asset.netasset.view;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Text;
 
 public class HolderPane extends Composite
 {
+	private Text demCourse;
 
 	/**
 	 * Create the composite.
@@ -15,10 +21,36 @@ public class HolderPane extends Composite
 	public HolderPane(Composite parent, int style)
 	{
 		super(parent, style);
+		setLayout(new RowLayout(SWT.HORIZONTAL));
 		
-		Button button = new Button(this, SWT.NONE);
-		button.setBounds(24, 69, 94, 30);
-		button.setText("New Button");
+		Composite composite = new Composite(this, SWT.NONE);
+		composite.setLayout(new RowLayout(SWT.HORIZONTAL));
+		
+		Group state = new Group(composite, SWT.NONE);
+		state.setLayout(new GridLayout(3, false));
+		
+		Label lblProperty = new Label(state, SWT.NONE);
+		lblProperty.setText("Property");
+		
+		Label lblDemanded = new Label(state, SWT.NONE);
+		lblDemanded.setText("Demanded");
+		
+		Label lblActual = new Label(state, SWT.NONE);
+		lblActual.setText("Actual");
+		
+		Label lblCourse = new Label(state, SWT.NONE);
+		lblCourse.setBounds(0, 0, 59, 14);
+		lblCourse.setText("Course");
+		
+		demCourse = new Text(state, SWT.BORDER);
+		demCourse.setText("000");
+		demCourse.setBounds(0, 0, 8, 19);
+		
+		Label actCourse = new Label(state, SWT.NONE);
+		actCourse.setBounds(0, 0, 59, 14);
+		actCourse.setText("000");
+		
+		Composite composite_1 = new Composite(this, SWT.NONE);
 
 	}
 
