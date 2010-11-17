cf9ceebb3136611c360689ce4e212dc43899e278
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2010-11-17 19:00:27 +0000

    placeholder for time
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3221 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.asset.NetAsset/src/org/mwc/asset/netasset/view/HolderPane.java
+++ trunk/org.mwc.asset.NetAsset/src/org/mwc/asset/netasset/view/HolderPane.java
@@ -12,6 +12,8 @@ import org.eclipse.swt.widgets.Text;
 public class HolderPane extends Composite
 {
 	private Text demCourse;
+	private Text demSpeed;
+	private Text demDepth;
 
 	/**
 	 * Create the composite.
@@ -24,7 +26,10 @@ public class HolderPane extends Composite
 		setLayout(new RowLayout(SWT.HORIZONTAL));
 		
 		Composite composite = new Composite(this, SWT.NONE);
-		composite.setLayout(new RowLayout(SWT.HORIZONTAL));
+		composite.setLayout(new RowLayout(SWT.VERTICAL));
+		
+		Group group = new Group(composite, SWT.NONE);
+		group.setSize(76, 83);
 		
 		Group state = new Group(composite, SWT.NONE);
 		state.setLayout(new GridLayout(3, false));
@@ -49,6 +54,42 @@ public class HolderPane extends Composite
 		Label actCourse = new Label(state, SWT.NONE);
 		actCourse.setBounds(0, 0, 59, 14);
 		actCourse.setText("000");
+		
+		Label lblSpeed = new Label(state, SWT.NONE);
+		lblSpeed.setBounds(0, 0, 59, 14);
+		lblSpeed.setText("Speed");
+		
+		demSpeed = new Text(state, SWT.BORDER);
+		demSpeed.setText("000");
+		demSpeed.setBounds(0, 0, 8, 19);
+		
+		Label actSpeed = new Label(state, SWT.NONE);
+		actSpeed.setSize(59, 14);
+		actSpeed.setText("000");
+		
+		Label lblDepth = new Label(state, SWT.NONE);
+		lblDepth.setBounds(0, 0, 59, 14);
+		lblDepth.setText("Depth");
+		
+		demDepth = new Text(state, SWT.BORDER);
+		demDepth.setText("000");
+		demDepth.setSize(8, 19);
+		
+		Label actDepth = new Label(state, SWT.NONE);
+		actDepth.setBounds(0, 0, 59, 14);
+		actDepth.setText("000");
+		
+		Label label = new Label(state, SWT.NONE);
+		label.setBounds(0, 0, 59, 14);
+		label.setText("New Label");
+		
+		Label label_1 = new Label(state, SWT.NONE);
+		label_1.setBounds(0, 0, 59, 14);
+		label_1.setText("New Label");
+		
+		Button newState = new Button(state, SWT.NONE);
+		newState.setBounds(0, 0, 94, 30);
+		newState.setText("Submit");
 		
 		Composite composite_1 = new Composite(this, SWT.NONE);
 
