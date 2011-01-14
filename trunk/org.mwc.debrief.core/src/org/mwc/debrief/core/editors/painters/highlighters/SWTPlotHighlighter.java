dd63cf93537e915e8c4774189eac54027d19981b
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-01-14 10:54:13 +0000

    only show tail highlight if that sensor is visible
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3385 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/editors/painters/highlighters/SWTPlotHighlighter.java
+++ trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/editors/painters/highlighters/SWTPlotHighlighter.java
@@ -30,33 +30,32 @@ import MWC.GenericData.WorldDistance.ArrayLength;
  * @author IAN MAYO
  * @version 1
  */
-public interface SWTPlotHighlighter extends Editable
-{
+public interface SWTPlotHighlighter extends Editable {
 
 	/**
 	 * Draw a highlight around this watchable
 	 * 
 	 * @param proj
-	 *          the current projection
+	 *            the current projection
 	 * @param dest
-	 *          the place to draw this highlight
+	 *            the place to draw this highlight
 	 * @param watch
-	 *          the current data point
+	 *            the current data point
 	 * @param isPrimary
-	 *          whether this is the primary track
+	 *            whether this is the primary track
 	 */
 	void highlightIt(MWC.Algorithms.PlainProjection proj, CanvasType dest,
-			MWC.GenericData.WatchableList list, MWC.GenericData.Watchable watch,
-			boolean isPrimary);
+			MWC.GenericData.WatchableList list,
+			MWC.GenericData.Watchable watch, boolean isPrimary);
 
 	// ////////////////////////////////////////////////////////////////////
-	// embedded class which just shows rectangular highlight around current point
+	// embedded class which just shows rectangular highlight around current
+	// point
 	// ////////////////////////////////////////////////////////////////////
 	/**
 	 * A simple (rectangular) highlighter
 	 */
-	public final class RectangleHighlight implements SWTPlotHighlighter
-	{
+	public final class RectangleHighlight implements SWTPlotHighlighter {
 
 		private Color _myColor = Color.white;
 		private int _mySize = 5;
@@ -65,32 +64,28 @@ public interface SWTPlotHighlighter extends Editable
 		 * Draw a highlight around this watchable
 		 * 
 		 * @param proj
-		 *          the current projection
+		 *            the current projection
 		 * @param dest
-		 *          the place to draw this highlight
+		 *            the place to draw this highlight
 		 * @param watch
-		 *          the current data point
+		 *            the current data point
 		 */
 		public final void highlightIt(MWC.Algorithms.PlainProjection proj,
 				CanvasType dest, MWC.GenericData.WatchableList list,
-				MWC.GenericData.Watchable watch, boolean isPrimary)
-		{
+				MWC.GenericData.Watchable watch, boolean isPrimary) {
 			// check that our graphics context is still valid -
 			// we can't, so we will just have to trap any exceptions it raises
-			try
-			{
+			try {
 
 				// sort out if this is an item that we plot
-				if (watch instanceof DoNotHighlightMe)
-				{
+				if (watch instanceof DoNotHighlightMe) {
 					// hey, don't bother...
 					return;
 				}
 
 				// see if we're painting to WMF, in which case we don't want a
 				// highlight.
-				if (dest instanceof MetafileCanvas)
-				{
+				if (dest instanceof MetafileCanvas) {
 					return;
 				}
 
@@ -121,46 +116,51 @@ public interface SWTPlotHighlighter extends Editable
 				// plot the rectangle
 				dest.drawRect(x, y, wid, ht);
 
-				// just see if we've got sensor data, so we can plot the array centre
-				if (watch instanceof FixWrapper)
-				{
+				// just see if we've got sensor data, so we can plot the array
+				// centre
+				if (watch instanceof FixWrapper) {
 					FixWrapper fw = (FixWrapper) watch;
 					TrackWrapper tw = fw.getTrackWrapper();
-					if (tw.getPlotArrayCentre())
-					{
-						Enumeration<Editable> enumer = tw.getSensors().elements();
-						while (enumer.hasMoreElements())
-						{
-							SensorWrapper sw = (SensorWrapper) enumer.nextElement();
-							ArrayLength len = sw.getSensorOffset();
-							if (len != null)
-							{
-								if (len.getValue() != 0)
-								{
-									WorldLocation centre = tw.getBacktraceTo(fw.getTime(), len,
-											sw.getWormInHole());
-									Point pt = dest.toScreen(centre);
-									dest.drawLine(pt.x - _mySize, pt.y - _mySize, pt.x + _mySize,
-											pt.y + _mySize);
-									dest.drawLine(pt.x + _mySize, pt.y - _mySize, pt.x - _mySize,
-											pt.y + _mySize);
+					if (tw.getPlotArrayCentre()) {
+						Enumeration<Editable> enumer = tw.getSensors()
+								.elements();
+						while (enumer.hasMoreElements()) {
+							SensorWrapper sw = (SensorWrapper) enumer
+									.nextElement();
 
-									// store the new screen update area
-									thisR = new Rectangle(pt.x - _mySize, pt.y - _mySize,
-											_mySize, _mySize);
-									_areaCovered.add(thisR);
-									thisR = new Rectangle(pt.x + _mySize, pt.y - _mySize,
-											_mySize, _mySize);
-									_areaCovered.add(thisR);
+							// is this sensor visible?
+							if (sw.getVisible()) {
+								ArrayLength len = sw.getSensorOffset();
+								if (len != null) {
+									if (len.getValue() != 0) {
+										WorldLocation centre = tw
+												.getBacktraceTo(fw.getTime(),
+														len, sw.getWormInHole());
+										Point pt = dest.toScreen(centre);
+										dest.drawLine(pt.x - _mySize, pt.y
+												- _mySize, pt.x + _mySize, pt.y
+												+ _mySize);
+										dest.drawLine(pt.x + _mySize, pt.y
+												- _mySize, pt.x - _mySize, pt.y
+												+ _mySize);
+
+										// store the new screen update area
+										thisR = new Rectangle(pt.x - _mySize,
+												pt.y - _mySize, _mySize,
+												_mySize);
+										_areaCovered.add(thisR);
+										thisR = new Rectangle(pt.x + _mySize,
+												pt.y - _mySize, _mySize,
+												_mySize);
+										_areaCovered.add(thisR);
+									}
 								}
 							}
 						}
 					}
 				}
 
-			}
-			catch (IllegalStateException e)
-			{
+			} catch (IllegalStateException e) {
 				MWC.Utilities.Errors.Trace.trace(e);
 			}
 
@@ -171,8 +171,7 @@ public interface SWTPlotHighlighter extends Editable
 		 * 
 		 * @return the name of this editable object
 		 */
-		public final String getName()
-		{
+		public final String getName() {
 			return "Default Highlight";
 		}
 
@@ -181,19 +180,17 @@ public interface SWTPlotHighlighter extends Editable
 		 * 
 		 * @return the name of this editable object
 		 */
-		public final String toString()
-		{
+		public final String toString() {
 			return getName();
 		}
 
 		/**
-		 * whether there is any edit information for this item this is a convenience
-		 * function to save creating the EditorType data first
+		 * whether there is any edit information for this item this is a
+		 * convenience function to save creating the EditorType data first
 		 * 
 		 * @return yes/no
 		 */
-		public final boolean hasEditor()
-		{
+		public final boolean hasEditor() {
 			return true;
 		}
 
@@ -202,8 +199,7 @@ public interface SWTPlotHighlighter extends Editable
 		 * 
 		 * @return the BeanInfo data for this editable object
 		 */
-		public final Editable.EditorType getInfo()
-		{
+		public final Editable.EditorType getInfo() {
 			return new RectangleHighlightInfo(this);
 		}
 
@@ -211,10 +207,9 @@ public interface SWTPlotHighlighter extends Editable
 		 * change the colour of the highlight
 		 * 
 		 * @param val
-		 *          the new colour
+		 *            the new colour
 		 */
-		public final void setColor(final Color val)
-		{
+		public final void setColor(final Color val) {
 			_myColor = val;
 		}
 
@@ -222,10 +217,9 @@ public interface SWTPlotHighlighter extends Editable
 		 * change the size of the highlight to plot
 		 * 
 		 * @param val
-		 *          the new size (stored with its constraints)
+		 *            the new size (stored with its constraints)
 		 */
-		public final void setSize(final BoundedInteger val)
-		{
+		public final void setSize(final BoundedInteger val) {
 			_mySize = val.getCurrent();
 		}
 
@@ -234,8 +228,7 @@ public interface SWTPlotHighlighter extends Editable
 		 * 
 		 * @return the colour
 		 */
-		public final Color getColor()
-		{
+		public final Color getColor() {
 			return _myColor;
 		}
 
@@ -244,8 +237,7 @@ public interface SWTPlotHighlighter extends Editable
 		 * 
 		 * @return current size, stored with it's constraints
 		 */
-		public final BoundedInteger getSize()
-		{
+		public final BoundedInteger getSize() {
 			return new BoundedInteger(_mySize, 0, 20);
 		}
 
@@ -255,17 +247,15 @@ public interface SWTPlotHighlighter extends Editable
 		/**
 		 * the set of editable details for the painter
 		 */
-		public final class RectangleHighlightInfo extends Editable.EditorType
-		{
+		public final class RectangleHighlightInfo extends Editable.EditorType {
 
 			/**
 			 * constructor for editable
 			 * 
 			 * @param data
-			 *          the object we are editing
+			 *            the object we are editing
 			 */
-			public RectangleHighlightInfo(final RectangleHighlight data)
-			{
+			public RectangleHighlightInfo(final RectangleHighlight data) {
 				super(data, "Default Highlight", "");
 			}
 
@@ -274,17 +264,13 @@ public interface SWTPlotHighlighter extends Editable
 			 * 
 			 * @return the properties
 			 */
-			public final java.beans.PropertyDescriptor[] getPropertyDescriptors()
-			{
-				try
-				{
-					final java.beans.PropertyDescriptor[] res =
-					{ prop("Color", "Color to paint highlight"),
+			public final java.beans.PropertyDescriptor[] getPropertyDescriptors() {
+				try {
+					final java.beans.PropertyDescriptor[] res = {
+							prop("Color", "Color to paint highlight"),
 							prop("Size", "size to paint highlight (pixels)"), };
 					return res;
-				}
-				catch (Exception e)
-				{
+				} catch (Exception e) {
 					MWC.Utilities.Errors.Trace.trace(e);
 					return super.getPropertyDescriptors();
 				}
