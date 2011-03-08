8ffddc7b85f94402dfe057919c3a003b64e8a0af
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-03-08 11:54:42 +0000

    initial import (non-polluted by Swing layout control)
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3527 cb33b658-6c9e-41a7-9690-cba343611204

---
+++ trunk/org.mwc.debrief.multipath2/src/org/mwc/debrief/multipath2/model/MultiPathModel.java
@@ -0,0 +1,260 @@
+package org.mwc.debrief.multipath2.model;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.jfree.data.time.FixedMillisecond;
+import org.jfree.data.time.TimeSeries;
+import org.mwc.debrief.multipath2.model.TimeDeltas.Observation;
+
+import Debrief.Wrappers.FixWrapper;
+import Debrief.Wrappers.LabelWrapper;
+import Debrief.Wrappers.TrackWrapper;
+import MWC.GenericData.HiResDate;
+import MWC.GenericData.Watchable;
+import MWC.GenericData.WatchableList;
+import MWC.GenericData.WorldLocation;
+import MWC.GenericData.WorldVector;
+import MWC.TacticalData.Fix;
+
+public class MultiPathModel
+{
+	/**
+	 * get the measured profile
+	 * 
+	 * @param deltas
+	 * @return a time series of measured time delays
+	 */
+	public TimeSeries getMeasuredProfileFor(TimeDeltas deltas)
+	{
+		TimeSeries res = new TimeSeries("Measured delay");
+
+		// ok, loop through the times
+		Iterator<Observation> iter = deltas.iterator();
+		while (iter.hasNext())
+		{
+			Observation thisO = iter.next();
+
+			// what's this time?
+			HiResDate tNow = thisO.getDate();
+			double delay = thisO.getInterval();
+
+			// and add it
+			res.add(new FixedMillisecond(tNow.getDate().getTime()), delay);
+		}
+
+		return res;
+	}
+
+	/**
+	 * get the calculated profile
+	 * 
+	 * @param primary
+	 *          the primary track
+	 * @param secondary
+	 *          the secondary track
+	 * @param svp
+	 *          the sound speed profile
+	 * @param deltas
+	 *          the series of time-delta observations
+	 * @param targetDepth
+	 *          target depth to experiment with
+	 * @return a time series of calculated time delays
+	 */
+	public TimeSeries getCalculatedProfileFor(WatchableList primary,
+			WatchableList secondary, SVP svp, TimeDeltas deltas, double targetDepth)
+	{
+		Boolean oldPrimaryInterp = null;
+		Boolean oldSecondaryInterp = null;
+
+		TimeSeries res = new TimeSeries("Calculated delay");
+
+		if (primary instanceof TrackWrapper)
+		{
+			TrackWrapper trk = (TrackWrapper) primary;
+			oldPrimaryInterp = trk.getInterpolatePoints();
+			trk.setInterpolatePoints(true);
+		}
+		if (secondary instanceof TrackWrapper)
+		{
+			TrackWrapper trk = (TrackWrapper) secondary;
+			oldSecondaryInterp = trk.getInterpolatePoints();
+			trk.setInterpolatePoints(true);
+		}
+
+		// ok, loop through the times
+		Iterator<Observation> iter = deltas.iterator();
+		while (iter.hasNext())
+		{
+			Observation thisO = iter.next();
+
+			// what's this time?
+			HiResDate tNow = thisO.getDate();
+
+			// find the locations
+			Watchable[] priLocs = primary.getNearestTo(tNow);
+			Watchable[] secLocs = secondary.getNearestTo(tNow);
+
+			// do we have data
+			if (priLocs.length == 0)
+				throw new RuntimeException("Insufficient primary data");
+			if (secLocs.length == 0)
+				throw new RuntimeException("Insufficient secondary data");
+
+			WorldLocation priLoc = priLocs[0].getLocation();
+			WorldLocation secLoc = secLocs[0].getLocation();
+			WorldVector sep = priLoc.subtract(secLoc);
+			double sepM = MWC.Algorithms.Conversions.Degs2m(sep.getRange());
+
+			double zR = priLoc.getDepth();
+			double zS = targetDepth;
+
+			// now sort out the sound speeds
+			double cD = svp.getMeanSpeedBetween(zS, zR);
+			double cS = svp.getMeanSpeedBetween(0, zS);
+			double cR = svp.getMeanSpeedBetween(0, zR);
+
+			// do the actual calculation
+			double time_delay = calculateDelay(sepM, zR, zS, cD, cS, cR);
+
+			res.add(new FixedMillisecond(tNow.getDate().getTime()), time_delay);
+		}
+
+		// restore the interpolation settings
+		if (oldPrimaryInterp != null)
+		{
+			TrackWrapper trk = (TrackWrapper) primary;
+			trk.setInterpolatePoints(oldPrimaryInterp.booleanValue());
+		}
+		if (oldSecondaryInterp != null)
+		{
+			TrackWrapper trk = (TrackWrapper) secondary;
+			trk.setInterpolatePoints(oldSecondaryInterp.booleanValue());
+		}
+
+		return res;
+	}
+
+	private double calculateDelay(double sepM, double zR, double zS, double cD,
+			double cS, double cR)
+	{
+		double hD = sepM;
+		double lDirect = Math.sqrt(hD * hD + Math.pow(zS - zR, 2));
+
+		double lsHoriz = (hD * zS) / (zS + zR);
+		double lS = Math.sqrt(lsHoriz * lsHoriz + zS * zS);
+		double lR = Math.sqrt(Math.pow(hD - lsHoriz, 2) + zR * zR);
+
+		double timeSurface = lS / cS;
+		double timeReceiver = lR / cR;
+		double timeDirect = lDirect / cD;
+		double time_delay = (timeSurface + timeReceiver) - timeDirect;
+		return time_delay;
+	}
+
+	// /////////////////////////////////////////////////
+	// and the testing goes here
+	// /////////////////////////////////////////////////
+	public static class IntervalTest extends junit.framework.TestCase
+	{
+
+		public void testMe()
+		{
+			WorldLocation loc = new WorldLocation(2, 2, 30);
+			LabelWrapper primary = new LabelWrapper("Sensor", loc, Color.red);
+
+			TrackWrapper secondary = new TrackWrapper();
+
+			TimeDeltas deltas = getDeltas();
+
+			HiResDate start = deltas.getStartTime();
+			HiResDate end = deltas.getEndTime();
+			long interval = 10000;
+
+			WorldLocation startLoc = loc.add(new WorldVector(Math.PI, 0.02, 0));
+
+			for (long thisT = start.getDate().getTime(); thisT <= end.getDate()
+					.getTime() + interval; thisT += interval)
+			{
+				HiResDate thisD = new HiResDate(thisT);
+				WorldLocation newLoc = new WorldLocation(startLoc.add(new WorldVector(
+						Math.PI * 1.5, 0.005, 0)));
+				Fix newF = new Fix(thisD, newLoc, 0, 0);
+				FixWrapper newFw = new FixWrapper(newF);
+				secondary.addFix(newFw);
+
+				startLoc = newLoc;
+			}
+
+			SVP svp = getSVP();
+
+			// ok, go for the calc
+			MultiPathModel model = new MultiPathModel();
+			TimeSeries calc = model.getCalculatedProfileFor(primary, secondary, svp,
+					deltas, 44);
+
+			assertNotNull("got some results", calc);
+		}
+
+		public void testCalc()
+		{
+			// ok, go for the calc
+			MultiPathModel model = new MultiPathModel();
+
+			double sepM = 2549.11;
+			double zR = 30;
+			double zS = 50;
+			double cD = 1500.760;
+			double cS = 1501.628;
+			double cR = 1502.207;
+			double delay = model.calculateDelay(sepM, zR, zS, cD, cS, cR);
+
+			assertEquals("wrong calc", -0.00044, delay, 0.001);
+
+			sepM = 2072.33;
+			assertEquals("wrong calc", -0.000034, delay, 0.001);
+
+		}
+
+		private SVP getSVP()
+		{
+			SVP svp = new SVP();
+			try
+			{
+				svp.load(SVP.SVP_Test.SVP_FILE);
+			}
+			catch (NumberFormatException e)
+			{
+				e.printStackTrace();
+				fail("number format");
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+				fail("file read problem");
+			}
+			return svp;
+		}
+
+		private static TimeDeltas getDeltas()
+		{
+			TimeDeltas deltas = new TimeDeltas();
+			try
+			{
+				deltas.load(TimeDeltas.IntervalTest.TEST_TIMES_FILE);
+			}
+			catch (NumberFormatException e)
+			{
+				e.printStackTrace();
+				fail("number format problem");
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+				fail("file read problem");
+			}
+			return deltas;
+		}
+	}
+}
---
+++ trunk/org.mwc.debrief.multipath2/src/org/mwc/debrief/multipath2/model/TimeDeltas.java
@@ -0,0 +1,158 @@
+package org.mwc.debrief.multipath2.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import MWC.GenericData.HiResDate;
+
+/**
+ * class that loads a series of time-delta observations
+ * 
+ * @author ian
+ * 
+ */
+public class TimeDeltas
+{
+	public static class Observation
+	{
+		private HiResDate time;
+		private double interval;
+
+		public Observation(HiResDate timeVal, double intervalVal)
+		{
+			time = timeVal;
+			interval = intervalVal;
+		}
+
+		public HiResDate getDate()
+		{
+			return time;
+		}
+
+		public double getInterval()
+		{
+			return interval;
+		}
+	}
+
+	private Vector<Observation> _myData;
+	
+	public HiResDate getStartTime()
+	{
+		return _myData.firstElement().getDate();
+	}
+	
+	public HiResDate getEndTime()
+	{
+		return _myData.lastElement().getDate();
+	}
+
+	/**
+	 * load an data file from the specified path
+	 * 
+	 * @param path
+	 *          where to get it from
+	 * @throws NumberFormatException
+	 *           if the numbers aren't legible
+	 * @throws IOException
+	 *           if the file can't be found
+	 */
+	public void load(String path) throws NumberFormatException, IOException
+	{
+		Vector<Long> times = new Vector<Long>();
+		Vector<Double> values = new Vector<Double>();
+
+		BufferedReader bufRdr = new BufferedReader(new FileReader(path));
+		String line = null;
+
+		// read each line of text file
+		while ((line = bufRdr.readLine()) != null)
+		{
+			StringTokenizer st = new StringTokenizer(line, ",");
+			if(st.hasMoreTokens())
+			{
+				times.add(Long.valueOf(st.nextToken()));
+			}
+			if(st.hasMoreTokens())
+			{
+				values.add(Double.valueOf(st.nextToken()));
+			}
+		}
+
+		final int numEntries = values.size();
+		_myData = new Vector<Observation>();
+		for (int i = 0; i < numEntries; i++)
+		{
+			long thisTime = times.elementAt(i);
+			HiResDate thisD = new HiResDate(thisTime);
+			Double thisInterval = values.elementAt(i);
+			Observation obs = new Observation(thisD, thisInterval);
+			_myData.add(obs);
+		}
+	}
+
+	/**
+	 * provide support for cycling through the observations
+	 * 
+	 * @return
+	 */
+	public Iterator<Observation> iterator()
+	{
+		return _myData.iterator();
+	}
+
+	// /////////////////////////////////////////////////
+	// and the testing goes here
+	// /////////////////////////////////////////////////
+	public static class IntervalTest extends junit.framework.TestCase
+	{
+		public static final String TEST_TIMES_FILE = "src/org/mwc/debrief/multipath/model/test_times.csv";
+
+		public void testMe()
+		{
+			TimeDeltas times = new TimeDeltas();
+
+			assertEquals("not got data", null, times._myData);
+
+			// and missing file
+			try
+			{
+				times.load("src/org/mwc/debrief/multipath/model/test_times_bad.csv");
+				fail("should not have found file");
+			}
+			catch (NumberFormatException e)
+			{
+				fail("wrong number format");
+			}
+			catch (IOException e)
+			{
+				// ok - this should have been thrown
+			}
+
+			assertEquals("still not got data", null, times._myData);
+
+			try
+			{
+				times.load(TEST_TIMES_FILE);
+			}
+			catch (NumberFormatException e)
+			{
+				fail("wrong number format");
+			}
+			catch (IOException e)
+			{
+				fail("unable to read lines");
+			}
+
+			assertEquals("got correct num entries", 379, times._myData.size());
+
+			assertNotNull("return iterator", times.iterator());
+
+		}
+
+	}
+}
