deda1c2f9121819c3ab313c0afa21fe1e7a4cc25
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-03-08 11:09:35 +0000

    start tieing in UI
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3525 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.debrief.multipath/src/org/mwc/debrief/multipath/views/MultiPathPresenter.java
+++ trunk/org.mwc.debrief.multipath/src/org/mwc/debrief/multipath/views/MultiPathPresenter.java
@@ -1,142 +1,260 @@
 package org.mwc.debrief.multipath.views;
 
+import java.io.IOException;
+
+import org.eclipse.core.runtime.Status;
+import org.jfree.data.time.TimeSeries;
+import org.mwc.cmap.core.CorePlugin;
+import org.mwc.cmap.core.DataTypes.TrackData.TrackDataProvider;
+import org.mwc.debrief.multipath.model.MultiPathModel;
+import org.mwc.debrief.multipath.model.SVP;
+import org.mwc.debrief.multipath.model.TimeDeltas;
 import org.mwc.debrief.multipath.views.MultiPathPresenter.Display.FileHandler;
 import org.mwc.debrief.multipath.views.MultiPathPresenter.Display.ValueHandler;
 
-import MWC.TacticalData.Track;
-
+import MWC.GenericData.WatchableList;
 
 public class MultiPathPresenter
 {
-	
-	/** UI component of multipath analysis
+
+	/**
+	 * UI component of multipath analysis
 	 * 
 	 * @author ianmayo
-	 *
+	 * 
 	 */
 	public static interface Display
 	{
-		/** interface for anybody that wants to know about files being dropped
+		/**
+		 * interface for anybody that wants to know about files being dropped
 		 * 
 		 * @author ianmayo
-		 *
+		 * 
 		 */
 		public static interface FileHandler
 		{
-			/** the specified file has been dropped
+			/**
+			 * the specified file has been dropped
 			 * 
 			 * @param path
 			 */
 			void newFile(String path);
 		}
-		
-		/** interface for anybody that wants to know about a slider being dragged
+
+		/**
+		 * interface for anybody that wants to know about a slider being dragged
 		 * 
 		 * @author ianmayo
-		 *
+		 * 
 		 */
 		public static interface ValueHandler
 		{
-			/** the new value on the slider
+			/**
+			 * the new value on the slider
 			 * 
 			 * @param val
 			 */
 			void newValue(double val);
 		}
-		
-		/** let someone know about a new SVP file being dropped
+
+		/**
+		 * let someone know about a new SVP file being dropped
 		 * 
 		 * @param handler
 		 */
 		public void addSVPListener(FileHandler handler);
-		
-		/** let someone know about a new time-delta file being dropped
+
+		/**
+		 * let someone know about a new time-delta file being dropped
 		 * 
 		 * @param handler
 		 */
 		public void addTimeDeltaListener(FileHandler handler);
-		
-		/** let someone know about the drag-handle being dragged
+
+		/**
+		 * let someone know about the drag-handle being dragged
 		 * 
 		 * @param handler
 		 */
 		public void addDragHandler(ValueHandler handler);
-	};
-	
-	public static interface Model
-	{
-		/** load the specified SVP data
-		 * 
-		 * @param array
-		 */
-		public void setSVP(double[][] array);
-		
-		/** load the specified interval data
-		 * 
-		 */
-		public void setTimeDelta(double[][] array);
-		
-		/** load the Sensor track
-		 * 
-		 */
-		public void setSensorTrack(Track track);
-		
-		/** load the target track
-		 * 
-		 */
-		public void setTargetTrack(Track track);
-		
-		/** specify the current depth estimate
-		 * 
-		 */
-		public void setDepth(double depth);
-		
-	}
 
+		/** see if anybody can provide tracks
+		 * 
+		 * @return
+		 */
+		public TrackDataProvider getDataProvider();
+
+		/** there's a problem, show it to the user
+		 * 
+		 * @param string
+		 */
+		public void showError(String string);
+
+		/** show these data series
+		 * 
+		 * @param _measuredSeries
+		 * @param calculated
+		 */
+		public void display(TimeSeries _measuredSeries, TimeSeries calculated);
+
+		/** indicate whether the slider should be enabled
+		 * 
+		 * @param b yes/no
+		 */
+		public void setEnabled(boolean b);
+	};
 
 	private final Display _display;
-	private final Model _model;
-	
+	private final MultiPathModel _model;
+	private TimeSeries _measuredSeries = null;
+	private SVP _svp = null;
+	private TimeDeltas _times = null;
 
-	/** initialise presenter
+	/**
+	 * initialise presenter
 	 * 
 	 * @param display
 	 * @param model
 	 */
-	public MultiPathPresenter(Display display, Model model)
+	public MultiPathPresenter(Display display)
 	{
 		_display = display;
-		_model = model;
+		_model = new MultiPathModel();
 
 		// setup assorted listeners
-		_display.addDragHandler(new ValueHandler(){
+		_display.addDragHandler(new ValueHandler()
+		{
 
 			@Override
 			public void newValue(double val)
 			{
-			}});
-		
-		_display.addSVPListener(new FileHandler(){
+				updateCalc(val);
+			}
+		});
+
+		_display.addSVPListener(new FileHandler()
+		{
 
 			public void newFile(String path)
 			{
-			}});
-		
-		_display.addTimeDeltaListener(new FileHandler(){
+
+				try
+				{
+					_svp = new SVP();
+
+					_svp.load(path);
+
+				}
+				catch (NumberFormatException e)
+				{
+					CorePlugin.logError(Status.ERROR, "time-delta formatting problem", e);
+					_svp = null;
+				}
+				catch (IOException e)
+				{
+					CorePlugin.logError(Status.ERROR, "time-delta file-read problem", e);
+					_svp = null;
+				}
+
+				// check if UI should be enabled
+				checkEnablement();
+
+			}
+		});
+
+		_display.addTimeDeltaListener(new FileHandler()
+		{
 
 			public void newFile(String path)
 			{
-				// TODO Auto-generated method stub
-				
-			}});
-		
+				try
+				{
+					_times = new TimeDeltas();
+
+					_times.load(path);
+
+					// reset the measured series
+					_measuredSeries = null;
+				}
+				catch (NumberFormatException e)
+				{
+					CorePlugin.logError(Status.ERROR, "time-delta formatting problem", e);
+					_times = null;
+				}
+				catch (IOException e)
+				{
+					CorePlugin.logError(Status.ERROR, "time-delta file-read problem", e);
+					_times = null;
+				}
+
+				// check if UI should be enabled
+				checkEnablement();
+
+			}
+		});
+
 		// show initial display
 		initDisplay();
 	}
 
+	/**
+	 * see if we have sufficient data to enable the display
+	 * 
+	 */
+	protected void checkEnablement()
+	{
+
+		if ((_svp == null) || (_times == null))
+		{
+			// disable it
+			_display.setEnabled(false);
+		}
+		else
+		{
+			// enable it
+			_display.setEnabled(true);
+		}
+	}
+
+	protected void disableUI()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void updateCalc(double val)
+	{
+		// do we have our measured series?
+		if (_measuredSeries == null)
+			_measuredSeries = _model.getMeasuredProfileFor(_times);
+
+		// do we have a tote?
+		TrackDataProvider tv = _display.getDataProvider();
+
+		// do we only have one seconary?
+		if (tv.getSecondaryTracks().length > 1)
+		{
+			_display.showError("Too many secondary tracks");
+		}
+		else if(tv.getPrimaryTrack() == null)
+		{
+			_display.showError("Needs a primary track (or location)");
+		}
+		else
+		{
+			WatchableList primary = tv.getPrimaryTrack();
+			WatchableList secondary = tv.getSecondaryTracks()[0];
+			TimeSeries calculated = _model.getCalculatedProfileFor(primary,
+					secondary, _svp, _times, val);
+	
+			// ok, ready to plot
+			_display.display(_measuredSeries, calculated);
+		}
+
+	}
 
 	private void initDisplay()
 	{
 	}
-	
+
 }
