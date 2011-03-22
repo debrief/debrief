a63b79a6f67907bbbd11c2a4f0aeb45edb614cbe
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-03-22 08:58:33 +0000

    move the 'contained' test to the WorldArea object
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3591 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.asset.legacy/src/ASSET/Models/Vessels/SonarBuoyField.java
+++ trunk/org.mwc.asset.legacy/src/ASSET/Models/Vessels/SonarBuoyField.java
@@ -8,76 +8,69 @@ import MWC.GenericData.WorldLocation;
 /**
  * Created by IntelliJ IDEA
  */
-public class SonarBuoyField extends CoreParticipant {
-  //////////////////////////////////////////////////
-  // member variables
-  //////////////////////////////////////////////////
+public class SonarBuoyField extends CoreParticipant
+{
+	// ////////////////////////////////////////////////
+	// member variables
+	// ////////////////////////////////////////////////
 
+	// ////////////////////////////////////////////////
+	// constructor
+	// ////////////////////////////////////////////////
 
-  //////////////////////////////////////////////////
-  // constructor
-  //////////////////////////////////////////////////
-
-  /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	/** the area covered by this buoy field
+
+	/**
+	 * the area covered by this buoy field
 	 * 
 	 */
 	private WorldArea _myCoverage;
 
-	/** normal constructor
-	 * @param coverage 
-   *
-   */
-  public SonarBuoyField(final int id, WorldArea coverage) {
-    // create the participant bits
-    super(id, null, null, null);
-    
-    // store the coverage
-    _myCoverage = coverage;
+	/**
+	 * normal constructor
+	 * 
+	 * @param coverage
+	 * 
+	 */
+	public SonarBuoyField(final int id, WorldArea coverage)
+	{
+		// create the participant bits
+		super(id, null, null, null);
 
-  }
+		// store the coverage
+		_myCoverage = coverage;
 
+	}
 
-  //////////////////////////////////////////////////
-  // member methods
-  //////////////////////////////////////////////////
+	// ////////////////////////////////////////////////
+	// member methods
+	// ////////////////////////////////////////////////
 
-  
-  
-  
-  /** return what this participant is currently doing
-   *
-   */
-  public String getActivity() {
-    return "Active";
-  }
+	/**
+	 * return what this participant is currently doing
+	 * 
+	 */
+	public String getActivity()
+	{
+		return "Active";
+	}
 
-
-  /** the range calculation goes from our area, not just a single point
-   * 
-   */
+	/**
+	 * the range calculation goes from our area, not just a single point
+	 * 
+	 */
 	public WorldDistance rangeFrom(WorldLocation point)
 	{
 		double dist = 0;
-		
-		// is this point inside us?
-		if(getCoverage().contains(point))
-		{
-			// done - return zero distance
-		}
-		else
-		{
-			// right, measure the distance from the sides
-			dist = getCoverage().rangeFromEdge(point);
-		}
-		
+
+		// right, measure the distance from the sides
+		dist = getCoverage().rangeFromEdge(point);
+
 		return new WorldDistance(dist, WorldDistance.DEGS);
 	}
-
 
 	public WorldArea getCoverage()
 	{
