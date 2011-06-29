4fd6d65337e051afe9a514269b123d36642a10b3
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-06-29 09:37:13 +0000

    first import
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3806 cb33b658-6c9e-41a7-9690-cba343611204

---
+++ trunk/org.mwc.asset.NetAsset2/src/org/mwc/asset/netasset2/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package org.mwc.asset.netasset2;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "org.mwc.asset.NetAsset2.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
