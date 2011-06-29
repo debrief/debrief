c4b275f866ad24ac27611e89109e69c90dc2d3f9
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-06-29 14:33:54 +0000

    start auto testing
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3819 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.asset.NetAsset2/src/org/mwc/asset/netasset2/core/AClient.java
+++ trunk/org.mwc.asset.NetAsset2/src/org/mwc/asset/netasset2/core/AClient.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 
 import org.mwc.asset.netasset2.common.Network;
+import org.mwc.asset.netasset2.common.Network.SomeRequest;
 import org.mwc.asset.netasset2.common.Network.SomeResponse;
 
 import com.esotericsoftware.kryonet.Client;
@@ -12,20 +13,43 @@ import com.esotericsoftware.kryonet.Listener;
 
 public class AClient
 {
+	private static class CModel
+	{
+		private Client _client;
 
-	private Client _client;
+		public CModel() throws IOException
+		{
+			_client = new Client();
+			Network.register(_client);
+			_client.start();
+
+			InetAddress address = _client.discoverHost(Network.UDP_PORT, 5000);
+			if (address != null)
+				_client.connect(5000, "LOCALHOST", Network.TCP_PORT, Network.UDP_PORT);
+		}
+
+		public void addListener(Listener listener)
+		{
+			_client.addListener(listener);
+		}
+		public void stop()
+		{
+			_client.stop();
+		}
+		public void send(Object data)
+		{
+			_client.sendTCP(data);
+		}
+
+
+	}
+
+	private CModel _model;
 
 	public AClient() throws IOException
 	{
-		_client = new Client();
-		Network.register(_client);
-		_client.start();
-
-		InetAddress address = _client.discoverHost(Network.UDP_PORT, 5000);
-		if (address != null)
-			_client.connect(5000, "LOCALHOST", Network.TCP_PORT, Network.UDP_PORT);
-
-		_client.addListener(new Listener()
+		_model = new CModel();
+		_model.addListener(new Listener()
 		{
 			public void received(Connection connection, Object object)
 			{
@@ -37,13 +61,18 @@ public class AClient
 			}
 		});
 	}
-	
-	
 
 	public void stop()
 	{
-		_client.stop();
+		_model.stop();
 	}
+	public void testSend()
+	{
+		SomeRequest request = new SomeRequest();
+		request.text = "Here is the request 6!";
+		_model.send(request);
+	}
+
 
 	/**
 	 * @param args
@@ -59,11 +88,5 @@ public class AClient
 		client.stop();
 	}
 
-
-
-	public void send(Object data)
-	{
-		_client.sendTCP(data);
-	}
 
 }
--- trunk/org.mwc.asset.NetAsset2/src/org/mwc/asset/netasset2/test/CoreTest.java
+++ trunk/org.mwc.asset.NetAsset2/src/org/mwc/asset/netasset2/test/CoreTest.java
@@ -1,34 +1,71 @@
 package org.mwc.asset.netasset2.test;
 
 import java.io.IOException;
+import java.util.Vector;
 
-import org.mwc.asset.netasset2.common.Network.SomeRequest;
 import org.mwc.asset.netasset2.core.AClient;
 import org.mwc.asset.netasset2.core.AServer;
+
+import com.esotericsoftware.minlog.Log;
+import com.esotericsoftware.minlog.Log.Logger;
 
 public class CoreTest
 {
 
+	public static class TrackWrapper_Test extends junit.framework.TestCase
+	{
+
+		private Vector<String> _events;
+
+		@Override
+		protected void setUp() throws Exception
+		{
+			// TODO Auto-generated method stub
+			super.setUp();
+
+			_events = new Vector<String>();
+			Logger logger = new Logger()
+			{
+
+				@Override
+				public void log(int level, String category, String message, Throwable ex)
+				{
+					_events.add(message);
+				}
+			};
+			Log.setLogger(logger);
+		}
+
+		public void testStartup() throws InterruptedException, IOException
+		{
+			// check events empty
+			assertEquals("events empty",0, _events.size());
+			AServer server = new AServer();
+			Thread.sleep(1000);
+			
+			assertEquals("events recorded",1, _events.size());
+			assertEquals("correct start event", "Server opened.", _events.elementAt(0));
+			server.stop();
+		}
+	}
+
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException
 	{
 		AServer server = new AServer();
-		
+
 		AClient client = new AClient();
-		
-		SomeRequest request = new SomeRequest();
-		request.text = "Here is the request 6!";
-		client.send(request);
-		
+
+		client.testSend();
+
 		System.out.println("pausing");
 		System.in.read();
-		
+
 		server.stop();
 		client.stop();
 	}
 
 }
-
