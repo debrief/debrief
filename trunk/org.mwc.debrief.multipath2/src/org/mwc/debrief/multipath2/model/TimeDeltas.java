9b162b512786c26ebe938e7f5f9eb16aac1881a6
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-07-09 06:29:23 +0000

    successfully reading in GAPS data
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3878 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.debrief.multipath2/src/org/mwc/debrief/multipath2/model/TimeDeltas.java
+++ trunk/org.mwc.debrief.multipath2/src/org/mwc/debrief/multipath2/model/TimeDeltas.java
@@ -3,8 +3,11 @@ package org.mwc.debrief.multipath2.model;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.TimeZone;
 import java.util.Vector;
 
 import org.mwc.debrief.multipath2.model.MultiPathModel.DataFormatException;
@@ -42,12 +45,12 @@ public class TimeDeltas
 	}
 
 	private Vector<Observation> _myData;
-	
+
 	public HiResDate getStartTime()
 	{
 		return _myData.firstElement().getDate();
 	}
-	
+
 	public HiResDate getEndTime()
 	{
 		return _myData.lastElement().getDate();
@@ -63,7 +66,8 @@ public class TimeDeltas
 	 * @throws IOException
 	 *           if the file can't be found
 	 */
-	public void load(String path) throws NumberFormatException, IOException, MultiPathModel.DataFormatException
+	public void load(String path) throws NumberFormatException, IOException,
+			MultiPathModel.DataFormatException
 	{
 		Vector<Double> times = new Vector<Double>();
 		Vector<Double> values = new Vector<Double>();
@@ -71,18 +75,51 @@ public class TimeDeltas
 		BufferedReader bufRdr = new BufferedReader(new FileReader(path));
 		String line = null;
 
+		// ditch the first line
+		line = bufRdr.readLine();
+
 		// read each line of text file
 		while ((line = bufRdr.readLine()) != null)
 		{
+			// here's the format:
+			// YYYY,MM,DD,HH,MM,SS,mmm,TIME_DELAY(msec),POWER(dB)
+			// 2009,04,42,18,37,00,254,6.797688,22
+
+
 			StringTokenizer st = new StringTokenizer(line, ",");
-			if(st.hasMoreTokens())
-			{
-				times.add(Double.valueOf(st.nextToken()));
-			}
-			if(st.hasMoreTokens())
-			{
-				values.add(Double.valueOf(st.nextToken()));
-			}
+			int year = Integer.valueOf(st.nextToken());
+			int month = Integer.valueOf(st.nextToken());
+			int day = Integer.valueOf(st.nextToken());
+			int hour = Integer.valueOf(st.nextToken());
+			int min = Integer.valueOf(st.nextToken());
+			int sec = Integer.valueOf(st.nextToken());
+			int milli = Integer.valueOf(st.nextToken());
+
+			double delay = Double.valueOf(st.nextToken());
+			@SuppressWarnings("unused")
+			double strength = Double.valueOf(st.nextToken());
+
+			// do the time
+			
+			GregorianCalendar cal2 = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+			cal2.clear();
+			cal2.set(Calendar.YEAR,year);
+			cal2.set(Calendar.MONTH, month-1);
+			cal2.set(Calendar.DAY_OF_MONTH,day);
+			cal2.set(Calendar.HOUR,hour);
+			cal2.set(Calendar.MINUTE, min);
+			cal2.set(Calendar.SECOND, sec);
+			cal2.set(Calendar.MILLISECOND, milli);
+			
+			
+			long tMillis = cal2.getTimeInMillis();
+			tMillis += milli;
+			double tSecs = tMillis / 1000;
+			times.add((double) tSecs);
+			
+			// to the delay
+			values.add(delay / 1000);
+			
 		}
 		if (values.size() > 0)
 		{
@@ -92,16 +129,16 @@ public class TimeDeltas
 				throw new MultiPathModel.DataFormatException(
 						"Doesn't look like time interval data");
 
-		final int numEntries = values.size();
-		_myData = new Vector<Observation>();
-		for (int i = 0; i < numEntries; i++)
-		{
-			Double thisTime = times.elementAt(i);
-			HiResDate thisD = new HiResDate((long)(thisTime * 1000d));
-			Double thisInterval = values.elementAt(i);
-			Observation obs = new Observation(thisD, thisInterval);
-			_myData.add(obs);
-		}
+			final int numEntries = values.size();
+			_myData = new Vector<Observation>();
+			for (int i = 0; i < numEntries; i++)
+			{
+				Double thisTime = times.elementAt(i);
+				HiResDate thisD = new HiResDate((long) (thisTime * 1000d));
+				Double thisInterval = values.elementAt(i);
+				Observation obs = new Observation(thisD, thisInterval);
+				_myData.add(obs);
+			}
 		}
 	}
 
@@ -147,7 +184,6 @@ public class TimeDeltas
 				fail("bad data");
 			}
 
-
 			assertEquals("still not got data", null, times._myData);
 
 			try
@@ -166,7 +202,6 @@ public class TimeDeltas
 			{
 				fail("bad data");
 			}
-
 
 			assertEquals("got correct num entries", 379, times._myData.size());
 
