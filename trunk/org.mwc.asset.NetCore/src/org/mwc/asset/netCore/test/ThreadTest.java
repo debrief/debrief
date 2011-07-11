62ccdbf27ab97ac41975c1a0746656a706513d6e
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-07-11 15:08:13 +0000

    refactor common networking into dedicated package
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3891 cb33b658-6c9e-41a7-9690-cba343611204

---
+++ trunk/org.mwc.asset.NetCore/src/org/mwc/asset/netCore/test/ThreadTest.java
@@ -0,0 +1,54 @@
+package org.mwc.asset.netCore.test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Server;
+
+public class ThreadTest
+{
+	public static void main(String[] args) throws IOException,
+			InterruptedException
+	{
+		Server s = new Server();
+		s.start();
+		s.bind(1927);
+		printThreads("server started");
+
+		Client c = new Client();
+		c.start();
+		c.connect(5000, "LOCALHOST", 1927);
+		printThreads("client connected");
+		Server s1 = s;
+		s.stop();
+		printThreads("server stopped");
+
+		s = new Server();
+		s.start();
+		s.bind(1928);
+		printThreads("new server started"); // new server thread will be last on
+		// the list.
+
+		c.stop();
+		printThreads("client stopped");
+
+		c.start();
+		c.connect(5000, "localhost", 1928);
+		printThreads("client connected to second server");
+
+		c.stop();
+		s.stop();
+		s1.stop();
+		printThreads("both stopped");
+	}
+
+	private static void printThreads(String message) throws InterruptedException
+	{
+		// tick:
+		Thread.sleep(2000L);
+		Thread[] threads = new Thread[Thread.activeCount()];
+		Thread.enumerate(threads);
+		System.out.println(message + " :  " + Arrays.asList(threads));
+	}
+}
