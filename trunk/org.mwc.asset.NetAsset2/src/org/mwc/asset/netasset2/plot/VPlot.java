6d918863ef389b7321baadb53ba2ad9c99b2e326
ianmayo <ianmayo@cb33b658-6c9e-41a7-9690-cba343611204>
2011-07-18 10:52:45 +0000

    use the Debrief TrackWrapper object instead of creating our own
    git-svn-id: http://subversion.assembla.com/svn/Debrief@3954 cb33b658-6c9e-41a7-9690-cba343611204

--- trunk/org.mwc.asset.NetAsset2/src/org/mwc/asset/netasset2/plot/VPlot.java
+++ trunk/org.mwc.asset.NetAsset2/src/org/mwc/asset/netasset2/plot/VPlot.java
@@ -1,25 +1,43 @@
 package org.mwc.asset.netasset2.plot;
 
+import java.awt.Color;
+import java.util.Enumeration;
+import java.util.Iterator;
+
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 import org.mwc.asset.netasset2.part.IVPartMovement;
 import org.mwc.asset.netasset2.time.IVTime;
 import org.mwc.cmap.plotViewer.editors.chart.SWTChart;
+import org.mwc.debrief.core.editors.painters.SnailHighlighter;
 
+import swing2swt.layout.BorderLayout;
 import ASSET.ScenarioType;
+import ASSET.Models.Detection.DetectionEvent;
 import ASSET.Models.Detection.DetectionList;
 import ASSET.Participants.ParticipantDetectedListener;
 import ASSET.Participants.Status;
+import Debrief.Wrappers.FixWrapper;
+import Debrief.Wrappers.SensorContactWrapper;
+import Debrief.Wrappers.SensorWrapper;
+import Debrief.Wrappers.TrackWrapper;
+import MWC.GUI.CanvasType;
+import MWC.GUI.Editable;
+import MWC.GUI.Layer;
 import MWC.GUI.Layers;
-import swing2swt.layout.BorderLayout;
-import org.eclipse.swt.widgets.ToolBar;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.ToolItem;
+import MWC.GenericData.HiResDate;
+import MWC.GenericData.WorldDistance;
+import MWC.GenericData.WorldSpeed;
+import MWC.TacticalData.Fix;
 
-public class VPlot extends Composite implements IVPartMovement, IVTime, ParticipantDetectedListener
+public class VPlot extends Composite implements IVPartMovement, IVTime,
+		ParticipantDetectedListener
 {
 
 	/**
@@ -32,10 +50,14 @@ public class VPlot extends Composite implements IVPartMovement, IVTime, Particip
 	 */
 	protected Layers _myLayers;
 
-	protected NetPartWrapper myPart;
-	
+	private TrackWrapper myTrack;
+
 	private int _numUpdates = 0;
-	
+
+	private SnailHighlighter _snailPainter;
+
+	protected HiResDate tNow;
+
 	@Override
 	protected void checkSubclass()
 	{
@@ -55,9 +77,12 @@ public class VPlot extends Composite implements IVPartMovement, IVTime, Particip
 
 		ToolBar toolBar = new ToolBar(this, SWT.PUSH);
 		toolBar.setLayoutData(BorderLayout.NORTH);
-		
+
+		_snailPainter = new SnailHighlighter(null);
+
 		_myLayers = new Layers();
-		_myChart = new SWTChart(_myLayers, this){
+		_myChart = new SWTChart(_myLayers, this)
+		{
 			/**
 			 * 
 			 */
@@ -66,23 +91,38 @@ public class VPlot extends Composite implements IVPartMovement, IVTime, Particip
 			@Override
 			public void chartFireSelectionChanged(ISelection sel)
 			{
-			}};
+			}
+
+			@Override
+			protected void paintThisLayer(Layer thisLayer, CanvasType dest)
+			{
+				if (tNow != null)
+					_snailPainter.paintThisLayer(thisLayer, dest, tNow);
+			}
+
+		};
 		_myChart.getCanvasControl().setLayoutData(BorderLayout.CENTER);
-		
+
+		// specify a snail painter
+
 		ToolItem btnZoomOut = new ToolItem(toolBar, SWT.NONE);
-		btnZoomOut.addSelectionListener(new SelectionAdapter(){
+		btnZoomOut.addSelectionListener(new SelectionAdapter()
+		{
 			public void widgetSelected(SelectionEvent e)
 			{
 				zoomOut();
-			}});
+			}
+		});
 		btnZoomOut.setText("Zoom out");
 
 		ToolItem btnFitToWin = new ToolItem(toolBar, SWT.NONE);
-		btnFitToWin.addSelectionListener(new SelectionAdapter(){
+		btnFitToWin.addSelectionListener(new SelectionAdapter()
+		{
 			public void widgetSelected(SelectionEvent e)
 			{
 				fitToWin();
-			}});
+			}
+		});
 		btnFitToWin.setText("Fit to win");
 
 	}
@@ -110,11 +150,13 @@ public class VPlot extends Composite implements IVPartMovement, IVTime, Particip
 			{
 				// ok, clear out...
 				_myLayers.clear();
-				
+
 				// create new participant painter
-				 myPart = new NetPartWrapper(name);
-				_myLayers.addThisLayer(myPart);
-				
+				myTrack = new TrackWrapper();
+				myTrack.setName(name);
+				myTrack.setColor(Color.blue);
+				_myLayers.addThisLayer(myTrack);
+
 			}
 		});
 	}
@@ -142,25 +184,87 @@ public class VPlot extends Composite implements IVPartMovement, IVTime, Particip
 			@Override
 			public void run()
 			{
-				myPart.setStatus(status);
-				_numUpdates ++;
-				
-				if(_numUpdates == 2)
+				Fix theFix = new Fix();
+				tNow = new HiResDate(status.getTime());
+				theFix.setTime(tNow);
+				theFix.setLocation(status.getLocation());
+				theFix.setCourse(MWC.Algorithms.Conversions.Degs2Rads(status
+						.getCourse()));
+				theFix.setSpeed(status.getSpeed().getValueIn(WorldSpeed.ft_sec / 3));
+				FixWrapper fw = new FixWrapper(theFix);
+				myTrack.addFix(fw);
+				_numUpdates++;
+
+				if (_numUpdates == 2)
 					_myChart.rescale();
 			}
 		});
 	}
 
+	// find hte sensor
+	private SensorWrapper getSensor(String name)
+	{
+		Enumeration<Editable> iter = myTrack.getSensors().elements();
+		SensorWrapper res = null;
+		while (iter.hasMoreElements())
+		{
+			SensorWrapper sensor = (SensorWrapper) iter.nextElement();
+			if (sensor.getName().equals(name))
+			{
+				res = sensor;
+				break;
+			}
+		}
+		
+		// did we find one?
+		if (res == null)
+		{
+			res = new SensorWrapper(name);
+			res.setVisible(true);
+			res.setWormInHole(false);
+			res.setColor(Color.yellow);
+			myTrack.add(res);
+		}
+		return res;
+	}
+
 	@Override
 	public void newDetections(DetectionList detections)
 	{
-		myPart.addDetections(detections);
+		Iterator<DetectionEvent> iter = detections.iterator();
+		while (iter.hasNext())
+		{
+			DetectionEvent det = iter.next();
+			String name = "Sensor_" + det.getSensor();
+			SensorWrapper sw = getSensor(name);
+			SensorContactWrapper scw = new SensorContactWrapper();
+
+			// sort out the time
+			HiResDate dtg = new HiResDate(det.getTime());
+			scw.setDTG(dtg);
+
+			// do we have bearing?
+			Float brg = det.getBearing();
+			if (brg != null)
+				scw.setBearing(brg);
+
+			// do we have range?
+			WorldDistance rng = det.getRange();
+			if (rng != null)
+			{
+				scw.setRange(rng);
+			}
+
+			// and store it
+			sw.add(scw);
+
+		}
 	}
 
 	@Override
 	public void restart(ScenarioType scenario)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 }
