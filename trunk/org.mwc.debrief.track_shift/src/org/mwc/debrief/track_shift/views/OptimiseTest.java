1bf9b4f4216a4922215b22b8cbcfffa82202b612
ianmayo <ian@planetmayo.com>
2012-03-30 04:44:23 +0100

    more fleshing out

--- trunk/org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/OptimiseTest.java
+++ trunk/org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/OptimiseTest.java
@@ -3,15 +3,19 @@ package org.mwc.debrief.track_shift.views;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Enumeration;
 
+import org.eclipse.swt.widgets.Composite;
+import org.jfree.chart.plot.XYPlot;
 import org.mockito.Mockito;
+import org.mwc.cmap.core.DataTypes.TrackData.TrackDataProvider;
 import org.mwc.cmap.core.interfaces.IControllableViewport;
 import org.mwc.debrief.core.editors.PlotEditor;
 import org.mwc.debrief.core.loaders.xml_handlers.DebriefEclipseXMLReaderWriter;
 
-import MWC.GUI.Editable;
+import Debrief.Wrappers.TrackWrapper;
+import MWC.GUI.ErrorLogger;
 import MWC.GUI.Layers;
+import MWC.GenericData.WatchableList;
 
 public class OptimiseTest
 {
@@ -22,18 +26,83 @@ public class OptimiseTest
 		Layers layers = getTheData();
 
 		// create a mockup of the plot
-		Enumeration<Editable> enumer = layers.elements();
-		while (enumer.hasMoreElements())
-		{
-			Editable editable = (Editable) enumer.nextElement();
-			System.out.println("this layer is:" + editable.getName());
-		}
+		StackedDotHelper helper = new StackedDotHelper();
 
+		XYPlot dotPlot = Mockito.mock(XYPlot.class);
+		XYPlot linePlot= Mockito.mock(XYPlot.class);
+		TrackDataProvider tracks = new TProv();
+		Composite holder  = Mockito.mock(Composite.class);
+		ErrorLogger logger= Mockito.mock(ErrorLogger.class);
 		// run setup optimise
+		helper.updateBearingData(dotPlot, linePlot, tracks, true, true, false, holder, logger, true);
 
+//		HAVE A LOOK AT THE UPDATE BEARING DATA - THE COST FUNCTION IS IN THERE 
+		
+		
 		// and optimise it
 	}
 
+	private static class TProv implements TrackDataProvider
+	{
+
+		@Override
+		public void addTrackDataListener(TrackDataListener listener)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void removeTrackDataListener(TrackDataListener listener)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void addTrackShiftListener(TrackShiftListener listener)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void removeTrackShiftListener(TrackShiftListener listener)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void fireTrackShift(TrackWrapper target)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void fireTracksChanged()
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public WatchableList getPrimaryTrack()
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public WatchableList[] getSecondaryTracks()
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
 	private static Layers getTheData() throws FileNotFoundException
 	{
 		DebriefEclipseXMLReaderWriter reader = new DebriefEclipseXMLReaderWriter();
