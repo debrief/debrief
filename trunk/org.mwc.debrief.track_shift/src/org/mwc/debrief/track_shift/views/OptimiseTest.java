470bbc8386c8153d5828c86482be5a37f3ed7e48
ianmayo <ian@planetmayo.com>
2012-04-06 15:27:57 +1000

    shifting target track

--- trunk/org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/OptimiseTest.java
+++ trunk/org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/OptimiseTest.java
@@ -3,104 +3,120 @@ package org.mwc.debrief.track_shift.views;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Iterator;
+import java.util.TreeSet;
 
-import org.eclipse.swt.widgets.Composite;
-import org.jfree.chart.plot.XYPlot;
+import junit.framework.TestCase;
+
 import org.mockito.Mockito;
-import org.mwc.cmap.core.DataTypes.TrackData.TrackDataProvider;
 import org.mwc.cmap.core.interfaces.IControllableViewport;
 import org.mwc.debrief.core.editors.PlotEditor;
 import org.mwc.debrief.core.loaders.xml_handlers.DebriefEclipseXMLReaderWriter;
 
+import Debrief.Wrappers.FixWrapper;
 import Debrief.Wrappers.TrackWrapper;
-import MWC.GUI.ErrorLogger;
+import Debrief.Wrappers.Track.Doublet;
+import MWC.GUI.Layer;
 import MWC.GUI.Layers;
-import MWC.GenericData.WatchableList;
+import MWC.GenericData.WorldArea;
+import MWC.GenericData.WorldLocation;
+import MWC.GenericData.WorldVector;
 
 public class OptimiseTest
 {
 
-	public static void main(String[] args) throws FileNotFoundException
+	public static class testMe extends TestCase
 	{
-		// get some data
-		Layers layers = getTheData();
-
-		// create a mockup of the plot
-		StackedDotHelper helper = new StackedDotHelper();
-
-		XYPlot dotPlot = Mockito.mock(XYPlot.class);
-		XYPlot linePlot= Mockito.mock(XYPlot.class);
-		TrackDataProvider tracks = new TProv();
-		Composite holder  = Mockito.mock(Composite.class);
-		ErrorLogger logger= Mockito.mock(ErrorLogger.class);
-		// run setup optimise
-		helper.updateBearingData(dotPlot, linePlot, tracks, true, true, false, holder, logger, true);
-
-//		HAVE A LOOK AT THE UPDATE BEARING DATA - THE COST FUNCTION IS IN THERE 
-		
-		
-		// and optimise it
-	}
-
-	private static class TProv implements TrackDataProvider
-	{
-
-		@Override
-		public void addTrackDataListener(TrackDataListener listener)
+		public void testLoad() throws FileNotFoundException
 		{
-			// TODO Auto-generated method stub
+			// get some data
+			Layers layers = getTheData();
 			
-		}
-
-		@Override
-		public void removeTrackDataListener(TrackDataListener listener)
-		{
-			// TODO Auto-generated method stub
+			assertNotNull("should have loaded data", layers);
 			
-		}
-
-		@Override
-		public void addTrackShiftListener(TrackShiftListener listener)
-		{
-			// TODO Auto-generated method stub
+			// ok, sort out the two tracks
+			assertEquals("wrong num tracks", 2, layers.size());
 			
-		}
-
-		@Override
-		public void removeTrackShiftListener(TrackShiftListener listener)
-		{
-			// TODO Auto-generated method stub
+			// ok, go for the primary
+			Layer layerPri = layers.elementAt(0);
+			Layer layerSec = layers.elementAt(1);
 			
-		}
-
-		@Override
-		public void fireTrackShift(TrackWrapper target)
-		{
-			// TODO Auto-generated method stub
+			TrackWrapper pri = (TrackWrapper) layerPri;
+			TrackWrapper sec = (TrackWrapper) layerSec;
 			
-		}
-
-		@Override
-		public void fireTracksChanged()
-		{
-			// TODO Auto-generated method stub
+			assertEquals("NONSUCH", pri.getName());
+			assertEquals("TMA_TGT", sec.getName());
 			
-		}
-
-		@Override
-		public WatchableList getPrimaryTrack()
-		{
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public WatchableList[] getSecondaryTracks()
-		{
-			// TODO Auto-generated method stub
-			return null;
+			// can we get some doublets?
+			TreeSet<Doublet> doublets = StackedDotHelper.getDoublets(pri, sec, true, true, false);
+			
+			assertNotNull("found some", doublets);
+			assertEquals("right num", 18, doublets.size());
+			
+			double brgRads = 1;
+			double rngDegs = 0.001;
+			// ok, try to get a performance figure for the doublets
+			WorldVector thisOffset = new WorldVector(brgRads, rngDegs, 0);
+			double score = scoreFor(doublets, thisOffset);
+			
+			assertTrue("created score", 100 == score);
 		}
 		
+		public void testShift() throws FileNotFoundException
+		{
+			// get some data
+			Layers layers = getTheData();
+			
+			assertNotNull("should have loaded data", layers);
+			
+			// ok, sort out the two tracks
+			assertEquals("wrong num tracks", 2, layers.size());
+			
+			// ok, go for the primary
+			Layer layerPri = layers.elementAt(0);
+			Layer layerSec = layers.elementAt(1);
+			
+			TrackWrapper pri = (TrackWrapper) layerPri;
+			TrackWrapper sec = (TrackWrapper) layerSec;
+			
+			assertEquals("NONSUCH", pri.getName());
+			assertEquals("TMA_TGT", sec.getName());
+			
+			// can we get some doublets?
+			TreeSet<Doublet> doublets = StackedDotHelper.getDoublets(pri, sec, true, true, false);
+			
+			assertEquals("right num", 18, doublets.size());
+			WorldArea coverage = areaFor(doublets);
+			assertEquals("right area", " Area TL: 04¡59'38.05\"N 029¡53'48.02\"E  BR: 04¡57'28.41\"N 029¡56'41.19\"E  (29.89667205395194) ", coverage.toString());
+			
+			WorldVector thisOffset = new WorldVector(0,0,0);
+			doublets = shiftDoublets(doublets, thisOffset);
+			coverage = areaFor(doublets);
+			assertEquals("still the right area", " Area TL: 04¡59'38.05\"N 029¡53'48.02\"E  BR: 04¡57'28.41\"N 029¡56'41.19\"E  (29.89667205395194) ", coverage.toString());
+
+			// apply some kind of offset - move it 1 deg north
+			 thisOffset = new WorldVector(0,1,0);
+			doublets = shiftDoublets(doublets, thisOffset);
+			coverage = areaFor(doublets);
+			assertEquals("area moved", " Area TL: 04¡59'38.05\"N 029¡53'48.02\"E  BR: 04¡57'28.41\"N 029¡56'41.19\"E  (29.89667205395194) ", coverage.toString());
+		}
+
+		private WorldArea areaFor(TreeSet<Doublet> doublets)
+		{
+			WorldArea res = null;
+			Iterator<Doublet> iter = doublets.iterator();
+			while (iter.hasNext())
+			{
+				Doublet doublet = (Doublet) iter.next();
+				WorldLocation loc = doublet.getTarget().getLocation();
+				if(res == null)
+					res = new WorldArea(loc, loc);
+				else
+					res.extend(loc);
+			}
+			return res;
+		}
+
 	}
 	
 	private static Layers getTheData() throws FileNotFoundException
@@ -114,6 +130,47 @@ public class OptimiseTest
 		PlotEditor plot = Mockito.mock(PlotEditor.class);
 		reader.importThis(fName, is, res, view, plot);
 		return res;
+	}
+
+	/**
+	 * 
+	 * @param doublets the list of objects we're playing with
+	 * @param thisOffset the offset we apply to this permutation
+	 * @return
+	 */
+	private static double scoreFor(TreeSet<Doublet> doublets, WorldVector thisOffset)
+	{
+		// apply the offset
+		doublets = shiftDoublets(doublets, thisOffset);
+		
+		// now calculate the error
+		
+		return 100;
+	}
+
+	private static TreeSet<Doublet> shiftDoublets(TreeSet<Doublet> doublets,
+			WorldVector thisOffset)
+	{
+		// loop through the doublets
+		Iterator<Doublet> iter = doublets.iterator();
+		while (iter.hasNext())
+		{
+			Doublet doublet = (Doublet) iter.next();
+			
+			// fudge the target location
+			FixWrapper fix = doublet.getTarget();
+			
+			// sort out where it is
+			FixWrapper newF = new FixWrapper(fix.getFix());
+
+			WorldLocation newLoc = new WorldLocation(newF.getLocation());
+			newLoc.addToMe(thisOffset);
+			newF.setLocation(newLoc);
+			
+			doublet.setTarget(newF);
+		}
+		
+		return doublets;
 	}
 
 }
