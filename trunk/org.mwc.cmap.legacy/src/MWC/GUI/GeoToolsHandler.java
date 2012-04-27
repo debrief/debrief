eacaef6b56ba5c13986c525ed65c5583ed851d9a
Ian Mayo <ian@planetmayo.com>
2012-04-27 15:37:56 +0100

    introduce non-GT classes, so fewer plugins have to know about it

---
+++ trunk/org.mwc.cmap.legacy/src/MWC/GUI/GeoToolsHandler.java
@@ -0,0 +1,13 @@
+package MWC.GUI;
+
+
+public interface GeoToolsHandler
+{
+
+	/** add this geo tools layer
+	 * 
+	 * @param layer
+	 */
+	public void addGeoToolsLayer(ExternallyManagedDataLayer layer);
+
+}
