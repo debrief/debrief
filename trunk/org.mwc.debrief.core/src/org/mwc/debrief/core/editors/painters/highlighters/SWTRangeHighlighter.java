738c062deddb360a823553a72834ec9dc8df080a
Ian Mayo <ian@planetmayo.com>
2012-09-24 16:44:08 +0100

    intro ability to have irregular arcs in RangeRingHighlighter

--- trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/editors/painters/highlighters/SWTRangeHighlighter.java
+++ trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/editors/painters/highlighters/SWTRangeHighlighter.java
@@ -31,15 +31,21 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	 * 
 	 */
 	private boolean _justPlotPrimary;
+
 	/**
-	 * how far either side the arcs should extend (degs)
+	 * where the displayed arcs start (degs)
 	 */
-	private int _arcs;
+	private int _arcStart;
+
+	/**
+	 * where the displayed arcs end (degs)
+	 */
+	private int _arcEnd;
 
 	/**
 	 * // how many rings to plot
 	 */
-	private int _rings;
+	private int _numRings;
 
 	/**
 	 * how far apart to plot the spokes (degs)
@@ -57,7 +63,7 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	private boolean _plainRectangle = false;
 
 	private boolean _useCurrentTrackColor = false;
-	
+
 	/**
 	 * whether to shade in the arcs
 	 * 
@@ -71,8 +77,9 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	{
 		// do some initialisation
 		_radius = 3000;
-		_arcs = 180;
-		_rings = 3;
+		_arcStart = -180;
+		_arcEnd = 180;
+		_numRings = 3;
 		_spoke_separation = 45;
 	}
 
@@ -103,7 +110,7 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 			}
 
 			// sort the colors out
-			if(getUseCurrentTrackColor())
+			if (getUseCurrentTrackColor())
 				dest.setColor(watch.getColor());
 			else
 				dest.setColor(_myColor);
@@ -115,10 +122,9 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 			else
 			{
 				WorldLocation center = watch.getLocation();
-				int header = (int) MWC.Algorithms.Conversions.Rads2Degs(watch
+				int headingDegs = (int) MWC.Algorithms.Conversions.Rads2Degs(watch
 						.getCourse());
-				drawRangeRings(center, _radius, header, _arcs, _rings,
-						_spoke_separation, dest);
+				drawRangeRings(center, headingDegs, dest);
 			}
 		}
 	}
@@ -187,8 +193,7 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	}
 
 	private void drawRangeRings(final WorldLocation worldCentre,
-			final double worldRadius, final int rawAxis, final int arcs,
-			final int rings, final int spoke_separation, final CanvasType dest)
+			final int rawAxisDegs, final CanvasType dest)
 	{
 
 		final MWC.Algorithms.PlainProjection _proj = dest.getProjection();
@@ -200,8 +205,8 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 
 		// sort out the range in screen coords
 		final WorldLocation worldEdge = worldCentre.add(new WorldVector(
-				MWC.Algorithms.Conversions.Degs2Rads(rawAxis),
-				MWC.Algorithms.Conversions.Yds2Degs(worldRadius), 0));
+				MWC.Algorithms.Conversions.Degs2Rads(rawAxisDegs),
+				MWC.Algorithms.Conversions.Yds2Degs(_radius), 0));
 		final Point screenEdge = _proj.toScreen(worldEdge);
 		final int dx = screenEdge.x - centrex;
 		final int dy = screenEdge.y - centrey;
@@ -212,15 +217,22 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 		final int axis = (int) MWC.Algorithms.Conversions.Rads2Degs(Math.atan2(dx,
 				-dy));
 
-		// now the centre stalk
-		final double axisRads = MWC.Algorithms.Conversions.Degs2Rads(axis);
-		final Point edge = new Point((int) ((double) radius * Math.sin(axisRads)),
-				-(int) ((double) radius * Math.cos(axisRads)));
-		edge.translate(centrex, centrey);
-		dest.drawLine(centrex, centrey, edge.x, edge.y);
+		Point edge = new Point(0, 0);
+
+		// do we do a centre stalk?
+		if ((_arcStart < 0) && (_arcEnd > 0))
+		{
+			final double axisRads = MWC.Algorithms.Conversions.Degs2Rads(axis
+					+ _arcStart);
+			edge.setLocation((int) ((double) radius * Math.sin(axisRads)),
+					-(int) ((double) radius * Math.cos(axisRads)));
+			edge.translate(centrex, centrey);
+			dest.drawLine(centrex, centrey, edge.x, edge.y);
+		}
 
 		// now the start edge stalk
-		final double startRads = MWC.Algorithms.Conversions.Degs2Rads(axis - arcs);
+		final double startRads = MWC.Algorithms.Conversions.Degs2Rads(axis
+				+ _arcStart);
 		edge.setLocation((int) ((double) radius * Math.sin(startRads)),
 				-(int) ((double) radius * Math.cos(startRads)));
 		edge.translate(centrex, centrey);
@@ -232,8 +244,8 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 		 * degrees either side (since in that case drawing it twice effectively
 		 * hides it
 		 */
-		final double endRads = MWC.Algorithms.Conversions.Degs2Rads(axis + arcs);
-		if (arcs != 180)
+		final double endRads = MWC.Algorithms.Conversions.Degs2Rads(axis + _arcEnd);
+		if ((_arcStart != -180) && (_arcEnd != 180))
 		{
 			edge.setLocation((int) ((double) radius * Math.sin(endRads)),
 					-(int) ((double) radius * Math.cos(endRads)));
@@ -242,43 +254,34 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 		}
 
 		// now draw the spokes, working out either side from the axis
-		int thisSpoke = spoke_separation; // on the axis
+		int thisSpoke = _arcStart; // on the axis
 		double spokeRads1 = 0;
-		double spokeRads2 = 0;
 		final Point edge1 = new Point();
-		final Point edge2 = new Point();
 
-		while (thisSpoke < arcs)
+		while (thisSpoke < _arcEnd)
 		{
 			// find the left/right angles in rads
-			spokeRads1 = MWC.Algorithms.Conversions.Degs2Rads(axis - thisSpoke);
-			spokeRads2 = MWC.Algorithms.Conversions.Degs2Rads(axis + thisSpoke);
+			spokeRads1 = MWC.Algorithms.Conversions.Degs2Rads(axis + thisSpoke);
 			// calculate the offset produced by this angle
 			edge1.setLocation((int) ((double) radius * Math.sin(spokeRads1)),
 					-(int) ((double) radius * Math.cos(spokeRads1)));
-			edge2.setLocation((int) ((double) radius * Math.sin(spokeRads2)),
-					-(int) ((double) radius * Math.cos(spokeRads2)));
 			// add this to the centre
 			edge1.translate(centrex, centrey);
-			edge2.translate(centrex, centrey);
 			// draw the line
 			dest.drawLine(centrex, centrey, edge1.x, edge1.y);
-			dest.drawLine(centrex, centrey, edge2.x, edge2.y);
 			// move on to the next spoke
-			thisSpoke += spoke_separation;
+			thisSpoke += _spoke_separation;
 		}
 
 		// now the range rings
-		final int ring_separation = radius / rings;
+		final int ring_separation = radius / _numRings;
 		int thisRadius = ring_separation;
 		final Point origin = new Point();
 
 		// sort out the angles and arcs (all in degs)
-		final int startAngle = 90 - axis + arcs;
-		final int endAngle = startAngle - 2 * arcs;
-		final int angle = endAngle - startAngle;
-
-		for (int i = 0; i < rings; i++)
+		final int startAngle = 90 - (axis + _arcStart);
+		final int angle = -(_arcEnd - _arcStart);
+		for (int i = 0; i < _numRings; i++)
 		{
 			origin.setLocation(new Point(centrex, centrey));
 
@@ -378,7 +381,7 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	{
 		_fillArcs = fillArcs;
 	}
-	
+
 	public boolean getUseCurrentTrackColor()
 	{
 		return _useCurrentTrackColor;
@@ -388,7 +391,6 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	{
 		_useCurrentTrackColor = useCurrentTrackColor;
 	}
-
 
 	/**
 	 * determine if we should just plot a ring around the primary track
@@ -412,36 +414,46 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 
 	// //////////////////////
 	/**
-	 * get the arcs either side of ownship heading
+	 * the start rel brg for the arcs
 	 * 
 	 * @return range of arcs in degrees
 	 */
-	public final BoundedInteger getArcs()
+	public final Integer getArcStart()
 	{
-		return new BoundedInteger(_arcs, 0, 180);
+		return _arcStart;
 	}
 
 	/**
-	 * get the arcs either side of ownship heading
+	 * the start rel brg for the arcs
+	 * 
+	 * @return range of arcs in degrees
+	 */
+	public final Integer getArcEnd()
+	{
+		return _arcEnd;
+	}
+
+	/**
+	 * the start rel brg for the arcs
 	 * 
 	 * @param val
 	 *          range of arcs in degrees
 	 */
-	public final void setArcs(final BoundedInteger val)
+	public final void setArcStart(final Integer val)
 	{
-		_arcs = val.getCurrent();
+		_arcStart = val;
 	}
 
 	/**
-	 * get the arcs either side of ownship heading - convenience method for XML
+	 * get the end arc related to ownship heading - convenience method for XML
 	 * persistence
 	 * 
 	 * @param val
 	 *          range of arcs in degrees
 	 */
-	public final void setArcs(final int val)
+	public final void setArcEnd(final Integer val)
 	{
-		_arcs = val;
+		_arcEnd = val;
 	}
 
 	// ////////////////////////
@@ -474,7 +486,7 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	 */
 	public final void setNumRings(final BoundedInteger val)
 	{
-		_rings = val.getCurrent();
+		_numRings = val.getCurrent();
 	}
 
 	/**
@@ -486,7 +498,7 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	 */
 	public final void setNumRings(final int val)
 	{
-		_rings = val;
+		_numRings = val;
 	}
 
 	/**
@@ -496,7 +508,7 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 	 */
 	public final BoundedInteger getNumRings()
 	{
-		return new BoundedInteger(_rings, 1, 10);
+		return new BoundedInteger(_numRings, 1, 10);
 	}
 
 	// ///////////////////////////////////////////////////////////
@@ -531,9 +543,12 @@ public final class SWTRangeHighlighter implements SWTPlotHighlighter
 				final java.beans.PropertyDescriptor[] res =
 				{
 						prop("Color", "Color to paint highlight", FORMAT),
-						prop("UseCurrentTrackColor", "Paint hightlight using current track color", FORMAT),
+						prop("UseCurrentTrackColor",
+								"Paint hightlight using current track color", FORMAT),
 						prop("Radius", "Radius of outer ring (yds)", SPATIAL),
-						prop("Arcs", "Angle of arcs each side (degs)", SPATIAL),
+						prop("ArcStart", "The rel angle where the arcs start (degs)",
+								SPATIAL),
+						prop("ArcEnd", "The rel angle where the arcs end (degs)", SPATIAL),
 						prop("FillArcs", "whether to shade in the arcs", FORMAT),
 						prop("SpokeSeparation", "Angle between spokes (degs)", SPATIAL),
 						prop("JustPlotPrimary",
