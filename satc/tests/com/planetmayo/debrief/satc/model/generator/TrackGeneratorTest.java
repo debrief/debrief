77d068b2e01b6f6a58a0e2a9fbf56ccafba3f715
Ian Mayo <ian@planetmayo.com>
2012-10-04 09:05:44 +0100

    some more tests, though mostly placeholders

---
+++ satc/tests/com/planetmayo/debrief/satc/model/generator/TrackGeneratorTest.java
@@ -0,0 +1,22 @@
+package com.planetmayo.debrief.satc.model.generator;
+
+import junit.framework.TestCase;
+
+public class TrackGeneratorTest extends TestCase
+{
+	public void testAddOrder()
+	{
+		// TODO: test this. add a range of contribution types, check they are stored in the correct order
+	}
+	
+	public void testListening()
+	{
+		// TODO: create some contributions, add them to generator, make some changes, check the re-generation happens
+	}
+	
+	public void testRegeneration()
+	{
+		// TODO: create some contributions, include some constraints, check that contraint restriction is happening
+	}
+	
+}
