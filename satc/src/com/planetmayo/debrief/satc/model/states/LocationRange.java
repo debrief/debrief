766ec4875bab22087968aee8a01101a2ff7dae46
Ian Mayo <ian@planetmayo.com>
2012-10-04 09:05:24 +0100

    introduce the location (polygon) bounds object

---
+++ satc/src/com/planetmayo/debrief/satc/model/states/LocationRange.java
@@ -0,0 +1,42 @@
+package com.planetmayo.debrief.satc.model.states;
+
+import com.vividsolutions.jts.geom.Polygon;
+
+/**
+ * class representing a bounded set of locations, stored as a polygon
+ * 
+ * @author ian
+ * 
+ */
+public class LocationRange extends BaseRange
+{
+	/** the range of locations we allow
+	 * 
+	 */
+	private Polygon _myArea;
+
+	public LocationRange(Polygon area)
+	{
+		_myArea = area;
+	}
+
+	/**
+	 * copy constructor
+	 * 
+	 * @param range
+	 */
+	public LocationRange(LocationRange range)
+	{
+		// TODO: this should do a deep copy, not a shallow copy
+		this(range._myArea);
+	}
+
+	/** trim my area to the area provided
+	 * 
+	 * @param sTwo
+	 */
+	public void constrainTo(LocationRange sTwo)
+	{
+		_myArea = (Polygon) _myArea.intersection(sTwo._myArea);
+	}
+}
