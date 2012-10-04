36824ceac6fac18782f1191f83a72eac83885fd6
Ian Mayo <ian@planetmayo.com>
2012-10-04 06:40:50 +0100

    introduce some unit tests

---
+++ satc/tests/com/planetmayo/debrief/satc/model/states/ProblemSpaceTest.java
@@ -0,0 +1,49 @@
+package com.planetmayo.debrief.satc.model.states;
+
+import java.util.Date;
+import java.util.Iterator;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class ProblemSpaceTest extends TestCase
+{
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testMissingDate()
+	{
+		ProblemSpace ps = new ProblemSpace();
+		BoundedState b1 = new BoundedState(new Date(2012, 4,4));
+		BoundedState b2 = new BoundedState(new Date(2012, 5,4));
+		BoundedState b3 = new BoundedState(new Date(2012, 2,4));
+		
+		ps.add(b1);
+		ps.add(b2);
+		ps.add(b3);
+		
+		Iterator<BoundedState> iter = ps.iterator();
+		assertEquals("correct order", b3, iter.next());
+		assertEquals("correct order", b1, iter.next());
+		assertEquals("correct order", b2, iter.next());
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void testAddSort()
+	{
+		ProblemSpace ps = new ProblemSpace();
+		BoundedState b1 = new BoundedState(new Date(2012, 4,4));
+		BoundedState b2 = new BoundedState(new Date(2012, 5,4));
+		BoundedState b3 = new BoundedState(new Date(2012, 2,4));
+		
+		ps.add(b1);
+		ps.add(b2);
+		ps.add(b3);
+		
+		Iterator<BoundedState> iter = ps.iterator();
+		assertEquals("correct order", b3, iter.next());
+		assertEquals("correct order", b1, iter.next());
+		assertEquals("correct order", b2, iter.next());
+	}
+}
