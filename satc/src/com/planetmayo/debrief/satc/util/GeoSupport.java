c3f4d46ab7dc924de299cbc34530b9098f4a02eb
Ian Mayo <ian@planetmayo.com>
2012-10-05 07:03:26 +0100

    introduce class to ease management of geometry factory

---
+++ satc/src/com/planetmayo/debrief/satc/util/GeoSupport.java
@@ -0,0 +1,26 @@
+package com.planetmayo.debrief.satc.util;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+
+/** utility class providing geospatial support
+ * 
+ * @author ian
+ *
+ */
+public class GeoSupport
+{
+	private static GeometryFactory _factory;
+
+	/** get our geometry factory
+	 * 
+	 * @return
+	 */
+	public static GeometryFactory getFactory()
+	{
+		if(_factory == null)
+			_factory = new GeometryFactory();
+		
+		return _factory;
+	}
+
+}
