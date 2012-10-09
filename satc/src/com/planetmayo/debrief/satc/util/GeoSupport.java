506752d49f2af9d221960bf0eeecd64fb3bb5436
Ian Mayo <ian@planetmayo.com>
2012-10-09 14:58:21 +0100

    include utils to output coords

--- satc/src/com/planetmayo/debrief/satc/util/GeoSupport.java
+++ satc/src/com/planetmayo/debrief/satc/util/GeoSupport.java
@@ -1,6 +1,10 @@
 package com.planetmayo.debrief.satc.util;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
 import com.vividsolutions.jts.geom.Polygon;
 
 /** utility class providing geospatial support
@@ -42,5 +46,43 @@ public class GeoSupport
 	public static double MSec2kts(double m_sec)
 	{
 		return m_sec / 0.514444444;
+	}
+	
+	public static void writeGeometry(String title, Geometry geo)
+	{
+		if(geo == null)
+			return;
+		
+		if(geo instanceof LineString)
+		{
+			LineString ring = (LineString) geo;
+			Coordinate[] coords = ring.getCoordinates();
+			writeGeometry(title, coords);
+		}
+		else if(geo instanceof Polygon)
+		{
+			Polygon poly = (Polygon) geo;
+			writeGeometry(title + " boundary " ,poly.getBoundary());
+		}
+		else if(geo instanceof MultiLineString)
+		{
+			MultiLineString lineS = (MultiLineString) geo;
+			int n = lineS.getNumGeometries();
+			for(int i=0;i<n;i++)
+			{
+				Geometry thisGeo = lineS.getGeometryN(i);
+				writeGeometry(title + " geo:" + i, thisGeo);
+			}
+		}
+	}
+
+	private static void writeGeometry(String title, Coordinate[] coords)
+	{
+		System.out.println("== " + title + " ==");
+		for (int i = 0; i < coords.length; i++)
+		{
+			Coordinate coordinate = coords[i];
+			System.out.println(coordinate.x + ", " + coordinate.y);
+		}
 	}
 }
