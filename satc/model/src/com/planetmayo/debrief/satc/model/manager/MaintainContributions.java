36a01121559bfadd67f3a5d323dd04b4e4ede3eb
Ian Mayo <ian@planetmayo.com>
2012-10-11 18:27:04 +0100

    Refactor manage contributions, make call to add contribution

--- satc/model/src/com/planetmayo/debrief/satc/model/manager/MaintainContributions.java
+++ satc/model/src/com/planetmayo/debrief/satc/model/manager/MaintainContributions.java
@@ -1,11 +1,13 @@
 package com.planetmayo.debrief.satc.model.manager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import com.planetmayo.debrief.satc.model.Precision;
+import com.planetmayo.debrief.satc.model.VehicleType;
 import com.planetmayo.debrief.satc.model.contributions.BaseContribution;
 import com.planetmayo.debrief.satc.model.contributions.BearingMeasurementContribution;
 import com.planetmayo.debrief.satc.model.contributions.CourseForecastContribution;
@@ -13,19 +15,73 @@ import com.planetmayo.debrief.satc.model.contributions.LocationAnalysisContribut
 import com.planetmayo.debrief.satc.model.contributions.LocationForecastContribution;
 import com.planetmayo.debrief.satc.model.contributions.RangeForecastContribution;
 import com.planetmayo.debrief.satc.model.contributions.SpeedForecastContribution;
+import com.planetmayo.debrief.satc.model.generator.ContributionsChangedListener;
 import com.planetmayo.debrief.satc.model.generator.TrackGenerator;
 import com.planetmayo.debrief.satc.support.SupportServices;
 import com.planetmayo.debrief.satc.support.VehicleTypesRepository;
 
 public class MaintainContributions {
 
+	public static interface MyView extends ContributionsChangedListener
+	{
+
+		/**
+		 * show the list of available contributions
+		 * 
+		 * @param items the types of contribution that the user may add
+		 */
+		public void populateContributionList(ArrayList<String> items);
+
+		/**
+		 * populate the list of vehicle types
+		 * 
+		 * @param vehicles
+		 */
+		public void populateVehicleTypesList(List<VehicleType> vehicles);
+
+		/**
+		 * populate the list of precisions
+		 * 
+		 * @param vehicles
+		 */
+		public void populatePrecisionsList(Precision[] precisions);
+
+		/**
+		 * allow us to listen to user asking to remove a contribution
+		 * 
+		 * @param listener
+		 */
+		public void setRemoveContributionListener(ChangeListener listener);
+
+		/**
+		 * allow us to listen to user asking to remove a contribution
+		 * 
+		 * @param listener
+		 */
+		public void setAddContributionListener(ChangeListener listener);
+
+		/**
+		 * allow us to listen to user asking to remove a contribution
+		 * 
+		 * @param listener
+		 */
+		public void setVehicleChangeListener(ChangeListener listener);
+
+		/**
+		 * allow us to listen to user asking to remove a contribution
+		 * 
+		 * @param listener
+		 */
+		public void setPrecisionChangeListener(ChangeListener listener);
+
+	}
 	/**
 	 * our track generator
 	 * 
 	 */
 	TrackGenerator _genny;
 
-	public MaintainContributions(MaintainContributionsView myView,
+	public MaintainContributions(MyView myView,
 			VehicleTypesRepository vehiclesRepository) {
 		// sort out our generator
 		_genny = new TrackGenerator();
