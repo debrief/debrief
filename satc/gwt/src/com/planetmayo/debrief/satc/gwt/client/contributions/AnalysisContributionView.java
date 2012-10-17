dfba8330e87583182ce6e9bdc558dbf8ccf32369
Akash Gupta <akash.bits@gmail.com>
2012-10-17 14:28:53 +0530

    Analysis Contributions Implemented.

--- satc/gwt/src/com/planetmayo/debrief/satc/gwt/client/contributions/AnalysisContributionView.java
+++ satc/gwt/src/com/planetmayo/debrief/satc/gwt/client/contributions/AnalysisContributionView.java
@@ -1,24 +1,41 @@
 package com.planetmayo.debrief.satc.gwt.client.contributions;
 
+import java.beans.PropertyChangeEvent;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.planetmayo.debrief.satc.model.contributions.BaseContribution;
 
-public class AnalysisContributionView extends Composite
-{
+public class AnalysisContributionView extends BaseContributionView {
 
 	interface AnalysisContributionViewUiBinder extends
-			UiBinder<Widget, AnalysisContributionView>
-	{
+			UiBinder<Widget, AnalysisContributionView> {
 	}
 
 	private static AnalysisContributionViewUiBinder uiBinder = GWT
 			.create(AnalysisContributionViewUiBinder.class);
 
-	public AnalysisContributionView()
-	{
+	public AnalysisContributionView() {
 		initWidget(uiBinder.createAndBindUi(this));
+		initHandlers();
+	}
+
+	private BaseContribution _myData;
+
+	@Override
+	protected BaseContribution getData() {
+		return _myData;
+	}
+
+	@Override
+	public void setData(BaseContribution contribution) {
+		super.setData(contribution);
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent arg0) {
+		super.propertyChange(arg0);
 	}
 
 }
