f5ec648ec7ec862f2efb768ae14138a36d444473
Akash Gupta <akash.bits@gmail.com>
2012-10-18 11:31:12 +0530

    error panel implementation changed.

--- satc/gwt/src/com/planetmayo/debrief/satc/gwt/client/ui/TrackStates.java
+++ satc/gwt/src/com/planetmayo/debrief/satc/gwt/client/ui/TrackStates.java
@@ -8,6 +8,7 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 import com.planetmayo.debrief.satc.gwt.client.Gwt;
@@ -17,11 +18,9 @@ import com.planetmayo.debrief.satc.model.generator.BoundedStatesListener;
 import com.planetmayo.debrief.satc.model.states.BaseRange.IncompatibleStateException;
 import com.planetmayo.debrief.satc.model.states.BoundedState;
 
-public class TrackStates extends Composite implements BoundedStatesListener
-{
+public class TrackStates extends Composite implements BoundedStatesListener {
 
-	interface TrackStatesUiBinder extends UiBinder<Widget, TrackStates>
-	{
+	interface TrackStatesUiBinder extends UiBinder<Widget, TrackStates> {
 	}
 
 	private static TrackStatesUiBinder uiBinder = GWT
@@ -30,14 +29,15 @@ public class TrackStates extends Composite implements BoundedStatesListener
 	@UiField
 	Grid grid;
 
-	public TrackStates()
-	{
+	@UiField
+	HTML errorPanel;
+
+	public TrackStates() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
 	public void addStates(String time, String location, String speed,
-			String course)
-	{
+			String course) {
 		int index = grid.insertRow(grid.getRowCount());
 		grid.setWidget(index, 0, new Label(time));
 		grid.setWidget(index, 1, new Label(location));
@@ -50,47 +50,42 @@ public class TrackStates extends Composite implements BoundedStatesListener
 	 * remove all rows
 	 * 
 	 */
-	private void clearGrid()
-	{
+	private void clearGrid() {
 		while (grid.getRowCount() > 1)
 			grid.removeRow(1);
 
 		// TODO: Akash, check the warning label is hidden
-		Gwt.eventBus.fireEvent(new ErrorEvent(null));
+		errorPanel.setText(""); //This clears the error label
 
 	}
 
 	@Override
-	public void debugStatesBounded(Collection<BoundedState> newStates)
-	{
+	public void debugStatesBounded(Collection<BoundedState> newStates) {
 		statesBounded(newStates);
 	}
 
 	@Override
 	public void incompatibleStatesIdentified(BaseContribution contribution,
-			IncompatibleStateException e)
-	{
+			IncompatibleStateException e) {
 		String message1 = "Incompatible States. Contribution: "
 				+ contribution.toString();
 		String message2 = "Adding:" + e.getNewRange().getConstraintSummary()
 				+ " to " + e.getExistingRange().getConstraintSummary();
 		System.err.println(message1);
 		System.err.println(message2);
-		
-		Gwt.eventBus.fireEvent(new ErrorEvent(message1,message2));
 
-		// TODO: Akash, make the label visible, put the text into it. It may need a
+		errorPanel.setHTML(message1 + "<BR>" + message2);
+		// TODO: Akash, make the label visible, put the text into it. It may
+		// need a
 		// multi-line message
 	}
 
 	@Override
-	public void statesBounded(Collection<BoundedState> newStates)
-	{
+	public void statesBounded(Collection<BoundedState> newStates) {
 		clearGrid();
 
 		Iterator<BoundedState> iter = newStates.iterator();
-		while (iter.hasNext())
-		{
+		while (iter.hasNext()) {
 			BoundedState state = iter.next();
 			@SuppressWarnings("deprecation")
 			String dateStr = state.getTime().toGMTString();
