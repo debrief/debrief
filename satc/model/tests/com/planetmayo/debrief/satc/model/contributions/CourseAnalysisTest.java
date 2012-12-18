cd366e4d611bd0ba91fbc7278c1ba95f282778ef
Ian Mayo <ian@planetmayo.com>
2012-12-18 14:37:02 +0000

    start on course analysis contribution. Fix CourseRange bugs found along the way.

--- satc/model/tests/com/planetmayo/debrief/satc/model/contributions/CourseAnalysisTest.java
+++ satc/model/tests/com/planetmayo/debrief/satc/model/contributions/CourseAnalysisTest.java
@@ -53,4 +53,50 @@ public class CourseAnalysisTest extends TestCase
 		assertEquals("new min course valid",30d, Math.toDegrees( bc.getCourse().getMin()), 0.001);
 		assertEquals("new max course valid",125d, Math.toDegrees(bc.getCourse().getMax()), 0.001);
 	}
+	
+
+	/** test that we trim the range to 0-360, and never go all the way around a circle
+	 * 
+	 * @throws IncompatibleStateException
+	 */
+	public void testWrapAround() throws IncompatibleStateException
+	{
+		// ok, create the state
+		Date oldDate = new Date(100000);
+		Date newDate1 = new Date(105000);
+		Date newDate2 = new Date(115000);
+		BoundedState ba = new BoundedState(oldDate);
+		BoundedState bb = new BoundedState(newDate1);
+		BoundedState bc = new BoundedState(newDate2);
+		ProblemSpace space = new ProblemSpace();
+		space.add(ba);
+		space.add(bb);
+		space.add(bc);
+		VehicleType vType = new VehicleType("UK Ferry", GeoSupport.kts2MSec(2),
+				GeoSupport.kts2MSec(30), GeoSupport.degsSec2radsMilli(0),
+				GeoSupport.degsSec2radsMilli(1), 0.2, 0.4, 0.2, 0.4);
+		space.setVehicleType(vType);
+
+		// set some course data
+		CourseRange cr = new CourseRange(Math.toRadians(5), Math.toRadians(350));
+		ba.constrainTo(cr);
+		
+		assertNull(" course constraint empty",bb.getCourse());
+		assertNull(" course constraint empty",bc.getCourse());
+	
+		CourseAnalysisContribution cac = new CourseAnalysisContribution();
+		cac.actUpon(space);
+		
+		assertNotNull(" course constriant not empty", bb.getCourse());
+		assertNotNull(" course constriant not empty", bc.getCourse());
+		
+		// have a look at the new min/max course
+		assertEquals("new min course valid",00d, Math.toDegrees( bb.getCourse().getMin()), 0.001);
+		assertEquals("new max course valid",355d, Math.toDegrees(bb.getCourse().getMax()), 0.001);
+		
+		// have a look at the new min/max course once we've run for a little longer
+		assertEquals("new min course valid",0d, Math.toDegrees( bc.getCourse().getMin()), 0.001);
+		assertEquals("new max course valid",360d, Math.toDegrees(bc.getCourse().getMax()), 0.001);
+	}
+	
 }
