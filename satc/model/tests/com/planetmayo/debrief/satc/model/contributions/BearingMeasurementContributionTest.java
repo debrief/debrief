50994afd0bb373cd401804c292e5037696879b6e
ymakhno <ymakhno@gmail.com>
2012-12-28 14:26:42 +0200

    BearingMeasurementContributionTest

--- satc/model/tests/com/planetmayo/debrief/satc/model/contributions/BearingMeasurementContributionTest.java
+++ satc/model/tests/com/planetmayo/debrief/satc/model/contributions/BearingMeasurementContributionTest.java
@@ -1,13 +1,10 @@
 package com.planetmayo.debrief.satc.model.contributions;
 
-import java.io.IOException;
 import java.util.Date;
-import java.util.Iterator;
 
 import org.junit.Test;
 
 import com.planetmayo.debrief.satc.model.ModelTestBase;
-import com.planetmayo.debrief.satc.model.states.BaseRange.IncompatibleStateException;
 import com.planetmayo.debrief.satc.model.states.BoundedState;
 import com.planetmayo.debrief.satc.model.states.ProblemSpace;
 import com.planetmayo.debrief.satc.support.SupportServices;
@@ -22,56 +19,38 @@ public class BearingMeasurementContributionTest extends ModelTestBase
 	public static final String THE_PATH = "tests/com/planetmayo/debrief/satc/model/contributions/data/bearing_measurement_data.txt";
 	public static final String THE_SHORT_PATH = "tests/com/planetmayo/debrief/satc/model/contributions/data/short_bearing_measurement_data.txt";
 
+	private BearingMeasurementContribution contribution;	
+	
+	@Test
+	public void testLoadFrom() throws Exception 
+	{
+		contribution = new BearingMeasurementContribution();
+		assertFalse("should be empty", contribution.hasData());
+		
+		contribution.loadFrom(SupportServices.INSTANCE.getIOService().readLinesFrom(THE_SHORT_PATH));
+		assertTrue("should not be empty", contribution.hasData());	
+		assertEquals("correct start date", new Date(110, 00, 12, 12, 13, 29), contribution.getStartDate());
+		assertEquals("correct finish date", new Date(110, 00, 12, 12, 24, 29), contribution.getFinishDate());
+		assertEquals(Math.toRadians(3d), contribution.getBearingError(), EPS);
+	}
 
 	@Test
-	public void testActUpon() throws IncompatibleStateException, IOException
+	public void testActUpon() throws Exception
 	{
-		BearingMeasurementContribution sc = new BearingMeasurementContribution();
-		
-		// check it's as we expect it to be
-		assertFalse("should be empty",sc.hasData() );
-		
-		// can we see any data?
-	//	assertTrue("can see datafile",new File(THE_PATH).exists());
-		
-		// ok, load some data		
-		sc.loadFrom(SupportServices.INSTANCE.getIOService().readLinesFrom(THE_SHORT_PATH));
-
-		// check it's as we expect it to be
-		assertTrue("should not be empty",sc.hasData() );
-
-		// did the dates get updated?
-		assertEquals("correct start date", new Date(110,00,12,12,13,29), sc.getStartDate());
-		assertEquals("correct finish date", new Date(110,00,12,13,05,29), sc.getFinishDate());
-		
-		// hmm, how about a polygon?
+		testLoadFrom();
 		ProblemSpace ps = new ProblemSpace();
 		
-		// go for it!
-		sc.actUpon(ps);
-		
-		Iterator<BoundedState> iter = ps.states().iterator();
-		int ctr = 0;
-		while (iter.hasNext())
+		contribution.actUpon(ps);		
+		for (BoundedState state : ps.states())
 		{
-			BoundedState newState = (BoundedState) iter.next();
-			
-			// ok, output it.
-			Geometry geo = newState.getLocation().getGeometry();
+			Geometry geo = state.getLocation().getGeometry();
 			Coordinate[] coords = geo.getCoordinates();
-			for (int i = 0; i <=4; i++)
+			for (int i = 0; i <= 4; i++)
 			{
 				Coordinate coordinate = coords[i];
 				assertNotNull("we should have a coordinate", coordinate);
-							}
-
-			ctr++;
+			}
 		}
-		
-		// did we find all of them?
-		assertEquals("read in all lines", 57, ctr);
-		
+		assertEquals("read in all lines", 8, ps.size());
 	}
-
-
 }
