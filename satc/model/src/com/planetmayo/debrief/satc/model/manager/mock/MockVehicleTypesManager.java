a9972f95a726a4415e41dc2d83ce406447bd917f
IanMayo <ian@planetmayo.com>
2013-02-13 17:27:13 +0000

    improve the values used for two slower test vehicles

--- satc/model/src/com/planetmayo/debrief/satc/model/manager/mock/MockVehicleTypesManager.java
+++ satc/model/src/com/planetmayo/debrief/satc/model/manager/mock/MockVehicleTypesManager.java
@@ -17,9 +17,9 @@ public class MockVehicleTypesManager implements IVehicleTypesManager
 				new VehicleType("UK Ferry", GeoSupport.kts2MSec(2), GeoSupport
 						.kts2MSec(30), Math.toRadians(0), Math.toRadians(2), 0.2, 0.4, 0.2, 0.4),
 				new VehicleType("Medium Tanker", GeoSupport.kts2MSec(1), GeoSupport
-						.kts2MSec(12), Math.toRadians(0), Math.toRadians(1), 1, 3, 1, 3),
+						.kts2MSec(12), Math.toRadians(0), Math.toRadians(1), 0.1, 0.4, 0.2, 0.3),
 				new VehicleType("Large Tanker", GeoSupport.kts2MSec(1), GeoSupport
-						.kts2MSec(15), Math.toRadians(0), Math.toRadians(0.5), 1, 3, 1, 3)
+						.kts2MSec(15), Math.toRadians(0), Math.toRadians(0.5), 0.1, 0.3, 0.2, 0.3)
 
 		);
 	}
