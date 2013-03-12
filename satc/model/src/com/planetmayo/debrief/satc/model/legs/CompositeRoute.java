e2bbb68d557688a56f49d69a1f0086c893c02911
IanMayo <ian@planetmayo.com>
2013-03-12 10:55:14 +0000

    flesh out the composite route class

--- satc/model/src/com/planetmayo/debrief/satc/model/legs/CompositeRoute.java
+++ satc/model/src/com/planetmayo/debrief/satc/model/legs/CompositeRoute.java
@@ -1,5 +1,8 @@
 package com.planetmayo.debrief.satc.model.legs;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /** a series of straight/altering legs
  * 
  * @author Ian
@@ -7,5 +10,27 @@ package com.planetmayo.debrief.satc.model.legs;
  */
 public class CompositeRoute
 {
- // TODO: sort out the composite route logic
+	private final ArrayList<CoreRoute> _legs;
+
+	public CompositeRoute()
+	{
+		_legs = new ArrayList<CoreRoute>();
+	}
+	
+	/** add this leg to our data
+	 * 
+	 * @param topR
+	 */
+	public void add(CoreRoute topR)
+	{
+		_legs.add(topR);
+	}
+	
+	/** get the legs that comprise this route
+	 * 
+	 */
+	public Collection<CoreRoute> getLegs()
+	{
+		return _legs;
+	}
 }
