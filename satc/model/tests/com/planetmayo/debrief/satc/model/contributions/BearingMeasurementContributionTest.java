458330d63ec854b19f56b5e2e025abce0331ff8c
IanMayo <ian@planetmayo.com>
2013-03-12 11:20:33 +0000

    first stage of checking BMC calcs

--- satc/model/tests/com/planetmayo/debrief/satc/model/contributions/BearingMeasurementContributionTest.java
+++ satc/model/tests/com/planetmayo/debrief/satc/model/contributions/BearingMeasurementContributionTest.java
@@ -5,16 +5,22 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.planetmayo.debrief.satc.model.ModelTestBase;
+import com.planetmayo.debrief.satc.model.legs.StraightRoute;
 import com.planetmayo.debrief.satc.model.states.BoundedState;
 import com.planetmayo.debrief.satc.model.states.ProblemSpace;
+import com.planetmayo.debrief.satc.support.SupportServices;
 import com.planetmayo.debrief.satc.support.TestSupport;
+import com.planetmayo.debrief.satc.util.GeoSupport;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
 
 @SuppressWarnings("deprecation")
 public class BearingMeasurementContributionTest extends ModelTestBase
@@ -22,19 +28,19 @@ public class BearingMeasurementContributionTest extends ModelTestBase
 	public static final String THE_PATH = "tests/com/planetmayo/debrief/satc/model/contributions/data/bearing_measurement_data.txt";
 	public static final String THE_SHORT_PATH = "tests/com/planetmayo/debrief/satc/model/contributions/data/short_bearing_measurement_data.txt";
 
-	private BearingMeasurementContribution contribution;	
+	private BearingMeasurementContribution bmc;	
 	
 	@Test
 	public void testLoadFrom() throws Exception 
 	{
-		contribution = new BearingMeasurementContribution();
-		assertFalse("should be empty", contribution.hasData());
+		bmc = new BearingMeasurementContribution();
+		assertFalse("should be empty", bmc.hasData());
 		
-		contribution.loadFrom(TestSupport.getShortData());
-		assertTrue("should not be empty", contribution.hasData());	
-		assertEquals("correct start date", new Date(110, 00, 12, 12, 13, 29), contribution.getStartDate());
-		assertEquals("correct finish date", new Date(110, 00, 12, 12, 24, 29), contribution.getFinishDate());
-		assertEquals(Math.toRadians(3d), contribution.getBearingError(), EPS);
+		bmc.loadFrom(TestSupport.getShortData());
+		assertTrue("should not be empty", bmc.hasData());	
+		assertEquals("correct start date", new Date(110, 00, 12, 12, 13, 29), bmc.getStartDate());
+		assertEquals("correct finish date", new Date(110, 00, 12, 12, 24, 29), bmc.getFinishDate());
+		assertEquals(Math.toRadians(3d), bmc.getBearingError(), EPS);
 	}
 
 	@Test
@@ -43,7 +49,7 @@ public class BearingMeasurementContributionTest extends ModelTestBase
 		testLoadFrom();
 		ProblemSpace ps = new ProblemSpace();
 		
-		contribution.actUpon(ps);		
+		bmc.actUpon(ps);		
 		for (BoundedState state : ps.states())
 		{
 			Geometry geo = state.getLocation().getGeometry();
@@ -55,5 +61,74 @@ public class BearingMeasurementContributionTest extends ModelTestBase
 			}
 		}
 		assertEquals("read in all lines", 5, ps.size());
+	}
+	
+	@Test
+	public void testErrorCalc()
+	{
+		bmc = new BearingMeasurementContribution();
+		bmc.loadFrom(TestSupport.getShortData());
+
+		double score;
+
+		// ok, create a route to use
+		Point startP = GeoSupport.getFactory().createPoint(new Coordinate(2,3));
+		Date startT =SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 121300");
+		Point endP = GeoSupport.getFactory().createPoint(new Coordinate(2,3));
+		Date endT = SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 130729");
+		StraightRoute route = new StraightRoute("rName", startP, startT, endP, endT);
+
+		// we'll need some states, so the route can correctly segment itself
+		ArrayList<BoundedState> states = new ArrayList<BoundedState>();
+
+		// inject some early states for which there isn't a measurement
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 121300")));
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 121301")));
+
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 121459")));
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 121629")));
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 121814")));
+		
+		// inject a state for which there isn't a measurement
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 122300")));
+		
+		// and carry on
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 122329")));
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 122829")));
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 123229")));
+
+		// inject some late states for which there isn't a measurement
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 130629")));
+		states.add(new BoundedState(SupportServices.INSTANCE.parseDate("yyMMdd HHmmss", "100112 130729")));
+
+		
+		// test when we shouldn't run
+		bmc.setActive(false);
+		bmc.setWeight(0);
+		
+		score = bmc.calculateErrorScoreFor(null);
+		assertEquals("should not be able to calc", 0, score,0.0001);
+
+		// ok, make it active
+		bmc.setActive(true);
+		
+		score = bmc.calculateErrorScoreFor(null);
+		assertEquals("still should not be able to calc", 0, score,0.0001);
+		
+		// ok, let it start
+		bmc.setWeight(1);
+		score = bmc.calculateErrorScoreFor(null);
+		assertEquals("still should not be able to calc", 0, score,0.0001);
+
+		score = bmc.calculateErrorScoreFor(route);
+		assertEquals("still should not be able to calc", 0, score,0.0001);
+		
+		// ok, flesh out the route
+		route.generateSegments(states);
+
+		score = bmc.calculateErrorScoreFor(route);
+
+		assertTrue("still should not be able to calc", 0 != score);
+
 	}
 }
