19c77f58374f969742beb16442ea5864f3bbcb64
Ian Mayo <ian@planetmayo.com>
2013-05-10 16:35:28 +0100

    introduce method for layer handlers to be defined outside the core debrief plugin

--- trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/loaders/xml_handlers/LayerHandlerExtension.java
+++ trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/loaders/xml_handlers/LayerHandlerExtension.java
@@ -1,8 +1,33 @@
 package org.mwc.debrief.core.loaders.xml_handlers;
 
+import MWC.GUI.Layer;
 import MWC.GUI.Layers;
 
 public interface LayerHandlerExtension
 {
+	/**
+	 * store the layers object that we're working on
+	 * 
+	 * @param theLayers
+	 */
 	public void setLayers(Layers theLayers);
+
+	/**
+	 * indicate if this handler can export objects of this type
+	 * 
+	 * @param subject
+	 * @return
+	 */
+	public boolean canExportThis(Layer subject);
+
+	/**
+	 * actually export this object
+	 * 
+	 * @param theLayer
+	 * @param parent
+	 * @param doc
+	 */
+	public void exportThis(Layer theLayer, org.w3c.dom.Element parent,
+			org.w3c.dom.Document doc);
+
 }
