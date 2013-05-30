fc25ca8f1cad14969b446e41abced01ed6e4e425
Ian Mayo <ian@planetmayo.com>
2013-05-30 14:52:37 +0100

    support dragging the ends of the shape, and follow user request to make calculated end point visible to user

--- trunk/org.mwc.cmap.legacy/src/MWC/GUI/Shapes/VectorShape.java
+++ trunk/org.mwc.cmap.legacy/src/MWC/GUI/Shapes/VectorShape.java
@@ -3,74 +3,122 @@ package MWC.GUI.Shapes;
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 
+import MWC.Algorithms.Conversions;
 import MWC.GUI.Editable;
-import MWC.GUI.Editable.EditorType;
-import MWC.GUI.Shapes.LineShape.LineInfo;
+import MWC.GUI.PlainWrapper;
 import MWC.GenericData.WorldDistance;
 import MWC.GenericData.WorldLocation;
 import MWC.GenericData.WorldVector;
 
-public class VectorShape extends LineShape {
-	private WorldVector _bearingVector;
-	private WorldDistance _distance = new WorldDistance(0,0);
+public class VectorShape extends LineShape
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private WorldDistance _distance = new WorldDistance(2, WorldDistance.NM);
 	private EditorType _myEditorLocal;
-	private double _bearing;
-	
-	public VectorShape(WorldLocation start, WorldLocation end)
+	private double _bearingDegs = 45;
+
+	public VectorShape(WorldLocation start, double bearing, WorldDistance distance)
 	{
-		super(start, end, "Vector");
-	}
-	
-	public VectorShape(WorldLocation start, double bearing, WorldDistance distance) {
-		super(start, new WorldLocation(0d,0d, 0d), "Vector");
-		_bearing=bearing;
-		_distance=distance;
-		calculateEnd();
-	}
-	
-	
-	public Double getBearing() {
-		return _bearing;
-	}
-
-	public void setBearing(Double _bearing) {
-		this._bearing = _bearing;
+		super(start, new WorldLocation(0d, 0d, 0d), "Vector");
+		_bearingDegs = bearing;
+		_distance = distance;
 		calculateEnd();
 	}
 
+	public Double getBearing()
+	{
+		return _bearingDegs;
+	}
 
-	private void calculateEnd() {
-		_bearingVector= new WorldVector(MWC.Algorithms.Conversions.Degs2Rads(_bearing), _distance, new WorldDistance(0,0));
+	public void setBearing(Double _bearing)
+	{
+		this._bearingDegs = _bearing;
+		calculateEnd();
+	}
+
+	@Override
+	public void shift(WorldLocation feature, WorldVector vector)
+	{
+		// ok, has the start point been dragged?
+		if (feature.equals(_start))
+		{
+			// ok, just apply it
+			_start.addToMe(vector);
+			
+			// and recalculate
+			calculateEnd();
+
+		}
+		else
+		{
+			// we're working with the trailing end!
+			feature.addToMe(vector);
+
+			// sort out the new vector back to the start
+			WorldVector newV = feature.subtract(_start);
+
+			// and store the components
+			_bearingDegs = Conversions.Rads2Degs(newV.getBearing());
+			_distance = new WorldDistance(newV.getRange(), WorldDistance.DEGS);
+
+			firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
+		}
+	}
+
+	private void calculateEnd()
+	{
+		WorldVector _bearingVector = new WorldVector(
+				MWC.Algorithms.Conversions.Degs2Rads(_bearingDegs), _distance,
+				new WorldDistance(0, 0));
 		_end = _start.add(_bearingVector);
+
+		firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
 	}
 
-	public WorldDistance getDistance() {
+	public WorldDistance getDistance()
+	{
 		return _distance;
 	}
 
-
-	public void setDistance(WorldDistance _distance) {
+	public void setDistance(WorldDistance _distance)
+	{
 		this._distance = _distance;
 		calculateEnd();
 	}
-	
-	
+
 	@Override
-	public void setLine_Start(WorldLocation loc) {
+	public void setLine_Start(WorldLocation loc)
+	{
 		super.setLine_Start(loc);
 		calculateEnd();
 	}
 
+	@Override
+	public void setLineEnd(WorldLocation loc)
+	{
+		// note: we make the line end an editable property so that users can switch
+		// between relative (vector) & absolute values
+		super.setLineEnd(loc);
+
+		// ok, sort out the offset
+		WorldVector vec = super._end.subtract(super._start);
+		_bearingDegs = Conversions.Rads2Degs(vec.getBearing());
+		_distance = new WorldDistance(vec.getRange(), WorldDistance.DEGS);
+		calculateEnd();
+	}
 
 	@Override
-	public EditorType getInfo() {
+	public EditorType getInfo()
+	{
 		if (_myEditorLocal == null)
 			_myEditorLocal = new VectorInfo(this, getName());
 
 		return _myEditorLocal;
 	}
-
-
 
 	public class VectorInfo extends Editable.EditorType
 	{
@@ -89,6 +137,7 @@ public class VectorShape extends LineShape {
 						prop("Line_Start", "the start of the line", SPATIAL),
 						prop("Bearing", "the bearing for the vector", SPATIAL),
 						prop("Distance", "the size of the vector", SPATIAL),
+						prop("LineEnd", "the end of the line", SPATIAL),
 						prop("ArrowAtEnd",
 								"whether to show an arrow at one end of the line", FORMAT), };
 
