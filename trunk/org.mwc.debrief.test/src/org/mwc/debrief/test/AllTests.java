1285b9b4ed144401c7d71e775e0d7503aa6c1296
Dariak <dashaa@equinox.(none)>
2013-06-11 23:36:32 +0700

    Added org.mwc.debrief.test project to run all JUnit tests

---
+++ trunk/org.mwc.debrief.test/src/org/mwc/debrief/test/AllTests.java
@@ -0,0 +1,10 @@
+package org.mwc.debrief.test;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.extensions.cpsuite.ClasspathSuite.SuiteTypes;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)  
+@SuiteTypes({org.junit.extensions.cpsuite.SuiteType.JUNIT38_TEST_CLASSES, 
+	org.junit.extensions.cpsuite.SuiteType.TEST_CLASSES})
+public class AllTests {}
