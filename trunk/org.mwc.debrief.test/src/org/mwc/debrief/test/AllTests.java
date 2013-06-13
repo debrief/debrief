cdc26a9db2fb2c0783223d881160bf3d9154a5bc
Dariak <dashaa@equinox.(none)>
2013-06-13 23:40:39 +0700

    Added classpathsuite.path path element

--- trunk/org.mwc.debrief.test/src/org/mwc/debrief/test/AllTests.java
+++ trunk/org.mwc.debrief.test/src/org/mwc/debrief/test/AllTests.java
@@ -5,10 +5,12 @@
 package org.mwc.debrief.test;
 
 import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.extensions.cpsuite.ClasspathSuite.ClasspathProperty;
 import org.junit.extensions.cpsuite.ClasspathSuite.SuiteTypes;
 import org.junit.runner.RunWith;
 
 @RunWith(ClasspathSuite.class)  
+@ClasspathProperty("classpathsuite.path")
 @SuiteTypes({org.junit.extensions.cpsuite.SuiteType.JUNIT38_TEST_CLASSES, 
 	org.junit.extensions.cpsuite.SuiteType.TEST_CLASSES})
 public class AllTests {}
