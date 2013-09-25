fe98cdc77bd8b2a35c253894c644730f0e16613f
Haris Peco <snpe@snpe.rs>
2013-09-25 19:53:52 +0200

    Issue #502 - Fixing ZoomIn
    Fixing the ticket #502 Zoomin Issue by using emulated Tracker
    Emulated Tracker uses color (111,111,111) and setXORMode

--- trunk/org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/actions/ZoomIn.java
+++ trunk/org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/actions/ZoomIn.java
@@ -4,9 +4,18 @@
 package org.mwc.cmap.plotViewer.actions;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.LineAttributes;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Tracker;
 import org.mwc.cmap.core.CorePlugin;
 import org.mwc.cmap.core.operations.DebriefActionWrapper;
@@ -34,10 +43,34 @@ public class ZoomIn extends CoreDragAction
 
 		private PlainChart _myChart;
 
+		private Rectangle res;
+
+		private int JITTER;
+
+		private Layers layers;
+		
+		private boolean dragResult;
+
+		private KeyListener listener = new KeyAdapter()
+		{
+
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+				if (e.keyCode == SWT.ESC) {
+					dragResult = false;
+				}
+			}
+			
+		};
+
 		@Override
 		public void doMouseDrag(final Point pt, final int JITTER,
 				final Layers theLayers, SWTCanvas theCanvas)
 		{
+			// redraw canvas
+			_myCanvas.getCanvas().redraw();
+			Display.getCurrent().update();
 			// just do a check that we have our start point (it may have been cleared
 			// at the end of the move operation)
 			if (_startPoint != null)
@@ -45,54 +78,68 @@ public class ZoomIn extends CoreDragAction
 				int deltaX = _startPoint.x - pt.x;
 				int deltaY = _startPoint.y - pt.y;
 
-				Tracker _dragTracker = new Tracker((Composite) _myCanvas.getCanvas(),
-						SWT.RESIZE);
-				Rectangle rect = new Rectangle(_startPoint.x, _startPoint.y, deltaX,
-						deltaY);
-				_dragTracker.setRectangles(new Rectangle[] { rect });
-				boolean dragResult = _dragTracker.open();
-				if (dragResult)
+				this.JITTER = JITTER;
+				this.layers = theLayers;
+				Rectangle rect = new Rectangle(_startPoint.x, _startPoint.y, -deltaX,
+						-deltaY);
+				
+				res = rect;
+				
+				GC gc = new GC(_myCanvas.getCanvas());
+				Color fc = new Color(Display.getDefault(), 111, 111, 111);
+				gc.setForeground(fc);
+				gc.setXORMode(true);
+				gc.setLineAttributes(new LineAttributes(2, SWT.CAP_FLAT, SWT.JOIN_MITER, SWT.LINE_SOLID, null, 0, 10));
+				gc.drawRectangle(rect);
+				fc.dispose();
+				gc.dispose();
+				
+			}
+		}
+
+		private void run()
+		{
+			if (dragResult)
+			{
+				// get world area
+				java.awt.Point tl = new java.awt.Point(res.x, res.y);
+				java.awt.Point br = new java.awt.Point(res.x + res.width, res.y
+						+ res.height);
+
+				if (res.width > JITTER || res.height > JITTER)
 				{
-					Rectangle[] rects = _dragTracker.getRectangles();
-					Rectangle res = rects[0];
-					// get world area
-					java.awt.Point tl = new java.awt.Point(res.x, res.y);
-					java.awt.Point br = new java.awt.Point(res.x + res.width, res.y
-							+ res.height);
 
-					if (res.width > JITTER || res.height > JITTER)
-					{
+					WorldLocation locA = new WorldLocation(_myCanvas.getProjection()
+							.toWorld(tl));
+					WorldLocation locB = new WorldLocation(_myCanvas.getProjection()
+							.toWorld(br));
+					WorldArea area = new WorldArea(locA, locB);
 
-						WorldLocation locA = new WorldLocation(_myCanvas.getProjection()
-								.toWorld(tl));
-						WorldLocation locB = new WorldLocation(_myCanvas.getProjection()
-								.toWorld(br));
-						WorldArea area = new WorldArea(locA, locB);
+					WorldArea oldArea = _myCanvas.getProjection().getDataArea();
+					Action theAction = new MWC.GUI.Tools.Chart.ZoomIn.ZoomInAction(
+														_myChart, oldArea, area);
+					
+					// and wrap it
+					DebriefActionWrapper daw = new DebriefActionWrapper(theAction,
+							layers, null);
 
-						WorldArea oldArea = _myCanvas.getProjection().getDataArea();
-						Action theAction = new MWC.GUI.Tools.Chart.ZoomIn.ZoomInAction(
-															_myChart, oldArea, area);
-						
-						// and wrap it
-						DebriefActionWrapper daw = new DebriefActionWrapper(theAction,
-								theLayers, null);
+					// and add it to the clipboard
+					CorePlugin.run(daw);
 
-						// and add it to the clipboard
-						CorePlugin.run(daw);
-
-					}
-
-					_dragTracker = null;
-					_startPoint = null;
-					_myChart = null;
-					_myCanvas = null;
 				}
+
 			}
 		}
 
 		@Override
 		public void doMouseUp(Point point, int keyState)
 		{
+			run();
+			_myCanvas.getCanvas().removeKeyListener(listener);
+			_myCanvas.getCanvas().redraw();
+			Display.getCurrent().update();
+			_myChart = null;
+			_myCanvas = null;
 			_startPoint = null;
 		}
 
@@ -102,6 +149,8 @@ public class ZoomIn extends CoreDragAction
 			_startPoint = point;
 			_myCanvas = canvas;
 			_myChart = theChart;
+			_myCanvas.getCanvas().addKeyListener(listener);
+			dragResult = true;
 		}
 
 	}
