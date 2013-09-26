e108def170cfd91aed40d9226219a5441b135e7b
Haris Peco <snpe@snpe.rs>
2013-09-26 12:10:18 +0200

    Issue #502 - Changing painting color to white
    Changes foreground (painting) color to white (255,255,255) for zoomin, range bearing and drag feature tools.
    Drag componet already use it.

--- trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/actions/DragFeature.java
+++ trunk/org.mwc.debrief.core/src/org/mwc/debrief/core/actions/DragFeature.java
@@ -458,7 +458,7 @@ public class DragFeature extends CoreDragAction
 		 */
 		private void drawHere(final GC graphics, final WorldVector newVector)
 		{
-			org.eclipse.swt.graphics.Color fc = new org.eclipse.swt.graphics.Color(Display.getDefault(), 111, 111,111);
+			org.eclipse.swt.graphics.Color fc = new org.eclipse.swt.graphics.Color(Display.getDefault(), 255, 255,255);
 			//graphics.setForeground(ColorHelper.getColor(java.awt.Color.WHITE));
 			graphics.setForeground(fc);
 
