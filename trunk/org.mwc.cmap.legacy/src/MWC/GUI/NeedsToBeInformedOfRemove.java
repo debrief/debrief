19b88ef71389742676d0932cd471a4533c24a977
Ian Mayo <ian@planetmayo.com>
2013-09-27 15:50:02 +0100

    introduce tracking for if the child elements in a layer have changed

---
+++ trunk/org.mwc.cmap.legacy/src/MWC/GUI/NeedsToBeInformedOfRemove.java
@@ -0,0 +1,16 @@
+package MWC.GUI;
+
+
+/** interface for objects that need to be informed when they are being removed from the Layers object
+ *
+ */
+
+public interface NeedsToBeInformedOfRemove
+{
+
+	/** this object is being removed from the Layers
+	 * 
+	 */
+	void beingRemoved();
+
+}
