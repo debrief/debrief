93b09b9070734e344cda28d6f372c31ec654903a
Dariak <dashaa@equinox.(none)>
2013-10-28 23:44:36 +0700

    Removed redundant LocationCalculator.Segment class; CrossSectionViewer started to grow meat

--- trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/LocationCalculator.java
+++ trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/LocationCalculator.java
@@ -1,13 +1,13 @@
 package org.mwc.cmap.xyplot.views;
 
-import java.awt.Point;
+import java.util.Date;
 
 import MWC.Algorithms.EarthModels.CompletelyFlatEarth;
 import MWC.GUI.Shapes.LineShape;
 import MWC.GenericData.Watchable;
 import MWC.GenericData.WorldDistance;
 import MWC.GenericData.WorldLocation;
-import MWC.GenericData.WorldVector;
+
 
 public class LocationCalculator implements ILocationCalculator
 {
@@ -19,12 +19,12 @@ public class LocationCalculator implements ILocationCalculator
 		this(WorldDistance.DEGS);
 	}
 	
-	public LocationCalculator(int units)
+	public LocationCalculator(final int units)
 	{
 		setUnits(units);
 	}
 	
-	public void setUnits(int units)
+	public void setUnits(final int units)
 	{
 		this._units = units;
 	}
@@ -46,55 +46,6 @@ public class LocationCalculator implements ILocationCalculator
 	}
 	
 	
-	protected double getDistance(final double x_begin, final double y_begin, 
-			final double x_end, final double y_end,
-			final double x, final double y)
-	{
-		return new Segment(x_begin, y_begin, x_end, y_end).getDistance(x, y);
-	}
-
-	final class Segment
-	{
-		private double Ox_begin;
-		private double Oy_begin;
-		  
-		private double Ox_end;
-		private double Oy_end;
-		 
-		Segment(final double x_begin, final double y_begin, 
-				final double x_end, final double y_end)
-		{
-		    this.Ox_begin = x_begin;
-		    this.Oy_begin = y_begin;
-		    
-		    this.Ox_end = x_end;
-		    this.Oy_end = y_end;		    
-		}
-		
-		/**
-		 * Returns distance from the Point(x, y) to the segment.
-		 * @param x
-		 * @param y
-		 * @return
-		 */
-		double getDistance(double x, double y)
-		{
-		    double a = Math.sqrt(Math.pow((Ox_begin - Ox_end), 2.0) + Math.pow((Oy_begin - Oy_end), 2.0));
-		    double b = Math.sqrt(Math.pow((Ox_begin - x), 2.0) + Math.pow((Oy_begin - y), 2.0));
-		    double c = Math.sqrt(Math.pow((x - Ox_end), 2.0) + Math.pow((y - Oy_end), 2.0));
-		    return (a + b + c)/2;
-		} 
-	}
-	
-	//TODO: unit tests
-	public static void main(String[] args)
-	{
-		LocationCalculator calc = new LocationCalculator();
-		System.out.println(calc.getDistance(0.82, 1.28, 4.7, 5.14, 4.8, 2.42));
-		
-		System.out.println(calc.getDistance(1.7, 2.8, 3.5, 24.2, 10, 10));
-	}
-	
 	static public final class LocationTest extends junit.framework.TestCase
 	{
 		WorldLocation start, end, watch;
@@ -102,12 +53,17 @@ public class LocationCalculator implements ILocationCalculator
 		public final void setUp()
 		{
 			// set the earth model we are expecting
-			MWC.GenericData.WorldLocation.setModel(new MWC.Algorithms.EarthModels.FlatEarth());
+			MWC.GenericData.WorldLocation.setModel(new CompletelyFlatEarth());
 
 			start = new WorldLocation(12.3, 12.4, 12.5);
 			end = new WorldLocation(12.3, 12.4, 12.5);			
 			watch = new WorldLocation(13.3, 12.4, 12.5);
-	
+			
+		}
+		
+		public void testX()
+		{
+			
 		}
 		
 	}
