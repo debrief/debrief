fd13726d787bf04f836fd04a2da5e7361cace1b7
Dariak <dashaa@equinox.(none)>
2013-10-28 12:52:25 +0700

    Removed redundant method getAngle; implemented getDistance in world coords

--- trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/ILocationCalculator.java
+++ trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/ILocationCalculator.java
@@ -5,12 +5,7 @@ import MWC.GenericData.Watchable;
 
 public interface ILocationCalculator 
 {
-	/**
-	 * Returns the angle between the two lines:
-	 * (line_start, line_end) and (line_end, watchable_location)
-	 */
-	public double getAngle(final LineShape line, final Watchable watchable);
-	
+
 	/**
 	 * Returns the distance between 
 	 * perpendicular projection of the watchable to the line
