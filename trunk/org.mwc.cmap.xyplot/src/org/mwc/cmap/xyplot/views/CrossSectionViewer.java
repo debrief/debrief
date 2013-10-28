9b3ab7379242429c270cf4c31891bcdf085a9b7e
Dariak <dashaa@equinox.(none)>
2013-10-28 12:40:30 +0700

    Started to implement CrossSectionView: introduced ILocationCalculator interface

---
+++ trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/CrossSectionViewer.java
@@ -0,0 +1,47 @@
+package org.mwc.cmap.xyplot.views;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.swt.widgets.Composite;
+
+public class CrossSectionViewer implements ISelectionProvider
+{
+	
+	protected CrossSectionViewer(Composite parent)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void addSelectionChangedListener(ISelectionChangedListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ISelection getSelection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeSelectionChangedListener(
+			ISelectionChangedListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSelection(ISelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void setFocus()
+    {
+		// TODO Auto-generated method stub
+    }
+    
+
+}
