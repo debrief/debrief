3070bf22c76778b406d5b30ccaae5879d00b3fff
Dariak <dashaa@equinox.(none)>
2013-11-24 10:57:39 +0700

    Changed series loop; fixed unit test

--- trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/XYPlotUtilities.java
+++ trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/XYPlotUtilities.java
@@ -30,46 +30,22 @@ public class XYPlotUtilities
 			final TimeSeriesCollection dataset)
 	{
 		StringBuffer dataStr = new StringBuffer();
-		dataStr.append(plotTitle);
-		dataStr.append(", ");
-		for (int j = 0; j < dataset.getSeriesCount(); j++) 
-		{	
-			final TimeSeries series = dataset.getSeries(dataset.getSeriesKey(j));
-			dataStr.append(series.getKey().toString());
-			dataStr.append(", ");
-		}
-		int len = dataStr.toString().length();
-		dataStr.delete(len-2, len-1);
-		dataStr.append("\n");
 		
-		int itemsCount = Integer.MAX_VALUE;
-		for (int i = 0; i < dataset.getSeriesCount(); i++) 
+		for(int i = 0; i < dataset.getSeriesCount(); i++)
 		{
 			final TimeSeries series = dataset.getSeries(dataset.getSeriesKey(i));
-			final int items = series.getItemCount();
-			if (items < itemsCount)
-				itemsCount = items;
-		}
-		
-		for(int i = 0; i < itemsCount; i++)
-		{
-			RegularTimePeriod period = null;
-			for (int j = 0; j < dataset.getSeriesCount(); j++) 
-			{	
-				final TimeSeries series = dataset.getSeries(dataset.getSeriesKey(j));
-				if (period == null)
-				{
-					period = series.getTimePeriod(i);
-					dataStr.append(df.format(period.getStart()));
-					dataStr.append(", ");
-				}
-				dataStr.append(series.getDataItem(i).getValue());
-				dataStr.append(", ");
-			}
-			period = null;
-			len = dataStr.toString().length();
-			dataStr.delete(len-2, len-1);
+			dataStr.append(plotTitle);
+			dataStr.append(", ");
+			dataStr.append(series.getKey().toString());
 			dataStr.append("\n");
+			
+			for (int j = 0; j < series.getItemCount(); j++) 
+			{	
+				dataStr.append(df.format(series.getTimePeriod(j).getStart()));
+				dataStr.append(", ");				
+				dataStr.append(series.getDataItem(j).getValue());
+				dataStr.append("\n");
+			}			
 		}
 		return dataStr.toString();
 	}
@@ -103,20 +79,23 @@ public class XYPlotUtilities
 			final TimeSeriesCollection dataset = new TimeSeriesCollection();
 			final TimeSeries series1 = new TimeSeries("name1");
 			Date time1 = new Date();
-			Date time2 = new Date(time1.getTime() + 5000);
-			series1.add(new FixedMillisecond((long) (time1.getTime() / 1000d)), 1.0);
-			series1.add(new FixedMillisecond((long) (time2.getTime() / 1000d)), 2.0);
+			Date time2 = new Date(time1.getTime() + 60000);
+			series1.add(new FixedMillisecond((long) (time1.getTime() / 1d)), 1.0);
+			series1.add(new FixedMillisecond((long) (time2.getTime() / 1d)), 2.0);
 			final TimeSeries series2 = new TimeSeries("name2");
-			series2.add(new FixedMillisecond((long) (time1.getTime() / 1000d)), 3.0);
-			series2.add(new FixedMillisecond((long) (time2.getTime() / 1000d)), 4.0);
+			series2.add(new FixedMillisecond((long) (time1.getTime() / 1d)), 3.0);
+			series2.add(new FixedMillisecond((long) (time2.getTime() / 1d)), 4.0);
 			dataset.addSeries(series1);
 			dataset.addSeries(series2);
 			
 			String result = XYPlotUtilities.textMatrix("test", dataset);
 			System.out.println(result);
-			String expected = "test, name1 name2 \n" + 
-					"17/jan/1970 06:43, 1.0, 3.0 \n" +  
-					"17/jan/1970 06:43, 2.0, 4.0 \n";
+			String expected = "test, name1\n" + 
+					df.format(time1) + ", 1.0\n" +  
+					df.format(time2) + ", 2.0\n" +
+					"test, name2\n" + 
+					df.format(time1) + ", 3.0\n" +  
+					df.format(time2) +", 4.0\n";
 			assertEquals(expected, result);
 		}
 	}
