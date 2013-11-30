08cff10a1a8bd3d2c56ca563b56adb0a71d4e4d0
Dariak <dashaa@equinox.(none)>
2013-11-30 23:04:57 +0700

    Fixed test case and the distance calculation

--- trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/LocationCalculator.java
+++ trunk/org.mwc.cmap.xyplot/src/org/mwc/cmap/xyplot/views/LocationCalculator.java
@@ -42,14 +42,14 @@ public class LocationCalculator implements ILocationCalculator
 		final double leg = watchableLocation.rangeFrom(start, end).getValueIn(_units);
 		final double hypotenus = new WorldDistance(watchableLocation.subtract(end)).getValueIn(_units);
 		final double angleA = Math.asin(leg / hypotenus);		
-		return hypotenus * Math.cos(Math.PI*angleA/180);
+		return hypotenus * Math.cos(angleA);
 	}
 	
 	
 	static public final class LocationTest extends junit.framework.TestCase
 	{
 		WorldLocation start, end, watch;
-		LocationCalculator calc = new LocationCalculator(WorldDistance.KM);
+		LocationCalculator calc = new LocationCalculator(WorldDistance.MINUTES);
 		
 		@Override
 		public final void setUp()
@@ -58,22 +58,22 @@ public class LocationCalculator implements ILocationCalculator
 			MWC.GenericData.WorldLocation.setModel(new CompletelyFlatEarth());
 
 			start = new WorldLocation(0, 0, 0);
-			end = new WorldLocation(0, 61, 0);			
-			watch = new WorldLocation(0, 1, 0);			
+			end = new WorldLocation(0, 1, 0);			
+			watch = new WorldLocation(1, 0.5, 0);		
 		}
 		
 		public void testGetDistance()
 		{
-			final double lineLenKM = new WorldDistance(end.subtract(start))
-					.getValueIn(WorldDistance.KM);
-			assertEquals(6778.320000000001, lineLenKM);
+			final double lineLen = new WorldDistance(end.subtract(start))
+				.getValueIn(WorldDistance.MINUTES);
+			assertEquals(60.0, lineLen);
+			
+			final double perpendicular = watch.rangeFrom(start, end).getValueIn(WorldDistance.MINUTES);
+			assertEquals(60.0, perpendicular);
 		
-			final double angleA = watch.rangeFrom(end); 
-			assertEquals(60.0, angleA);
-						
 			final double d = calc.getDistance(start, end, watch);
-			assertEquals(6667.200000000001, d);
-		}
+			assertEquals(30.0, d, 0.00001 /* epsilon */);
+		}	
 		
 	}
 }
