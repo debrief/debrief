25b257a8a1cc0da9e1c40d69b871cfe3696aac3b
Ian Mayo <ian@planetmayo.com>
2014-05-09 13:41:53 +0100

    show a bit of UI

--- trunk/org.mwc.debrief.satc.core/src/com/planetmayo/debrief/satc_rcp/ui/contributions/CompositeStraightLegForecastContributionView.java
+++ trunk/org.mwc.debrief.satc.core/src/com/planetmayo/debrief/satc_rcp/ui/contributions/CompositeStraightLegForecastContributionView.java
@@ -1,10 +1,16 @@
 package com.planetmayo.debrief.satc_rcp.ui.contributions;
 
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Text;
 
 import com.planetmayo.debrief.satc.model.contributions.CompositeStraightLegForecastContribution;
 import com.planetmayo.debrief.satc.model.generator.IContributions;
+import com.planetmayo.debrief.satc_rcp.ui.UIUtils;
 
 public class CompositeStraightLegForecastContributionView extends
 		BaseContributionView<CompositeStraightLegForecastContribution>
@@ -17,6 +23,52 @@ public class CompositeStraightLegForecastContributionView extends
 		super(parent, contribution, contributions);
 		initUI();
 	}
+
+	
+	
+	@SuppressWarnings("unused")
+	@Override
+	protected void initUI()
+	{
+		// do the parent bits now
+		super.initUI();
+		
+		Group others = new Group(bodyGroup, SWT.SHADOW_ETCHED_IN);
+		others.setLayout(new FillLayout(SWT.VERTICAL));
+		others.setText("Others");
+		
+		// add the speed controls
+		RowLayout speedLayout = new RowLayout();
+		Group speed = new Group(others, SWT.SHADOW_ETCHED_IN);
+		speed.setLayout(speedLayout);
+		speed.setText("Speed");
+		UIUtils.createLabel(speed, "Min:", null);
+		UIUtils.createSpacer(speed, null);
+		Text minSpeed = new Text(speed, SWT.BORDER);
+		UIUtils.createLabel(speed, "Max:",null);
+		UIUtils.createSpacer(speed,null);
+		Text maxSpeed = new Text(speed, SWT.BORDER);
+		UIUtils.createLabel(speed, "Estimate:",null);
+		UIUtils.createSpacer(speed,null);
+		Text speedEstimate = new Text(speed, SWT.BORDER);
+
+		// now add the course
+		RowLayout layout = new RowLayout();
+		Group course = new Group(others, SWT.SHADOW_ETCHED_IN);
+		course.setLayout(layout);
+		course.setText("Course");
+		UIUtils.createLabel(course, "Min:", null);
+		UIUtils.createSpacer(course, null);
+		Text minCrse = new Text(course, SWT.BORDER);
+		UIUtils.createLabel(course, "Max:",null);
+		UIUtils.createSpacer(course,null);
+		Text maxCrse = new Text(course, SWT.BORDER);
+		UIUtils.createLabel(course, "Estimate:",null);
+		UIUtils.createSpacer(course,null);
+		Text crseEstimate = new Text(course, SWT.BORDER);
+	}
+
+
 
 	@Override
 	protected void createLimitAndEstimateSliders()
