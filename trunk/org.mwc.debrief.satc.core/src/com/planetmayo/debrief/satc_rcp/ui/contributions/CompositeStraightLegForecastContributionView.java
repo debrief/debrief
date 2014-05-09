11d9c6d77f746e39f996c8020f3a075e7cf607ee
Haris Peco <snpe@snpe.rs>
2014-05-09 23:11:29 +0200

    Issue #595 - Introduce Composite Straight Leg that allows Course/Speed to be specified - UI

--- trunk/org.mwc.debrief.satc.core/src/com/planetmayo/debrief/satc_rcp/ui/contributions/CompositeStraightLegForecastContributionView.java
+++ trunk/org.mwc.debrief.satc.core/src/com/planetmayo/debrief/satc_rcp/ui/contributions/CompositeStraightLegForecastContributionView.java
@@ -2,10 +2,9 @@ package com.planetmayo.debrief.satc_rcp.ui.contributions;
 
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
 
 import com.planetmayo.debrief.satc.model.contributions.CompositeStraightLegForecastContribution;
@@ -23,49 +22,52 @@ public class CompositeStraightLegForecastContributionView extends
 		super(parent, contribution, contributions);
 		initUI();
 	}
-
 	
-	
-	@SuppressWarnings("unused")
 	@Override
 	protected void initUI()
 	{
 		// do the parent bits now
 		super.initUI();
+				
+		UIUtils.createLabel(bodyGroup, "Speed:", new GridData(70, SWT.DEFAULT));
+		UIUtils.createSpacer(bodyGroup, new GridData(95, SWT.DEFAULT));
 		
-		Group others = new Group(bodyGroup, SWT.SHADOW_ETCHED_IN);
-		others.setLayout(new FillLayout(SWT.VERTICAL));
-		others.setText("Others");
-		
-		// add the speed controls
-		RowLayout speedLayout = new RowLayout();
-		Group speed = new Group(others, SWT.SHADOW_ETCHED_IN);
-		speed.setLayout(speedLayout);
-		speed.setText("Speed");
+		// add the speed
+		Composite speed = new Composite(bodyGroup, SWT.NONE);
+		speed.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		speed.setLayout(new GridLayout(9, false));
 		UIUtils.createLabel(speed, "Min:", null);
 		UIUtils.createSpacer(speed, null);
 		Text minSpeed = new Text(speed, SWT.BORDER);
+		minSpeed.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		UIUtils.createLabel(speed, "Max:",null);
 		UIUtils.createSpacer(speed,null);
 		Text maxSpeed = new Text(speed, SWT.BORDER);
+		maxSpeed.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		UIUtils.createLabel(speed, "Estimate:",null);
 		UIUtils.createSpacer(speed,null);
 		Text speedEstimate = new Text(speed, SWT.BORDER);
+		speedEstimate.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 
 		// now add the course
-		RowLayout layout = new RowLayout();
-		Group course = new Group(others, SWT.SHADOW_ETCHED_IN);
-		course.setLayout(layout);
-		course.setText("Course");
+		UIUtils.createLabel(bodyGroup, "Course:", new GridData(70, SWT.DEFAULT));
+		UIUtils.createSpacer(bodyGroup, new GridData(95, SWT.DEFAULT));
+		
+		Composite course = new Composite(bodyGroup, SWT.NONE);
+		course.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		course.setLayout(new GridLayout(9, false));
 		UIUtils.createLabel(course, "Min:", null);
 		UIUtils.createSpacer(course, null);
 		Text minCrse = new Text(course, SWT.BORDER);
+		minCrse.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		UIUtils.createLabel(course, "Max:",null);
 		UIUtils.createSpacer(course,null);
 		Text maxCrse = new Text(course, SWT.BORDER);
+		maxCrse.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		UIUtils.createLabel(course, "Estimate:",null);
 		UIUtils.createSpacer(course,null);
 		Text crseEstimate = new Text(course, SWT.BORDER);
+		crseEstimate.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 	}
 
 
