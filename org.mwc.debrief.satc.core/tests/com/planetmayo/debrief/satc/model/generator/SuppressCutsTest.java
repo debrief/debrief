697d4afbce518611132e4ca3e4fa383541d16b97
Ian Mayo <ian@planetmayo.com>
2014-06-13 13:56:06 +0100

    introduce tests for cut suppression

---
+++ org.mwc.debrief.satc.core/tests/com/planetmayo/debrief/satc/model/generator/SuppressCutsTest.java
@@ -0,0 +1,137 @@
+/**
+ * 
+ */
+package com.planetmayo.debrief.satc.model.generator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.planetmayo.debrief.satc.model.contributions.BearingMeasurementContribution;
+import com.planetmayo.debrief.satc.model.contributions.CourseAnalysisContribution;
+import com.planetmayo.debrief.satc.model.contributions.CourseForecastContribution;
+import com.planetmayo.debrief.satc.model.contributions.LocationAnalysisContribution;
+import com.planetmayo.debrief.satc.model.contributions.StraightLegForecastContribution;
+import com.planetmayo.debrief.satc.model.generator.impl.AbstractSolutionGenerator;
+import com.planetmayo.debrief.satc.model.generator.impl.BoundsManager;
+import com.planetmayo.debrief.satc.model.generator.impl.Contributions;
+import com.planetmayo.debrief.satc.model.states.BoundedState;
+import com.planetmayo.debrief.satc.model.states.ProblemSpace;
+import com.planetmayo.debrief.satc.support.TestSupport;
+
+/**
+ * @author ian
+ *
+ */
+public class SuppressCutsTest
+{
+
+	private BoundsManager boundsManager;
+	private BearingMeasurementContribution bearingMeasurementContribution;
+	private CourseForecastContribution courseForecastContribution;
+	private ProblemSpace space;
+
+	@SuppressWarnings("deprecation")
+	final Date start1 = new Date(110, 0, 12, 12, 15, 0);
+	@SuppressWarnings("deprecation")
+	final Date end1 = new Date(110, 0, 12, 12, 18, 0);
+	@SuppressWarnings("deprecation")
+	final Date start3 = new Date(110, 0, 12, 12, 42, 0);
+	@SuppressWarnings("deprecation")
+	final Date end3 = new Date(110, 0, 12, 12, 56, 0);
+
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@SuppressWarnings("deprecation")
+	@Before
+	public void setUp() throws Exception
+	{
+		IContributions contributions = new Contributions();
+		space = new ProblemSpace();
+		boundsManager = new BoundsManager(contributions, space);
+
+		bearingMeasurementContribution = new BearingMeasurementContribution();
+		bearingMeasurementContribution.loadFrom(TestSupport.getLongData());
+		bearingMeasurementContribution.setAutoDetect(false);
+		contributions.addContribution(bearingMeasurementContribution);
+
+		courseForecastContribution = new CourseForecastContribution();
+		courseForecastContribution.setStartDate(start1);
+		courseForecastContribution.setFinishDate(new Date(110, 0, 12, 12, 20, 0));
+		courseForecastContribution.setMinCourse(Math.toRadians(110d));
+		courseForecastContribution.setMaxCourse(Math.toRadians(300d));
+		contributions.addContribution(courseForecastContribution);
+
+		StraightLegForecastContribution sl = new StraightLegForecastContribution();
+		sl.setStartDate(start1);
+		sl.setFinishDate(end1);
+		sl.setName("Straight leg 1");
+		contributions.addContribution(sl);
+
+		sl = new StraightLegForecastContribution();
+		sl.setStartDate(new Date(110, 0, 12, 12, 25, 0));
+		sl.setFinishDate(new Date(110, 0, 12, 12, 34, 0));
+		sl.setName("Straight leg 2");
+		contributions.addContribution(sl);
+
+		sl = new StraightLegForecastContribution();
+		sl.setStartDate(start3);
+		sl.setFinishDate(end3);
+		sl.setName("Straight leg 3");
+		contributions.addContribution(sl);
+
+		contributions.addContribution(new LocationAnalysisContribution());
+		contributions.addContribution(new CourseAnalysisContribution());
+		
+		// run the constraints
+		boundsManager.run();
+	}
+
+	@Test
+	public void testNormalProcessing()
+	{
+		Collection<BoundedState> states = space.states();
+		
+		// check the data is there before-hand
+		assertEquals("got states", 64, states.size());
+		
+    Collection<BoundedState> res = AbstractSolutionGenerator.suppressCuts(states, 32);
+    
+    // and how many are there now?
+		assertEquals("trimmed states", 32, res.size());
+		
+		// check the leg-start/end constraints are still in there
+		assertNotNull("got start of leg 1", space.getBoundedStateAt(start1));
+		assertNotNull("got end of leg 1", space.getBoundedStateAt(end1));
+		assertNotNull("got start of leg 3", space.getBoundedStateAt(start3));
+		assertNotNull("got end of leg 3", space.getBoundedStateAt(end3));
+	}
+
+	@Test
+	public void testTooShort()
+	{
+		Collection<BoundedState> states = space.states();
+		
+		// check the data is there before-hand
+		assertEquals("got states", 64, states.size());
+		
+    Collection<BoundedState> res = AbstractSolutionGenerator.suppressCuts(states, 5);
+    
+    // and how many are there now?
+		assertEquals("trimmed states", 22, res.size());
+		
+		// check the leg-start/end constraints are still in there
+		assertNotNull("got start of leg 1", space.getBoundedStateAt(start1));
+		assertNotNull("got end of leg 1", space.getBoundedStateAt(end1));
+		assertNotNull("got start of leg 3", space.getBoundedStateAt(start3));
+		assertNotNull("got end of leg 3", space.getBoundedStateAt(end3));
+	}
+
+}
