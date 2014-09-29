c961dbe06fbb688c3fb6ff5848a8933ef9f5f7e5
Ian Mayo <ian@planetmayo.com>
2014-09-29 13:47:00 +0100

    pull tests into here.

--- org.mwc.debrief.satc.core/src/com/planetmayo/debrief/satc/model/contributions/Range1959ForecastContribution.java
+++ org.mwc.debrief.satc.core/src/com/planetmayo/debrief/satc/model/contributions/Range1959ForecastContribution.java
@@ -1,5 +1,9 @@
 package com.planetmayo.debrief.satc.model.contributions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -8,9 +12,10 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.math3.stat.regression.SimpleRegression;
+import org.junit.Before;
+import org.junit.Test;
 
 import com.planetmayo.debrief.satc.model.GeoPoint;
-import com.planetmayo.debrief.satc.model.contributions.RangeForecastContribution.ROrigin;
 import com.planetmayo.debrief.satc.model.legs.CoreRoute;
 import com.planetmayo.debrief.satc.model.legs.LegType;
 import com.planetmayo.debrief.satc.model.states.BaseRange.IncompatibleStateException;
@@ -18,6 +23,7 @@ import com.planetmayo.debrief.satc.model.states.BoundedState;
 import com.planetmayo.debrief.satc.model.states.LocationRange;
 import com.planetmayo.debrief.satc.model.states.ProblemSpace;
 import com.planetmayo.debrief.satc.model.states.State;
+import com.planetmayo.debrief.satc.support.TestSupport;
 import com.planetmayo.debrief.satc.util.GeoSupport;
 import com.planetmayo.debrief.satc.util.ObjectUtils;
 import com.planetmayo.debrief.satc.util.calculator.GeodeticCalculator;
@@ -32,8 +38,8 @@ public class Range1959ForecastContribution extends BaseContribution
 	private ArrayList<FrequencyMeasurement> measurements = new ArrayList<FrequencyMeasurement>();
 	public static final String OBSERVATIONS_NUMBER = "numObservations";
 
-	private double _fNought = 150;
-	private double _C = 3000;
+	private double fNought = 150;
+	private double speedSound = 3000;
 
 	private transient Double _range;
 
@@ -185,6 +191,8 @@ public class Range1959ForecastContribution extends BaseContribution
 
 		// sanity check on minR
 		_minR = Math.max(_minR, 100);
+		
+		System.out.println("1959:" + this.getName() + " range:" + (int)_range.doubleValue() + " min:" + (int)_minR.doubleValue() + " max:" + (int)_maxR.doubleValue());
 
 		for (BoundedState state : space.getBoundedStatesBetween(startDate,
 				finishDate))
@@ -274,38 +282,6 @@ public class Range1959ForecastContribution extends BaseContribution
 		return bestM;
 	}
 
-	private void applyConstraint(ProblemSpace space, Point pt, double minRange,
-			double maxRange, Date time) throws IncompatibleStateException
-	{
-		// yes, ok we can centre our donut on that
-		LinearRing outer = getRing(pt, minRange);
-		LinearRing inner = getRing(pt, maxRange);
-		LinearRing[] holes = new LinearRing[]
-		{ inner };
-
-		// and create a polygon for it.
-		Polygon thePoly = GeoSupport.getFactory().createPolygon(outer, holes);
-
-		System.out.println("1959:" + thePoly);
-
-		// create a LocationRange for the poly
-		// now define the polygon
-		final LocationRange myRa = new LocationRange(thePoly);
-
-		// is there already a bounded state at this time?
-		BoundedState thisS = space.getBoundedStateAt(time);
-
-		if (thisS == null)
-		{
-			// nope, better create it
-			thisS = new BoundedState(time);
-			space.add(thisS);
-		}
-
-		// apply the range
-		thisS.constrainTo(myRa);
-	}
-
 	private LinearRing getRing(Point pt, double rangeM)
 	{
 		// ok, now we create the outer circle
@@ -314,7 +290,7 @@ public class Range1959ForecastContribution extends BaseContribution
 
 	public double calcRDotKts(double rDotDotHz)
 	{
-		return rDotDotHz * _C / _fNought;
+		return rDotDotHz * speedSound / fNought;
 	}
 
 	public double calculateRange(double rDotDotKts, double bDot)
@@ -358,6 +334,8 @@ public class Range1959ForecastContribution extends BaseContribution
 		ArrayList<Double> values = new ArrayList<Double>();
 		ArrayList<Double> times = new ArrayList<Double>();
 
+		Long firstTime = null;
+		
 		Iterator<FrequencyMeasurement> iter = measurements.iterator();
 		while (iter.hasNext())
 		{
@@ -365,7 +343,13 @@ public class Range1959ForecastContribution extends BaseContribution
 					.next();
 			values.add(frequencyMeasurement.getFrequency());
 			long millis = frequencyMeasurement.getDate().getTime();
-			double mins = millis / 1000 / 60;
+
+			if(firstTime == null)
+			{
+				firstTime = millis;
+			}
+			
+			double mins = (millis - firstTime) / 1000d / 60;
 			times.add(mins);
 		}
 
@@ -466,12 +450,12 @@ public class Range1959ForecastContribution extends BaseContribution
 
 	public double getfNought()
 	{
-		return _fNought;
+		return fNought;
 	}
 
 	public void setfNought(double fNought)
 	{
-		this._fNought = fNought;
+		this.fNought = fNought;
 	}
 
 	public void loadFrom(List<String> lines)
@@ -547,5 +531,126 @@ public class Range1959ForecastContribution extends BaseContribution
 			addMeasurement(measure);
 
 		}
+	}
+	
+	/////////////////////////////////////////
+	
+	protected static class Range1959ForecastContributionTest
+	{
+
+		private Range1959ForecastContribution _freq;
+
+		@Before
+		public void setUp() throws Exception
+		{
+			_freq = new Range1959ForecastContribution();
+		}
+		
+		@Test
+		public void testLoadFromOne() throws Exception
+		{
+			assertFalse("should be empty", _freq.hasData());
+
+			_freq.loadFrom(TestSupport.getFreqDataOne());
+			assertTrue("should not be empty", _freq.hasData());
+			assertEquals("correct start date", new Date(110, 00, 12, 13, 10, 00),
+					_freq.getStartDate());
+			assertEquals("correct finish date", new Date(110, 00, 12, 13, 20, 00),
+					_freq.getFinishDate());
+
+			// and apply it to a problem space
+			double rDotDotHz = _freq.calculateFreqRate();
+			assertEquals("correct freq", -0.0239, rDotDotHz, 0.0001);
+			ProblemSpace space = new ProblemSpace();
+			// and add the bearings
+			addBearing(space, "100112", "131000", -99.53);
+			addBearing(space, "100112", "131050", -97.18);
+			addBearing(space, "100112", "131140", -94.8);
+			addBearing(space, "100112", "131230", -92.42);
+			addBearing(space, "100112", "131320", -90.04);
+			addBearing(space, "100112", "131410", -87.66);
+			addBearing(space, "100112", "131500", -85.3);
+			addBearing(space, "100112", "131550", -82.96);
+			addBearing(space, "100112", "131640", -80.65);
+			addBearing(space, "100112", "131730", -78.37);
+			addBearing(space, "100112", "131820", -76.14);
+			addBearing(space, "100112", "131910", -73.95);
+			addBearing(space, "100112", "132000", -71.82);
+
+			double rDotDotKts = _freq.calcRDotKts(rDotDotHz);
+			double bDot = _freq.calculateBearingRate(space);
+			double range = _freq.calculateRange(rDotDotKts, bDot);
+			assertEquals("correct rDotDotHz", -0.0239763, rDotDotHz, 0.000001);
+			assertEquals("correct rDotDotKts", -0.47952, rDotDotKts, 0.00001);
+			assertEquals("correct bDot", 2.7869, bDot,
+					0.00001);
+			assertEquals("correct range", -6.8439, range, 0.001);
+			
+			// ok, now check that the new bound is generated
+			_freq.actUpon(space);
+			
+			// check the new constraint is in there.
+		}
+		
+		@Test
+		public void testMidPointDerivation()
+		{
+			_freq.loadFrom(TestSupport.getFreqDataOne());
+			FrequencyMeasurement thisM = _freq.getMidWayPoint();
+			assertEquals("found mid way", "12 Jan 2010 13:15:00 GMT", thisM.getDate().toGMTString());
+			_freq.clear();
+			
+			_freq.loadFrom(TestSupport.getFreqDataTwo());
+			thisM = _freq.getMidWayPoint();
+			assertEquals("found mid way", "12 Jan 2010 13:37:30 GMT", thisM.getDate().toGMTString());
+
+		}
+
+		private void addBearing(ProblemSpace space, String date, String time,
+				double bearingDegs) throws IncompatibleStateException
+		{
+			Date theDate = ObjectUtils.safeParseDate(new SimpleDateFormat(
+					"yyMMdd HHmmss"), date + " " + time);
+
+			BoundedState state = new BoundedState(theDate);
+			state.setBearingValue(Math.toRadians(bearingDegs));
+			space.add(state);
+		}
+
+		@Test
+		public void testLoadFromTwo() throws Exception
+		{
+			assertFalse("should be empty", _freq.hasData());
+
+			_freq.loadFrom(TestSupport.getFreqDataTwo());
+			assertTrue("should not be empty", _freq.hasData());
+			assertEquals("correct start date", new Date(110, 00, 12, 13, 35, 00),
+					_freq.getStartDate());
+			assertEquals("correct finish date", new Date(110, 00, 12, 13, 40, 00),
+					_freq.getFinishDate());
+
+			// and apply it to a problem space
+			double rDotDotHz = _freq.calculateFreqRate();
+			ProblemSpace space = new ProblemSpace();
+			// and add the bearings
+			addBearing(space, "100112", "133500", -48.41);
+			addBearing(space, "100112", "133550", -47.79);
+			addBearing(space, "100112", "133640", -47.19);
+			addBearing(space, "100112", "133730", -46.6);
+			addBearing(space, "100112", "133820", -46.03);
+			addBearing(space, "100112", "133910", -45.48);
+			addBearing(space, "100112", "134000", -44.94);
+			addBearing(space, "100112", "134050", -44.42);
+
+			double rDotDotKts = _freq.calcRDotKts(rDotDotHz);
+			double bDot = _freq.calculateBearingRate(space);
+			double range = _freq.calculateRange(rDotDotKts, bDot);
+			assertEquals("correct freq", -0.001943, rDotDotHz, 0.00001);
+			assertEquals("correct rDotDotKts", -0.03887, rDotDotKts, 0.0001);
+			assertEquals("correct bDot", 0.69385, bDot,
+					0.00001);
+			assertEquals("correct range", -8.95088, range, 0.00001);
+		}
+
 	}
 }
