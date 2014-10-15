e999c11a92713dd8e7c8e27c06dcc165904f0cc8
Ian Mayo <ian@planetmayo.com>
2014-10-15 12:44:15 +0100

    remove stray lines

--- org.mwc.asset.legacy/src/ASSET/Models/Mediums/Optic.java
+++ org.mwc.asset.legacy/src/ASSET/Models/Mediums/Optic.java
@@ -7,12 +7,10 @@
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the Eclipse Public License v1.0
  *    (http://www.eclipse.org/legal/epl-v10.html)
-
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
  */
 package ASSET.Models.Mediums;
 
