f79c661d38a3050b576e8e6045a0da87b633ab39
Ian Mayo <ian@planetmayo.com>
2014-10-15 12:32:49 +0100

    line three

--- org.mwc.cmap.legacy/src/MWC/GUI/Swing/Spinner/SpinEvent.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/Swing/Spinner/SpinEvent.java
@@ -7,7 +7,7 @@
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the Eclipse Public License v1.0
  *    (http://www.eclipse.org/legal/epl-v10.html)
- *    version 2.1 of the License.
+
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
