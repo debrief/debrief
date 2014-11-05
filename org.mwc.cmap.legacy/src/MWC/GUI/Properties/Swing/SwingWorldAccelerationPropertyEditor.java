7baf8ce9a708c852bb2364625ac98c56af4ea80c
Ian Mayo <ian@planetmayo.com>
2014-11-05 15:17:06 +0000

    avoid masking the parent object

--- org.mwc.cmap.legacy/src/MWC/GUI/Properties/Swing/SwingWorldAccelerationPropertyEditor.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/Properties/Swing/SwingWorldAccelerationPropertyEditor.java
@@ -108,11 +108,6 @@ public class SwingWorldAccelerationPropertyEditor extends
    */
   protected java.beans.PropertyChangeSupport _pSupport = new java.beans.PropertyChangeSupport(this);
 
-  /**
-   * the amount of columns the users wants us to create
-   */
-  protected int _numColumns = 3;
-
   /////////////////////////////////////////////////////////////
   // constructor
   ////////////////////////////////////////////////////////////
