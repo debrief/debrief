92f65cdc8e1b1bc32b005e88f3ccb0af3520f705
Ian Mayo <ian@planetmayo.com>
2014-12-15 13:06:37 +0000

    introduce some basic class objects

---
+++ org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/model/NEPolygonLayer.java
@@ -0,0 +1,6 @@
+package org.mwc.cmap.naturalearth.model;
+
+public class NEPolygonLayer
+{
+
+}
---
+++ org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/wrapper/NELayer.java
@@ -0,0 +1,29 @@
+package org.mwc.cmap.naturalearth.wrapper;
+
+import org.mwc.cmap.naturalearth.view.NEStyle;
+
+import MWC.GUI.BaseLayer;
+
+public class NELayer extends BaseLayer
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public NELayer(NEStyle styles)
+	{
+		
+	}
+	
+	/** switch to the supplied style set
+	 * 
+	 * @param styles
+	 */
+	public void applyLayer(NEStyle newStyle)
+	{
+		
+	}
+	
+}
