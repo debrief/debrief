ceef0af95e66e9e1b042aafa385f40ddbcb75c5d
Ian Mayo <ian@planetmayo.com>
2014-12-15 13:06:37 +0000

    introduce some basic class objects

---
+++ org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/view/NEFeatureStyle.java
@@ -0,0 +1,15 @@
+package org.mwc.cmap.naturalearth.view;
+
+public class NEFeatureStyle
+{
+	
+	/** the NE filename that this style applies to
+	 * 
+	 * @return
+	 */
+	public String getFileName()
+	{
+		return "pending";
+	}
+
+}
