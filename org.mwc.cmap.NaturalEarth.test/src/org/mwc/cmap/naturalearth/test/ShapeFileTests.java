c45241ad9dca3d0943aad67205de8b82fb6a0354
Ian Mayo <ian@planetmayo.com>
2014-12-16 07:12:09 +0000

    simplify tests

--- org.mwc.cmap.NaturalEarth.test/src/org/mwc/cmap/naturalearth/test/ShapeFileTests.java
+++ org.mwc.cmap.NaturalEarth.test/src/org/mwc/cmap/naturalearth/test/ShapeFileTests.java
@@ -1,7 +1,7 @@
 package org.mwc.cmap.naturalearth.test;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mwc.cmap.naturalearth.wrapper.CachedShapefile;
+import org.mwc.cmap.gt2plot.data.CachedNauticalEarthFile;
 
 import MWC.GenericData.WorldLocation;
 import MWC.GenericData.WorldPath;
@@ -25,220 +25,64 @@ import com.bbn.openmap.layer.shape.ShapeFile;
 public class ShapeFileTests
 {
 
-	private static File coastline;
+	private static File boundaryLines;
 	private static File marineAreas;
+	private static File pointNames;
 
 	@BeforeClass
 	public static void setUp() throws Exception
 	{
-		// Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
-		// URL fileURL = FileLocator.resolve(
-		// bundle.getEntry());
-		// String zipLocation = fileURL.getFile();
-		coastline = new File("data/ne_50m_coastline/ne_50m_coastline.shp");
+		boundaryLines = new File("data/ne_10m_admin_0_boundary_lines_land/ne_10m_admin_0_boundary_lines_land.shp");
 		marineAreas = new File("data/ne_10m_geography_marine_polys/ne_10m_geography_marine_polys.shp");
-		// String dest = Platform.getConfigurationLocation().getURL().getFile();
-		// destination = new File(dest, "data");
-		// extract(file, destination, new NullProgressMonitor());
+		pointNames = new File("data/ne_10m_geography_regions_points/ne_10m_geography_regions_points.shp");
 	}
 
 	@Test
-	public void loadMultiFormatFile()
+	public void loadPolygons()
 	{
 		// check we have the marine areas
 		assertTrue(marineAreas.getAbsolutePath() + " doesn't exists.", marineAreas.isFile());
 		 
 		// ok, load it 
-		CachedShapefile csf = new CachedShapefile(marineAreas.getAbsolutePath());
+		CachedNauticalEarthFile csf = new CachedNauticalEarthFile(marineAreas.getAbsolutePath());
 		
 		assertNotNull("found polygons", csf.getPolygons());
-		assertEquals("loaded polygons", 1472, csf.getPolygons().size());
+		assertEquals("loaded polygons", 312, csf.getPolygons().size());
+	}
+	
+	@Test
+	public void loadPoints()
+	{
+		// check we have the marine areas
+		assertTrue(pointNames.getAbsolutePath() + " doesn't exists.", pointNames.isFile());
+		 
+		// ok, load it 
+		CachedNauticalEarthFile csf = new CachedNauticalEarthFile(pointNames.getAbsolutePath());
+		
+		assertNotNull("found points", csf.getPoints());
+		assertEquals("loaded points", 235, csf.getPoints().size());
+	}
+
+	
+	@Test
+	public void loadLines()
+	{
+		// check we have the marine areas
+		assertTrue(boundaryLines.getAbsolutePath() + " doesn't exists.", boundaryLines.isFile());
+		 
+		// ok, load it 
+		CachedNauticalEarthFile csf = new CachedNauticalEarthFile(boundaryLines.getAbsolutePath());
+		
+		assertNotNull("found lines", csf.getLines());
+		assertEquals("loaded lines", 113, csf.getLines().size());
 	}
 	
 	@Test
 	public void testExists()
 	{
-		assertTrue(coastline.getAbsolutePath() + " doesn't exists.", coastline.isFile());
+		assertTrue(boundaryLines.getAbsolutePath() + " doesn't exists.", boundaryLines.isFile());
 		assertTrue(marineAreas.getAbsolutePath() + " doesn't exists.", marineAreas.isFile());
+		assertTrue(pointNames.getAbsolutePath() + " doesn't exists.", pointNames.isFile());
 	}
-
-	@Test
-	public void testVerify()
-	{
-		File file = coastline;
-		try
-		{
-			ShapeFile sf = new ShapeFile(file);
-			System.out.println("\tmin: " + sf.getBoundingBox().min);
-			System.out.println("\tmax: " + sf.getBoundingBox().max);
-			
-			int nRecords = 0;
-			ESRIRecord record = sf.getNextRecord();
-			while (record != null)
-			{
-				if (record instanceof ESRIPointRecord)
-				{
-					// TODO WorldLocation
-					double lat = ((ESRIPointRecord) record).getY();
-					double lon = ((ESRIPointRecord) record).getX();
-					System.out.println("point record: " + lat + ", " + lon);
-				}
-				else if (record instanceof ESRIPolygonRecord)
-				{
-					ESRIPolygonRecord polygon = (ESRIPolygonRecord) record;
-					ArrayList<WorldPath> polyList = readPolygon(polygon);
-					System.out.println("found " + polyList.size() + " polys");
-				}
-				else
-				{
-					System.out.println("record: " + record.getClass().getName());
-				}
-				nRecords++;
-				record = sf.getNextRecord();
-			}
-			System.out.println("records: " + nRecords);
-		}
-		catch (IOException e)
-		{
-			Activator.log(e);
-			assertTrue("invalid file " + file.getAbsolutePath(), false);
-		}
-	}
-
-	private ArrayList<WorldPath> readPolygon(ESRIPolygonRecord record)
-	{
-		ArrayList<WorldPath> polyList = new ArrayList<WorldPath>();
-
-		ESRIPoly[] polygons = record.polygons;
-		int nPolys = polygons.length;
-		if (nPolys <= 0)
-			return null;		
-
-		for (int i = 0; i < nPolys; i++)
-		{
-			// these points are in RADIAN lat,lon order!...
-			ESRIPoly thisPoly = polygons[i];
-			ESRIFloatPoly floatP = (ESRIFloatPoly) thisPoly;
-			double[] pts = floatP.getRadians();
-			int nPoints = polygons[i].nPoints;
-//			System.out.println("record: " + record.recordNumber);
-//			System.out.println("nPoints: " + nPoints);
-			
-			WorldPath wp = new WorldPath();
-			
-			for (int j = 0; j < pts.length; j++)
-			{
-				double d = pts[j];
-				wp.addPoint(new WorldLocation(pts[j],pts[++j], 0));				
-			}
-			
-			polyList.add(wp);
-			
-			// FIXME WorldPath
-			// System.out.print("points (rad): ");
-			// for (int j = 0; j < pts.length; j++)
-			// {
-			// System.out.print("(" + pts[j] + "," + pts[++j] + ") ");
-			// }
-			// System.out.println();
-			
-		}
-		
-		return polyList;
-	}
-
-
-	//
-	// public static boolean extract(File file, File destination,
-	// IProgressMonitor monitor)
-	// {
-	// ZipFile zipFile = null;
-	// destination.mkdirs();
-	// try
-	// {
-	// zipFile = new ZipFile(file);
-	// Enumeration<? extends ZipEntry> entries = zipFile.entries();
-	// while (entries.hasMoreElements())
-	// {
-	// if (monitor.isCanceled())
-	// {
-	// return false;
-	// }
-	// ZipEntry entry = (ZipEntry) entries.nextElement();
-	// if (entry.isDirectory())
-	// {
-	// monitor.setTaskName("Extracting " + entry.getName());
-	// File dir = new File(destination, entry.getName());
-	// dir.mkdirs();
-	// continue;
-	// }
-	// monitor.setTaskName("Extracting " + entry.getName());
-	// File entryFile = new File(destination, entry.getName());
-	// entryFile.getParentFile().mkdirs();
-	// InputStream input = null;
-	// OutputStream output = null;
-	// try
-	// {
-	// input = zipFile.getInputStream(entry);
-	// output = new FileOutputStream(entryFile);
-	// copyFile(input, output);
-	// }
-	// finally
-	// {
-	// if (input != null)
-	// {
-	// try
-	// {
-	// input.close();
-	// }
-	// catch (Exception e)
-	// {
-	// }
-	// }
-	// if (output != null)
-	// {
-	// try
-	// {
-	// output.close();
-	// }
-	// catch (Exception e)
-	// {
-	// }
-	// }
-	// }
-	// }
-	// }
-	// catch (IOException e)
-	// {
-	// Activator.log(e);
-	// return false;
-	// }
-	// finally
-	// {
-	// if (zipFile != null)
-	// {
-	// try
-	// {
-	// zipFile.close();
-	// }
-	// catch (IOException e)
-	// {
-	// // ignore
-	// }
-	// }
-	// }
-	// return true;
-	// }
-	//
-	// public static void copyFile(InputStream in, OutputStream out)
-	// throws IOException
-	// {
-	// byte[] buffer = new byte[16 * 1024];
-	// int len;
-	// while ((len = in.read(buffer)) >= 0)
-	// {
-	// out.write(buffer, 0, len);
-	// }
-	// }
 
 }
