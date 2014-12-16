87954f59330e6b8f457623ac1077d9da4f15fe25
Ian Mayo <ian@planetmayo.com>
2014-12-16 10:09:00 +0000

    more implementation

--- org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/view/NEFeatureStyle.java
+++ org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/view/NEFeatureStyle.java
@@ -1,6 +1,8 @@
 package org.mwc.cmap.naturalearth.view;
 
-import org.mwc.cmap.naturalearth.wrapper.CachedShapefile;
+import java.awt.Color;
+
+import org.mwc.cmap.gt2plot.data.CachedNauticalEarthFile;
 
 public class NEFeatureStyle
 {
@@ -8,7 +10,23 @@ public class NEFeatureStyle
 	/** the set of data that we will render
 	 * 
 	 */
-	private CachedShapefile _myData = null;
+	private CachedNauticalEarthFile _myData = null;
+	private String _featureType;
+	private String _filename;
+	private Color _lineCol;
+	private Color _fillCol;
+	private Color _textCol;
+	private boolean _isVisible;
+
+	public NEFeatureStyle(String featureType, String filename, Color fillCol,
+			Color lineCol, Color textCol)
+	{
+		_featureType = featureType;
+		_filename = filename;
+		_lineCol = lineCol;
+		_fillCol = fillCol;
+		_textCol = textCol;
+	}
 
 	/** the NE filename that this style applies to
 	 * 
@@ -16,7 +34,7 @@ public class NEFeatureStyle
 	 */
 	public String getFileName()
 	{
-		return "pending";
+		return _filename;
 	}
 
 	/** if this feature is visible
@@ -25,7 +43,12 @@ public class NEFeatureStyle
 	 */
 	public boolean isVisible()
 	{
-		return true;
+		return _isVisible;
+	}
+
+	public void setVisible(boolean visible)
+	{
+		_isVisible = visible;
 	}
 
 	/** whether this feature has loaded its data
@@ -41,9 +64,25 @@ public class NEFeatureStyle
 	 * 
 	 * @param data
 	 */
-	public void setData(CachedShapefile data)
+	public void setData(CachedNauticalEarthFile data)
 	{
 		_myData = data;
+	}
+
+	public Color getLineColor()
+	{
+		return _lineCol;
+	}
+
+	public Color getFillColor()
+	{
+		return _fillCol;
+	}
+
+
+	public Color getTextColor()
+	{
+		return _textCol;
 	}
 
 }
--- org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/wrapper/NELayer.java
+++ org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/wrapper/NELayer.java
@@ -1,8 +1,14 @@
 package org.mwc.cmap.naturalearth.wrapper;
 
+import java.awt.Point;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
 import java.util.Iterator;
 
+import org.eclipse.core.runtime.Status;
+import org.mwc.cmap.gt2plot.data.CachedNauticalEarthFile;
 import org.mwc.cmap.naturalearth.Activator;
 import org.mwc.cmap.naturalearth.model.NEFeature;
 import org.mwc.cmap.naturalearth.model.NEPointLayer;
@@ -12,20 +18,25 @@ import org.mwc.cmap.naturalearth.view.NEStyle;
 
 import MWC.GUI.BaseLayer;
 import MWC.GUI.CanvasType;
+import MWC.GUI.Editable;
+import MWC.GenericData.NamedWorldLocation;
+import MWC.GenericData.NamedWorldPath;
+import MWC.GenericData.NamedWorldPathList;
+import MWC.GenericData.WorldArea;
+import MWC.GenericData.WorldLocation;
+import MWC.GenericData.WorldPath;
 
 public class NELayer extends BaseLayer
 {
-
-	private ArrayList<NEFeature> _features = new ArrayList<NEFeature>();
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public NELayer(NEStyle styles)
+	public NELayer()
 	{
-
+		setName("Natural Earth");
 	}
 
 	/**
@@ -58,77 +69,195 @@ public class NELayer extends BaseLayer
 				while (iter.hasNext())
 				{
 					NEFeatureStyle neFeatureStyle = (NEFeatureStyle) iter.next();
-					NEFeature newF = featureFor(neFeatureStyle);
-
-					_features.add(newF);
+					NEFeature newF = new NEFeature(neFeatureStyle);
+					super.add(newF);
 				}
 			}
 
-			// ok, get painting
-			Iterator<NEFeature> iter = _features.iterator();
-			while (iter.hasNext())
+			// ok, get painting - start off with the polygons
+			Enumeration<Editable> children = super.elements();
+			while (children.hasMoreElements())
 			{
-				NEFeature neFeature = (NEFeature) iter.next();
+				NEFeature neFeature = (NEFeature) children.nextElement();
 				paintThisFeatureType(dest, neFeature);
 			}
 		}
 	}
 
-	/**
-	 * create the correct feature type for this style
-	 * 
-	 * @param neFeatureStyle
-	 * @return
-	 */
-	private NEFeature featureFor(NEFeatureStyle neFeatureStyle)
-	{
-		return new NEPointLayer(neFeatureStyle);
-	}
-
 	private boolean hasLayers()
 	{
-		return _features.size() > 0;
+		return super.size() > 0;
 	}
 
 	private void paintThisFeatureType(CanvasType dest, NEFeature thisF)
 	{
+		// double check if this style should be visible
+		if (!thisF.getStyle().isVisible())
+			return;
+
 		// ok, is it loaded?
 		if (!thisF.isLoaded())
 		{
 			String fName = thisF.getStyle().getFileName();
 
 			// get the datafile
-			CachedShapefile thisData = Activator.getDefault().loadData(fName);
+			CachedNauticalEarthFile thisData = Activator.getDefault().loadData(fName);
 
 			// did we find the shapefile?
 			if (thisData != null)
 			{
-				thisF.setFeature(thisData);
+				thisF.setDataSource(thisData);
 			}
 		}
 
-		// just check we have our data
-		if (thisF.isLoaded())
+		// double-check it has some data
+		if (thisF.getData() != null)
 		{
-//			Iterator<CachedFeature> iter = thisF.iterator();
-//			while (iter.hasNext())
-//			{
-//				CachedFeature cachedFeature = (CachedFeature) iter.next();
-//				paintThisFeature(dest, cachedFeature, thisF.getStyle());
-//			}
-		}
+			// ok, do the load
+			thisF.getData().init();
 
+			// draw the data from the bottom up
+			drawPolygons(dest, thisF.getData().getPolygons(), thisF.getStyle());
+			drawLines(dest, thisF.getData().getLines(), thisF.getStyle());
+			drawPoints(dest, thisF.getData().getPoints(), thisF.getStyle());
+		}
 	}
 
-	private void paintThisFeature(CanvasType dest, CachedFeature cachedFeature,
+	private void drawPolygons(CanvasType dest,
+			ArrayList<NamedWorldPath> polygons, NEFeatureStyle style)
+	{
+		if(polygons == null)
+			return;
+		
+		// store the screen size
+		WorldArea visArea = dest.getProjection().getVisibleDataArea();
+
+		// ok, loop through the polys
+		Iterator<NamedWorldPath> iter = polygons.iterator();
+		while (iter.hasNext())
+		{
+			NamedWorldPath namedWorldPath = (NamedWorldPath) iter.next();
+
+			if (!visArea.overlaps(namedWorldPath.getBounds()))
+			{
+				// ok, skip to the next one
+				continue;
+			}
+
+			dest.setColor(style.getLineColor());
+
+			Collection<WorldLocation> _nodes = namedWorldPath.getPoints();
+
+			// create our point lists
+			final int[] xP = new int[_nodes.size()];
+			final int[] yP = new int[_nodes.size()];
+
+			// ok, step through the area
+			final Iterator<WorldLocation> points = _nodes.iterator();
+
+			int counter = 0;
+
+			while (points.hasNext())
+			{
+				final WorldLocation next = points.next();
+
+				// convert to screen
+				final Point thisP = dest.toScreen(next);
+
+				if ((thisP.x <= 0) || (thisP.y <= 0))
+				{
+					System.err.println("here");
+				}
+				else
+				{
+					// remember the coords
+					xP[counter] = thisP.x;
+					yP[counter] = thisP.y;
+
+					// move the counter
+					counter++;
+				}
+			}
+
+			dest.fillPolygon(xP, yP, counter / 2);
+		}
+	}
+
+	private void drawLines(CanvasType dest, ArrayList<NamedWorldPathList> paths,
 			NEFeatureStyle style)
 	{
-		// ok, this paint is dependent on the feature type, and the styling.
+		if(paths == null)
+			return;
+		
+		dest.setColor(style.getLineColor());
+
+		// ok, loop through the polys
+		Iterator<NamedWorldPathList> iter = paths.iterator();
+		while (iter.hasNext())
+		{
+			NamedWorldPathList next2 = iter.next();
+			Iterator<WorldPath> iter2 = next2.iterator();
+
+			while (iter2.hasNext())
+			{
+				WorldPath namedWorldPath = (WorldPath) iter2.next();
+
+				Collection<WorldLocation> _nodes = namedWorldPath.getPoints();
+
+				// create our point lists
+				final int[] xPoints = new int[2 * _nodes.size()];
+
+				// ok, step through the area
+				final Iterator<WorldLocation> points = _nodes.iterator();
+
+				int counter = 0;
+
+				while (points.hasNext())
+				{
+					final WorldLocation next = points.next();
+
+					// convert to screen
+					final Point thisP = dest.toScreen(next);
+
+					// remember the coords
+					xPoints[counter++] = thisP.x;
+					xPoints[counter++] = thisP.y;
+				}
+
+				dest.drawPolyline(xPoints);
+			}
+		}
+	}
+
+	private void drawPoints(CanvasType dest,
+			ArrayList<NamedWorldLocation> points, NEFeatureStyle style)
+	{
+		if(points == null)
+			return;
+		
+		dest.setColor(style.getTextColor());
+
+		// ok, loop through the polys
+		Iterator<NamedWorldLocation> iter = points.iterator();
+		while (iter.hasNext())
+		{
+			NamedWorldLocation namedLoc = (NamedWorldLocation) iter.next();
+			final WorldLocation next = namedLoc;
+			final Point thisP = dest.toScreen(next);
+			dest.drawText(namedLoc.getName(), thisP.x, thisP.y);
+		}
 	}
 
 	private NEResolution getStyleSetFor(double curScale)
 	{
 		return Activator.getStyleFor(curScale);
+	}
+
+	public static boolean hasGoodPath()
+	{
+		final File dataPath = new File(Activator.getDefault().getLibraryPath());
+
+		return dataPath.exists();
 	}
 
 }
