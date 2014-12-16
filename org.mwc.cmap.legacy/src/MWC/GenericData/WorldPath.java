8d571ecc37d3b1b46f7382ae885673f99da5101e
Ian Mayo <ian@planetmayo.com>
2014-12-16 07:00:33 +0000

    introduce new named data types

--- org.mwc.cmap.legacy/src/MWC/GenericData/WorldPath.java
+++ org.mwc.cmap.legacy/src/MWC/GenericData/WorldPath.java
@@ -27,7 +27,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Vector;
 
-public final class WorldPath implements java.io.Serializable
+public class WorldPath implements java.io.Serializable
 {
 
 
