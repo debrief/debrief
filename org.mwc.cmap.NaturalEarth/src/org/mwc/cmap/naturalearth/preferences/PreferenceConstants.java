4493b4de32ab6ac94d92a4a47439298853323d16
Haris Peco <snpe@snpe.rs>
2014-12-17 04:14:09 +0100

    NE Prefs page - UI

---
+++ org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/preferences/PreferenceConstants.java
@@ -0,0 +1,10 @@
+package org.mwc.cmap.naturalearth.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String DATA_FOLDER = "dataFolder";
+	
+}
---
+++ org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/preferences/PreferenceInitializer.java
@@ -0,0 +1,26 @@
+package org.mwc.cmap.naturalearth.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import org.mwc.cmap.naturalearth.Activator;
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer
+{
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#
+	 * initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences()
+	{
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		store.setDefault(PreferenceConstants.DATA_FOLDER, "");
+	}
+
+}
