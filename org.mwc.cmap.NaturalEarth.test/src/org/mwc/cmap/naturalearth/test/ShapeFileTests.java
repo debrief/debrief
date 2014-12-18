51532d32923b9b8295f68fadcec89db870e440eb
Haris Peco <snpe@snpe.rs>
2014-12-18 14:39:43 +0100

    Issue #1003 - Classpath problem
    - removing geotools from gt2plot and satc.core and using o.m.cmap.geotools

--- org.mwc.cmap.NaturalEarth.test/src/org/mwc/cmap/naturalearth/test/ShapeFileTests.java
+++ org.mwc.cmap.NaturalEarth.test/src/org/mwc/cmap/naturalearth/test/ShapeFileTests.java
@@ -1,10 +1,10 @@
 package org.mwc.cmap.naturalearth.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -23,12 +23,6 @@ import MWC.GenericData.NamedWorldPath;
 import MWC.GenericData.WorldLocation;
 import MWC.GenericData.WorldPath;
 
-import com.bbn.openmap.layer.shape.ESRIPointRecord;
-import com.bbn.openmap.layer.shape.ESRIPoly;
-import com.bbn.openmap.layer.shape.ESRIPoly.ESRIFloatPoly;
-import com.bbn.openmap.layer.shape.ESRIPolygonRecord;
-import com.bbn.openmap.layer.shape.ESRIRecord;
-import com.bbn.openmap.layer.shape.ShapeFile;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.MultiPolygon;
 
@@ -232,91 +226,91 @@ public class ShapeFileTests
 		return res;
 	}
 
-	public void unTestOpenMapPerformance()
-	{
-		File file = landAreas;
-		if (!file.exists())
-			return;
-
-		try
-		{
-			ShapeFile sf = new ShapeFile(file);
-			System.out.println("\tmin: " + sf.getBoundingBox().min);
-			System.out.println("\tmax: " + sf.getBoundingBox().max);
-
-			int nRecords = 0;
-			ESRIRecord record = sf.getNextRecord();
-			while (record != null)
-			{
-				if (record instanceof ESRIPointRecord)
-				{
-					// TODO WorldLocation
-					double lat = ((ESRIPointRecord) record).getY();
-					double lon = ((ESRIPointRecord) record).getX();
-					System.out.println("point record: " + lat + ", " + lon);
-				}
-				else if (record instanceof ESRIPolygonRecord)
-				{
-					ESRIPolygonRecord polygon = (ESRIPolygonRecord) record;
-					ArrayList<WorldPath> polyList = readPolygon(polygon);
-					System.out.println("found " + polyList.size() + " polys");
-				}
-				else
-				{
-					System.out.println("record: " + record.getClass().getName());
-				}
-				nRecords++;
-				record = sf.getNextRecord();
-			}
-			System.out.println("records: " + nRecords);
-		}
-		catch (IOException e)
-		{
-			Activator.log(e);
-			assertTrue("invalid file " + file.getAbsolutePath(), false);
-		}
-	}
-
-	private ArrayList<WorldPath> readPolygon(ESRIPolygonRecord record)
-	{
-		ArrayList<WorldPath> polyList = new ArrayList<WorldPath>();
-
-		ESRIPoly[] polygons = record.polygons;
-		int nPolys = polygons.length;
-		if (nPolys <= 0)
-			return null;
-
-		for (int i = 0; i < nPolys; i++)
-		{
-			// these points are in RADIAN lat,lon order!...
-			ESRIPoly thisPoly = polygons[i];
-			ESRIFloatPoly floatP = (ESRIFloatPoly) thisPoly;
-			double[] pts = floatP.getRadians();
-			int nPoints = polygons[i].nPoints;
-			System.out.println("record: " + record.recordNumber);
-			System.out.println("nPoints: " + nPoints);
-
-			WorldPath wp = new WorldPath();
-
-			for (int j = 0; j < pts.length; j++)
-			{
-				WorldLocation newLoc = new WorldLocation(pts[j], pts[++j], 0);
-				wp.addPoint(newLoc);
-			}
-
-			polyList.add(wp);
-
-			// FIXME WorldPath
-			// System.out.print("points (rad): ");
-			// for (int j = 0; j < pts.length; j++)
-			// {
-			// System.out.print("(" + pts[j] + "," + pts[++j] + ") ");
-			// }
-			// System.out.println();
-
-		}
-
-		return polyList;
-	}
+//	public void unTestOpenMapPerformance()
+//	{
+//		File file = landAreas;
+//		if (!file.exists())
+//			return;
+//
+//		try
+//		{
+//			ShapeFile sf = new ShapeFile(file);
+//			System.out.println("\tmin: " + sf.getBoundingBox().min);
+//			System.out.println("\tmax: " + sf.getBoundingBox().max);
+//
+//			int nRecords = 0;
+//			ESRIRecord record = sf.getNextRecord();
+//			while (record != null)
+//			{
+//				if (record instanceof ESRIPointRecord)
+//				{
+//					// TODO WorldLocation
+//					double lat = ((ESRIPointRecord) record).getY();
+//					double lon = ((ESRIPointRecord) record).getX();
+//					System.out.println("point record: " + lat + ", " + lon);
+//				}
+//				else if (record instanceof ESRIPolygonRecord)
+//				{
+//					ESRIPolygonRecord polygon = (ESRIPolygonRecord) record;
+//					ArrayList<WorldPath> polyList = readPolygon(polygon);
+//					System.out.println("found " + polyList.size() + " polys");
+//				}
+//				else
+//				{
+//					System.out.println("record: " + record.getClass().getName());
+//				}
+//				nRecords++;
+//				record = sf.getNextRecord();
+//			}
+//			System.out.println("records: " + nRecords);
+//		}
+//		catch (IOException e)
+//		{
+//			Activator.log(e);
+//			assertTrue("invalid file " + file.getAbsolutePath(), false);
+//		}
+//	}
+//
+//	private ArrayList<WorldPath> readPolygon(ESRIPolygonRecord record)
+//	{
+//		ArrayList<WorldPath> polyList = new ArrayList<WorldPath>();
+//
+//		ESRIPoly[] polygons = record.polygons;
+//		int nPolys = polygons.length;
+//		if (nPolys <= 0)
+//			return null;
+//
+//		for (int i = 0; i < nPolys; i++)
+//		{
+//			// these points are in RADIAN lat,lon order!...
+//			ESRIPoly thisPoly = polygons[i];
+//			ESRIFloatPoly floatP = (ESRIFloatPoly) thisPoly;
+//			double[] pts = floatP.getRadians();
+//			int nPoints = polygons[i].nPoints;
+//			System.out.println("record: " + record.recordNumber);
+//			System.out.println("nPoints: " + nPoints);
+//
+//			WorldPath wp = new WorldPath();
+//
+//			for (int j = 0; j < pts.length; j++)
+//			{
+//				WorldLocation newLoc = new WorldLocation(pts[j], pts[++j], 0);
+//				wp.addPoint(newLoc);
+//			}
+//
+//			polyList.add(wp);
+//
+//			// FIXME WorldPath
+//			// System.out.print("points (rad): ");
+//			// for (int j = 0; j < pts.length; j++)
+//			// {
+//			// System.out.print("(" + pts[j] + "," + pts[++j] + ") ");
+//			// }
+//			// System.out.println();
+//
+//		}
+//
+//		return polyList;
+//	}
 
 }
