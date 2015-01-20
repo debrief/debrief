e0f110b8e47b9542595e7a049303f220950608e7
Haris Peco <snpe@snpe.rs>
2015-01-20 01:34:56 +0100

    Issue #1046 - polishing

--- org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/wrapper/NEFeatureLayer.java
+++ org.mwc.cmap.NaturalEarth/src/org/mwc/cmap/naturalearth/wrapper/NEFeatureLayer.java
@@ -9,12 +9,14 @@ public class NEFeatureLayer extends FeatureLayer
 {
 
 	private NEFeature neFeature;
+	private String fileName;
 
 	public NEFeatureLayer(NEFeature neFeature,
-			SimpleFeatureSource featureSource, Style sld)
+			String fileName, SimpleFeatureSource featureSource, Style sld)
 	{
 		super(featureSource, sld);
 		this.neFeature = neFeature;
+		this.fileName = fileName;
 	}
 
 	@Override
@@ -30,6 +32,35 @@ public class NEFeatureLayer extends FeatureLayer
 			parent = parent.getParent();
 		}
 		return neFeature.getVisible();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fileName == null) ? 0 : fileName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NEFeatureLayer other = (NEFeatureLayer) obj;
+		if (fileName == null)
+		{
+			if (other.fileName != null)
+				return false;
+		}
+		else if (!fileName.equals(other.fileName))
+			return false;
+		return true;
 	}
 
 }
