1d795ec46e027d70e1954a7f5ff1c0809f94a1b1
Ian Mayo <ian@planetmayo.com>
2015-03-13 14:56:39 +0000

    Hotfix for #1123

--- org.mwc.cmap.core/src/org/mwc/cmap/core/property_support/RightClickSupport.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/core/property_support/RightClickSupport.java
@@ -409,6 +409,12 @@ public class RightClickSupport
 								// find the common ones
 								res = (MethodDescriptor[]) getIntersectionFor(res, newSet, demo);
 							}
+							else
+							{
+								// this type doesn't even have an editor, so it can't have any 
+								// matching items!
+								res = null;
+							}
 						}
 					}
 				}
@@ -505,28 +511,6 @@ public class RightClickSupport
 			{
 				res = firstInfo.getPropertyDescriptors();
 
-				// // and are there any more?
-				// BeanInfo[] abl = firstInfo.getAdditionalBeanInfo();
-				// for (int i = 0; i < abl.length; i++)
-				// {
-				// BeanInfo thisB = abl[i];
-				// PropertyDescriptor[] newPd = thisB.getPropertyDescriptors();
-				//
-				// // and extend the existing list
-				// PropertyDescriptor[] tmpList = new PropertyDescriptor[res.length +
-				// newPd.length];
-				//
-				// // copy in the originals
-				// System.arraycopy(res, 0, tmpList, 0, res.length);
-				//
-				// // and the new ones
-				// System.arraycopy(newPd, 0, tmpList, res.length, newPd.length);
-				//
-				// // and take over the list
-				// res = tmpList;
-				// }
-				//
-
 				// only continue if there are any property descriptors
 				if (res != null)
 				{
@@ -550,6 +534,12 @@ public class RightClickSupport
 								// find the common ones
 								res = (PropertyDescriptor[]) getIntersectionFor(res, newSet,
 										demo);
+							}
+							else
+							{
+								// this type doesn't even have an editor, so it can't have any 
+								// matching items!
+								res = null;
 							}
 						}
 					}
@@ -1089,19 +1079,19 @@ public class RightClickSupport
 									"Setter call failed:" + thisSubject.getName() + " Error was:"
 											+ e.getTargetException().getMessage(),
 									e.getTargetException());
-					res = null;
+					res = Status.CANCEL_STATUS;
 				}
 				catch (final IllegalArgumentException e)
 				{
 					CorePlugin.logError(Status.ERROR, "Wrong parameters pass to:"
 							+ thisSubject.getName(), e);
-					res = null;
+					res = Status.CANCEL_STATUS;
 				}
 				catch (final IllegalAccessException e)
 				{
 					CorePlugin.logError(Status.ERROR, "Illegal access problem for:"
 							+ thisSubject.getName(), e);
-					res = null;
+					res = Status.CANCEL_STATUS;
 				}
 			}
 
