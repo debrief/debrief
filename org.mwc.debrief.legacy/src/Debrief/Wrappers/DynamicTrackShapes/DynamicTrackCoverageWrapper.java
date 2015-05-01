cdde6911b378d749db9a351368b13219b35da7e6
Ian Mayo <ian@planetmayo.com>
2015-05-01 11:26:35 +0100

    ditch the parts we no longer require

--- org.mwc.debrief.legacy/src/Debrief/Wrappers/DynamicTrackShapes/DynamicTrackCoverageWrapper.java
+++ org.mwc.debrief.legacy/src/Debrief/Wrappers/DynamicTrackShapes/DynamicTrackCoverageWrapper.java
@@ -9,11 +9,9 @@ import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 import MWC.GUI.CanvasType;
 import MWC.GUI.ExtendedCanvasType;
-import MWC.GUI.Shapes.CircleShape;
 import MWC.GenericData.HiResDate;
 import MWC.GenericData.Watchable;
 import MWC.GenericData.WorldDistance;
@@ -117,59 +115,59 @@ public class DynamicTrackCoverageWrapper extends DynamicTrackShapeWrapper
 		 * 
 		 * @param trackCourseDegs
 		 */
-		private Vector<WorldLocation> calcDataPoints(final WorldLocation origin,
-				final double orient)
-		{
-			// get ready to store the list
-			final Vector<WorldLocation> res = new Vector<WorldLocation>(0, 1);
+//		private Vector<WorldLocation> calcDataPoints(final WorldLocation origin,
+//				final double orient)
+//		{
+//			// get ready to store the list
+//			final Vector<WorldLocation> res = new Vector<WorldLocation>(0, 1);
+//
+//			final double minDegs = new WorldDistance(minYds, WorldDistance.YARDS)
+//					.getValueIn(WorldDistance.DEGS);
+//			final double maxDegs = new WorldDistance(maxYds, WorldDistance.YARDS)
+//					.getValueIn(WorldDistance.DEGS);
+//
+//			// draw the outer ring
+//			createCircle(origin, orient, res, maxDegs);
+//
+//			// and now the inner ring
+//			createCircle(origin, orient, res, minDegs);
+//
+//			return res;
+//
+//		}
 
-			final double minDegs = new WorldDistance(minYds, WorldDistance.YARDS)
-					.getValueIn(WorldDistance.DEGS);
-			final double maxDegs = new WorldDistance(maxYds, WorldDistance.YARDS)
-					.getValueIn(WorldDistance.DEGS);
-
-			// draw the outer ring
-			createCircle(origin, orient, res, maxDegs);
-
-			// and now the inner ring
-			createCircle(origin, orient, res, minDegs);
-
-			return res;
-
-		}
-
-		private void createCircle(final WorldLocation origin, final double orient,
-				final Vector<WorldLocation> res, final double maxDegs)
-		{
-			for (int i = 0; i <= CircleShape.NUM_SEGMENTS; i++)
-			{
-				// produce the current bearing
-				final double this_brg = (360.0 / CircleShape.NUM_SEGMENTS * i) / 180.0
-						* Math.PI;
-
-				// first produce a standard ellipse of the correct size
-				final double x1 = Math.sin(this_brg) * maxDegs;
-				double y1 = Math.cos(this_brg) * maxDegs;
-
-				// now produce the range out to the edge of the ellipse at
-				// this point
-				final double r = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
-
-				// to prevent div/0 error in atan, make y1 small if zero
-				if (y1 == 0)
-					y1 = 0.0000001;
-
-				// and the new bearing to the correct point on the ellipse
-				final double tr = Math.atan2(y1, x1) + (orient - Math.PI / 2);
-
-				// use our "add" function to add a vector, rather than the
-				// x-y components as we did, so that the ellipse stays correctly
-				// shaped as it travels further from the equator.
-				final WorldLocation wl = origin.add(new WorldVector(tr, r, 0));
-
-				res.add(wl);
-			}
-		}
+//		private void createCircle(final WorldLocation origin, final double orient,
+//				final Vector<WorldLocation> res, final double maxDegs)
+//		{
+//			for (int i = 0; i <= CircleShape.NUM_SEGMENTS; i++)
+//			{
+//				// produce the current bearing
+//				final double this_brg = (360.0 / CircleShape.NUM_SEGMENTS * i) / 180.0
+//						* Math.PI;
+//
+//				// first produce a standard ellipse of the correct size
+//				final double x1 = Math.sin(this_brg) * maxDegs;
+//				double y1 = Math.cos(this_brg) * maxDegs;
+//
+//				// now produce the range out to the edge of the ellipse at
+//				// this point
+//				final double r = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
+//
+//				// to prevent div/0 error in atan, make y1 small if zero
+//				if (y1 == 0)
+//					y1 = 0.0000001;
+//
+//				// and the new bearing to the correct point on the ellipse
+//				final double tr = Math.atan2(y1, x1) + (orient - Math.PI / 2);
+//
+//				// use our "add" function to add a vector, rather than the
+//				// x-y components as we did, so that the ellipse stays correctly
+//				// shaped as it travels further from the equator.
+//				final WorldLocation wl = origin.add(new WorldVector(tr, r, 0));
+//
+//				res.add(wl);
+//			}
+//		}
 
 		/**
 	   * Create a donut or donut section.  A full donut results if the
@@ -186,17 +184,25 @@ public class DynamicTrackCoverageWrapper extends DynamicTrackShapeWrapper
 	                                          double minAngle,
 	                                          double maxAngle,
 	                                          double course) {
+	  	
+	  	// convenience variables, for simpler coding
 	    double rO = outerRadius, rI = innerRadius;
+	    
 	    // angles from degrees clockwise from the positive y axis.
 	    // convert to degress counter-clockwise from positive x axis.
 	    double aBeg =  90 - (maxAngle + course), aExt = maxAngle - minAngle;
+	    
 	    // x and y are upper left corner of bounding rectangle of full circle
-	    if (Math.abs(minAngle % 360 - maxAngle % 360) < 0.1) {
+	    if (Math.abs(minAngle % 360 - maxAngle % 360) < 0.1) 
+	    {
+	    	// ok - it's a plain donut
 	      Area outer = new Area(new Ellipse2D.Double(-rO/2, -rO/2, rO, rO));
 	      Area inner = new Area(new Ellipse2D.Double(-rI/2, -rI/2, rI, rI));
 	      outer.subtract(inner);
 	      return outer;
-	    } else {
+	    } else 
+	    {
+	    	// this is a pie slice of a donut
 	      Area outer = new Area(new Arc2D.Double(-rO/2, -rO/2, rO, rO, aBeg, aExt,
 	                                             Arc2D.PIE));
 	      Area inner = new Area(new Arc2D.Double(-rI/2, -rI/2, rI, rI, aBeg, aExt,
@@ -206,55 +212,48 @@ public class DynamicTrackCoverageWrapper extends DynamicTrackShapeWrapper
 	    }
 	  }
 
-		public void paint(CanvasType dest, Watchable hostState, Color color)
+		public void paint(CanvasType dest, Watchable hostState, Color color, boolean semiTransparent)
 		{
+			// update the color
+			dest.setColor(color);
+
+			WorldLocation originWd = hostState.getLocation();
+			// get the host origin in screen coords
+			final Point originPt = dest.toScreen(originWd);
+
 			// get the polygon at this location
 			//Vector<WorldLocation> _theDataPoints = calcDataPoints(
 			//		hostState.getLocation(), hostState.getCourse());
 
-			// update the color
-			dest.setColor(color);
-			//final double minDegs = new WorldDistance(minYds, WorldDistance.YARDS)
-			//	.getValueIn(WorldDistance.DEGS);
-			//final double maxDegs = new WorldDistance(maxYds, WorldDistance.YARDS)
-			//	.getValueIn(WorldDistance.DEGS);
+			final WorldDistance minDist = new WorldDistance(minYds, WorldDistance.YARDS);
+			final WorldDistance maxDist = new WorldDistance(maxYds, WorldDistance.YARDS);
+
+			// sort out the sizes in pixels
+			WorldVector minOffset = new WorldVector(0d, minDist, null);
+			WorldVector maxOffset = new WorldVector(0d, maxDist, null);
+			Point minPt = dest.toScreen(originWd.add(minOffset));
+			Point maxPt = dest.toScreen(originWd.add(maxOffset));
+			
+			long minOffsetPt = originPt.y - minPt.y;
+			long maxOffsetPt = originPt.y - maxPt.y;
+			
 			double course = Math.toDegrees(hostState.getCourse());
-			// FIXME
-			Area area = makeDonutSectionArea(minYds/20, maxYds/20, minAngleDegs, maxAngleDegs, course);
-			Point origin = dest.toScreen(hostState.getLocation());
-			final AffineTransform af = AffineTransform.getTranslateInstance(origin.x, origin.y);
+			Area area = makeDonutSectionArea(minOffsetPt*2, maxOffsetPt*2, minAngleDegs, maxAngleDegs, course);
+			
+			final AffineTransform af = AffineTransform.getTranslateInstance(originPt.x, originPt.y);
 	    Shape shape = af.createTransformedShape(area);
 
 			if (dest instanceof ExtendedCanvasType)
 			{
-				((ExtendedCanvasType) dest).drawShape(shape);
-			}
-			
-			// create a polygon to represent the ellipse (so that we can fill or draw
-			// it)
-//			final int len = _theDataPoints.size();
-//			final int[] xPoints = new int[len];
-//			final int[] yPoints = new int[len];
-//
-//			// work through the list to create the list of screen coordinates
-//			for (int i = 0; i < _theDataPoints.size(); i++)
-//			{
-//				final WorldLocation location = (WorldLocation) _theDataPoints
-//						.elementAt(i);
-//
-//				final Point p2 = dest.toScreen(location);
-//
-//				xPoints[i] = p2.x;
-//				yPoints[i] = p2.y;
-//			}
-//
-//			// if (getSemiTransparent() && dest instanceof ExtendedCanvasType)
-//			// {
-//			ExtendedCanvasType ext = (ExtendedCanvasType) dest;
-//			ext.semiFillPolygon(xPoints, yPoints, len);
-			// }
-			// else
-			// dest.fillPolygon(xPoints, yPoints, len);
+				if(semiTransparent)
+				{
+				((ExtendedCanvasType) dest).semiFillShape(shape);
+				}
+				else
+				{
+					((ExtendedCanvasType) dest).fillShape(shape);
+				}
+			}			
 		}
 
 		public String toString()
