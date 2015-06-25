da01ec582b1434fbab6cee7c00e22b671ac0afbe
Ian Mayo <ian@planetmayo.com>
2015-06-25 08:20:29 +0100

    Fix to allow TimeDisplay objects to be added

--- org.mwc.cmap.legacy/src/MWC/GUI/DynamicLayer.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/DynamicLayer.java
@@ -2,10 +2,26 @@ package MWC.GUI;
 
 import java.util.Enumeration;
 
+/** specialised type of layer that is able to plot dynamic children,
+ * that-is elements that are time-sensitive.
+ * @author ian
+ *
+ */
 public class DynamicLayer extends BaseLayer implements MovingPlottable
 {
 
 	private static final long serialVersionUID = 1L;
+
+	
+	
+	@Override
+	public void add(Editable thePlottable)
+	{
+		// SPECIAL HANDLING.  We can't allow DynamicPlottables to be added to normal
+		// layers, since normal layers don't provide the time integration.  
+		// We only allow them to be pasted into a DynamicLayer like this.
+		super.getData().add(thePlottable);
+	}
 
 	@Override
 	public void paint(CanvasType dest)
--- org.mwc.cmap.legacy/src/MWC/GUI/NotInBaseLayer.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/NotInBaseLayer.java
@@ -14,6 +14,11 @@
  */
 package MWC.GUI;
 
+/** marker interface for objects that may not be placed in a base layer - but which must
+ * sit in their own specialised layer
+ * @author ian
+ *
+ */
 public interface NotInBaseLayer
 {
 }
