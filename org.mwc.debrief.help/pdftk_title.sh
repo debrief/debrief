2b59156b2dddf348f3911467b3d704deb8764a00
Haris Peco <snpe@snpe.rs>
2015-06-28 00:25:45 +0200

    Issue #1275 - Split PDF tutorial into two documents

--- org.mwc.debrief.help/pdftk_title.sh
+++ org.mwc.debrief.help/pdftk_title.sh
@@ -1,5 +1,7 @@
 cd ../org.mwc.cmap.combined.feature/root_installs
-pdftk A=UserGuide_FlattenedForViewing.pdf B=DebriefNG.pdf cat A1 B2-end output out.pdf
+pdftk A=../../org.mwc.debrief.help/front_covers/UserGuide_FlattenedForViewing.pdf B=DebriefNG.pdf cat A1 B2-end output out.pdf
 mv out.pdf DebriefNG.pdf
-pdftk A=Tutorial_FlattenedForViewing.pdf B=DebriefTutorial.pdf cat A1 B2-end output out.pdf
-mv out.pdf DebriefTutorial.pdf
+pdftk A=../../org.mwc.debrief.help/front_covers/Tutorial_FlattenedForViewing.pdf B=DebriefNGTutorial.pdf cat A1 B2-end output out.pdf
+mv out.pdf DebriefNGTutorial.pdf
+pdftk A=../../org.mwc.debrief.help/front_covers/Tutorial_FlattenedForViewing.pdf B=DebriefNG_TMA_Tutorial.pdf cat A1 B2-end output out.pdf
+mv out.pdf DebriefNG_TMA_Tutorial.pdf
