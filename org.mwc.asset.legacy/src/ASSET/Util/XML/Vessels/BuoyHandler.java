bb6e3fe1f6fb004c0706c4066b7b625947c53f69
Ian Mayo <ian@planetmayo.com>
2015-07-03 14:51:57 +0100

    introduce buoy entity

---
+++ org.mwc.asset.legacy/src/ASSET/Util/XML/Vessels/BuoyHandler.java
@@ -0,0 +1,76 @@
+/*
+ *    Debrief - the Open Source Maritime Analysis Application
+ *    http://debrief.info
+ *
+ *    (C) 2000-2014, PlanetMayo Ltd
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the Eclipse Public License v1.0
+ *    (http://www.eclipse.org/legal/epl-v10.html)
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+package ASSET.Util.XML.Vessels;
+
+import MWC.Utilities.ReaderWriter.XML.MWCXMLReader.HandleDoubleAttribute;
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+abstract public class BuoyHandler extends ParticipantHandler{
+
+
+  static private final String myType = "Buoy";
+  static private final String IS_TRANSMITTING = "IsTransmitting";
+  private boolean _isTransmitting = false;
+  
+
+  public BuoyHandler() {
+    super(myType);
+
+
+    // add handlers for the Helo properties
+    // none in this instance
+
+    addHandler(new ASSET.Util.XML.Movement.SSMovementCharsHandler()
+    {
+      public void setMovement(final ASSET.Models.Movement.MovementCharacteristics chars)
+      {
+        _myMoveChars = chars;
+      }
+    });
+    addAttributeHandler(new HandleBooleanAttribute(IS_TRANSMITTING)
+    {
+      public void setValue(String name, final boolean val)
+      {
+        _isTransmitting = val;
+      }
+    });
+
+  }
+
+  protected ASSET.ParticipantType getParticipant(final int index)
+  {
+    final ASSET.ParticipantType thisVessel = new ASSET.Models.Vessels.Buoy(index, _isTransmitting);
+    return thisVessel;
+  }
+
+  static public void exportThis(final Object toExport,final org.w3c.dom.Element parent, final org.w3c.dom.Document doc)
+  {
+      // create ourselves
+    final org.w3c.dom.Element thisPart = doc.createElement(myType);
+
+    ParticipantHandler.exportThis(toExport, thisPart, doc);
+
+    parent.appendChild(thisPart);
+
+  }
+}
