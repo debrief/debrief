9bda9387d9209f90f87c5eb87b3936ea4410b869
Ian Mayo <ian@deepbluec.com>
2016-01-07 17:15:44 +0000

    create integration plugin

---
+++ org.debrief.limpet_integration/src/org/debrief/limpet_integration/DebriefIntegrationActivator.java
@@ -0,0 +1,85 @@
+package org.debrief.limpet_integration;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.mwc.cmap.core.ui_support.CoreViewLabelProvider;
+import org.osgi.framework.BundleContext;
+
+import ui.LimpetImageHelper;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class DebriefIntegrationActivator extends AbstractUIPlugin implements
+		IStartup
+{
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.debrief.limpet_integration"; //$NON-NLS-1$
+
+	// The shared instance
+	private static DebriefIntegrationActivator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public DebriefIntegrationActivator()
+	{
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception
+	{
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception
+	{
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static DebriefIntegrationActivator getDefault()
+	{
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *          the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path)
+	{
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+	@Override
+	public void earlyStartup()
+	{
+		// give the LayerManager our image creator.
+		CoreViewLabelProvider.addImageHelper(new LimpetImageHelper());	}
+}
---
+++ org.debrief.limpet_integration/src/ui/LimpetImageHelper.java
@@ -0,0 +1,42 @@
+/*
+ *    Debrief - the Open Source Maritime Analysis Application
+ *    http://debrief.info
+ *
+ *    (C) 2000-2014, PlanetMayo Ltd
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the Eclipse Public License v1.0
+ *    (http://www.eclipse.org/legal/epl-v10.html)
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+package ui;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.mwc.cmap.core.ui_support.CoreViewLabelProvider.ViewLabelImageHelper;
+
+import Debrief.Wrappers.Measurements.SupplementalData;
+import MWC.GUI.Editable;
+import info.limpet.rcp.Activator;
+
+public class LimpetImageHelper implements ViewLabelImageHelper
+{
+
+	public ImageDescriptor getImageFor(final Editable subject)
+	{
+		final ImageDescriptor res;
+		if (subject instanceof SupplementalData)
+		{
+			res = Activator.getImageDescriptor("icons/folder.png");
+		}
+		else
+		{
+			res = null;
+		}
+
+		return res;
+	}
+
+}
