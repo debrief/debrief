31809aa0b8c11ff97892da8ef0728a04502c1f1a
Ian Mayo <ian@deepbluec.com>
2016-01-07 17:18:17 +0000

    First placeholder for Supplemental datasets

---
+++ org.mwc.debrief.legacy/src/Debrief/Wrappers/Measurements/SupplementalData.java
@@ -0,0 +1,18 @@
+package Debrief.Wrappers.Measurements;
+
+import MWC.GUI.BaseLayer;
+
+public class SupplementalData extends BaseLayer
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SupplementalData()
+	{
+		setName("Measurements");
+	}
+
+}
