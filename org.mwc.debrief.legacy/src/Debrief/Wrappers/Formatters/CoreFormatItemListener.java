94f2f17ebd8a1a305e8f7ce7b6f2699b6a02252b
ianmayo <ian@planetmayo.com>
2016-01-31 14:21:27 +0000

    add initial tests, plus support for regular intervals

--- org.mwc.debrief.legacy/src/Debrief/Wrappers/Formatters/CoreFormatItemListener.java
+++ org.mwc.debrief.legacy/src/Debrief/Wrappers/Formatters/CoreFormatItemListener.java
@@ -3,6 +3,8 @@ package Debrief.Wrappers.Formatters;
 import java.util.HashMap;
 import java.util.Map;
 
+import junit.framework.TestCase;
+
 import Debrief.Wrappers.FixWrapper;
 import Debrief.Wrappers.TrackWrapper;
 import MWC.GUI.CanvasType;
@@ -14,10 +16,112 @@ import MWC.GUI.PlainWrapper;
 import MWC.GUI.TimeStampedDataItem;
 import MWC.GenericData.HiResDate;
 import MWC.GenericData.WorldArea;
+import MWC.GenericData.WorldLocation;
+import MWC.TacticalData.Fix;
 
 public abstract class CoreFormatItemListener extends PlainWrapper implements
     INewItemListener, INewLayerListener
 {
+
+  public static class TestMe extends TestCase
+  {
+    public void testRegularIntervals()
+    {
+      CoreFormatItemListener cf =
+          new CoreFormatItemListener("Test", null, null, 5000L, true)
+          {
+
+            /**
+         * 
+         */
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void formatTrack(TrackWrapper track, HiResDate interval)
+            {
+              // skip
+            }
+
+            @Override
+            protected void applyFormat(FixWrapper fix)
+            {
+              fix.setSymbolShowing(true);
+            }
+          };
+      TrackWrapper tw = new TrackWrapper();
+      tw.setName("Name");
+      FixWrapper f1 = createFix(4000);
+      FixWrapper f2 = createFix(5000);
+      FixWrapper f3 = createFix(6000);
+      FixWrapper f4 = createFix(10000);
+      FixWrapper f5 = createFix(12000);
+
+      cf.newItem(tw, f1);
+      cf.newItem(tw, f2);
+      cf.newItem(tw, f3);
+      cf.newItem(tw, f4);
+      cf.newItem(tw, f5);
+
+      assertTrue(f1.getSymbolShowing());
+      assertTrue(f2.getSymbolShowing());
+      assertFalse(f3.getSymbolShowing());
+      assertTrue(f4.getSymbolShowing());
+      assertFalse(f5.getSymbolShowing());
+    }
+    
+    public void testIntervals()
+    {
+      CoreFormatItemListener cf =
+          new CoreFormatItemListener("Test", null, null, 5000L, false)
+          {
+
+            /**
+         * 
+         */
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void formatTrack(TrackWrapper track, HiResDate interval)
+            {
+              // skip
+            }
+
+            @Override
+            protected void applyFormat(FixWrapper fix)
+            {
+              fix.setSymbolShowing(true);
+            }
+          };
+      TrackWrapper tw = new TrackWrapper();
+      tw.setName("Name");
+      FixWrapper f1 = createFix(4000);
+      FixWrapper f2 = createFix(5000);
+      FixWrapper f3 = createFix(9000);
+      FixWrapper f4 = createFix(10000);
+      FixWrapper f5 = createFix(14100);
+
+      cf.newItem(tw, f1);
+      cf.newItem(tw, f2);
+      cf.newItem(tw, f3);
+      cf.newItem(tw, f4);
+      cf.newItem(tw, f5);
+
+      assertTrue(f1.getSymbolShowing());
+      assertFalse(f2.getSymbolShowing());
+      assertTrue(f3.getSymbolShowing());
+      assertFalse(f4.getSymbolShowing());
+      assertTrue(f5.getSymbolShowing());
+    }
+
+    private FixWrapper createFix(int time)
+    {
+      Fix newF =
+          new Fix(new HiResDate(time), new WorldLocation(2, 2, 0), 22, 33);
+      FixWrapper fw = new FixWrapper(newF);
+      return fw;
+
+    }
+  }
 
   /**
    * 
@@ -30,13 +134,16 @@ public abstract class CoreFormatItemListener extends PlainWrapper implements
   private Map<String, Long> _lastTimes = new HashMap<String, Long>();
   private String _formatName;
 
+  private boolean _regularInterval;
+
   public CoreFormatItemListener(String name, String layerName,
-      String symbology, long interval)
+      String symbology, long interval, boolean regularInterval)
   {
     _formatName = name;
     _layerName = layerName;
     _sym = symbology;
     _interval = interval;
+    _regularInterval = regularInterval;
   }
 
   @Override
@@ -92,8 +199,15 @@ public abstract class CoreFormatItemListener extends PlainWrapper implements
       Long lastTime = _lastTimes.get(hisName);
       TimeStampedDataItem tsd = (TimeStampedDataItem) item;
       long thisTime = tsd.getDTG().getDate().getTime();
-      if (lastTime == null || thisTime > lastTime + _interval)
+      if (lastTime == null || thisTime >= lastTime + _interval)
       {
+        // do we need to clip the time to a regular interval?
+        if (_regularInterval)
+        {
+          // ok - store the last instance of
+          thisTime = thisTime - (thisTime % _interval);
+        }
+
         // store the new time
         _lastTimes.put(hisName, thisTime);
 
