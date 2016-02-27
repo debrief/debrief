3d08c9de60cffcda34add82801b6ac6ab3801f6e
Ian Mayo <ian@planetmayo.com>
2016-02-27 16:37:02 +0000

    remove debug lines

--- org.mwc.debrief.legacy/src/Debrief/Wrappers/TrackWrapper.java
+++ org.mwc.debrief.legacy/src/Debrief/Wrappers/TrackWrapper.java
@@ -35,7 +35,6 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.Vector;
 
-import Debrief.GUI.Frames.Application;
 import Debrief.ReaderWriter.Replay.FormatTracks;
 import Debrief.Wrappers.DynamicTrackShapes.DynamicTrackShapeSetWrapper;
 import Debrief.Wrappers.DynamicTrackShapes.DynamicTrackShapeWrapper;
@@ -1233,7 +1232,7 @@ public class TrackWrapper extends MWC.GUI.PlainWrapper implements
   public final void filterListTo(final HiResDate start, final HiResDate end)
   {
     // TODO: DEBUG: REMOVE: remove this diagnostics message
-    Application.logStack2(Application.WARNING, "DEBUG: Filtering track");
+   // Application.logStack2(Application.WARNING, "DEBUG: Filtering track");
 
     final Enumeration<Editable> fixWrappers = getPositions();
     while (fixWrappers.hasMoreElements())
