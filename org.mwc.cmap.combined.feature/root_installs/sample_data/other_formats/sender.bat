66d68df1bb5509e9e2a883b8ab0d08e504786e2f
Ian Mayo <ian@planetmayo.com>
2016-03-07 10:38:45 +0000

    fix jar name in classpath (for .bat file)

--- org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/sender.bat
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/sender.bat
@@ -1,4 +1,4 @@
 # Parameters:
 #
-java -cp ../../plugins/org.mwc.debrief.dis-1.0.2.jar org.mwc.debrief.dis.diagnostics.PduGenerator %1
+java -cp ../../plugins/org.mwc.debrief.dis_1.0.2.jar org.mwc.debrief.dis.diagnostics.PduGenerator %1
 
