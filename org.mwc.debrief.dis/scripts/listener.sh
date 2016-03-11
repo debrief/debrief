b7bf5ba142926164512d9a150a92b259f5bef01e
Ian Mayo <ian@planetmayo.com>
2016-03-11 13:34:05 +0000

    increment version of DIS jar file

--- org.mwc.debrief.dis/scripts/listener.sh
+++ org.mwc.debrief.dis/scripts/listener.sh
@@ -4,4 +4,4 @@
 # root = the folder destination for file outputs
 # screen = whether to output status messages to the screen
 #
-java -Dgroup=239.1.2.3 -Dport=62040 -Droot=out -Dscreen=false -cp ../target/org.mwc.debrief.dis-1.0.4.jar:../libs/dis-enums_1.1.jar org.mwc.debrief.dis.diagnostics.HeadlessDISLogger
+java -Dgroup=239.1.2.3 -Dport=62040 -Droot=out -Dscreen=false -cp ../target/org.mwc.debrief.dis-1.0.5.jar:../libs/dis-enums_1.1.jar org.mwc.debrief.dis.diagnostics.HeadlessDISLogger
