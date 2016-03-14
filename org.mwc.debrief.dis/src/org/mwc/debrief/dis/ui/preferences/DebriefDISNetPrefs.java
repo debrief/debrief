6cfa764031cd8efe081329d80bac60b295ac88a3
ianmayo <ian@planetmayo.com>
2016-03-14 19:43:28 +0000

    first try to get address & port from command-line, use users preferences as fallback

--- org.mwc.debrief.dis/src/org/mwc/debrief/dis/ui/preferences/DebriefDISNetPrefs.java
+++ org.mwc.debrief.dis/src/org/mwc/debrief/dis/ui/preferences/DebriefDISNetPrefs.java
@@ -9,30 +9,41 @@ import org.mwc.debrief.dis.providers.network.IDISNetworkPrefs;
 
 /**
  * @author ian
- *
+ * 
  */
 public class DebriefDISNetPrefs implements IDISNetworkPrefs
 {
-  
-  
-  /* (non-Javadoc)
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.mwc.debrief.dis.providers.network.IDISNetworkPrefs#getIPAddress()
    */
   @Override
   public String getIPAddress()
   {
     IPreferenceStore store = DisActivator.getDefault().getPreferenceStore();
-    return store.getString(DisActivator.IP_ADDRESS);
+
+    // try for system property, use preference as fallback
+    return System.getProperties().getProperty(DisActivator.IP_ADDRESS,
+        store.getString(DisActivator.IP_ADDRESS));
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.mwc.debrief.dis.providers.network.IDISNetworkPrefs#getPort()
    */
   @Override
   public int getPort()
   {
     IPreferenceStore store = DisActivator.getDefault().getPreferenceStore();
-    return store.getInt(DisActivator.PORT);
+
+    // try for system property, use preference as fallback
+    String res =
+        System.getProperty(DisActivator.PORT, store
+            .getString(DisActivator.PORT));
+    return Integer.parseInt(res);
   }
 
 }
