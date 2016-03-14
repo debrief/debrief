8a5c072670dee3663e508993ba6dda6b7ac4f77d
Ian Mayo <ian@planetmayo.com>
2016-03-14 14:57:40 +0000

    introduce the console png

--- org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/sender.bat
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/sender.bat
@@ -2,4 +2,4 @@ REM run the PDU generator, passing to it the control file (as parameter one)
 REM $1 the control file, containing ip, port, network mode, time-step, num participants, num messages
 REM e.g.  239.1.2.3 62040 multicast 1000 12 15
 REM 
-java -cp ../../plugins/org.mwc.debrief.dis_1.0.5.jar org.mwc.debrief.dis.diagnostics.PduGenerator %1
+java -cp ../../plugins/org.mwc.debrief.dis_1.0.6.jar;dis-enums_1.1.jar org.mwc.debrief.dis.diagnostics.PduGenerator %1
