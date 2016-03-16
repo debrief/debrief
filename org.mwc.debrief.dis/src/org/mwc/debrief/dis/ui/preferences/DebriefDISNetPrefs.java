44d03f89b117d6d671c04d0e8696510c69e6432a
Ian Mayo <ian@planetmayo.com>
2016-03-16 11:00:33 +0000

    refactor

--- org.mwc.debrief.dis/src/org/mwc/debrief/dis/ui/preferences/DebriefDISNetPrefs.java
+++ org.mwc.debrief.dis/src/org/mwc/debrief/dis/ui/preferences/DebriefDISNetPrefs.java
@@ -3,6 +3,9 @@
  */
 package org.mwc.debrief.dis.ui.preferences;
 
+import junit.framework.TestCase;
+
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.mwc.debrief.dis.DisActivator;
 import org.mwc.debrief.dis.providers.network.IDISNetworkPrefs;
@@ -22,11 +25,7 @@ public class DebriefDISNetPrefs implements IDISNetworkPrefs
   @Override
   public String getIPAddress()
   {
-    IPreferenceStore store = DisActivator.getDefault().getPreferenceStore();
-
-    // try for system property, use preference as fallback
-    return System.getProperties().getProperty(DisActivator.IP_ADDRESS,
-        store.getString(DisActivator.IP_ADDRESS));
+    return getThisArg(DisActivator.IP_ADDRESS);
   }
 
   /*
@@ -37,13 +36,53 @@ public class DebriefDISNetPrefs implements IDISNetworkPrefs
   @Override
   public int getPort()
   {
-    IPreferenceStore store = DisActivator.getDefault().getPreferenceStore();
+    return Integer.parseInt(getThisArg(DisActivator.PORT));
+  }
 
-    // try for system property, use preference as fallback
-    String res =
-        System.getProperty(DisActivator.PORT, store
-            .getString(DisActivator.PORT));
-    return Integer.parseInt(res);
+  private String getThisArg(String name)
+  {
+    // do a command-line check, just in case it was supplied in the command line
+    // args
+    String address = getArg(Platform.getCommandLineArgs(), name);
+
+    // was there one?
+    if (address == null)
+    {
+      IPreferenceStore store = DisActivator.getDefault().getPreferenceStore();
+
+      // try for system property, use preference as fallback
+      address = System.getProperty(name, store.getString(name));
+    }
+    return address;
+  }
+
+  private String getArg(String[] args, String name)
+  {
+    for (int i = 0; i < args.length; i++)
+    {
+      String thisArg = args[i];
+      if (thisArg.contains(name))
+      {
+        int startI = thisArg.indexOf(name) + name.length() + 1;
+        String endStr = thisArg.substring(startI).trim();
+        return endStr;
+      }
+    }
+    return null;
+  }
+
+  public static class TestMe extends TestCase
+  {
+    public void testArgs()
+    {
+      DebriefDISNetPrefs prefs = new DebriefDISNetPrefs();
+      assertNull("didn't find it", prefs.getArg(new String[]
+      {"aa", "bb"}, "val"));
+      assertNotNull("found it", prefs.getArg(new String[]
+      {"aa", "-Dval=23"}, "val"));
+      assertEquals("found it", "23", prefs.getArg(new String[]
+      {"aa", "-Dval=23"}, "val"));
+    }
   }
 
 }
