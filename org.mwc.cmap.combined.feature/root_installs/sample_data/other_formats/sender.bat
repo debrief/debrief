e69084068ea167ad6ca077463c710dc45c56be68
Ian Mayo <ian@planetmayo.com>
2016-03-31 14:57:06 +0100

    correct comment marker for windows

--- org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/sender.bat
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/sender.bat
@@ -1,5 +1,5 @@
-REM run the PDU generator, passing to it the control file (as parameter one)
-REM $1 the control file, containing ip, port, network mode, time-step, num participants, num messages
-REM e.g.  239.1.2.3 62040 multicast 1000 12 15
-REM 
+:: run the PDU generator, passing to it the control file (as parameter one)
+:: $1 the control file, containing ip, port, network mode, time-step, num participants, num messages
+:: e.g.  239.1.2.3 62040 multicast 1000 12 15
+:: 
 java -cp ../../plugins/org.mwc.debrief.dis_1.0.10.jar org.mwc.debrief.dis.diagnostics.PduGenerator %1
