709df1611977e31153055f8592fe3e8efe4687ac
Ian Mayo <ian@planetmayo.com>
2016-04-26 14:54:10 +0100

    introduce new rule

--- org.mwc.debrief.dis/src/org/mwc/debrief/dis/editor/InputFileRuleScanner.java
+++ org.mwc.debrief.dis/src/org/mwc/debrief/dis/editor/InputFileRuleScanner.java
@@ -20,6 +20,7 @@ import org.eclipse.jface.text.rules.IRule;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
 import org.eclipse.jface.text.rules.Token;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
@@ -27,14 +28,29 @@ import org.eclipse.swt.widgets.Display;
 public class InputFileRuleScanner extends RuleBasedScanner
 {
   private static Color COMMENT_COLOR = new Color(Display.getCurrent(), new RGB(
-      0, 200, 0));
+      63, 127, 95));
+  private static Color BLOCK_COLOR = new Color(Display.getCurrent(), new RGB(
+      95, 2, 8));
+  private static Color BLOCK_BACK_COLOR = new Color(Display.getCurrent(),
+      new RGB(195, 192, 228));
+
+  // the color
 
   public InputFileRuleScanner()
   {
-    IToken commentToken = new Token(new TextAttribute(COMMENT_COLOR));
+    // get ready for list
+    IRule[] rules = new IRule[2];
 
-    IRule[] rules = new IRule[1];
+    // start with comment marker
+    IToken commentToken =
+        new Token(new TextAttribute(COMMENT_COLOR, null, SWT.ITALIC));
     rules[0] = (new EndOfLineRule("//", commentToken));
+
+    // and a block token
+    IToken blockToken =
+        new Token(new TextAttribute(BLOCK_COLOR, BLOCK_BACK_COLOR, SWT.NONE));
+    rules[1] = (new EndOfLineRule("<<", blockToken));
+
     setRules(rules);
   }
 }
