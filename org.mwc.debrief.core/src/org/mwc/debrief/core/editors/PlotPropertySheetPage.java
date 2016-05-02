f50eaa845342602a0b7f89d9eafa80d953799e80
Anuradha Gunasekara <theanuradha@gmail.com>
2016-05-02 13:13:21 +0530

    #1688 Show Context/selection info in Properties view

--- org.mwc.debrief.core/src/org/mwc/debrief/core/editors/PlotPropertySheetPage.java
+++ org.mwc.debrief.core/src/org/mwc/debrief/core/editors/PlotPropertySheetPage.java
@@ -14,19 +14,125 @@
  */
 package org.mwc.debrief.core.editors;
 
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.views.properties.PropertySheetPage;
+import org.mwc.cmap.core.property_support.EditableWrapper;
 
 public class PlotPropertySheetPage extends PropertySheetPage
 {
 
 	private PlotEditor _plotEditor;
+	private Composite _base;
+	private Label context;
 
 	public PlotPropertySheetPage(PlotEditor plotEditor)
 	{
 		this._plotEditor = plotEditor;
+		
 	}
+	@Override
+	public void createControl(Composite parent)
+	{
+	  _base = new Composite(parent, SWT.NONE);
+	  //clean layout
+	  GridLayout layout = new GridLayout();
+	  layout.marginTop = 0;
+	  layout.marginBottom = 0;
+	  layout.marginRight = 0;
+	  layout.marginLeft = 0;
+	  layout.verticalSpacing = 5;
+	  layout.marginWidth=0;
+	  layout.marginHeight=0;
+    _base.setLayout(layout);
+	  context = new Label(_base,SWT.NONE); 
+	  FontData fontData = context.getFont().getFontData()[0];
+	  final Font font = new Font(parent.getDisplay(), new FontData(fontData.getName(), fontData
+	      .getHeight(), SWT.BOLD));
+	  context.setFont(font);
+	  context.addDisposeListener(new DisposeListener()
+    {
+      
+      @Override
+      public void widgetDisposed(DisposeEvent e)
+      {
+        font.dispose();
+        
+      }
+    });
+
+	  context.setText("<selection>");
+	 
+	  super.createControl(_base);
+	  context.setLayoutData(new GridData(GridData.FILL_HORIZONTAL|GridData.GRAB_HORIZONTAL));
+	  super.getControl().setLayoutData(new GridData(GridData.FILL_BOTH|GridData.GRAB_HORIZONTAL|GridData.GRAB_VERTICAL));
+
+	   
+	}
+	@Override
+	public void selectionChanged(IWorkbenchPart part, ISelection selection)
+	{
+	  super.selectionChanged(part, selection);
+	  if(selection instanceof StructuredSelection)
+	  {
+	    StructuredSelection treeSelection =(StructuredSelection) selection;
+	    
+	    Object[] objs = treeSelection.toArray();
+	    if(objs.length>0)
+	    {
+	      StringBuilder builder = new StringBuilder();
+	      boolean addSep = false;
+	      for (Object object : objs)
+        {
+	        if(object instanceof EditableWrapper)
+	        {
+	          if(addSep)
+	          {
+	            builder.append(", ");
+	          }
+	          EditableWrapper wrapper = (EditableWrapper) object;
+	          builder.append(wrapper.getEditable().toString());
+	          
+	          addSep = true;
+	        }
+        }
+	      
+	      
+	      context.setText(builder.toString());
+	      context.setToolTipText(builder.toString());
+	      return;
+	    }
+	    
+	   
+	  }
+	  context.setText("<selection>");
+	  context.setToolTipText("");
+	}
+	
+	
+	
+	@Override
+	public Control getControl()
+	{
+	  return _base;
+	}
+	
 
 	@Override
 	public void setActionBars(IActionBars actionBars)
