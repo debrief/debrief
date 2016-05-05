f23d3dd5d6601de09616e98af405608721fd7751
Ian Mayo <ian@planetmayo.com>
2016-05-05 10:46:05 +0100

    apply project cleanup settings

--- org.mwc.debrief.core/src/org/mwc/debrief/core/editors/PlotPropertySheetPage.java
+++ org.mwc.debrief.core/src/org/mwc/debrief/core/editors/PlotPropertySheetPage.java
@@ -35,116 +35,119 @@ import org.mwc.cmap.core.property_support.EditableWrapper;
 public class PlotPropertySheetPage extends PropertySheetPage
 {
 
-	private PlotEditor _plotEditor;
-	private Composite _base;
-	private Label context;
+  private final PlotEditor _plotEditor;
+  private Composite _base;
+  private Label context;
 
-	public PlotPropertySheetPage(PlotEditor plotEditor)
-	{
-		this._plotEditor = plotEditor;
-		
-	}
-	@Override
-	public void createControl(Composite parent)
-	{
-	  _base = new Composite(parent, SWT.NONE);
-	  //clean layout
-	  GridLayout layout = new GridLayout();
-	  layout.marginTop = 0;
-	  layout.marginBottom = 0;
-	  layout.marginRight = 0;
-	  layout.marginLeft = 0;
-	  layout.verticalSpacing = 5;
-	  layout.marginWidth=0;
-	  layout.marginHeight=0;
+  public PlotPropertySheetPage(final PlotEditor plotEditor)
+  {
+    this._plotEditor = plotEditor;
+
+  }
+
+  @Override
+  public void createControl(final Composite parent)
+  {
+    _base = new Composite(parent, SWT.NONE);
+    // clean layout
+    final GridLayout layout = new GridLayout();
+    layout.marginTop = 0;
+    layout.marginBottom = 0;
+    layout.marginRight = 0;
+    layout.marginLeft = 0;
+    layout.verticalSpacing = 5;
+    layout.marginWidth = 0;
+    layout.marginHeight = 0;
     _base.setLayout(layout);
-	  context = new Label(_base,SWT.NONE); 
-	  FontData fontData = context.getFont().getFontData()[0];
-	  final Font font = new Font(parent.getDisplay(), new FontData(fontData.getName(), fontData
-	      .getHeight(), SWT.ITALIC));
-	  context.setFont(font);
-	  context.addDisposeListener(new DisposeListener()
+    context = new Label(_base, SWT.NONE);
+    final FontData fontData = context.getFont().getFontData()[0];
+    final Font font =
+        new Font(parent.getDisplay(), new FontData(fontData.getName(), fontData
+            .getHeight(), SWT.ITALIC));
+    context.setFont(font);
+    context.addDisposeListener(new DisposeListener()
     {
-      
+
       @Override
-      public void widgetDisposed(DisposeEvent e)
+      public void widgetDisposed(final DisposeEvent e)
       {
         font.dispose();
-        
+
       }
     });
 
-	  context.setText("<selection>");
-	 
-	  super.createControl(_base);
-	  context.setLayoutData(new GridData(GridData.FILL_HORIZONTAL|GridData.GRAB_HORIZONTAL));
-	  super.getControl().setLayoutData(new GridData(GridData.FILL_BOTH|GridData.GRAB_HORIZONTAL|GridData.GRAB_VERTICAL));
+    context.setText("<selection>");
 
-	   
-	}
-	@Override
-	public void selectionChanged(IWorkbenchPart part, ISelection selection)
-	{
-	  super.selectionChanged(part, selection);
-	  if(selection instanceof StructuredSelection)
-	  {
-	    StructuredSelection treeSelection =(StructuredSelection) selection;
-	    
-	    Object[] objs = treeSelection.toArray();
-	    if(objs.length>0)
-	    {
-	      StringBuilder builder = new StringBuilder();
-	      boolean addSep = false;
-	      final int MAX_SIZE = 5;
-	      for(int ctr = 0; ctr < objs.length && ctr < MAX_SIZE; ctr++)
-	      {
-	        Object object = objs[ctr];
-	        if(object instanceof EditableWrapper)
-	        {
-	          if(addSep)
-	          {
-	            builder.append(", ");
-	          }
-	          EditableWrapper wrapper = (EditableWrapper) object;
-	          builder.append(wrapper.getEditable().toString());
-	          
-	          addSep = true;
-	        }
+    super.createControl(_base);
+    context.setLayoutData(new GridData(GridData.FILL_HORIZONTAL
+        | GridData.GRAB_HORIZONTAL));
+    super.getControl().setLayoutData(
+        new GridData(GridData.FILL_BOTH | GridData.GRAB_HORIZONTAL
+            | GridData.GRAB_VERTICAL));
+
+  }
+
+  @Override
+  public Control getControl()
+  {
+    return _base;
+  }
+
+  @Override
+  public void selectionChanged(final IWorkbenchPart part,
+      final ISelection selection)
+  {
+    super.selectionChanged(part, selection);
+    if (selection instanceof StructuredSelection)
+    {
+      final StructuredSelection treeSelection = (StructuredSelection) selection;
+
+      final Object[] objs = treeSelection.toArray();
+      if (objs.length > 0)
+      {
+        final StringBuilder builder = new StringBuilder();
+        boolean addSep = false;
+        final int MAX_SIZE = 5;
+        for (int ctr = 0; ctr < objs.length && ctr < MAX_SIZE; ctr++)
+        {
+          final Object object = objs[ctr];
+          if (object instanceof EditableWrapper)
+          {
+            if (addSep)
+            {
+              builder.append(", ");
+            }
+            final EditableWrapper wrapper = (EditableWrapper) object;
+            builder.append(wrapper.getEditable().toString());
+
+            addSep = true;
+          }
         }
-	      
-	      // see if we need to append ...
-	      if(objs.length > MAX_SIZE)
-	      {
-	        builder.append(", ...");
-	      }
 
-	      context.setText(builder.toString());
-	      context.setToolTipText(builder.toString());
-	      return;
-	    }
-	  }
-	  context.setText("<pending>");
-	  context.setToolTipText("");
-	}
-	
-	
-	
-	@Override
-	public Control getControl()
-	{
-	  return _base;
-	}
-	
+        // see if we need to append ...
+        if (objs.length > MAX_SIZE)
+        {
+          builder.append(", ...");
+        }
 
-	@Override
-	public void setActionBars(IActionBars actionBars)
-	{
-		super.setActionBars(actionBars);
-		actionBars.setGlobalActionHandler(
-				ActionFactory.UNDO.getId(), _plotEditor.getUndoAction());
-		actionBars.setGlobalActionHandler(
-				ActionFactory.REDO.getId(), _plotEditor.getRedoAction());
-		actionBars.updateActionBars();
-	}
+        context.setText(builder.toString());
+        context.setToolTipText(builder.toString());
+        return;
+      }
+    }
+    context.setText("<pending>");
+    context.setToolTipText("");
+  }
+
+  @Override
+  public void setActionBars(final IActionBars actionBars)
+  {
+    super.setActionBars(actionBars);
+    actionBars.setGlobalActionHandler(ActionFactory.UNDO.getId(), _plotEditor
+        .getUndoAction());
+    actionBars.setGlobalActionHandler(ActionFactory.REDO.getId(), _plotEditor
+        .getRedoAction());
+    actionBars.updateActionBars();
+  }
 
 }
