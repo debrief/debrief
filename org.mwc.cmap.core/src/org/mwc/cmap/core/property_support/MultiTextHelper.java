78c1e8cdc2f8affd69ee4b2e1f0894b6a7d88012
Ian Mayo <ian@planetmayo.com>
2016-05-05 10:02:10 +0100

    perform code cleanup - using project settings

--- org.mwc.cmap.core/src/org/mwc/cmap/core/property_support/MultiTextHelper.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/core/property_support/MultiTextHelper.java
@@ -33,54 +33,13 @@ import org.eclipse.swt.widgets.Text;
 public class MultiTextHelper extends EditorHelper
 {
 
-  public MultiTextHelper()
-  {
-    super(String.class);
-  }
-
-  @Override
-  public CellEditor getCellEditorFor(Composite parent)
-  {
-    return new DialogCellEditor(parent){
-      
-      private Label text;
-
-      @Override
-      protected Control createContents(Composite cell)
-      {
-        text = (Label) super.createContents(cell);
-
-        return text;
-      }
-
-
-      @Override
-      protected Object openDialogBox(Control cellEditorWindow)
-      {
-        MultiLineInputDialog dialog =
-            new MultiLineInputDialog(text.getShell(), text.getText());
-        dialog.create();
-        Point location = text.toDisplay(text.getLocation());
-        location.x -=2;
-        //location.y +=text.getBounds().height; 
-        dialog.getShell().setLocation(location);
-        dialog.open();
-        return dialog.mContent;
-      }
-      
-      
-    };
-  }
-  
- 
-
   static class MultiLineInputDialog extends Dialog
   {
 
     public Text mContentText;
     public String mContent;
 
-    public MultiLineInputDialog(Shell parentShell, String content)
+    public MultiLineInputDialog(final Shell parentShell, final String content)
     {
       super(parentShell);
       setShellStyle(getShellStyle() | SWT.NO_TRIM);
@@ -89,38 +48,7 @@ public class MultiTextHelper extends EditorHelper
     }
 
     @Override
-    protected Control createDialogArea(Composite parent)
-    {
-      Composite container = (Composite) super.createDialogArea(parent);
-      container.setLayout(new FillLayout(SWT.HORIZONTAL));
-
-      mContentText = new Text(container, SWT.BORDER | SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-      mContentText.setText(mContent);
-
-      // hide scrollbars when not necessary
-      Listener scrollBarListener = new Listener () {
-        @Override
-        public void handleEvent(Event event) {
-          Text t = (Text)event.widget;
-          Rectangle r1 = t.getClientArea();
-          Rectangle r2 = t.computeTrim(r1.x, r1.y, r1.width, r1.height);
-          Point p = t.computeSize(SWT.DEFAULT,  SWT.DEFAULT,  true);
-          t.getHorizontalBar().setVisible(r2.width <= p.x);
-          t.getVerticalBar().setVisible(r2.height <= p.y);
-          if (event.type == SWT.Modify) {
-            t.getParent().layout(true);
-            t.showSelection();
-          }
-        }
-      };
-      mContentText.addListener(SWT.Resize, scrollBarListener);
-      mContentText.addListener(SWT.Modify, scrollBarListener);
-      
-      return container;
-    }
-
-    @Override
-    protected void buttonPressed(int buttonId)
+    protected void buttonPressed(final int buttonId)
     {
       if (buttonId == IDialogConstants.OK_ID)
       {
@@ -130,10 +58,81 @@ public class MultiTextHelper extends EditorHelper
     }
 
     @Override
+    protected Control createDialogArea(final Composite parent)
+    {
+      final Composite container = (Composite) super.createDialogArea(parent);
+      container.setLayout(new FillLayout(SWT.HORIZONTAL));
+
+      mContentText =
+          new Text(container, SWT.BORDER | SWT.MULTI | SWT.H_SCROLL
+              | SWT.V_SCROLL);
+      mContentText.setText(mContent);
+
+      // hide scrollbars when not necessary
+      final Listener scrollBarListener = new Listener()
+      {
+        @Override
+        public void handleEvent(final Event event)
+        {
+          final Text t = (Text) event.widget;
+          final Rectangle r1 = t.getClientArea();
+          final Rectangle r2 = t.computeTrim(r1.x, r1.y, r1.width, r1.height);
+          final Point p = t.computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
+          t.getHorizontalBar().setVisible(r2.width <= p.x);
+          t.getVerticalBar().setVisible(r2.height <= p.y);
+          if (event.type == SWT.Modify)
+          {
+            t.getParent().layout(true);
+            t.showSelection();
+          }
+        }
+      };
+      mContentText.addListener(SWT.Resize, scrollBarListener);
+      mContentText.addListener(SWT.Modify, scrollBarListener);
+
+      return container;
+    }
+
+    @Override
     protected Point getInitialSize()
     {
       return new Point(300, 150);
     }
   }
 
+  public MultiTextHelper()
+  {
+    super(String.class);
+  }
+
+  @Override
+  public CellEditor getCellEditorFor(final Composite parent)
+  {
+    return new DialogCellEditor(parent)
+    {
+
+      private Label text;
+
+      @Override
+      protected Control createContents(final Composite cell)
+      {
+        text = (Label) super.createContents(cell);
+        return text;
+      }
+
+      @Override
+      protected Object openDialogBox(final Control cellEditorWindow)
+      {
+        final MultiLineInputDialog dialog =
+            new MultiLineInputDialog(text.getShell(), text.getText());
+        dialog.create();
+        final Point location = text.toDisplay(text.getLocation());
+        location.x -= 2;
+        // location.y +=text.getBounds().height;
+        dialog.getShell().setLocation(location);
+        dialog.open();
+        return dialog.mContent;
+      }
+    };
+  }
 }
