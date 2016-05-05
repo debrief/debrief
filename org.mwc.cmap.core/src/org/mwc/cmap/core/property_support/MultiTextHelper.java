b162bfba196948f8df4cb0906d1eff81c451d279
Anuradha Gunasekara <theanuradha@gmail.com>
2016-05-05 14:01:51 +0530

    added missing imports

--- org.mwc.cmap.core/src/org/mwc/cmap/core/property_support/MultiTextHelper.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/core/property_support/MultiTextHelper.java
@@ -20,10 +20,13 @@ import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.DialogCellEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
