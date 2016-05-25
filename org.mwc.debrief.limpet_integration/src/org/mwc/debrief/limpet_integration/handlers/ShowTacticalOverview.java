0b90c708327cb80c3a094a4a818b693948ed0446
Ian Mayo <ian@planetmayo.com>
2016-05-25 14:17:28 +0100

    introduce per-state graphs, plus sensor coverage

--- org.mwc.debrief.limpet_integration/src/org/mwc/debrief/limpet_integration/handlers/ShowTacticalOverview.java
+++ org.mwc.debrief.limpet_integration/src/org/mwc/debrief/limpet_integration/handlers/ShowTacticalOverview.java
@@ -4,17 +4,21 @@ import info.limpet.stackedcharts.model.Chart;
 import info.limpet.stackedcharts.model.ChartSet;
 import info.limpet.stackedcharts.model.DataItem;
 import info.limpet.stackedcharts.model.Dataset;
+import info.limpet.stackedcharts.model.Datum;
 import info.limpet.stackedcharts.model.DependentAxis;
 import info.limpet.stackedcharts.model.IndependentAxis;
 import info.limpet.stackedcharts.model.MarkerStyle;
 import info.limpet.stackedcharts.model.Orientation;
 import info.limpet.stackedcharts.model.PlainStyling;
+import info.limpet.stackedcharts.model.ScatterSet;
+import info.limpet.stackedcharts.model.SelectiveAnnotation;
 import info.limpet.stackedcharts.model.StackedchartsFactory;
 import info.limpet.stackedcharts.model.impl.StackedchartsFactoryImpl;
 import info.limpet.stackedcharts.ui.view.StackedChartsView;
 
 import java.awt.Color;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.Iterator;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -36,6 +40,10 @@ import Debrief.Tools.Tote.Calculations.bearingCalc;
 import Debrief.Tools.Tote.Calculations.plainCalc;
 import Debrief.Tools.Tote.Calculations.rangeCalc;
 import Debrief.Tools.Tote.Calculations.relBearingCalc;
+import Debrief.Wrappers.SensorContactWrapper;
+import Debrief.Wrappers.SensorWrapper;
+import Debrief.Wrappers.TrackWrapper;
+import MWC.GUI.BaseLayer;
 import MWC.GUI.Editable;
 import MWC.GenericData.HiResDate;
 import MWC.GenericData.Watchable;
@@ -100,64 +108,12 @@ public class ShowTacticalOverview extends AbstractHandler
 
     // do we have secondaries?
     WatchableList[] secs = data.getSecondaryTracks();
-
-    // produce the ChartSet
-    StackedchartsFactory factory = StackedchartsFactoryImpl.init();
-    ChartSet charts = factory.createChartSet();
-    charts.setOrientation(Orientation.VERTICAL);
-
-    // sort out the time axis
-    IndependentAxis ia = factory.createIndependentAxis();
-    ia.setAxisType(factory.createDateAxis());
-    ia.setName("Time");
-    charts.setSharedAxis(ia);
-
-    // produce the state charts
-    Chart thisChart = createStateChartFor(factory, pri);
-    charts.getCharts().add(thisChart);
-
-    // and the secondaries
-    for (int i = 0; i < secs.length; i++)
-    {
-      // ok, single secondary - we just need on relative plot
-      thisChart = createStateChartFor(factory, secs[i]);
-      charts.getCharts().add(thisChart);
-    }
-
-    // produce the relative chart
-    if (secs != null)
-    {
-      // ok, we have some secondaries. how many?
-      if (secs.length == 1)
-      {
-        // ok, single secondary - we just need on relative plot
-        thisChart =
-            createRelativeChartFor(factory, pri, secs[0], "Relative State");
-        charts.getCharts().add(thisChart);
-      }
-      else
-      {
-        // multiple secondaries - we need multiple relative plots
-
-        // loop through them
-        for (int i = 0; i < secs.length; i++)
-        {
-          // ok, single secondary - we just need on relative plot
-          thisChart =
-              createRelativeChartFor(factory, pri, secs[i], secs[i].getName()
-                  + " vs " + pri.getName());
-          charts.getCharts().add(thisChart);
-        }
-      }
-    }
-
-    // produce the sensor coverage
+    
+    // ok, produce the chartset model
+    ChartSet charts = produceChartSet(pri, secs);
 
     // create a new instance of the Tactical Overview
-
     String ID = StackedChartsView.ID;
-
-    // TODO: produce unique name?
     String title = editor.getTitle() + " - Tactical Overview";
     try
     {
@@ -188,6 +144,216 @@ public class ShowTacticalOverview extends AbstractHandler
     }
 
     return null;
+  }
+
+  private ChartSet produceChartSet(WatchableList pri, WatchableList[] secs)
+  {
+    // produce the ChartSet
+    StackedchartsFactory factory = StackedchartsFactoryImpl.init();
+    ChartSet charts = factory.createChartSet();
+    charts.setOrientation(Orientation.VERTICAL);
+
+    // sort out the time axis
+    IndependentAxis ia = factory.createIndependentAxis();
+    ia.setAxisType(factory.createDateAxis());
+    ia.setName("Time");
+    charts.setSharedAxis(ia);
+
+//    producePerPlatformCharts(pri, secs, factory, charts);
+
+    producePerStateCharts(pri, secs, factory, charts);
+    
+    Chart thisChart = null;
+    
+    // produce the relative chart
+    if (secs != null)
+    {
+      // ok, we have some secondaries. how many?
+      if (secs.length == 1)
+      {
+        // ok, single secondary - we just need on relative plot
+        thisChart =
+            createRelativeChartFor(factory, pri, secs[0], "Relative State");
+        charts.getCharts().add(thisChart);
+      }
+      else
+      {
+        // multiple secondaries - we need multiple relative plots
+
+        // loop through them
+        for (int i = 0; i < secs.length; i++)
+        {
+          // ok, single secondary - we just need on relative plot
+          thisChart =
+              createRelativeChartFor(factory, pri, secs[i], secs[i].getName()
+                  + " vs " + pri.getName());
+          charts.getCharts().add(thisChart);
+        }
+      }
+    }
+
+    // produce the sensor coverage
+    produceSensorCoverage(pri, ia, thisChart, factory);
+    return charts;
+  }
+
+  private void produceSensorCoverage(WatchableList pri, 
+      IndependentAxis ia, Chart thisChart, StackedchartsFactory factory)
+  {
+    ScatterSet scatter = null;
+    
+    // see what contact we have
+    if(pri instanceof TrackWrapper)
+    {
+      TrackWrapper track = (TrackWrapper) pri;
+      BaseLayer sensors = track.getSensors();
+      if(sensors.size() > 0)
+      {
+        Enumeration<Editable> numer = sensors.elements();
+        while (numer.hasMoreElements())
+        {
+          SensorWrapper thisS = (SensorWrapper) numer.nextElement();
+          if(thisS.getVisible())
+          {
+            Collection<Editable> matches = thisS.getItemsBetween(track.getStartDTG(), track.getEndDTG());
+            Iterator<Editable> sEnum = matches.iterator();
+            while (sEnum.hasNext())
+            {
+              SensorContactWrapper thisC = (SensorContactWrapper) sEnum.next();
+              if(thisC.getVisible())
+              {
+                // represent it as a datum
+                Datum datum = factory.createDatum();
+                datum.setVal(thisC.getDTG().getDate().getTime());
+
+                // ok, add it.
+                if(scatter == null)
+                {
+                  SelectiveAnnotation sel = factory.createSelectiveAnnotation();
+                  sel.getAppearsIn().add(thisChart);
+                  scatter = factory.createScatterSet();
+                  scatter.setColor(thisS.getColor());
+                  sel.setAnnotation(scatter);
+                  ia.getAnnotations().add(sel);
+                }
+                
+                scatter.getDatums().add(datum);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private void producePerStateCharts(WatchableList pri, WatchableList[] secs,
+      StackedchartsFactory factory, ChartSet charts)
+  {
+    // ok, loop through the calculations
+    Chart courseChart = factory.createChart();
+    DependentAxis courseAxis = factory.createDependentAxis();
+    courseAxis.setAxisType(factory.createNumberAxis());
+    courseAxis.setName("Course (\u00b0)");
+    courseChart.getMinAxes().add(courseAxis);
+    charts.getCharts().add(courseChart);
+    
+    Chart speedChart = factory.createChart();
+    DependentAxis speedAxis = factory.createDependentAxis();
+    speedAxis.setAxisType(factory.createNumberAxis());
+    speedAxis.setName("Speed (Kts)");
+    speedChart.getMinAxes().add(speedAxis);
+    charts.getCharts().add(speedChart);
+
+    DependentAxis depthAxis = factory.createDependentAxis();
+    depthAxis.setAxisType(factory.createNumberAxis());
+    depthAxis.setName("Depth (m)");
+    // don't add it - we won't bother until we have depth
+//    speedChart.getMaxAxes().add(speedAxis);
+
+    // do the primary
+    processTrack(pri, courseAxis, speedAxis, depthAxis, factory);
+    
+    for (int i = 0; i < secs.length; i++)
+    {
+      WatchableList sec = secs[i];
+      processTrack(sec, courseAxis, speedAxis, depthAxis, factory);
+    }
+  }
+
+  private void processTrack(WatchableList track, DependentAxis courseAxis,
+      DependentAxis speedAxis, DependentAxis depthAxis,
+      StackedchartsFactory factory)
+  {
+    // now for the actual data
+    final Collection<Editable> items =
+        track.getItemsBetween(track.getStartDTG(), track.getEndDTG());
+
+    Dataset courseData = factory.createDataset();
+    courseData.setName(track.getName() + "- Course");
+    courseAxis.getDatasets().add(courseData);
+    PlainStyling courseStyle = factory.createPlainStyling();
+    courseStyle.setColor(track.getColor());
+    Dataset speedData = factory.createDataset();
+    speedData.setName(track.getName() + "- Speed");
+    PlainStyling speedStyle = factory.createPlainStyling();
+    speedStyle.setColor(track.getColor().brighter());
+    speedData.setStyling(speedStyle);    
+    speedAxis.getDatasets().add(speedData);
+    Dataset depthData = factory.createDataset();
+    depthData.setName(track.getName() + "- Depth");
+    PlainStyling depthStyle = factory.createPlainStyling();
+    depthStyle.setColor(track.getColor().darker().darker());
+    depthData.setStyling(depthStyle);
+    
+    boolean hasDepth = false;
+    
+    for (Iterator<Editable> iterator = items.iterator(); iterator.hasNext();)
+    {
+      Watchable thisF = (Watchable) iterator.next();
+      final long thisT = thisF.getTime().getDate().getTime();
+
+      DataItem course = factory.createDataItem();
+      course.setDependentVal(Math.toDegrees(thisF.getCourse()));
+      course.setIndependentVal(thisT);
+      courseData.getMeasurements().add(course);
+
+      DataItem speed = factory.createDataItem();
+      speed.setDependentVal(thisF.getSpeed());
+      speed.setIndependentVal(thisT);
+      speedData.getMeasurements().add(speed);
+
+      DataItem depth = factory.createDataItem();
+      final double theDepth = thisF.getDepth();
+      if (theDepth != 0)
+      {
+        hasDepth = true;
+        depth.setDependentVal(theDepth);
+        depth.setIndependentVal(thisT);
+        depthData.getMeasurements().add(depth);
+      }
+    }
+    
+    if(hasDepth)
+    {
+      depthAxis.getDatasets().add(depthData);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private void producePerPlatformCharts(WatchableList pri,
+      WatchableList[] secs, StackedchartsFactory factory, ChartSet charts)
+  {
+    // produce the state charts
+    Chart thisChart = createStateChartFor(factory, pri);
+    charts.getCharts().add(thisChart);
+
+    // and the secondaries
+    for (int i = 0; i < secs.length; i++)
+    {
+      // ok, single secondary - we just need on relative plot
+      thisChart = createStateChartFor(factory, secs[i]);
+      charts.getCharts().add(thisChart);
+    }
   }
 
   private Chart createStateChartFor(StackedchartsFactory factory,
@@ -287,15 +453,18 @@ public class ShowTacticalOverview extends AbstractHandler
 
     // prepare our axes
     DependentAxis bearingAxis = factory.createDependentAxis();
+    bearingAxis.setAxisType(factory.createNumberAxis());
     bearingAxis.setName("Bearing");
     chart.getMinAxes().add(bearingAxis);
 
     DependentAxis relBearingAxis = factory.createDependentAxis();
     relBearingAxis.setName("Rel Bearing");
+    relBearingAxis.setAxisType(factory.createNumberAxis());
     chart.getMinAxes().add(relBearingAxis);
 
     DependentAxis rangeAxis = factory.createDependentAxis();
     rangeAxis.setName("Range");
+    rangeAxis.setAxisType(factory.createNumberAxis());
     chart.getMaxAxes().add(rangeAxis);
 
     // prepare our datasets
@@ -374,16 +543,5 @@ public class ShowTacticalOverview extends AbstractHandler
     }
 
     return chart;
-  }
-
-  private static String getHTMLColorString(Color color)
-  {
-    String red = Integer.toHexString(color.getRed());
-    String green = Integer.toHexString(color.getGreen());
-    String blue = Integer.toHexString(color.getBlue());
-
-    return "#" + (red.length() == 1 ? "0" + red : red)
-        + (green.length() == 1 ? "0" + green : green)
-        + (blue.length() == 1 ? "0" + blue : blue);
   }
 }
