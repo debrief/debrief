ea02ce4058c353b95157411dd48366aba0a7ca88
IanMayo <ian@planetmayo.com>
2016-07-28 13:42:07 +0100

    reflect split scatterset/dataset functionality

--- org.mwc.debrief.limpet_integration/src/org/mwc/debrief/limpet_integration/adapter/TrackConverter.java
+++ org.mwc.debrief.limpet_integration/src/org/mwc/debrief/limpet_integration/adapter/TrackConverter.java
@@ -2,7 +2,9 @@ package org.mwc.debrief.limpet_integration.adapter;
 
 import info.limpet.stackedcharts.model.DataItem;
 import info.limpet.stackedcharts.model.Dataset;
+import info.limpet.stackedcharts.model.Datum;
 import info.limpet.stackedcharts.model.PlainStyling;
+import info.limpet.stackedcharts.model.ScatterSet;
 import info.limpet.stackedcharts.model.impl.StackedchartsFactoryImpl;
 
 import java.util.ArrayList;
@@ -23,14 +25,9 @@ import MWC.GenericData.WatchableList;
 public class TrackConverter
 {
 
-  private final WatchableList list;
-
-  public TrackConverter(final WatchableList list)
+  public TrackConverter()
   {
-    this.list = list;
   }
-  
-
 
   protected abstract static class DataChoiceProvider
   {
@@ -49,10 +46,11 @@ public class TrackConverter
     }
 
     abstract List<Dataset> getDatasets(WatchableList track);
+
+    abstract List<ScatterSet> getScatterSets(WatchableList track);
   }
 
-  private abstract static class StateChoiceProvider extends
-      DataChoiceProvider
+  private abstract static class StateChoiceProvider extends DataChoiceProvider
   {
 
     StateChoiceProvider(String name, String units)
@@ -101,14 +99,50 @@ public class TrackConverter
       return res;
     }
 
+    List<ScatterSet> getScatterSets(WatchableList track)
+    {
+      List<ScatterSet> res = null;
+
+      final StackedchartsFactoryImpl factory = new StackedchartsFactoryImpl();
+
+      ScatterSet scatter = factory.createScatterSet();
+      scatter.setName(track.getName() + "-" + _name);
+      PlainStyling ps = factory.createPlainStyling();
+      ps.setColor(track.getColor());
+      ps.setLineThickness(2.0d);
+
+      Collection<Editable> elements =
+          track.getItemsBetween(track.getStartDTG(), track.getEndDTG());
+      Iterator<Editable> items = elements.iterator();
+      while (items.hasNext())
+      {
+        FixWrapper fix = (FixWrapper) items.next();
+        Datum item = factory.createDatum();
+        item.setVal(fix.getDateTimeGroup().getDate().getTime());
+
+        scatter.getDatums().add(item);
+      }
+
+      if (scatter.getDatums().size() > 0)
+      {
+        if (res == null)
+        {
+          res = new ArrayList<ScatterSet>();
+        }
+        res.add(scatter);
+      }
+
+      return res;
+    }
+
     abstract protected double valueFor(FixWrapper fix);
   }
 
-
   private Object[] _previousChoices = null;
 
-  public List<Object> convert(List<Object> res)
+  public List<Dataset> convertToDataset(final WatchableList list)
   {
+    List<Dataset> res = new ArrayList<Dataset>();
 
     // have a look at the type
     DataChoiceProvider[] choices = new DataChoiceProvider[]
@@ -157,19 +191,84 @@ public class TrackConverter
 
       for (int i = 0; i < _previousChoices.length; i++)
       {
-        DataChoiceProvider provider =
-            (DataChoiceProvider) _previousChoices[i];
+        DataChoiceProvider provider = (DataChoiceProvider) _previousChoices[i];
         List<Dataset> datasets = provider.getDatasets(list);
 
         if (datasets != null && datasets.size() > 0)
           if (res == null)
           {
-            res = new ArrayList<Object>();
+            res = new ArrayList<Dataset>();
           }
         res.addAll(datasets);
 
       }
-    } 
+    }
+    return res;
+  }
+
+  public List<ScatterSet> convertToScatterset(final WatchableList list)
+  {
+    List<ScatterSet> res = new ArrayList<ScatterSet>();
+
+    // have a look at the type
+    DataChoiceProvider[] choices = new DataChoiceProvider[]
+    {new StateChoiceProvider("Course", "degs")
+    {
+      @Override
+      protected double valueFor(FixWrapper fix)
+      {
+        return fix.getCourseDegs();
+      }
+    }, new StateChoiceProvider("Speed", "kts")
+    {
+      @Override
+      protected double valueFor(FixWrapper fix)
+      {
+        return fix.getSpeed();
+      }
+    }, new StateChoiceProvider("Depth", "m")
+    {
+      @Override
+      protected double valueFor(FixWrapper fix)
+      {
+        return fix.getLocation().getDepth();
+      }
+    }};
+
+    Shell shell = Display.getCurrent().getActiveShell();
+    ListSelectionDialog dialog =
+        new ListSelectionDialog(shell, choices, ArrayContentProvider
+            .getInstance(), new LabelProvider(),
+            "Please choose which data to display");
+
+    dialog.setTitle("Loading track:" + list.getName());
+
+    if (_previousChoices != null)
+    {
+      dialog.setInitialSelections(_previousChoices);
+    }
+
+    dialog.open();
+
+    _previousChoices = dialog.getResult();
+
+    if (_previousChoices != null)
+    {
+
+      for (int i = 0; i < _previousChoices.length; i++)
+      {
+        DataChoiceProvider provider = (DataChoiceProvider) _previousChoices[i];
+        List<ScatterSet> datasets = provider.getScatterSets(list);
+
+        if (datasets != null && datasets.size() > 0)
+          if (res == null)
+          {
+            res = new ArrayList<ScatterSet>();
+          }
+        res.addAll(datasets);
+
+      }
+    }
     return res;
   }
 
