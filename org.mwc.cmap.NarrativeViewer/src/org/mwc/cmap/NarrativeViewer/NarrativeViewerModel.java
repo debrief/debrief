d8755a443fca37a1f9244eeef124f0f3778e32eb
IanMayo <ian@planetmayo.com>
2016-10-06 10:59:17 +0100

    introduce visible filter

--- org.mwc.cmap.NarrativeViewer/src/org/mwc/cmap/NarrativeViewer/NarrativeViewerModel.java
+++ org.mwc.cmap.NarrativeViewer/src/org/mwc/cmap/NarrativeViewer/NarrativeViewerModel.java
@@ -243,7 +243,7 @@ public class NarrativeViewerModel
 
     for (final NarrativeEntry entry : myAllEntries)
     {
-      if (mySourceFilter.accept(entry) && myTypeFilter.accept(entry)
+      if (entry.getVisible() && mySourceFilter.accept(entry) && myTypeFilter.accept(entry)
           && textFilter.accept(entry))
       {
         myVisibleRows.add(entry);
