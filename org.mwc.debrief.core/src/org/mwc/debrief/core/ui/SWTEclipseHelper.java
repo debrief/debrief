a74a4ce197805de97dc9d56f994c5fea0cbe3b4a
Ian Mayo <ian@planetmayo.com>
2017-01-12 11:55:37 +0000

    implement Question Helper utility

--- org.mwc.debrief.core/src/org/mwc/debrief/core/ui/SWTEclipseHelper.java
+++ org.mwc.debrief.core/src/org/mwc/debrief/core/ui/SWTEclipseHelper.java
@@ -1,22 +1,43 @@
 package org.mwc.debrief.core.ui;
 
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 
 import Debrief.ReaderWriter.Word.ImportNarrativeDocument.QuestionHelper;
 
 public class SWTEclipseHelper implements QuestionHelper
 {
 
+  private boolean answer = false;
+  
   @Override
-  public boolean askYes(String title, String question)
+  public boolean askYes(final String title, final String question)
   {
-    MessageDialog dialog =
-        new MessageDialog(null, title, null, question, MessageDialog.QUESTION,
-            new String[]
-            {"Yes", "No"}, 0); // yes is the default
-    int result = dialog.open();
 
-    return result == 0;
+    final Display targetDisplay;
+    if(Display.getCurrent() == null)
+    {
+      targetDisplay = Display.getDefault();
+    }
+    else
+    {
+      targetDisplay = Display.getCurrent();
+    }
+    
+    targetDisplay.syncExec(new Runnable(){
+
+      @Override
+      public void run()
+      {
+        MessageDialog dialog =
+            new MessageDialog(null, title, null, question, MessageDialog.QUESTION,
+                new String[]
+                {"Yes", "No"}, 0); // yes is the default
+        answer = dialog.open() == 0;
+      }});
+    
+
+    return answer;
   }
 
 }
