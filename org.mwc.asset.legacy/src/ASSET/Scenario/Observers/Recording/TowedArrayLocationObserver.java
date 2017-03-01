873e56d155050ef295386e4892a2c9fd8aff462d
IanMayo <ian@planetmayo.com>
2017-03-01 14:12:05 +0000

    refine implementation

--- org.mwc.asset.legacy/src/ASSET/Scenario/Observers/Recording/TowedArrayLocationObserver.java
+++ org.mwc.asset.legacy/src/ASSET/Scenario/Observers/Recording/TowedArrayLocationObserver.java
@@ -24,7 +24,6 @@ import ASSET.ParticipantType;
 import ASSET.ScenarioType;
 import ASSET.Models.Decision.TargetType;
 import ASSET.Models.Detection.DetectionList;
-import ASSET.Participants.Category;
 import Debrief.Wrappers.FixWrapper;
 import Debrief.Wrappers.TrackWrapper;
 import MWC.GUI.Editable;
@@ -37,190 +36,178 @@ import MWC.GenericData.WorldVector;
 import MWC.TacticalData.Fix;
 import MWC.Utilities.TextFormatting.DebriefFormatDateTime;
 
-/** record the probable location of array elements
+/**
+ * record the probable location of array elements
  * 
  * @author ian
- *
+ * 
  */
-public class TowedArrayLocationObserver
-  extends RecordStatusToFileObserverType
+public class TowedArrayLocationObserver extends RecordStatusToFileObserverType
 {
   /***************************************************************
-   *  member variables
+   * member variables
    ***************************************************************/
-  final private HashMap<String, TrackWrapper> _trackList = new HashMap<String, TrackWrapper>();
+  final private HashMap<String, TrackWrapper> _trackList =
+      new HashMap<String, TrackWrapper>();
   final private List<Double> _offsets;
   final private String _messageName;
   final private String _sensorName;
-  final private double _defaultDepth; 
-  
+  final private double _defaultDepth;
+
   final private RecorderType _recorderType;
-  
-  private enum RecorderType
+
+  public static enum RecorderType
   {
-    HDG_DEPTH,
-    LOC_RELATIVE,
-    LOC_ABS
+    HDG_DEPTH, LOC_RELATIVE, LOC_ABS
   }
 
-
   /***************************************************************
-   *  constructor
+   * constructor
    ***************************************************************/
   /**
    * create a new monitor
-   *
-   * @param directoryName    the directory to output the plots to
-   * @param defaultDepth 
-   * @param recordDetections whether to record detections
+   * 
+   * @param directoryName
+   *          the directory to output the plots to
+   * @param defaultDepth
+   * @param recordDetections
+   *          whether to record detections
    */
   public TowedArrayLocationObserver(final String directoryName,
-                          final String fileName,
-                          final TargetType subjectType,
-                          final String observerName,
-                          final boolean isActive,
-                          final List<Double> offsets,
-                          final String messageName,
-                          final double defaultDepth,
-                          final String sensorName)
+      final String fileName, final TargetType subjectType,
+      final String observerName, final boolean isActive,
+      final List<Double> offsets, final RecorderType recorderType,
+      final String messageName, final double defaultDepth,
+      final String sensorName)
   {
-    super(directoryName, fileName, false, false, true, subjectType, observerName, isActive);
-    
+    super(directoryName, fileName, false, false, true, subjectType,
+        observerName, isActive);
+
     // store the list of offsets
     _offsets = offsets;
-    
-    // store the message name
-    _messageName = messageName;
-    
+
     _defaultDepth = defaultDepth;
     _sensorName = sensorName;
-    
-    // what are we recording?
-    switch (_messageName)
-    {
-    case "TA_FORE_AFT":
-    case "TA_MODULES":
-      _recorderType = RecorderType.HDG_DEPTH;
-      break;
-    case "LOC_RELATIVE":
-      _recorderType = RecorderType.LOC_RELATIVE;
-      break;
-    case "LOC_ABSOLUTE":
-      _recorderType = RecorderType.LOC_ABS;
-      break;
-    default:
-      _recorderType = RecorderType.HDG_DEPTH;
-    }
-
+    _recorderType = recorderType;
+    _messageName = messageName;
   }
 
   /**
-   * ************************************************************
-   * member methods
+   * ************************************************************ member methods
    * *************************************************************
    */
 
-  
-  
-  public void writeThesePositionDetails(MWC.GenericData.WorldLocation loc,
-                                        final ASSET.Participants.Status stat,
-                                        final ASSET.ParticipantType pt,
-                                        long newTime)
+  public void writeThesePositionDetails(
+      MWC.GenericData.WorldLocation platformLocation,
+      final ASSET.Participants.Status stat, final ASSET.ParticipantType pt,
+      long newTime)
   {
     final String ptName = pt.getName();
     final HiResDate debTime = new HiResDate(newTime);
 
     // find this track
     TrackWrapper track = _trackList.get(ptName);
-    if(track == null)
+    if (track == null)
     {
       track = new TrackWrapper();
       track.setName(ptName);
       _trackList.put(ptName, track);
     }
-    
+
     // find the last position in the track
     Watchable[] lastItems = track.getNearestTo(track.getEndDTG());
     WorldLocation lastLoc = null;
     double lastDepth = _defaultDepth;
-    if(lastItems != null)
+    if (lastItems != null)
     {
-      if(lastItems.length > 0)
+      if (lastItems.length > 0)
       {
         Watchable lastItem = lastItems[0];
         lastLoc = lastItem.getLocation();
         lastDepth = lastLoc.getDepth();
       }
     }
-    
+
     // sort out the heading for the item
     final double bearingRads;
-    if(lastLoc != null)
+    if (lastLoc != null)
     {
-      bearingRads = loc.subtract(lastLoc).getBearing();
+      bearingRads = platformLocation.subtract(lastLoc).getBearing();
     }
     else
     {
       bearingRads = 0d;
     }
-    
+
     // add the new fix to the track
-    Fix newFix = new Fix(debTime, loc, bearingRads, 4d);
-    
+    Fix newFix = new Fix(debTime, platformLocation, bearingRads, 4d);
+
     final double randomDepth = lastDepth + Math.random() * 1d;
-    
+
     newFix.getLocation().setDepth(randomDepth);
     FixWrapper wrapper = new FixWrapper(newFix);
     track.addFix(wrapper);
 
     final StringBuffer theseValues = new StringBuffer();
-    
+
     DecimalFormat df = new DecimalFormat("0.00");
-    
+
     // ok, see if we are ready to sort out the array locations
-    for(Double thisO: _offsets)
+    for (Double thisO : _offsets)
     {
       ArrayLength sensorOffset = new ArrayLength(thisO);
-      
+
       // get the backtrace to this distance
       FixWrapper pos = track.getBacktraceTo(debTime, sensorOffset, true);
-      if(pos != null)
+      if (pos != null)
       {
-        WorldLocation aLoc = pos.getLocation();
-        
+        final WorldLocation aLoc = pos.getLocation();
+        final double depthM = pos.getDepth();
+
         // ok, output the relevant data
-        switch(_recorderType)
+        switch (_recorderType)
         {
         case HDG_DEPTH:
           // get the heading and depth
           double hdgDegs = pos.getCourseDegs();
-          double depthM = pos.getDepth();
-          theseValues.append("[" + df.format(depthM) + " " + df.format(hdgDegs) + "] ");
+          theseValues.append("[" + df.format(depthM) + " " + df.format(hdgDegs)
+              + "] ");
           break;
         case LOC_ABS:
-          theseValues.append("[" + aLoc.getLat() + " " + aLoc.getLong() + "] ");
+          theseValues.append("" + aLoc.getLat() + " " + aLoc.getLong() + " "
+              + df.format(depthM));
           break;
         case LOC_RELATIVE:
           // ok, we need an offset here
-          WorldVector offset = aLoc.subtract(loc);
-          
-          theseValues.append(offset);
+          WorldVector offset = aLoc.subtract(platformLocation);
+
+          // ok, we need to convert to metres
+          double rangeM =
+              new WorldDistance(offset.getRange(), WorldDistance.DEGS)
+                  .getValueIn(WorldDistance.METRES);
+
+          // and the two components
+          double xDelta = rangeM * Math.sin(offset.getBearing());
+          double yDelta = rangeM * Math.cos(offset.getBearing());
+
+          theseValues.append(df.format(xDelta) + " " + df.format(yDelta) + " "
+              + depthM);
           // create xy from this
           break;
         }
-        
+
       }
     }
-    
+
     // do we have anything to output?
-    if(theseValues.length() > 0)
+    if (theseValues.length() > 0)
     {
       // ok, go for it.
       final StringBuffer buff = new StringBuffer();
 
       // dtg
       final String dtg = DebriefFormatDateTime.toString(newTime);
-      
+
       // message type
       buff.append(";");
       buff.append(_messageName);
@@ -232,7 +219,6 @@ public class TowedArrayLocationObserver
       buff.append(_sensorName);
       buff.append(" ");
       buff.append(theseValues.toString());
-      
 
       final String res = buff.toString();
 
@@ -250,54 +236,58 @@ public class TowedArrayLocationObserver
         }
       }
     }
-    
-  }
 
+  }
 
   @Override
   protected void performSetupProcessing(ScenarioType scenario)
   {
     // ok, let the parent do its stuff
     super.performSetupProcessing(scenario);
-    
+
     // clear our set of tracks
     _trackList.clear();
   }
 
   /**
    * write the current decision description to file
-   *
-   * @param pt       the participant we're looking at
-   * @param activity a description of the current activity
-   * @param dtg      the dtg at which the description was recorded
+   * 
+   * @param pt
+   *          the participant we're looking at
+   * @param activity
+   *          a description of the current activity
+   * @param dtg
+   *          the dtg at which the description was recorded
    */
-  protected void writeThisDecisionDetail(NetworkParticipant pt, String activity, long dtg)
+  protected void writeThisDecisionDetail(NetworkParticipant pt,
+      String activity, long dtg)
   {
-    //To change body of implemented methods use File | Settings | File Templates.
+    // To change body of implemented methods use File | Settings | File Templates.
   }
 
   /**
    * write these detections to file
-   *
-   * @param pt         the participant we're on about
-   * @param detections the current set of detections
-   * @param dtg        the dtg at which the detections were observed
+   * 
+   * @param pt
+   *          the participant we're on about
+   * @param detections
+   *          the current set of detections
+   * @param dtg
+   *          the dtg at which the detections were observed
    */
-  protected void writeTheseDetectionDetails(ParticipantType pt, DetectionList detections, long dtg)
-  {
-    throw new UnsupportedOperationException("Method not implemented");
-  }
-
-  public void outputThisDetection(WorldLocation loc, long dtg, String hostName, Category hostCategory,
-                                  double bearing, WorldDistance range, String sensor_name,
-                                  String label)
+  protected void writeTheseDetectionDetails(ParticipantType pt,
+      DetectionList detections, long dtg)
   {
     throw new UnsupportedOperationException("Method not implemented");
   }
 
   protected String newName(final String name)
   {
-    return "res_" + name + "_" + MWC.Utilities.TextFormatting.DebriefFormatDateTime.toString(System.currentTimeMillis()) + ".csv";
+    return "res_"
+        + name
+        + "_"
+        + MWC.Utilities.TextFormatting.DebriefFormatDateTime.toString(System
+            .currentTimeMillis()) + ".csv";
   }
 
   /**
@@ -310,14 +300,17 @@ public class TowedArrayLocationObserver
 
   /**
    * write out the file header details for this scenario
-   *
-   * @param title the scenario we're describing
+   * 
+   * @param title
+   *          the scenario we're describing
    * @throws IOException
    */
 
-  protected void writeFileHeaderDetails(final String title, long currentDTG) throws IOException
+  protected void writeFileHeaderDetails(final String title, long currentDTG)
+      throws IOException
   {
-    _os.write(";; ASSET Output recorded at:," + new java.util.Date() + ", name:," + title);
+    _os.write(";; ASSET Output recorded at:," + new java.util.Date()
+        + ", name:," + title);
     _os.write("" + System.getProperty("line.separator"));
     _os.write(";; See Debrief reference document for file formats");
     _os.write("" + System.getProperty("line.separator"));
@@ -325,20 +318,23 @@ public class TowedArrayLocationObserver
 
   protected void writeBuildDate(String details) throws IOException
   {
-    _os.write(";; ASSET build:," + details + System.getProperty("line.separator"));
+    _os.write(";; ASSET build:," + details
+        + System.getProperty("line.separator"));
   }
-  //////////////////////////////////////////////////////////////////////
+
+  // ////////////////////////////////////////////////////////////////////
   // editable properties
-  //////////////////////////////////////////////////////////////////////
+  // ////////////////////////////////////////////////////////////////////
 
-  static public class TowedArrayLocationObserverInfo extends Editable.EditorType
+  static public class TowedArrayLocationObserverInfo extends
+      Editable.EditorType
   {
-
 
     /**
      * constructor for editable details of a set of Layers
-     *
-     * @param data the Layers themselves
+     * 
+     * @param data
+     *          the Layers themselves
      */
     public TowedArrayLocationObserverInfo(final TowedArrayLocationObserver data)
     {
@@ -347,17 +343,16 @@ public class TowedArrayLocationObserver
 
     /**
      * editable GUI properties for our participant
-     *
+     * 
      * @return property descriptions
      */
     public java.beans.PropertyDescriptor[] getPropertyDescriptors()
     {
       try
       {
-        final java.beans.PropertyDescriptor[] res = {
-          prop("Directory", "The directory to place Debrief data-files"),
-          prop("Active", "Whether this observer is active"),
-        };
+        final java.beans.PropertyDescriptor[] res =
+            {prop("Directory", "The directory to place Debrief data-files"),
+                prop("Active", "Whether this observer is active"),};
 
         return res;
       }
--- org.mwc.asset.legacy/src/ASSET/Util/XML/Control/Observers/TowedArrayLocationObserverHandler.java
+++ org.mwc.asset.legacy/src/ASSET/Util/XML/Control/Observers/TowedArrayLocationObserverHandler.java
@@ -30,9 +30,11 @@ import ASSET.Models.Decision.TargetType;
 import ASSET.Scenario.Observers.CoreObserver;
 import ASSET.Scenario.Observers.ScenarioObserver;
 import ASSET.Scenario.Observers.Recording.TowedArrayLocationObserver;
+import ASSET.Scenario.Observers.Recording.TowedArrayLocationObserver.RecorderType;
 import ASSET.Util.XML.Decisions.Util.TargetTypeHandler;
 
-abstract class TowedArrayLocationObserverHandler extends CoreFileObserverHandler
+abstract class TowedArrayLocationObserverHandler extends
+    CoreFileObserverHandler
 {
 
   private final static String type = "TowedArrayLocationObserver";
@@ -42,7 +44,7 @@ abstract class TowedArrayLocationObserverHandler extends CoreFileObserverHandler
   String messageName;
   Double defaultDepth;
   String sensorName;
-  
+
   public TowedArrayLocationObserverHandler()
   {
     super(type);
@@ -60,7 +62,7 @@ abstract class TowedArrayLocationObserverHandler extends CoreFileObserverHandler
       public void setValue(String name, final String val)
       {
         offsets = new ArrayList<Double>();
-        
+
         String[] items = val.split(",");
         for (int i = 0; i < items.length; i++)
         {
@@ -96,15 +98,86 @@ abstract class TowedArrayLocationObserverHandler extends CoreFileObserverHandler
   public void elementClosed()
   {
     defaultDepth = 12d;
-    // create ourselves
-    final CoreObserver timeO = new TowedArrayLocationObserver(_directory, _fileName, _subjectName, _name, _isActive, offsets, messageName,
-        defaultDepth, sensorName);
-    
-    setObserver(timeO);
+
+    // handle the message type
+    final RecorderType recorderType;
+
+    // sort out the recorder type, and do some checking
+    switch (messageName)
+    {
+    case "TA_FORE_AFT":
+      // just two lengths
+      if (offsets.size() == 2)
+      {
+        // ok, safe
+        recorderType = RecorderType.HDG_DEPTH;
+      }
+      else
+      {
+        System.err
+            .println("Wrong number of array offsets supplied. Should be two (fore/after), but got "
+                + offsets.size());
+        recorderType = null;
+      }
+      break;
+    case "TA_MODULES":
+      recorderType = RecorderType.HDG_DEPTH;
+      break;
+    case "TA_COG_REL":
+      // just two lengths
+      if (offsets.size() == 1)
+      {
+        // ok, safe
+        recorderType = RecorderType.LOC_RELATIVE;
+      }
+      else
+      {
+        System.err
+            .println("Wrong number of array offsets supplied. Should be one (Centre of Graviry), but got "
+                + offsets.size());
+        recorderType = null;
+      }
+      break;
+    case "TA_COG_ABS":
+      // just two lengths
+      if (offsets.size() == 1)
+      {
+        // ok, safe
+        recorderType = RecorderType.LOC_ABS;
+      }
+      else
+      {
+        System.err
+            .println("Wrong number of array offsets supplied. Should be one (Centre of Graviry), but got "
+                + offsets.size());
+        recorderType = null;
+      }
+      break;
+    default:
+    {
+      System.err
+          .println("Towed Array Location Observer Handler : Message format not found for:"
+              + messageName);
+      recorderType = null;
+    }
+    }
+
+    // have we got suitable data?
+    if (recorderType != null)
+    {
+      // create ourselves
+      final CoreObserver timeO =
+          new TowedArrayLocationObserver(_directory, _fileName, _subjectName,
+              _name, _isActive, offsets, recorderType, messageName,
+              defaultDepth, sensorName);
+
+      setObserver(timeO);
+
+    }
 
     // and reset
     super.elementClosed();
-    
+
     // clear the params
     _subjectName = null;
     messageName = null;
@@ -112,14 +185,12 @@ abstract class TowedArrayLocationObserverHandler extends CoreFileObserverHandler
     sensorName = null;
   }
 
-
   abstract public void setObserver(ScenarioObserver obs);
 
-  static public void exportThis(final Object toExport, final org.w3c.dom.Element parent,
-                                final org.w3c.dom.Document doc)
+  static public void exportThis(final Object toExport,
+      final org.w3c.dom.Element parent, final org.w3c.dom.Document doc)
   {
     throw new UnsupportedOperationException("Method not implemented");
   }
-
 
 }
