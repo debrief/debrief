6aeea8ecb5e3c719723d20509b72772aa886b0fb
IanMayo <ian@planetmayo.com>
2017-03-07 09:09:18 +0000

    code clean up

--- org.mwc.debrief.legacy/src/Debrief/Wrappers/Extensions/Measurements/CoreDataset.java
+++ org.mwc.debrief.legacy/src/Debrief/Wrappers/Extensions/Measurements/CoreDataset.java
@@ -4,31 +4,31 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CoreDataset<IndexType extends Number, ValueType> implements DataItem, Serializable
+public class CoreDataset<IndexType extends Number, ValueType> implements
+    DataItem, Serializable
 {
   /**
    * 
    */
   private static final long serialVersionUID = 1L;
-  
+
   private final List<IndexType> _indices = new ArrayList<IndexType>();
   private final List<ValueType> _values = new ArrayList<ValueType>();
-  private String _name;
-  private String _units;
-  
-  
+  private final String _name;
+  private final String _units;
+
   public CoreDataset(final String name, final String units)
   {
     _name = name;
     _units = units;
   }
-  
+
   public void add(IndexType index, ValueType value)
   {
     _indices.add(index);
     _values.add(value);
   }
-  
+
   @Override
   public String getName()
   {
@@ -39,7 +39,7 @@ public class CoreDataset<IndexType extends Number, ValueType> implements DataIte
   {
     return _units;
   }
-  
+
   /**
    * convenience function, to describe this plottable as a string
    */
@@ -47,12 +47,12 @@ public class CoreDataset<IndexType extends Number, ValueType> implements DataIte
   {
     return getName() + " (" + size() + " items)";
   }
-  
+
   public void printAll()
   {
-    System.out.println(":"  + getName());
+    System.out.println(":" + getName());
     final int len = _indices.size();
-    for(int i=0;i<len;i++)
+    for (int i = 0; i < len; i++)
     {
       System.out.println("i:" + _indices.get(i) + " v:" + _values.get(i));
     }
@@ -62,5 +62,4 @@ public class CoreDataset<IndexType extends Number, ValueType> implements DataIte
   {
     return _indices.size();
   }
-
 }
