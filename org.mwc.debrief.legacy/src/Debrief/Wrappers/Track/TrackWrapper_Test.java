0b8abce4b4f9bf01cb97fd4564d68ebea8fc8e39
Ian Mayo <ian@planetmayo.com>
2017-05-25 17:16:26 +0100

    fix test

--- org.mwc.debrief.legacy/src/Debrief/Wrappers/Track/TrackWrapper_Test.java
+++ org.mwc.debrief.legacy/src/Debrief/Wrappers/Track/TrackWrapper_Test.java
@@ -1758,7 +1758,7 @@ public class TrackWrapper_Test extends junit.framework.TestCase
         .firstElement());
     assertEquals(
         "correct title",
-        "Sorry, 'Positions' and 'test track' overlap in time. Please correct this and retry",
+        "Sorry, '010005.10' and 'test track' overlap in time. Please correct this and retry",
         _messages._messages.firstElement());
     assertEquals("correct title", MessageProvider.ERROR,
         (int) _messages._statuses.firstElement());
