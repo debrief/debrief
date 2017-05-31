2f67e1e8a2fce8d8c47b6e14b858d177495432f6
Ian Mayo <ian@planetmayo.com>
2017-05-31 11:35:43 +0100

    prevent Layers returning single-point area

--- org.mwc.debrief.legacy/src/Debrief/Wrappers/Track/TrackWrapper_Test.java
+++ org.mwc.debrief.legacy/src/Debrief/Wrappers/Track/TrackWrapper_Test.java
@@ -473,6 +473,27 @@ public class TrackWrapper_Test extends junit.framework.TestCase
     assertEquals("has segments", "Track segments (3 items)", sl.toString());
  //   assertEquals("has all fixes", 49, tw.numFixes());
   }
+  
+  public void testEmptyLayerBounds()
+  {
+    Layers layers = new Layers();
+    TrackWrapper track = new TrackWrapper();
+    track.setName("Track");
+    FixWrapper fix = createFix(1000, 10, 20);
+    track.addFix(fix);
+    
+    assertNotNull("returned an area", layers.getBounds());
+    assertEquals("correct location", " 50°51'17.63\"N 001°20'32.10\"W ", layers.getBounds().getCentre().toString());
+    assertEquals("correct location", " 51°12'08.27\"N 001°58'07.62\"W ", layers.getBounds().getTopLeft().toString());
+    assertEquals("correct location", " 50°30'26.99\"N 000°42'56.58\"W ", layers.getBounds().getBottomRight().toString());
+    
+    // ok, now put the track in the layers
+    layers.addThisLayer(track);
+    assertEquals("correct location", " 10°00'00.00\"N 020°00'00.00\"E ", layers.getBounds().getCentre().toString());
+    assertEquals("correct location", " 10°00'42.43\"N 019°59'16.92\"E ", layers.getBounds().getTopLeft().toString());
+    assertEquals("correct location", " 09°59'17.57\"N 020°00'43.08\"E ", layers.getBounds().getBottomRight().toString());
+    
+  }
 
   public void testDecimatePositionsAndData() throws InterruptedException
   {
