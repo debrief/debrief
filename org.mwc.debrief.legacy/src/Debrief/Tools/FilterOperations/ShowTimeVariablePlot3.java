2176dde70760537276a09053fd5922f22d110ced
Ian Mayo <ian@planetmayo.com>
2017-06-01 17:15:44 +0100

    improve static analysis

--- org.mwc.debrief.legacy/src/Debrief/Tools/FilterOperations/ShowTimeVariablePlot3.java
+++ org.mwc.debrief.legacy/src/Debrief/Tools/FilterOperations/ShowTimeVariablePlot3.java
@@ -930,7 +930,8 @@ public final class ShowTimeVariablePlot3 implements FilterOperation
                   connectToPrevious =
                       insertWrappingPoints(lastSecondaryValue, thisVal,
                           lastTime, currentTime, thisColor, thisSeries,
-                          connectToPrevious, provider, theAdder, myOperation._clipMax);
+                          connectToPrevious, provider, theAdder,
+                          myOperation._clipMax);
                 }
                 // ////////////////////////////////////////////////
                 // THANK YOU, WE'RE PLEASED TO RETURN YOU TO
@@ -1045,8 +1046,8 @@ public final class ShowTimeVariablePlot3 implements FilterOperation
               {
                 // add extra points, if we need to
                 connectToPrevious =
-                    insertWrappingPoints(lastSecondaryValue, thisVal,
-                        lastTime, currentTime, thisColor, thisSeries, connectToPrevious,
+                    insertWrappingPoints(lastSecondaryValue, thisVal, lastTime,
+                        currentTime, thisColor, thisSeries, connectToPrevious,
                         provider, theAdder, myOperation._clipMax);
               }
 
@@ -1066,15 +1067,11 @@ public final class ShowTimeVariablePlot3 implements FilterOperation
 
                     final TrackSegment prevSeg = prevFix.getSegment();
                     // is it from a different segment?
-                    if (!thisSeg.equals(prevSeg))
+                    if (!thisSeg.equals(prevSeg)
+                        && (prevSeg instanceof DynamicInfillSegment && !(thisSeg instanceof DynamicInfillSegment)))
                     {
-                      // was it dynamic, but we're not?
-                      if (prevSeg instanceof DynamicInfillSegment
-                          && !(thisSeg instanceof DynamicInfillSegment))
-                      {
-                        // ok, use the previous color
-                        thisColor = prevFix.getColor();
-                      }
+                      // ok, use the previous color
+                      thisColor = prevFix.getColor();
                     }
                   }
                 }
@@ -1128,6 +1125,7 @@ public final class ShowTimeVariablePlot3 implements FilterOperation
   /**
    * method to decide if we need to insert extra (non-joined) points to reflect fact that data wraps
    * through 360 degs
+   * 
    * @param lastSecondaryValue
    *          the last value calculated
    * @param thisVal
@@ -1147,13 +1145,12 @@ public final class ShowTimeVariablePlot3 implements FilterOperation
    * 
    * @return whether the next line segment should connect to this one
    */
-  private static boolean insertWrappingPoints(
-      final double lastSecondaryValue, final double thisVal,
-      final HiResDate lastTime, final HiResDate currentTime,
-      final Color thisColor, final Series thisSeries,
-      final boolean connectToPrevious, final ColouredDataItem.OffsetProvider provider,
-      final VersatileSeriesAdder theAdder,
-      final double clipMax)
+  private static boolean insertWrappingPoints(final double lastSecondaryValue,
+      final double thisVal, final HiResDate lastTime,
+      final HiResDate currentTime, final Color thisColor,
+      final Series thisSeries, final boolean connectToPrevious,
+      final ColouredDataItem.OffsetProvider provider,
+      final VersatileSeriesAdder theAdder, final double clipMax)
   {
     boolean connectToPrev = connectToPrevious;
     // is this the first point?
