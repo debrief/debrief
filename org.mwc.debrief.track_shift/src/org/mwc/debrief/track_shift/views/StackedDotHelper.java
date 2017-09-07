fed539011b40ab4565fa09a4ea4e6889fbdad415
Ian Mayo <ian@planetmayo.com>
2017-09-07 13:35:40 +0100

    Robustly handle instance where we can't find O/S location for a sensor cut

--- org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/StackedDotHelper.java
+++ org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/StackedDotHelper.java
@@ -358,7 +358,7 @@ public final class StackedDotHelper
                 // if there are any matching items
 
               } // if we find a match
-              else if(targetTrack == null)
+              else if(targetTrack == null && hostFix != null)
               {
                 // no target data, just use ownship sensor data
                 thisDub = new Doublet(scw, null, null, hostFix);
