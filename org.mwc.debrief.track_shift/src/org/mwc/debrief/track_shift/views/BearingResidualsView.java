892175a2be8c03ef15741a7c2c8549dc9bc7d886
Ian Mayo <ian@planetmayo.com>
2017-10-04 18:13:55 +0100

    fix tests

--- org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/BearingResidualsView.java
+++ org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/BearingResidualsView.java
@@ -504,13 +504,13 @@ public class BearingResidualsView extends BaseStackedDotsView implements
       assertEquals("found correct number of zones", 12, legs.size());
 
       assertNotNull("found zigs", zigs);
-      assertEquals("found correct number of zig cuts", 23, zigs.size());
+      assertEquals("found correct number of zig cuts", 22, zigs.size());
 
       // ok, ditch those cuts
       final int fullSensorLen = sensor.size();
       Map<SensorWrapper, LegOfCuts> deleted =
           DeleteCutsOperation.deleteTheseCuts(zigs);
-      assertEquals("fewer cuts", 98, sensor.size());
+      assertEquals("fewer cuts", 99, sensor.size());
 
       // ok, and undo them
       BearingResidualsView.restoreCuts(deleted);
