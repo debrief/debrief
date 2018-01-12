a4f95047c623086defd978c6d28b11eef377a7bb
Ian Mayo <ian@planetmayo.com>
2018-01-12 15:02:18 +0000

    First steps to calculating F-nought

--- org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/FrequencyResidualsView.java
+++ org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/views/FrequencyResidualsView.java
@@ -14,16 +14,29 @@
  */
 package org.mwc.debrief.track_shift.views;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IToolBarManager;
+import org.jfree.data.Range;
+import org.jfree.data.time.DateRange;
+import org.jfree.data.time.FixedMillisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.TimeSeriesDataItem;
 import org.mwc.cmap.core.CorePlugin;
-import org.mwc.debrief.track_shift.TrackShiftActivator;
 import org.mwc.debrief.track_shift.controls.ZoneChart.ColorProvider;
 import org.mwc.debrief.track_shift.controls.ZoneChart.ZoneSlicer;
+import org.mwc.debrief.track_shift.freq.DopplerCurve;
+
+import MWC.GUI.JFreeChart.ColouredDataItem;
 
 public class FrequencyResidualsView extends BaseStackedDotsView
 {
+  private static final int NUM_DOPPLER_STEPS = 30;
   private Action calcBaseFreq;
 
   public FrequencyResidualsView()
@@ -46,16 +59,15 @@ public class FrequencyResidualsView extends BaseStackedDotsView
     // note: we now ignore the parent doublets value.
     // this is because we need to regenerate the target fix each time.
     // For frequency data the target fix is interpolated - this means
-    // it doesn't update as we drag the solution.  We need to 
+    // it doesn't update as we drag the solution. We need to
     // regenerate the interpolated fix, to ensure we're using up-to-date
     // values
     boolean updateDoubletsVal = true;
-    
+
     // update the current datasets
     _myHelper.updateFrequencyData(_dotPlot, _linePlot, _myTrackDataProvider,
         _onlyVisible.isChecked(), _holder, this, updateDoubletsVal);
   }
-
 
   @Override
   protected void addExtras(final IToolBarManager toolBarManager)
@@ -63,33 +75,99 @@ public class FrequencyResidualsView extends BaseStackedDotsView
     super.addExtras(toolBarManager);
     toolBarManager.add(calcBaseFreq);
   }
-  
+
   @Override
   protected void makeActions()
   {
     super.makeActions();
-    
-    // now frequency calculator
-    calcBaseFreq = new Action("Calculate base frequency", IAction.AS_PUSH_BUTTON)
-    {
-      @Override
-      public void run()
-      {
-        super.run();
 
-        calculateBaseFreq();
-      }
-    };
+    // now frequency calculator
+    calcBaseFreq =
+        new Action("Calculate base frequency", IAction.AS_PUSH_BUTTON)
+        {
+          @Override
+          public void run()
+          {
+            super.run();
+
+            calculateBaseFreq();
+          }
+        };
     calcBaseFreq.setChecked(true);
-    calcBaseFreq.setToolTipText("Calculate the base frequency of the visible frequency cuts");
+    calcBaseFreq
+        .setToolTipText("Calculate the base frequency of the visible frequency cuts");
     calcBaseFreq.setImageDescriptor(CorePlugin
         .getImageDescriptor("icons/24/calculator.png"));
   }
 
   protected void calculateBaseFreq()
   {
-    // TODO Auto-generated method stub
-    
+    // get the currently visible data
+    TimeSeriesCollection lineData =
+        (TimeSeriesCollection) _linePlot.getDataset();
+    TimeSeries measured = lineData.getSeries(StackedDotHelper.MEASURED_DATASET);
+
+    if (measured != null)
+    {
+      ArrayList<Long> times = new ArrayList<Long>();
+      ArrayList<Double> freqs = new ArrayList<Double>();
+
+      // get the visible time range
+      DateRange curRange = (DateRange) _linePlot.getDomainAxis().getRange();
+
+      // loop through the measured data
+      Iterator<?> items = measured.getItems().iterator();
+      while (items.hasNext())
+      {
+        ColouredDataItem next = (ColouredDataItem) items.next();
+        if (curRange.contains(next.getPeriod().getMiddleMillisecond()))
+        {
+          times.add(next.getPeriod().getMiddleMillisecond());
+          freqs.add(next.getValue().doubleValue());
+        }
+      }
+
+      if (!times.isEmpty())
+      {
+        // generate the doppler curve object
+        final DopplerCurve curve = new DopplerCurve(times, freqs);
+
+        // plot the curve
+        TimeSeries calculatedData = calcSeries(curve, curRange);
+        lineData.addSeries(calculatedData);
+
+        // does it contain an inflection ponit?
+        if (curve.hasInflection())
+        {
+          // yes, update the sensor
+          CorePlugin.showMessage("Calculate f-nought",
+              "F=Nought is:" + curve.inflectionFreq());
+
+        }
+        else
+        {
+          // no, display warning
+          CorePlugin.showMessage("Calculate f-nought",
+              "Sorry, an inflection point cannot be found in the data");
+        }
+
+        // and remove the curve
+        lineData.removeSeries(calculatedData);
+      }
+    }
+  }
+
+  private TimeSeries calcSeries(DopplerCurve curve, DateRange curRange)
+  {
+    TimeSeries res = new TimeSeries("Fitted curve");
+    long start = curRange.getLowerMillis();
+    long end = curRange.getUpperMillis();
+    long step = (end - start) / NUM_DOPPLER_STEPS;
+    for (long t = start; t <= end; t += step)
+    {
+      res.add(new TimeSeriesDataItem(new FixedMillisecond(t), curve.valueAt(t)));
+    }
+    return res;
   }
 
   @Override
@@ -98,7 +176,7 @@ public class FrequencyResidualsView extends BaseStackedDotsView
     // don't bother, it's for bearing data
     return null;
   }
-  
+
   @Override
   protected String formatValue(double value)
   {
