7806674f8f354a6cade2aa4e20cc5fc7fca7c178
Ian Mayo <ian@planetmayo.com>
2018-02-09 11:18:33 +0000

    Improve static analysis

--- org.mwc.debrief.legacy/src/Debrief/Wrappers/Track/Doublet.java
+++ org.mwc.debrief.legacy/src/Debrief/Wrappers/Track/Doublet.java
@@ -27,127 +27,133 @@ import MWC.TacticalData.Fix;
 
 public final class Doublet implements Comparable<Doublet>
 {
-	// ////////////////////////////////////////////////////////////////////////////////////////////////
-	// testing for this class
-	// ////////////////////////////////////////////////////////////////////////////////////////////////
-	static public final class testCalc extends junit.framework.TestCase
-	{
+  // ////////////////////////////////////////////////////////////////////////////////////////////////
+  // testing for this class
+  // ////////////////////////////////////////////////////////////////////////////////////////////////
+  static public final class testCalc extends junit.framework.TestCase
+  {
 
-		private double convertAndTest(final double myCrseDegs, final double bearingDegs,
-				final double mySpeedKts, final double observedFreq)
-		{
-			final double myCrseRads = MWC.Algorithms.Conversions.Degs2Rads(myCrseDegs);
-			final double bearingRads = MWC.Algorithms.Conversions.Degs2Rads(bearingDegs);
-			return FrequencyCalcs.calcDopplerComponent(bearingRads, myCrseRads, mySpeedKts,
-					observedFreq);
-		}
+    private double convertAndTest(final double myCrseDegs,
+        final double bearingDegs, final double mySpeedKts,
+        final double observedFreq)
+    {
+      final double myCrseRads =
+          MWC.Algorithms.Conversions.Degs2Rads(myCrseDegs);
+      final double bearingRads =
+          MWC.Algorithms.Conversions.Degs2Rads(bearingDegs);
+      return FrequencyCalcs.calcDopplerComponent(bearingRads, myCrseRads,
+          mySpeedKts, observedFreq);
+    }
 
-		public void testCorrected()
-		{
-			double res = convertAndTest(320, 28, 8, 300);
-			assertEquals("right freq", 0.304, res, 0.1);
+    public void testCorrected()
+    {
+      double res = convertAndTest(320, 28, 8, 300);
+      assertEquals("right freq", 0.304, res, 0.1);
 
-			res = convertAndTest(320, 328, 8, 300);
-			assertEquals("right freq", 0.805, res, 0.1);
+      res = convertAndTest(320, 328, 8, 300);
+      assertEquals("right freq", 0.805, res, 0.1);
 
-			res = convertAndTest(320, 158, 8, 300);
-			assertEquals("right freq", -0.7734, res, 0.01);
+      res = convertAndTest(320, 158, 8, 300);
+      assertEquals("right freq", -0.7734, res, 0.01);
 
-			res = convertAndTest(320, 158, 9, 300);
-			assertEquals("right freq", -0.870, res, 0.01);
+      res = convertAndTest(320, 158, 9, 300);
+      assertEquals("right freq", -0.870, res, 0.01);
 
-			res = convertAndTest(150, 158, 9, 300);
-			assertEquals("right freq", 0.906, res, 0.01);
-		}
-	}
+      res = convertAndTest(150, 158, 9, 300);
+      assertEquals("right freq", 0.906, res, 0.01);
+    }
+  }
 
-	public static final double INVALID_BASE_FREQUENCY = Double.NaN;
+  public static final double INVALID_BASE_FREQUENCY = Double.NaN;
 
-	/**
-	 * Calculate the current doppler shift between the two positons
-	 * 
-	 * @param SpeedOfSound
-	 *          the speed of sound to use, m/sec
-	 * @return
-	 */
-	public static double getDopplerShift(final double SpeedOfSound, final double fNought, final Fix host, final Fix tgt)
-	{
-		final double osKts = Conversions.Yps2Kts(host.getSpeed());
-		final double tgtKts = Conversions.Yps2Kts(tgt.getSpeed());
-		
-		final double osSpeed = MWC.Algorithms.Conversions.Kts2Mps(osKts);
-		final double tgtSpeed = MWC.Algorithms.Conversions.Kts2Mps(tgtKts);
-		final double osHeadingRads = host.getCourse();
-		final double tgtHeadingRads = tgt.getCourse();
+  /**
+   * Calculate the current doppler shift between the two positons
+   * 
+   * @param SpeedOfSound
+   *          the speed of sound to use, m/sec
+   * @return
+   */
+  public static double getDopplerShift(final double SpeedOfSound,
+      final double fNought, final Fix host, final Fix tgt)
+  {
+    final double osKts = Conversions.Yps2Kts(host.getSpeed());
+    final double tgtKts = Conversions.Yps2Kts(tgt.getSpeed());
 
-		// produce dLat, dLong at the correct point on the earth
-		final WorldVector offset = tgt.getLocation().subtract(host.getLocation());
+    final double osSpeed = MWC.Algorithms.Conversions.Kts2Mps(osKts);
+    final double tgtSpeed = MWC.Algorithms.Conversions.Kts2Mps(tgtKts);
+    final double osHeadingRads = host.getCourse();
+    final double tgtHeadingRads = tgt.getCourse();
 
-		// done, go for it.
-		return FrequencyCalcs.calcPredictedFreqSI(SpeedOfSound, osHeadingRads, tgtHeadingRads,
-				osSpeed, tgtSpeed, offset.getBearing(), fNought);
-	}
+    // produce dLat, dLong at the correct point on the earth
+    final WorldVector offset = tgt.getLocation().subtract(host.getLocation());
 
-	private final SensorContactWrapper _sensor;
+    // done, go for it.
+    return FrequencyCalcs.calcPredictedFreqSI(SpeedOfSound, osHeadingRads,
+        tgtHeadingRads, osSpeed, tgtSpeed, offset.getBearing(), fNought);
+  }
 
-	private final FixWrapper _targetFix;
+  private final SensorContactWrapper _sensor;
 
-	private final FixWrapper _hostFix;
+  private final FixWrapper _targetFix;
 
-	private final TrackSegment _targetTrack;
+  private final FixWrapper _hostFix;
 
-	// ////////////////////////////////////////////////
-	// working variables to help us along.
-	// ////////////////////////////////////////////////
-	private static final WorldLocation _workingSensorLocation = new WorldLocation(0.0,
-			0.0, 0.0);
+  private final TrackSegment _targetTrack;
 
-	private static final WorldLocation _workingTargetLocation = new WorldLocation(0.0,
-			0.0, 0.0);
+  // ////////////////////////////////////////////////
+  // working variables to help us along.
+  // ////////////////////////////////////////////////
+  private static final WorldLocation _workingSensorLocation =
+      new WorldLocation(0.0, 0.0, 0.0);
 
-	// ////////////////////////////////////////////////
-	// constructor
-	// ////////////////////////////////////////////////
-	public Doublet(final SensorContactWrapper sensor, final FixWrapper targetFix,
-			final TrackSegment parent, final FixWrapper hostFix)
-	{
-		_sensor = sensor;
-		_targetFix = targetFix;
-		_targetTrack = parent;
-		_hostFix = hostFix;
-	}
+  private static final WorldLocation _workingTargetLocation =
+      new WorldLocation(0.0, 0.0, 0.0);
 
-	/**
-	 * ok find bearing error (wrapped to -..360)
-	 * 
-	 * @param measuredValue
-	 * @param calcValue
-	 * @return
-	 */
-	public double calculateBearingError(final double measuredValue, final double calcValue)
-	{
-		double theError = measuredValue - calcValue;
+  // ////////////////////////////////////////////////
+  // constructor
+  // ////////////////////////////////////////////////
+  public Doublet(final SensorContactWrapper sensor, final FixWrapper targetFix,
+      final TrackSegment parent, final FixWrapper hostFix)
+  {
+    _sensor = sensor;
+    _targetFix = targetFix;
+    _targetTrack = parent;
+    _hostFix = hostFix;
+  }
 
-		while (theError > 180)
-			theError -= 360.0;
+  /**
+   * ok find bearing error (wrapped to -..360)
+   * 
+   * @param measuredValue
+   * @param calcValue
+   * @return
+   */
+  public double calculateBearingError(final double measuredValue,
+      final double calcValue)
+  {
+    double theError = measuredValue - calcValue;
 
-		while (theError < -180)
-			theError += 360.0;
+    while (theError > 180)
+      theError -= 360.0;
 
-		return theError;
-	}
+    while (theError < -180)
+      theError += 360.0;
 
-	/**
-	 * ok find frequency error
-	 * 
-	 * @param measuredValue
-	 * @param calcValue
-	 * @return
-	 */
-	public double calculateFreqError(final double measuredValue, final double calcValue)
-	{
-		return measuredValue - calcValue;
-	}
+    return theError;
+  }
+
+  /**
+   * ok find frequency error
+   * 
+   * @param measuredValue
+   * @param calcValue
+   * @return
+   */
+  public double calculateFreqError(final double measuredValue,
+      final double calcValue)
+  {
+    return measuredValue - calcValue;
+  }
 
   @Override
   public int compareTo(final Doublet o)
@@ -187,154 +193,154 @@ public final class Doublet implements Comparable<Doublet>
     return res;
   }
 
-	public double getAmbiguousMeasuredBearing()
-	{
-		return _sensor.getAmbiguousBearing();
-	}
-	
-	public boolean getHasBeenResolved()
-	{
-	  return !_sensor.getHasAmbiguousBearing();
-	}
+  public double getAmbiguousMeasuredBearing()
+  {
+    return _sensor.getAmbiguousBearing();
+  }
 
-	/**
-	 * get the base frequency for this measured tonal, if there is one
-	 * 
-	 * @return
-	 */
-	public double getBaseFrequency()
-	{
-		return _sensor.getSensor().getBaseFrequency();
-	}
+  /**
+   * get the base frequency for this measured tonal, if there is one
+   * 
+   * @return
+   */
+  public double getBaseFrequency()
+  {
+    return _sensor.getSensor().getBaseFrequency();
+  }
 
-	public double getCalculatedBearing(final WorldVector sensorOffset,
-			final WorldVector targetOffset)
-	{
-		// copy our locations
-		_workingSensorLocation.copy(_sensor.getCalculatedOrigin(null));
-		_workingTargetLocation.copy(_targetFix.getLocation());
+  public double getCalculatedBearing(final WorldVector sensorOffset,
+      final WorldVector targetOffset)
+  {
+    // copy our locations
+    _workingSensorLocation.copy(_sensor.getCalculatedOrigin(null));
+    _workingTargetLocation.copy(_targetFix.getLocation());
 
-		// apply the offsets
-		if (sensorOffset != null)
-			_workingSensorLocation.addToMe(sensorOffset);
-		if (targetOffset != null)
-			_workingTargetLocation.addToMe(targetOffset);
+    // apply the offsets
+    if (sensorOffset != null)
+      _workingSensorLocation.addToMe(sensorOffset);
+    if (targetOffset != null)
+      _workingTargetLocation.addToMe(targetOffset);
 
-		// calculate the current bearing
-		final WorldVector error = _workingTargetLocation
-				.subtract(_workingSensorLocation);
-		double calcBearing = error.getBearing();
-		calcBearing = Conversions.Rads2Degs(calcBearing);
+    // calculate the current bearing
+    final WorldVector error =
+        _workingTargetLocation.subtract(_workingSensorLocation);
+    double calcBearing = error.getBearing();
+    calcBearing = Conversions.Rads2Degs(calcBearing);
 
-		if (calcBearing < 0)
-			calcBearing += 360;
+    if (calcBearing < 0)
+      calcBearing += 360;
 
-		return calcBearing;
-	}
+    return calcBearing;
+  }
 
-	/**
-	 * get the colour of this sensor fix
-	 */
-	public Color getColor()
-	{
-		return _sensor.getColor();
-	}
+  /**
+   * get the colour of this sensor fix
+   */
+  public Color getColor()
+  {
+    return _sensor.getColor();
+  }
 
-	/**
-	 * calculate the corrected frequency (take out ownship doppler)
-	 * 
-	 * @return
-	 */
-	public double getCorrectedFrequency()
-	{
-		double correctedFreq = 0;
+  /**
+   * calculate the corrected frequency (take out ownship doppler)
+   * 
+   * @return
+   */
+  public double getCorrectedFrequency()
+  {
+    double correctedFreq = 0;
 
-		final double theBearingDegs = getCalculatedBearing(null, null);
-		final double theBearingRads = MWC.Algorithms.Conversions
-				.Degs2Rads(theBearingDegs);
-		final double myCourseRads = _hostFix.getCourse();
+    final double theBearingDegs = getCalculatedBearing(null, null);
+    final double theBearingRads =
+        MWC.Algorithms.Conversions.Degs2Rads(theBearingDegs);
+    final double myCourseRads = _hostFix.getCourse();
 
-		final double mySpeedKts = _hostFix.getSpeed();
-		final double observedFreq = _sensor.getFrequency();
-		final double dopplerComponent = FrequencyCalcs.calcDopplerComponent(theBearingRads,
-				myCourseRads, mySpeedKts, observedFreq);
+    final double mySpeedKts = _hostFix.getSpeed();
+    final double observedFreq = _sensor.getFrequency();
+    final double dopplerComponent =
+        FrequencyCalcs.calcDopplerComponent(theBearingRads, myCourseRads,
+            mySpeedKts, observedFreq);
 
-		correctedFreq = observedFreq + dopplerComponent;
+    correctedFreq = observedFreq + dopplerComponent;
 
-		return correctedFreq;
-	}
+    return correctedFreq;
+  }
 
-	// ////////////////////////////////////////////////
-	// member methods
-	// ////////////////////////////////////////////////
-	/**
-	 * get the DTG of this contact
-	 * 
-	 * @return the DTG
-	 */
-	public HiResDate getDTG()
-	{
-		return _sensor.getDTG();
-	}
+  // ////////////////////////////////////////////////
+  // member methods
+  // ////////////////////////////////////////////////
+  /**
+   * get the DTG of this contact
+   * 
+   * @return the DTG
+   */
+  public HiResDate getDTG()
+  {
+    return _sensor.getDTG();
+  }
 
-	public FixWrapper getHost()
-	{
-		return _hostFix;
-	}
+  public boolean getHasBeenResolved()
+  {
+    return !_sensor.getHasAmbiguousBearing();
+  }
 
-	public double getMeasuredBearing()
-	{
-		return _sensor.getBearing();
-	}
+  public FixWrapper getHost()
+  {
+    return _hostFix;
+  }
 
-	public double getMeasuredFrequency()
-	{
-		return _sensor.getFrequency();
-	}
+  public double getMeasuredBearing()
+  {
+    return _sensor.getBearing();
+  }
 
-	/**
-	 * calculate what the frequency of the target should be (base freq plus both
-	 * dopplers)
-	 * 
-	 * @return
-	 */
-	public double getPredictedFrequency(final double speedOfSoundKts)
-	{
-		double predictedFreq = Double.NaN;
+  public double getMeasuredFrequency()
+  {
+    return _sensor.getFrequency();
+  }
 
-		if (_targetTrack instanceof CoreTMASegment)
-		{
+  /**
+   * calculate what the frequency of the target should be (base freq plus both dopplers)
+   * 
+   * @return
+   */
+  public double getPredictedFrequency(final double speedOfSoundKts)
+  {
+    double predictedFreq = Double.NaN;
+
+    if (_targetTrack instanceof CoreTMASegment)
+    {
       final double baseFreq = getBaseFrequency();
-      
+
       final double theBearingDegs = getCalculatedBearing(null, null);
 
-      final double myCourseDegs =  _hostFix.getCourseDegs();
+      final double myCourseDegs = _hostFix.getCourseDegs();
       final double hisCourseDegs = _targetFix.getCourseDegs();
 
-			final double mySpeedKts = _hostFix.getSpeed();
-			final double hisSpeedKts = _targetFix.getSpeed();
+      final double mySpeedKts = _hostFix.getSpeed();
+      final double hisSpeedKts = _targetFix.getSpeed();
 
       predictedFreq =
           FrequencyCalcs.getPredictedFreq(baseFreq, speedOfSoundKts,
               mySpeedKts, myCourseDegs, hisSpeedKts, hisCourseDegs,
               theBearingDegs);
-		}
-		return predictedFreq;
-	}
+    }
+    return predictedFreq;
+  }
 
-	public SensorContactWrapper getSensorCut()
-	{
-		return _sensor;
-	}
+  public SensorContactWrapper getSensorCut()
+  {
+    return _sensor;
+  }
 
-	public FixWrapper getTarget()
-	{
-		return _targetFix;
-	}
+  public FixWrapper getTarget()
+  {
+    return _targetFix;
+  }
 
-	public TrackSegment getTargetTrack()
-	{
-		return _targetTrack;
-	}
+  public TrackSegment getTargetTrack()
+  {
+    return _targetTrack;
+  }
 
 }
