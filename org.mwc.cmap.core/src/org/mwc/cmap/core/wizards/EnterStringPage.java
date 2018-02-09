30881d55bd4084dcef7566e63c0760c8fff56f60
Ian Mayo <ian@planetmayo.com>
2018-02-09 15:35:59 +0000

    Allow preference name to be over-ridden

--- org.mwc.cmap.core/src/org/mwc/cmap/core/wizards/EnterStringPage.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/core/wizards/EnterStringPage.java
@@ -24,111 +24,134 @@ import MWC.GUI.Editable;
 public class EnterStringPage extends CoreEditableWizardPage
 {
 
-	public static class DataItem implements Editable
-	{
+  public static class DataItem implements Editable
+  {
 
-		String newName;
+    String newName;
 
-		public EditorType getInfo()
-		{
-			return null;
-		}
+    public EditorType getInfo()
+    {
+      return null;
+    }
 
-		public String getName()
-		{
-			return newName;
-		}
+    public String getName()
+    {
+      return newName;
+    }
 
-		public boolean hasEditor()
-		{
-			return false;
-		}
+    public boolean hasEditor()
+    {
+      return false;
+    }
 
-		public void setName(final String name)
-		{
-			newName = name;
-		}
+    public void setName(final String name)
+    {
+      newName = name;
+    }
 
-	}
+  }
 
-	private static final String VALUE = "VALUE";
+  private static final String DEFAULT_PREF_VALUE = "VALUE";
 
-	public static String NAME = "Get Name";
-	DataItem _myWrapper;
-	protected String _startName;
-	private final String _fieldExplanation;
+  public static String NAME = "Get Name";
+  DataItem _myWrapper;
+  protected String _startName;
+  private final String _fieldExplanation;
+  private final String _prefName;
 
-	/**
-	 * 
-	 * @param selection the current selection
-	 * @param startName the string to put in the box
-	 * @param pageTitle what to call the page
-	 * @param pageExplanation help for the page
-	 * @param fieldExplanation help for the field
-	 * @param imagePath an image to show
-	 * @param helpContext context-senstivie help
-	 * @param useDefaults whether to re-use the last string value for this page 
-	 * @param trailingMessage 
-	 */
-	public EnterStringPage(final ISelection selection, final String startName,
-			final String pageTitle, final String pageExplanation, final String fieldExplanation,
-			final String imagePath, final String helpContext, final boolean useDefaults, String trailingMessage)
-	{
-		super(selection, NAME, pageTitle, pageExplanation, imagePath, helpContext,
-				false, trailingMessage);
-		_startName = startName;
-		_fieldExplanation = fieldExplanation;
-		if (useDefaults)
-			setDefaults();
-	}
+  /**
+   * 
+   * @param selection
+   *          the current selection
+   * @param startName
+   *          the string to put in the box
+   * @param pageTitle
+   *          what to call the page
+   * @param pageExplanation
+   *          help for the page
+   * @param fieldExplanation
+   *          help for the field
+   * @param imagePath
+   *          an image to show
+   * @param helpContext
+   *          context-senstivie help
+   * @param useDefaults
+   *          whether to re-use the last string value for this page
+   * @param trailingMessage
+   */
+  public EnterStringPage(final ISelection selection, final String startName,
+      final String pageTitle, final String pageExplanation,
+      final String fieldExplanation, final String imagePath,
+      final String helpContext, final boolean useDefaults,
+      String trailingMessage, final String prefName)
+  {
+    super(selection, NAME, pageTitle, pageExplanation, imagePath, helpContext,
+        false, trailingMessage);
+    _startName = startName;
+    _fieldExplanation = fieldExplanation;
+    _prefName = prefName;
+    if (useDefaults)
+      setDefaults();
+  }
 
-	@Override
-	protected String getIndex()
-	{
-		return "" + this.getClass() + "," + _fieldExplanation;
-	}
+  public EnterStringPage(final ISelection selection, final String startName,
+      final String pageTitle, final String pageExplanation,
+      final String fieldExplanation, final String imagePath,
+      final String helpContext, final boolean useDefaults,
+      String trailingMessage)
+  {
+    this(selection, startName, pageTitle, pageExplanation, fieldExplanation,
+        imagePath, helpContext, useDefaults, trailingMessage,
+        DEFAULT_PREF_VALUE);
+  }
 
-	private void setDefaults()
-	{
-		final Preferences prefs = getPrefs();
+  @Override
+  protected String getIndex()
+  {
+    return "" + this.getClass() + "," + _prefName + "_"
+        + _fieldExplanation.hashCode();
+  }
 
-		if (prefs != null)
-		{
-			_startName = prefs.get(VALUE, _startName);
-		}
-	}
+  private void setDefaults()
+  {
+    final Preferences prefs = getPrefs();
 
-	@Override
-	public void dispose()
-	{
-		// try to store some defaults
-		final Preferences prefs = getPrefs();
-		prefs.put(VALUE, _myWrapper.getName());
+    if (prefs != null)
+    {
+      _startName = prefs.get(_prefName, _startName);
+    }
+  }
 
-		super.dispose();
-	}
+  @Override
+  public void dispose()
+  {
+    // try to store some defaults
+    final Preferences prefs = getPrefs();
+    prefs.put(_prefName, _myWrapper.getName());
+    super.dispose();
+  }
 
-	public String getString()
-	{
-		return _myWrapper.getName();
-	}
+  public String getString()
+  {
+    return _myWrapper.getName();
+  }
 
-	protected PropertyDescriptor[] getPropertyDescriptors()
-	{
-		final PropertyDescriptor[] descriptors =
-		{ prop("Name", _fieldExplanation, getEditable()) };
-		return descriptors;
-	}
+  protected PropertyDescriptor[] getPropertyDescriptors()
+  {
+    final PropertyDescriptor[] descriptors =
+    {prop("Name", _fieldExplanation, getEditable())};
+    return descriptors;
+  }
 
-	protected Editable createMe()
-	{
-		if (_myWrapper == null)
-		{
-			_myWrapper = new DataItem();
-			_myWrapper.setName(_startName);
-		}
+  protected Editable createMe()
+  {
+    if (_myWrapper == null)
+    {
+      _myWrapper = new DataItem();
+      _myWrapper.setName(_startName);
+    }
 
-		return _myWrapper;
-	}
+    return _myWrapper;
+  }
 
 }
