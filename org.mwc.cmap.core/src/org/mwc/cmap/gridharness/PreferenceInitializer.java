648ce39a79938b5b366452881a66299ef556f80c
Ian Mayo <ian@planetmayo.com>
2018-02-22 21:19:56 +0000

    specify a platform-specific font specifier string

--- org.mwc.cmap.core/src/org/mwc/cmap/gridharness/PreferenceInitializer.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/gridharness/PreferenceInitializer.java
@@ -16,18 +16,27 @@ package org.mwc.cmap.gridharness;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.FontData;
 import org.mwc.cmap.core.CorePlugin;
 
 import Debrief.Wrappers.SensorContactWrapper;
 
-public class PreferenceInitializer extends AbstractPreferenceInitializer {
+public class PreferenceInitializer extends AbstractPreferenceInitializer
+{
 
-	@Override
-	public void initializeDefaultPreferences() {
-		final
-		IPreferenceStore preferences = CorePlugin.getDefault().getPreferenceStore();
-		preferences.setDefault(CorePlugin.PREF_BASE60_FORMAT_NO_SECONDS, false);
-    preferences.setDefault(CorePlugin.DEFAULT_FONT, "Arial-regular-12");
+  @Override
+  public void initializeDefaultPreferences()
+  {
+    final IPreferenceStore preferences = CorePlugin.getDefault()
+        .getPreferenceStore();
+    preferences.setDefault(CorePlugin.PREF_BASE60_FORMAT_NO_SECONDS, false);
+
+    // the font string is platform specific. So, we have to generate it for this platform.
+    final FontData fd = new FontData("Arial", 12, SWT.NORMAL);
+    final String initialFont = fd.toString();
+
+    preferences.setDefault(CorePlugin.DEFAULT_FONT, initialFont);
     preferences.setDefault(SensorContactWrapper.TRANSPARENCY, 100);
-	}
+  }
 }
