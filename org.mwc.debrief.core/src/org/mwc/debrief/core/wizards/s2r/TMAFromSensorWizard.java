920fe2a62925fd192b836bdee0eb823e52da6487
Ian Mayo <ian@planetmayo.com>
2018-02-22 12:53:21 +0000

    introduce new color page

--- org.mwc.debrief.core/src/org/mwc/debrief/core/wizards/s2r/TMAFromSensorWizard.java
+++ org.mwc.debrief.core/src/org/mwc/debrief/core/wizards/s2r/TMAFromSensorWizard.java
@@ -14,9 +14,12 @@
  */
 package org.mwc.debrief.core.wizards.s2r;
 
+import java.awt.Color;
+
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.mwc.cmap.core.wizards.RangeBearingPage;
+import org.mwc.cmap.core.wizards.SelectColorPage;
 import org.mwc.debrief.core.wizards.EnterSolutionPage;
 
 import MWC.GenericData.WorldDistance;
@@ -24,69 +27,82 @@ import MWC.GenericData.WorldSpeed;
 
 public class TMAFromSensorWizard extends Wizard
 {
-	private static final String PAGE_TITLE = "Generate TMA segment";
-	RangeBearingPage selectOffsetPage;
-	EnterSolutionPage enterSolutionPage;
-	private final double _brgDegs;
-	private final WorldDistance _range;
-	private final double _initialCourse;
-	private final WorldSpeed _initialSpeed;
-	private boolean _showOffset;
+  private static final String PAGE_TITLE = "Generate TMA segment";
+  RangeBearingPage selectOffsetPage;
+  EnterSolutionPage enterSolutionPage;
+  private final double _brgDegs;
+  private final WorldDistance _range;
+  private final double _initialCourse;
+  private final WorldSpeed _initialSpeed;
+  private boolean _showOffset;
+  private SelectColorPage colorPage;
+  private final Color _color;
 
-	public TMAFromSensorWizard(final double brgDegs, final WorldDistance range,
-			final double initialCourse, final WorldSpeed initialSpeed)
-	{
-		this(brgDegs, range, initialCourse, initialSpeed, true);
-	}
+  public TMAFromSensorWizard(final double brgDegs, final WorldDistance range,
+      final double initialCourse, final WorldSpeed initialSpeed,
+      final Color color)
+  {
+    this(brgDegs, range, initialCourse, initialSpeed, true, color);
+  }
 
-	public TMAFromSensorWizard(final double brgDegs, final WorldDistance range,
-			final double initialCourse, final WorldSpeed initialSpeed,
-			boolean showOffset)
-	{
-		_brgDegs = brgDegs;
-		_range = range;
-		_initialCourse = initialCourse;
-		_initialSpeed = initialSpeed;
-		_showOffset = showOffset;
-	}
+  public TMAFromSensorWizard(final double brgDegs, final WorldDistance range,
+      final double initialCourse, final WorldSpeed initialSpeed,
+      boolean showOffset, final Color color)
+  {
+    _brgDegs = brgDegs;
+    _range = range;
+    _initialCourse = initialCourse;
+    _initialSpeed = initialSpeed;
+    _showOffset = showOffset;
+    _color = color;
+  }
 
-	public void addPages()
-	{
-		final String imagePath = "images/grid_wizard.gif";
+  public void addPages()
+  {
+    final String imagePath = "images/grid_wizard.gif";
 
-		final String helpContext = null;
+    final String helpContext = null;
 
     // now for the easy fields
     // ok, we need to let the user enter the solution wrapper name
-    selectOffsetPage =
-        new RangeBearingPage(null, PAGE_TITLE,
-            "Now specify the offset to the track start",
-            "range from ownship to start of track",
-            "bearing from ownship to start of track", imagePath, helpContext,
-            _range, _brgDegs);
+    selectOffsetPage = new RangeBearingPage(null, PAGE_TITLE,
+        "Now specify the offset to the track start",
+        "range from ownship to start of track",
+        "bearing from ownship to start of track", imagePath, helpContext,
+        _range, _brgDegs);
 
-		if (_showOffset)
-		{
-			addPage(selectOffsetPage);
-		}
+    if (_showOffset)
+    {
+      addPage(selectOffsetPage);
+    }
 
-    enterSolutionPage =
-        new EnterSolutionPage(null, PAGE_TITLE,
-            "This page lets you enter an initial solution", imagePath,
-            helpContext, _initialSpeed, _initialCourse);
+    enterSolutionPage = new EnterSolutionPage(null, PAGE_TITLE,
+        "This page lets you enter an initial solution", imagePath, helpContext,
+        _initialSpeed, _initialCourse);
 
-		addPage(enterSolutionPage);
-	}
+    addPage(enterSolutionPage);
 
-	public boolean performFinish()
-	{
-		return true;
-	}
+    // do we have color?
+    if (_color != null)
+    {
+      // ok, we need to let the user enter the solution wrapper name
+      colorPage = new SelectColorPage(null, _color, PAGE_TITLE,
+          "Format the sensor cuts used for this leg",
+          "The colour to shade the selected sensor cuts (or leave unchanged)",
+          imagePath, helpContext, null, true);
+      addPage(colorPage);
+    }
+  }
 
-	@Override
-	public IWizardPage getPage(final String name)
-	{
-		return super.getPage(name);
-	}
+  public boolean performFinish()
+  {
+    return true;
+  }
+
+  @Override
+  public IWizardPage getPage(final String name)
+  {
+    return super.getPage(name);
+  }
 
 }
