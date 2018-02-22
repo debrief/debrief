66cf622460be0d0248bec400914f142224c43ac0
Ian Mayo <ian@planetmayo.com>
2018-02-22 17:31:02 +0000

    introduce option to skip setting default color from the existing preference

--- org.mwc.cmap.core/src/org/mwc/cmap/core/wizards/SelectColorPage.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/core/wizards/SelectColorPage.java
@@ -25,115 +25,124 @@ import MWC.GUI.Editable;
 public class SelectColorPage extends CoreEditableWizardPage
 {
 
-	public static class DataItem implements Editable
-	{
+  public static class DataItem implements Editable
+  {
 
-		Color _myColor;
+    Color _myColor;
 
-		public Color getColor()
-		{
-			return _myColor;
-		}
+    public Color getColor()
+    {
+      return _myColor;
+    }
 
-		public void setColor(final Color color)
-		{
-			this._myColor = color;
-		}
+    public void setColor(final Color color)
+    {
+      this._myColor = color;
+    }
 
-		public EditorType getInfo()
-		{
-			return null;
-		}
+    public EditorType getInfo()
+    {
+      return null;
+    }
 
-		public String getName()
-		{
-			return null;
-		}
+    public String getName()
+    {
+      return null;
+    }
 
-		public boolean hasEditor()
-		{
-			return false;
-		}
+    public boolean hasEditor()
+    {
+      return false;
+    }
 
-	}
+  }
 
-	private static final String COLOR_RED = "RED";
+  private static final String COLOR_RED = "RED";
 
-	private static final String COLOR_BLUE = "GREEN";
+  private static final String COLOR_BLUE = "GREEN";
 
-	private static final String COLOR_GREEN = "BLUE";
+  private static final String COLOR_GREEN = "BLUE";
 
-	public static String NAME = "Get Color";
-	DataItem _myWrapper;
-	private Color _startColor;
-	private final String _fieldExplanation;
+  public static String NAME = "Get Color";
+  DataItem _myWrapper;
+  private Color _startColor;
+  private final String _fieldExplanation;
 
-	public SelectColorPage(final ISelection selection, final Color startColor,
-			final String pageTitle, final String pageExplanation, final String fieldExplanation,
-			final String imagePath, final String helpContext, String trailingMessage)
-	{
-		super(selection, NAME, pageTitle, pageExplanation, imagePath, helpContext,
-				false, trailingMessage);
-		_startColor = startColor;
-		_fieldExplanation = fieldExplanation;
-		setDefaults();
-	}
+  private boolean _skipPrefs;
 
-	private void setDefaults()
-	{
-		final Preferences prefs = getPrefs();
+  public SelectColorPage(final ISelection selection, final Color startColor,
+      final String pageTitle, final String pageExplanation,
+      final String fieldExplanation, final String imagePath,
+      final String helpContext, String trailingMessage, final boolean skipPrefs)
+  {
+    super(selection, NAME, pageTitle, pageExplanation, imagePath, helpContext,
+        false, trailingMessage);
+    _startColor = startColor;
+    _skipPrefs = skipPrefs;
+    _fieldExplanation = fieldExplanation;
+    setDefaults();
+  }
 
-		if (prefs != null)
-		{
-			final int red = prefs.getInt(COLOR_RED, 255);
-			final int green = prefs.getInt(COLOR_GREEN, 0);
-			final int blue = prefs.getInt(COLOR_BLUE, 0);
-			_startColor = new Color(red, green, blue);
-		}
-	}
+  private void setDefaults()
+  {
+    if (!_skipPrefs)
+    {
+      final Preferences prefs = getPrefs();
 
-	@Override
-	public void dispose()
-	{
-		// try to store some defaults
-		final Preferences prefs = getPrefs();
+      if (prefs != null)
+      {
+        final int red = prefs.getInt(COLOR_RED, 255);
+        final int green = prefs.getInt(COLOR_GREEN, 0);
+        final int blue = prefs.getInt(COLOR_BLUE, 0);
+        _startColor = new Color(red, green, blue);
+      }
+    }
+  }
 
-		prefs.putInt(COLOR_RED, _myWrapper.getColor().getRed());
-		prefs.putInt(COLOR_BLUE, _myWrapper.getColor().getBlue());
-		prefs.putInt(COLOR_GREEN, _myWrapper.getColor().getGreen());
+  @Override
+  public void dispose()
+  {
+    if (!_skipPrefs)
+    {
+      // try to store some defaults
+      final Preferences prefs = getPrefs();
 
-		super.dispose();
-	}
+      prefs.putInt(COLOR_RED, _myWrapper.getColor().getRed());
+      prefs.putInt(COLOR_BLUE, _myWrapper.getColor().getBlue());
+      prefs.putInt(COLOR_GREEN, _myWrapper.getColor().getGreen());
+    }
+    super.dispose();
+  }
 
-	public String getString()
-	{
-		return _myWrapper.getName();
-	}
+  public String getString()
+  {
+    return _myWrapper.getName();
+  }
 
-	protected PropertyDescriptor[] getPropertyDescriptors()
-	{
-		final PropertyDescriptor[] descriptors =
-		{ prop("Color", _fieldExplanation, getEditable()) };
-		return descriptors;
-	}
+  protected PropertyDescriptor[] getPropertyDescriptors()
+  {
+    final PropertyDescriptor[] descriptors =
+    {prop("Color", _fieldExplanation, getEditable())};
+    return descriptors;
+  }
 
-	protected Editable createMe()
-	{
-		if (_myWrapper == null)
-		{
-			_myWrapper = new DataItem();
-			_myWrapper.setColor(_startColor);
-		}
+  protected Editable createMe()
+  {
+    if (_myWrapper == null)
+    {
+      _myWrapper = new DataItem();
+      _myWrapper.setColor(_startColor);
+    }
 
-		return _myWrapper;
-	}
+    return _myWrapper;
+  }
 
-	public Color getColor()
-	{
-		Color res = Color.red;
-		if (_myWrapper.getColor() != null)
-			res = _myWrapper.getColor();
-		return res;
-	}
+  public Color getColor()
+  {
+    Color res = Color.red;
+    if (_myWrapper.getColor() != null)
+      res = _myWrapper.getColor();
+    return res;
+  }
 
 }
