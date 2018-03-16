2a3d21775454abd182dc21d401c385ce5d4c067d
Ian Mayo <ian@planetmayo.com>
2018-03-16 12:54:08 +0000

    Introduce menu option to select children at set interval

--- org.mwc.debrief.core/src/org/mwc/debrief/core/ContextOperations/SelectAllChildren.java
+++ org.mwc.debrief.core/src/org/mwc/debrief/core/ContextOperations/SelectAllChildren.java
@@ -26,6 +26,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IEditorPart;
@@ -40,6 +41,7 @@ import org.mwc.cmap.core.property_support.EditableWrapper;
 import org.mwc.cmap.core.property_support.RightClickSupport.RightClickContextItemGenerator;
 import org.mwc.debrief.core.DebriefPlugin;
 
+import Debrief.Wrappers.FixWrapper;
 import Debrief.Wrappers.TacticalDataWrapper;
 import Debrief.Wrappers.Track.TrackSegment;
 import Debrief.Wrappers.Track.TrackWrapper_Support.SegmentList;
@@ -48,6 +50,8 @@ import MWC.GUI.Editable;
 import MWC.GUI.HasEditables;
 import MWC.GUI.Layer;
 import MWC.GUI.Layers;
+import MWC.GUI.Properties.TimeFrequencyPropertyEditor;
+import MWC.GenericData.TimePeriod;
 
 /**
  * @author ian.mayo
@@ -55,10 +59,72 @@ import MWC.GUI.Layers;
  */
 public class SelectAllChildren implements RightClickContextItemGenerator
 {
+  private static class SelectChildrenAtFrequencyOperation extends
+      SelectChildrenOperation
+  {
+
+    @SuppressWarnings("unused")
+    private final long _interval;
+
+    public SelectChildrenAtFrequencyOperation(Layers theLayers,
+        HasEditables selected, final long interval)
+    {
+      super(theLayers, selected);
+      _interval = interval;
+    }
+
+    @Override
+    protected List<EditableWrapper> itemsFor(HasEditables parent)
+    {
+      final List<EditableWrapper> res = new ArrayList<>();
+
+      TrackSegment wrap = (TrackSegment) parent;
+      EditableWrapper track = new EditableWrapper(wrap.getWrapper(), null,
+          _theLayers);
+      EditableWrapper segment = new EditableWrapper(wrap, track, _theLayers);
+
+      Enumeration<Editable> items = wrap.elements();
+      long lastStamp = -1;
+      while (items.hasMoreElements())
+      {
+        Editable item = items.nextElement();
+
+        FixWrapper fix = (FixWrapper) item;
+        final long time = fix.getDateTimeGroup().getDate().getTime();
+
+        // is this the first element?
+        if (lastStamp == -1)
+        {
+          // wrap the fix
+          res.add(new EditableWrapper(item, segment, _theLayers));
+
+          // remember how many intervals there have been
+          lastStamp = time / _interval;
+        }
+        else
+        {
+          // find the new number of intervals
+          final long hour = time / _interval;
+
+          // have we passed a new interval?
+          if (hour > lastStamp)
+          {
+            // store this new interval
+            lastStamp = hour;
+
+            // wrap the fix
+            res.add(new EditableWrapper(item, segment, _theLayers));
+          }
+        }
+      }
+      return res;
+    }
+  }
+
   private static class SelectChildrenOperation extends CMAPOperation
   {
     final private HasEditables _hasEditables;
-    final private Layers _theLayers;
+    protected final Layers _theLayers;
 
     public SelectChildrenOperation(Layers theLayers, HasEditables selected)
     {
@@ -68,9 +134,8 @@ public class SelectAllChildren implements RightClickContextItemGenerator
     }
 
     @Override
-    public IStatus
-        execute(final IProgressMonitor monitor, final IAdaptable info)
-            throws ExecutionException
+    public IStatus execute(final IProgressMonitor monitor,
+        final IAdaptable info) throws ExecutionException
     {
       List<EditableWrapper> selection = itemsFor(_hasEditables);
 
@@ -81,26 +146,17 @@ public class SelectAllChildren implements RightClickContextItemGenerator
         final IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
         final IWorkbenchPage page = win.getActivePage();
         final IEditorPart editor = page.getActiveEditor();
-        
+
         if (editor != null)
         {
-          IContentOutlinePage outline =
-              (IContentOutlinePage) editor
-                  .getAdapter(IContentOutlinePage.class);
+          IContentOutlinePage outline = (IContentOutlinePage) editor.getAdapter(
+              IContentOutlinePage.class);
           if (outline != null)
           {
             // now set the selection
             IStructuredSelection str = new StructuredSelection(selection);
 
             outline.setSelection(str);
-
-            // see uf we can expand the selection
-            // if (outline instanceof PlotOutlinePage)
-            // {
-            // PlotOutlinePage plotOutline = (PlotOutlinePage) outline;
-            // EditableWrapper ew = (EditableWrapper) str.getFirstElement();
-            // plotOutline.editableSelected(str, ew);
-            // }
           }
         }
       }
@@ -108,14 +164,14 @@ public class SelectAllChildren implements RightClickContextItemGenerator
       return Status.OK_STATUS;
     }
 
-    private List<EditableWrapper> itemsFor(HasEditables parent)
+    protected List<EditableWrapper> itemsFor(HasEditables parent)
     {
       final List<EditableWrapper> res;
       if (parent instanceof TacticalDataWrapper)
       {
         TacticalDataWrapper wrap = (TacticalDataWrapper) parent;
-        EditableWrapper track =
-            new EditableWrapper(wrap.getHost(), null, _theLayers);
+        EditableWrapper track = new EditableWrapper(wrap.getHost(), null,
+            _theLayers);
         EditableWrapper thisList = new EditableWrapper(wrap, track, _theLayers);
 
         res = new ArrayList<EditableWrapper>();
@@ -145,8 +201,8 @@ public class SelectAllChildren implements RightClickContextItemGenerator
       else if (parent instanceof TrackSegment)
       {
         TrackSegment wrap = (TrackSegment) parent;
-        EditableWrapper track =
-            new EditableWrapper(wrap.getWrapper(), null, _theLayers);
+        EditableWrapper track = new EditableWrapper(wrap.getWrapper(), null,
+            _theLayers);
         EditableWrapper thisList = new EditableWrapper(wrap, track, _theLayers);
 
         res = new ArrayList<EditableWrapper>();
@@ -161,13 +217,12 @@ public class SelectAllChildren implements RightClickContextItemGenerator
       else if (parent instanceof SegmentList)
       {
         SegmentList wrap = (SegmentList) parent;
-        EditableWrapper track =
-            new EditableWrapper(wrap.getWrapper(), null, _theLayers);
-        EditableWrapper segList =
-            new EditableWrapper(wrap.getWrapper().getSegments(), track,
-                _theLayers);
-        EditableWrapper thisList =
-            new EditableWrapper(wrap, segList, _theLayers);
+        EditableWrapper track = new EditableWrapper(wrap.getWrapper(), null,
+            _theLayers);
+        EditableWrapper segList = new EditableWrapper(wrap.getWrapper()
+            .getSegments(), track, _theLayers);
+        EditableWrapper thisList = new EditableWrapper(wrap, segList,
+            _theLayers);
 
         res = new ArrayList<EditableWrapper>();
 
@@ -221,6 +276,27 @@ public class SelectAllChildren implements RightClickContextItemGenerator
   }
 
   /**
+   * the list of tags shown in the drop-down list
+   */
+  private final String _stringTags[] =
+  {"5 Mins", "15 Mins", "30 Mins", "60 Mins", "2 Hours", "6 Hours", "12 Hours",
+      "24 Hours", "48 Hours", "72 Hours"};
+
+  /**
+   * the values to use for the tags in the list
+   */
+  private final long _freqs[] =
+  {TimeFrequencyPropertyEditor._5_MINS, TimeFrequencyPropertyEditor._15_MINS,
+      TimeFrequencyPropertyEditor._30_MINS,
+      TimeFrequencyPropertyEditor._60_MINS, 2
+          * TimeFrequencyPropertyEditor._60_MINS, 6
+              * TimeFrequencyPropertyEditor._60_MINS, 12
+                  * TimeFrequencyPropertyEditor._60_MINS, 24
+                      * TimeFrequencyPropertyEditor._60_MINS, 48
+                          * TimeFrequencyPropertyEditor._60_MINS, 72
+                              * TimeFrequencyPropertyEditor._60_MINS};
+
+  /**
    * @param parent
    * @param theLayers
    * @param parentLayers
@@ -241,8 +317,8 @@ public class SelectAllChildren implements RightClickContextItemGenerator
       if (selected instanceof HasEditables)
       {
         // ok, generate the operation
-        final IUndoableOperation action =
-            getOperation(theLayers, (HasEditables) selected);
+        final IUndoableOperation action = getOperation(theLayers,
+            (HasEditables) selected);
 
         // and now wrap it in an action
         final Action doIt = new Action("Select all child elements")
@@ -253,13 +329,83 @@ public class SelectAllChildren implements RightClickContextItemGenerator
             runIt(action);
           }
         };
-        doIt.setImageDescriptor(DebriefPlugin
-        .getImageDescriptor("icons/16/show.png"));
+        doIt.setImageDescriptor(DebriefPlugin.getImageDescriptor(
+            "icons/16/show.png"));
 
         // ok, go for it
         parent.add(doIt);
+
+        // are the children time-stamped?
+        TimePeriod coverage = timePeriodFor(selected);
+        if (coverage != null)
+        {
+          // ok, create the children at freq action
+
+          // and the new drop-down list of interpolation frequencies
+          final MenuManager newMenu = new MenuManager(
+              "Select child elements with this interval");
+          newMenu.setImageDescriptor(DebriefPlugin.getImageDescriptor(
+              "icons/16/show.png"));
+
+          for (int i = 0; i < _freqs.length; i++)
+          {
+            long thisLen = _freqs[i] / 1000;
+
+            if (thisLen < coverage.getExtent())
+            {
+              final IUndoableOperation op = getTimedOperation(theLayers,
+                  (HasEditables) selected, thisLen);
+              // create the new menu item
+              final Action selectItem = new Action(_stringTags[i])
+              {
+                @Override
+                public void run()
+                {
+                  runIt(op);
+                }
+              };
+
+              newMenu.add(selectItem);
+              selectItem.setImageDescriptor(DebriefPlugin.getImageDescriptor(
+                  "icons/16/show.png"));
+            }
+          }
+          parent.add(newMenu);
+        }
       }
     }
+  }
+
+  private TimePeriod timePeriodFor(Editable selected)
+  {
+    TimePeriod res;
+    if (selected instanceof TrackSegment)
+    {
+      TrackSegment track = (TrackSegment) selected;
+      res = new TimePeriod.BaseTimePeriod(track.startDTG(), track.endDTG());
+    }
+    else
+    {
+      res = null;
+    }
+
+    return res;
+  }
+
+  /**
+   * move the operation generation to a method, so it can be overwritten (in testing)
+   * 
+   * 
+   * @param theLayers
+   * @param suitableSegments
+   * @param commonParent
+   * @return
+   */
+  protected IUndoableOperation getTimedOperation(final Layers theLayers,
+      final HasEditables selected, final long interval)
+  {
+    return new SelectChildrenAtFrequencyOperation(theLayers, selected,
+        interval);
   }
 
   /**
