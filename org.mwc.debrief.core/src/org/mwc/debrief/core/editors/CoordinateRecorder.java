235e772a962abab0359831ab6a12727cde775262
Ian Mayo <ian@planetmayo.com>
2018-06-14 21:12:12 +0100

    switch to GPX export format

--- org.mwc.debrief.core/src/org/mwc/debrief/core/editors/CoordinateRecorder.java
+++ org.mwc.debrief.core/src/org/mwc/debrief/core/editors/CoordinateRecorder.java
@@ -3,10 +3,13 @@ package org.mwc.debrief.core.editors;
 import java.awt.Point;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.mwc.debrief.core.gpx.ImportGPX;
 
 import Debrief.Wrappers.FixWrapper;
 import Debrief.Wrappers.TrackWrapper;
@@ -17,6 +20,9 @@ import MWC.GUI.Layers.OperateFunction;
 import MWC.GenericData.HiResDate;
 import MWC.GenericData.SteppingListener;
 import MWC.GenericData.Watchable;
+import MWC.GenericData.WorldLocation;
+import MWC.GenericData.WorldSpeed;
+import MWC.TacticalData.Fix;
 import MWC.Utilities.TextFormatting.FormatRNDateTime;
 
 public class CoordinateRecorder implements PropertyChangeListener,
@@ -24,8 +30,8 @@ public class CoordinateRecorder implements PropertyChangeListener,
 {
   private final Layers _myLayers;
   private final PlainProjection _projection;
-  final private Map<String, TrackListing> _tracks =
-      new HashMap<String, TrackListing>();
+  final private Map<String, TrackWrapper> _tracks =
+      new HashMap<String, TrackWrapper>();
   final private List<String> _times = new ArrayList<String>();
   private boolean _running = false;
 
@@ -36,22 +42,6 @@ public class CoordinateRecorder implements PropertyChangeListener,
     _projection = plainProjection;
   }
 
-  final private class TrackListing extends ArrayList<Point>
-  {
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
-
-    @SuppressWarnings("unused")
-    final private String _name;
-
-    public TrackListing(String name)
-    {
-      _name = name;
-    }
-  }
-
   @Override
   public void propertyChange(PropertyChangeEvent evt)
   {
@@ -60,7 +50,7 @@ public class CoordinateRecorder implements PropertyChangeListener,
 
     // get the new time.
     final HiResDate timeNow = (HiResDate) evt.getNewValue();
-    
+
     _times.add(FormatRNDateTime.toMediumString(timeNow.getDate().getTime()));
 
     OperateFunction outputIt = new OperateFunction()
@@ -75,13 +65,25 @@ public class CoordinateRecorder implements PropertyChangeListener,
         {
           FixWrapper fix = (FixWrapper) items[0];
 
-          TrackListing match = _tracks.get(track.getName());
+          TrackWrapper match = _tracks.get(track.getName());
           if (match == null)
           {
-            match = new TrackListing(track.getName());
+            match = new TrackWrapper();
+            match.setName(track.getName());
             _tracks.put(track.getName(), match);
           }
-          match.add(_projection.toScreen(fix.getLocation()));
+
+          Point point = _projection.toScreen(fix.getLocation());
+          WorldLocation newLoc = new WorldLocation(point.getY(), point.getX(),
+              fix.getLocation().getDepth());
+          final double courseRads = MWC.Algorithms.Conversions.Degs2Rads(fix.getCourseDegs());
+          final double speedYps = new WorldSpeed(fix.getSpeed(),
+          WorldSpeed.Kts).getValueIn(WorldSpeed.ft_sec)/3;
+          Fix fix2 = new Fix(timeNow, newLoc,
+              courseRads,
+              speedYps);
+          FixWrapper fw2 = new FixWrapper(fix2);
+          match.addFix(fw2);
         }
       }
     };
@@ -100,22 +102,16 @@ public class CoordinateRecorder implements PropertyChangeListener,
   public void stopStepping(HiResDate now)
   {
     _running = false;
+
+    StringWriter writer = new StringWriter();
     
-    // output timestamps
-    for(String time:_times)
-    {
-      System.out.println(time);
-    }
+    List<TrackWrapper> list = new ArrayList<TrackWrapper>();
+    list.addAll(_tracks.values());
     
     // output tracks object.
-    for(String name:_tracks.keySet())
-    {
-      System.out.println(name);
-      TrackListing track = _tracks.get(name);
-      for(Point pt: track)
-      {
-        System.out.println(pt.getX() + ", " + pt.getY());
-      }
-    }
+    ImportGPX.doExport(list, writer);
+    
+    System.out.println(writer.toString());
+    
   }
 }
