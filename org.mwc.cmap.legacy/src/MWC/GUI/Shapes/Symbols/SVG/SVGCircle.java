05faabbdb6662653b9c6802e094b0f46d2d8cd8a
saul <saulhidalgoaular@gmail.com>
2018-09-06 12:28:52 +0000

    Work in progress

---
+++ org.mwc.cmap.legacy/src/MWC/GUI/Shapes/Symbols/SVG/SVGCircle.java
@@ -0,0 +1,40 @@
+/*
+ *    Debrief - the Open Source Maritime Analysis Application
+ *    http://debrief.info
+ *
+ *    (C) 2000-2018, Deep Blue C Technology Ltd
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the Eclipse Public License v1.0
+ *    (http://www.eclipse.org/legal/epl-v10.html)
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ */
+package MWC.GUI.Shapes.Symbols.SVG;
+
+import java.awt.Point;
+
+import org.w3c.dom.Element;
+
+import MWC.GUI.CanvasType;
+
+public class SVGCircle extends SVGElement
+{
+
+  public SVGCircle(Element dom)
+  {
+    super(dom);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void render(CanvasType dest, double sym_size, Point origin_coords,
+      double rotation_degs)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
