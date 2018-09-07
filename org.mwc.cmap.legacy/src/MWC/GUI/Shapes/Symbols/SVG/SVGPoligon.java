80281d8ff986966d72d6defbf7f22cb95b07aab2
saul <saulhidalgoaular@gmail.com>
2018-09-07 11:13:09 +0000

    Rectangle and Poligon Centered and Scaled.

--- org.mwc.cmap.legacy/src/MWC/GUI/Shapes/Symbols/SVG/SVGPoligon.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/Shapes/Symbols/SVG/SVGPoligon.java
@@ -23,6 +23,12 @@ import MWC.GUI.CanvasType;
 public class SVGPoligon extends SVGPolyline
 {
 
+  
+  /**
+   * the size of the symbol
+   */
+  protected final int wid = 40;
+  
   public SVGPoligon(Element dom)
   {
     super(dom);
@@ -32,22 +38,35 @@ public class SVGPoligon extends SVGPolyline
   public void render(CanvasType dest, double sym_size, Point origin_coords,
       double rotation_degs, final java.awt.Point rotationPoint)
   {
-    int [] x = new int[_x.length];
-    int [] y = new int[_y.length];
-    
-    for ( int i = 0 ; i < x.length ; i++ ) {
-      x[i] = (int)(_x[i] * sym_size + origin_coords.getX());
-      y[i] = (int)(_y[i] * sym_size + origin_coords.getY());
+
+    // Lets assume that the viewbox is 0 0 100 100
+    double magnitude = Math.sqrt(100 * 100 + 100 * 100);
+
+    // centering and scaling to 1.0
+    double[] x = new double[_x.length];
+    double[] y = new double[_y.length];
+    for (int i = 0; i < _x.length; i++)
+    {
+      x[i] = (_x[i] - rotationPoint.x) / magnitude * wid;
+      y[i] = (_y[i] - rotationPoint.y) / magnitude * wid;
     }
-    
+
+    int [] intX = new int[x.length];
+    int [] intY = new int[y.length];
+    for (int i = 0; i < _x.length; i++)
+    {
+      intX[i] = (int)(x[i] * sym_size + origin_coords.getX());
+      intY[i] = (int)(y[i] * sym_size + origin_coords.getY());
+    }
+
     if (_fill != null)
     {
       dest.setColor(_fill);
-      dest.fillPolygon(x, y, x.length);
+      dest.fillPolygon(intX, intY, intX.length);
     }
     else
     {
-      dest.drawPolygon(x, y, x.length);
+      dest.drawPolygon(intX, intY, intX.length);
     }
   }
 
--- org.mwc.cmap.legacy/src/MWC/GUI/Shapes/Symbols/SVG/SVGRectangle.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/Shapes/Symbols/SVG/SVGRectangle.java
@@ -21,23 +21,37 @@ import MWC.GUI.CanvasType;
 public class SVGRectangle extends SVGElement
 {
 
-  private double _x;
+  private double[] _x;
 
-  private double _y;
-
-  private double _width;
-
-  private double _height;
+  private double[] _y;
+  
+  /**
+   * the size of the symbol
+   */
+  protected final int wid = 40;
 
   public SVGRectangle(Element dom)
   {
     super(dom);
     try
     {
-      _x = Double.parseDouble(getDom().getAttribute("x"));
-      _y = Double.parseDouble(getDom().getAttribute("y"));
-      _width = Double.parseDouble(getDom().getAttribute("width"));
-      _height = Double.parseDouble(getDom().getAttribute("height"));
+      double x = Double.parseDouble(getDom().getAttribute("x"));
+      double y = Double.parseDouble(getDom().getAttribute("y"));
+      double width = Double.parseDouble(getDom().getAttribute("width"));
+      double height = Double.parseDouble(getDom().getAttribute("height"));
+
+      _x = new double[4];
+      _y = new double[4];
+
+      _x[0] = x;
+      _y[0] = y;
+      _x[1] = x + width;
+      _y[1] = y;
+      _x[2] = x + width;
+      _y[2] = y + height;
+      _x[3] = x;
+      _y[3] = y + height;
+
     }
     catch (Exception e)
     {
@@ -50,19 +64,35 @@ public class SVGRectangle extends SVGElement
       java.awt.Point origin_coords, double rotation_degs,
       final java.awt.Point rotationPoint)
   {
-    final double x = _x * sym_size + origin_coords.getX();
-    final double y = _y * sym_size + origin_coords.getY();
-    final double width = _width * sym_size;
-    final double height = _height * sym_size;
+
+    // Lets assume that the viewbox is 0 0 100 100
+    double magnitude = Math.sqrt(100 * 100 + 100 * 100);
+
+    // centering and scaling to 1.0
+    double[] x = new double[_x.length];
+    double[] y = new double[_y.length];
+    for (int i = 0; i < _x.length; i++)
+    {
+      x[i] = (_x[i] - rotationPoint.x) / magnitude * wid;
+      y[i] = (_y[i] - rotationPoint.y) / magnitude * wid;
+    }
+
+    int [] intX = new int[x.length];
+    int [] intY = new int[y.length];
+    for (int i = 0; i < _x.length; i++)
+    {
+      intX[i] = (int)(x[i] * sym_size + origin_coords.getX());
+      intY[i] = (int)(y[i] * sym_size + origin_coords.getY());
+    }
 
     if (_fill != null)
     {
       dest.setColor(_fill);
-      dest.fillRect((int) x, (int) y, (int) width, (int) height);
+      dest.fillPolygon(intX, intY, intX.length);
     }
     else
     {
-      dest.drawRect((int) x, (int) y, (int) width, (int) height);
+      dest.drawPolygon(intX, intY, intX.length);
     }
 
   }
