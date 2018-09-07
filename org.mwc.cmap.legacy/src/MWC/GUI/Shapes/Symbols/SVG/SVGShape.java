c6344ac932259f479885254208140a4be79604b2
Ian Mayo <ian@planetmayo.com>
2018-09-07 12:58:51 +0100

    Improve static analysis

--- org.mwc.cmap.legacy/src/MWC/GUI/Shapes/Symbols/SVG/SVGShape.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/Shapes/Symbols/SVG/SVGShape.java
@@ -43,11 +43,12 @@ public class SVGShape extends PlainSymbol
    */
   private static final long serialVersionUID = 1L;
 
-  /** our symbol type
+  /**
+   * our symbol type
    * 
    */
   private final String _svgFileName;
-  
+
   /**
    * Elements of the shape (Cache)
    */
@@ -58,7 +59,8 @@ public class SVGShape extends PlainSymbol
    */
   private boolean _canRotate;
 
-  /** the origin, at which we centre the symbol
+  /**
+   * the origin, at which we centre the symbol
    * 
    */
   @SuppressWarnings("unused")
@@ -109,93 +111,25 @@ public class SVGShape extends PlainSymbol
   {
     if (_elements == null)
     {
-      try
+
+      // get the XML document
+      final Document doc = getDocument();
+
+      if (doc != null)
       {
-        /**
-         * We get the SVG as String
-         */
-
-        // TODO. Change this. If we use org.mwc.cmap.core, it gives a cyclic dependency error.
-        // final java.net.URL resource = ;
-        // SymbolFactory.class.getReso.
-
-        // String svgFilePath =
-        // java.nio.file.Paths.get(resource.toURI()).toAbsolutePath().toString();
-        //final File fXmlFile = new File(SVGShape.class.getResource("/" + _svgPath).getFile());
-        // System.out.println(fXmlFile.getAbsolutePath());
-        
-        // remove the svg: prefix, if necesary
-        final String fName = _svgFileName.contains("svg:") ? _svgFileName.substring(4) : _svgFileName;
-        
-        String svgPath = fName + SymbolFactory.SVG_EXTENSION;
-        
-        final File fXmlFile = new File(svgPath);
-        final DocumentBuilderFactory dbFactory = DocumentBuilderFactory
-            .newInstance();
-        final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        final Document doc = dBuilder.parse(fXmlFile);
-
-        // read this -
-        // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-        doc.getDocumentElement().normalize();
-
-        final Element svgRoot = doc.getDocumentElement();
-
-        final SVGElementFactory elementFactory = new SVGElementFactory();
-        _elements = new ArrayList<>();
-        for (int i = 0; i < svgRoot.getChildNodes().getLength(); i++)
-        {
-          Node element = svgRoot.getChildNodes().item(i);
-
-          if (element.getNodeType() == Node.ELEMENT_NODE)
-          {
-
-            // check it's not the origin node
-            Node id = element.getAttributes().getNamedItem("id");
-            if(id != null && "origin".equals(id.getNodeValue()))
-            {
-              parseOrigin(element);
-              parseRotation(element);
-            }
-            else
-            {
-              SVGElement newElement = elementFactory.getInstance(
-                  (Element) element);
-
-              // We are ignoring unknown elements for now.
-              if (newElement != null)
-              {
-                _elements.add(newElement);
-              }
-            }
-          }
-        }
-      }
-      catch (IOException e)
-      {
-        // TODO I coudln't read the given file.
-        e.printStackTrace();
-      }
-      catch (SAXException e)
-      {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-      catch (ParserConfigurationException e)
-      {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+        // parse the XML document
+        _elements = parseDocument(doc);
       }
     }
 
     // At this point, we have our file loaded (pre-cached)
-    if(_elements == null)
+    if (_elements == null)
+
     {
       Trace.trace("Failed to parse SVG file for: " + _svgFileName);
     }
     else
     {
-
       double directionToUse = .0;
       if (_canRotate)
       {
@@ -203,7 +137,7 @@ public class SVGShape extends PlainSymbol
       }
 
       // create our centre point
-      final java.awt.Point centre = dest.toScreen(center);
+      final Point centre = dest.toScreen(center);
       for (SVGElement element : _elements)
       {
         element.render(dest, getScaleVal(), centre, directionToUse);
@@ -211,37 +145,141 @@ public class SVGShape extends PlainSymbol
     }
   }
 
+  /** retrieve the nodes from the SVG document
+   * 
+   * @param doc
+   * @return
+   */
+  private List<SVGElement> parseDocument(final Document doc)
+  {
+
+    final Element svgRoot = doc.getDocumentElement();
+
+    final SVGElementFactory elementFactory = new SVGElementFactory();
+    ArrayList<SVGElement> elements = new ArrayList<SVGElement>();
+    for (int i = 0; i < svgRoot.getChildNodes().getLength(); i++)
+    {
+      Node element = svgRoot.getChildNodes().item(i);
+
+      if (element.getNodeType() == Node.ELEMENT_NODE)
+      {
+
+        // check it's not the origin node
+        Node id = element.getAttributes().getNamedItem("id");
+        if (id != null && "origin".equals(id.getNodeValue()))
+        {
+          // ok, it's the origin marker. get the origin coords
+          parseOrigin(element);
+          
+          // and determine if the shape should rotate
+          parseRotation(element);
+        }
+        else
+        {
+          SVGElement newElement = elementFactory.getInstance((Element) element);
+
+          // We are ignoring unknown elements for now.
+          if (newElement != null)
+          {
+            elements.add(newElement);
+          }
+        }
+      }
+    }
+    return elements;
+  }
+
+  /**
+   * retrieve the SVG document
+   * 
+   * @return
+   */
+  private Document getDocument()
+  {
+    // TODO. Change this. If we use org.mwc.cmap.core, it gives a cyclic dependency error.
+    // final java.net.URL resource = ;
+    // SymbolFactory.class.getReso.
+
+    // String svgFilePath =
+    // java.nio.file.Paths.get(resource.toURI()).toAbsolutePath().toString();
+    // final File fXmlFile = new File(SVGShape.class.getResource("/" + _svgPath).getFile());
+    // System.out.println(fXmlFile.getAbsolutePath());
+
+    try
+    {
+
+      // remove the svg: prefix, if necesary
+      final String fName = _svgFileName.contains("svg:") ? _svgFileName
+          .substring(4) : _svgFileName;
+
+      String svgPath = fName + SymbolFactory.SVG_EXTENSION;
+
+      final File fXmlFile = new File(svgPath);
+      final DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+          .newInstance();
+      final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      final Document doc = dBuilder.parse(fXmlFile);
+
+      // read this -
+      // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+      doc.getDocumentElement().normalize();
+
+      return doc;
+    }
+    catch (IOException e)
+    {
+      // TODO I coudln't read the given file.
+      e.printStackTrace();
+    }
+    catch (SAXException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    catch (ParserConfigurationException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    // failed, drop out
+    return null;
+  }
+
   private void parseOrigin(Node element)
   {
-    final int x = Integer.parseInt(element.getAttributes().getNamedItem("cx").getNodeValue());
-    final int y = Integer.parseInt(element.getAttributes().getNamedItem("cy").getNodeValue());
+    final int x = Integer.parseInt(element.getAttributes().getNamedItem("cx")
+        .getNodeValue());
+    final int y = Integer.parseInt(element.getAttributes().getNamedItem("cy")
+        .getNodeValue());
     _origin = new Point(x, y);
   }
 
   private void parseRotation(Node element)
   {
-     final String style = element.getAttributes().getNamedItem("style").getNodeValue();
-     if(style == null || style.length() == 0)
-     {
-       Trace.trace("Style parameter missing for " + _svgFileName);
-     }
-     else
-     {
-       final boolean rotates;
-       if(style.equals("fill: rgb(255, 0, 0);"))
-       {
-         // red - doesn't rotate
-         rotates = false;
-       }
-       else
-       {
-         // green = does rotate
-         rotates = true;
-       }
-       
-       _canRotate = rotates;
-     }
-    
+    final String style = element.getAttributes().getNamedItem("style")
+        .getNodeValue();
+    if (style == null || style.length() == 0)
+    {
+      Trace.trace("Style parameter missing for " + _svgFileName);
+    }
+    else
+    {
+      final boolean rotates;
+      if ("fill: rgb(255, 0, 0);".equals(style))
+      {
+        // red - doesn't rotate
+        rotates = false;
+      }
+      else
+      {
+        // green = does rotate
+        rotates = true;
+      }
+
+      _canRotate = rotates;
+    }
+
   }
 
   @Override
