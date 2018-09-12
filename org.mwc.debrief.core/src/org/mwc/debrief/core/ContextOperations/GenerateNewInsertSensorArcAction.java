57b1bd9118fe7ae5fbc7d99be77cb1d17a71f472
ayesha.ma <ayesha.ma@gmail.com>
2018-09-12 13:24:51 +0530

    added copyright text

--- org.mwc.debrief.core/src/org/mwc/debrief/core/ContextOperations/GenerateNewInsertSensorArcAction.java
+++ org.mwc.debrief.core/src/org/mwc/debrief/core/ContextOperations/GenerateNewInsertSensorArcAction.java
@@ -1,5 +1,16 @@
-/**
- * 
+/*
+ *    Debrief - the Open Source Maritime Analysis Application
+ *    http://debrief.info
+ *
+ *    (C) 2000-2018, Deep Blue C Technology Ltd
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the Eclipse Public License v1.0
+ *    (http://www.eclipse.org/legal/epl-v10.html)
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 package org.mwc.debrief.core.ContextOperations;
 
@@ -213,7 +224,7 @@ public class GenerateNewInsertSensorArcAction implements
           // get the supporting data
           String[] tracks = new String[1];
           tracks[0]=theTrack.getName();
-          NewSensorArcWizard wizard = new NewSensorArcWizard(tracksMap,theTrack.getName(), startTime, endTime) ;
+          NewSensorArcWizard wizard = new NewSensorArcWizard(theTrack.getName(), startTime, endTime) ;
           runOperation(theLayers, theTrack, wizard);
         }
       };
