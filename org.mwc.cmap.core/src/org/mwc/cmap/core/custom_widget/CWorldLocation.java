be6e4f0986117058e7eee871bc7096df69d2f1b0
Ian Mayo <ian@planetmayo.com>
2018-09-17 11:43:43 +0100

    Provide both MS-Windows and MACOS event handlers for CWorldLocation editor controls.

--- org.mwc.cmap.core/src/org/mwc/cmap/core/custom_widget/CWorldLocation.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/core/custom_widget/CWorldLocation.java
@@ -11,6 +11,8 @@ import org.eclipse.nebula.widgets.formattedtext.MaskFormatter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SegmentEvent;
+import org.eclipse.swt.events.SegmentListener;
 import org.eclipse.swt.events.VerifyEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -56,20 +58,44 @@ public class CWorldLocation extends Composite
     myLatitude.setFormatter(new IgnoreTabsMaskFormatter(getFormat().getNebulaPattern(false)));
     myLongitude.setFormatter(new IgnoreTabsMaskFormatter(getFormat().getNebulaPattern(true)));
 
-    myLatitude.getControl().addModifyListener(new ModifyListener() {
-
+    // note, we're having trouble hearing the correct events.
+    // we only hear SegmentListener events on MS-Windows, and 
+    // we only hear ModifyListener events on MACOS.  So,
+    // we'll listen to both
+    
+    myLatitude.getControl().addModifyListener(new ModifyListener()
+    {
       @Override
       public void modifyText(ModifyEvent event)
       {
         valueModified(event.getSource());
-      }});
-    myLatitude.getControl().addModifyListener(new ModifyListener() {
-
+      }
+    });
+    myLongitude.getControl().addModifyListener(new ModifyListener()
+    {
       @Override
       public void modifyText(ModifyEvent event)
       {
         valueModified(event.getSource());
-      }});
+      }
+    });
+    myLatitude.getControl().addSegmentListener(new SegmentListener()
+    {
+      @Override
+      public void getSegments(SegmentEvent event)
+      {
+        valueModified(event.getSource());
+      }
+    });
+    myLongitude.getControl().addSegmentListener(new SegmentListener()
+    {
+      @Override
+      public void getSegments(SegmentEvent event)
+      {
+        valueModified(event.getSource());
+      }
+    });
+
   }
   
   public void addLocationModifiedListener(LocationModifiedListener listener) {
