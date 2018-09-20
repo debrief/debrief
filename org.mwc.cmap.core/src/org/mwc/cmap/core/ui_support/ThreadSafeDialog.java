e8aa59872a1d22104a079f2337601bd3966f4830
Ian Mayo <ian@planetmayo.com>
2018-09-20 16:20:57 +0100

    Tidy implementation

--- org.mwc.cmap.core/src/org/mwc/cmap/core/ui_support/ThreadSafeDialog.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/core/ui_support/ThreadSafeDialog.java
@@ -2,38 +2,79 @@ package org.mwc.cmap.core.ui_support;
 
 import java.lang.reflect.Method;
 
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
-public abstract class ThreadSafeDialog<T> extends
-    org.eclipse.jface.dialogs.Dialog
+/**
+ * custom version of dialog class that opens without use of syncExec. We need to run dialogs async
+ * so that RCPTT runtime can interact with dialog controls.
+ *
+ * @author ian
+ *
+ * @param <T>
+ *          Return type
+ */
+public abstract class ThreadSafeDialog<T> extends Dialog
 {
 
+  /** are we on the UI thread?
+   * 
+   * @return yes/no
+   */
+  private static boolean isUIThread()
+  {
+    Object uiThread = null;
+    try
+    {
+      final Class<?> displayClass = Display.class;
+
+      final Method getThreadMethod = displayClass.getDeclaredMethod("getThread",
+          new Class[]
+          {});
+      uiThread = getThreadMethod.invoke(Display.getDefault(), new Object[]
+      {});
+    }
+    catch (final Exception e)
+    {
+
+      e.printStackTrace();
+    }
+
+    return (Thread.currentThread() == uiThread);
+  }
+
+  /** we need to store the selection locally, so it can be passed into
+   * the lock object
+   */
   private T selection;
 
-  protected ThreadSafeDialog(Shell parentShell)
+  protected ThreadSafeDialog(final Shell parentShell)
   {
     super(parentShell);
   }
 
+  /** child classes should implement this to actually open the dialog
+   * 
+   * @return
+   */
   protected abstract T internalOpenDialog();
 
   public T openDialog()
   {
-    
-    if(isUIThread())
+    // if we're already on the UI thread we can just open dialog.    
+    if (isUIThread())
     {
-
       return selection = internalOpenDialog();
-      
     }
-    
+
     final Object lock = new Object();
 
     final Display current = Display.getDefault();
 
     current.asyncExec(new Runnable()
     {
+      @Override
       public void run()
       {
         synchronized (lock)
@@ -56,34 +97,12 @@ public abstract class ThreadSafeDialog<T> extends
       {
         lock.wait();
       }
-      catch (InterruptedException e)
+      catch (final InterruptedException e)
       {
         e.printStackTrace();
       }
     }
-
     return selection;
   }
 
-  public static boolean isUIThread()
-  {
-      Object uiThread = null;
-      try
-      {
-          Class<?> displayClass = Display.class;
-         
-          Method getThreadMethod = displayClass.getDeclaredMethod("getThread", new Class[] { });
-          uiThread = getThreadMethod.invoke(Display.getDefault(), new Object[] { });
-      }
-      catch(Exception e)
-      {
-
-        e.printStackTrace();
-      }
-
-      return (Thread.currentThread() == uiThread);
-  }
-  
 }
-
-
