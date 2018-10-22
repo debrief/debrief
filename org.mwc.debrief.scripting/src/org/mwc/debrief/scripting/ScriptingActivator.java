0d98d275114cc515e2fd8024c4c05336bf35d107
Ian Mayo <ian@planetmayo.com>
2018-10-22 22:31:27 +0200

    try to register for time events

---
+++ org.mwc.debrief.scripting/src/org/mwc/debrief/scripting/ScriptingActivator.java
@@ -0,0 +1,120 @@
+package org.mwc.debrief.scripting;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.mwc.cmap.core.DataTypes.Temporal.TimeProvider;
+import org.mwc.cmap.core.ui_support.PartMonitor;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import MWC.GenericData.HiResDate;
+
+public class ScriptingActivator extends AbstractUIPlugin implements BundleActivator
+{
+
+  private static BundleContext context;
+
+  static BundleContext getContext()
+  {
+    return context;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+   */
+  public void start(BundleContext bundleContext) throws Exception
+  {
+    super.start(bundleContext);
+    ScriptingActivator.context = bundleContext;
+    System.out.println("STARTUP");
+
+ //   listenToMyParts();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop(BundleContext bundleContext) throws Exception
+  {
+    super.stop(bundleContext);
+    ScriptingActivator.context = null;
+  }
+
+  /**
+   * helper application to help track activation/closing of new plots
+   */
+  private PartMonitor _partMonitor;
+
+  private TimeProvider _timeProvider;
+
+  private void listenToMyParts()
+  {
+    final PropertyChangeListener listener = new PropertyChangeListener()
+    {
+
+      @Override
+      public void propertyChange(PropertyChangeEvent evt)
+      {
+        HiResDate date = (HiResDate) evt.getNewValue();
+        fireNewTime(date);
+      }
+    };
+
+    // Listen for anyone that can provide time
+    _partMonitor.addPartListener(TimeProvider.class, PartMonitor.ACTIVATED,
+        new PartMonitor.ICallback()
+        {
+          public void eventTriggered(final String type, final Object part,
+              final IWorkbenchPart parentPart)
+          {
+            TimeProvider provider = (TimeProvider) part;
+
+            if (!provider.equals(_timeProvider))
+            {
+              // changed.
+              if (_timeProvider != null)
+              {
+                _timeProvider.removeListener(listener,
+                    TimeProvider.TIME_CHANGED_PROPERTY_NAME);
+              }
+
+              _timeProvider = provider;
+              _timeProvider.addListener(listener,
+                  TimeProvider.TIME_CHANGED_PROPERTY_NAME);
+            }
+          }
+        });
+
+    // Listen for anyone that can provide time
+    _partMonitor.addPartListener(TimeProvider.class, PartMonitor.CLOSED,
+        new PartMonitor.ICallback()
+        {
+          public void eventTriggered(final String type, final Object part,
+              final IWorkbenchPart parentPart)
+          {
+            TimeProvider provider = (TimeProvider) part;
+
+            if (provider.equals(_timeProvider))
+            {
+              // changed.
+              _timeProvider.removeListener(listener,
+                  TimeProvider.TIME_CHANGED_PROPERTY_NAME);
+            }
+          }
+        });
+
+  }
+
+  protected void fireNewTime(HiResDate date)
+  {
+    System.out.println("CAUGHT NEW TIME");
+  }
+
+}
