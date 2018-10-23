8a2ccc2499832f284336b608c6523f405f917af9
Ian Mayo <ian@planetmayo.com>
2018-10-23 23:02:30 +0200

    introduce some delays, draw more lines

--- org.mwc.cmap.combined.feature/root_installs/sample_data/scripts/geometry.js
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/scripts/geometry.js
@@ -4,6 +4,19 @@
  */
 loadModule("/Debrief", false);
 
+/** utility to introduce pause
+ * 
+ * @param ms milliseconds wait
+ * @returns nothing
+ */
+function wait(ms) {
+	var d = new Date();
+	var d2 = null;
+	do {
+		d2 = new Date();
+	} while (d2 - d < ms);
+}
+
 
 var editor = getEditor();
 
@@ -16,12 +29,15 @@ if (editor == null) {
 	var centre = editor.getCentre();
 	for(i=0;i<10;i++)
 	{
-		for(j=0;j<10;j++)
+		for(j=0;j<20;j++)
 		{
+			var color = getColor(20 * i, 255 - 10 * j, 200); 
 			var startP = centre.add(createVector(i * 5000, (j + 6) * 10));
 			var endP = centre.add(createVector(j * 5000, (i + 2) * 10));
-			var shape = createLine(startP, endP, "name" + (i * 10 + j));
+			var shape = createLine(startP, endP, "name" + (i * 10 + j), color);
 			shapes.add(shape);
+			wait(50);
+			layers.fireModified();
 		}
 	}
 	layers.fireModified();
