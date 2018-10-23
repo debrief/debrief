4abc51b4bb791636495a0d9b0de74e32629ffd58
Ian Mayo <ian@planetmayo.com>
2018-10-23 21:54:02 +0200

    loop through selection

--- org.mwc.cmap.combined.feature/root_installs/sample_data/scripts/format_track.js
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/scripts/format_track.js
@@ -1,6 +1,6 @@
 /**
  * name : Format track (JS) 
- * popup : enabledFor(Debrief.Wrappers.TrackWrapper)
+ * popup : enableFor(Debrief.Wrappers.TrackWrapper)
  */
 
 loadModule("/System/UI", false);
@@ -8,16 +8,20 @@ loadModule("/Debrief", false);
 
 var selection = getSelection();
 
-var editable = selection.getFirstElement();
-var track = editable.getEditable();
-
 var minutesStr = showInputDialog("How many minutes apart for symbols?",
-		"10");
+"10");
 var minutes = parseInt(minutesStr);
 var interval = createDate(1000 * 60 * minutes)
-track.setSymbolFrequency(interval);
+
+var items = selection.toArray();
+var length = items.length;
+
+for (var i = 0; i < length; i++) {
+    var editable = items[i];    
+    var track = editable.getEditable();
+	track.setSymbolFrequency(interval);
+}
 
 var layers = getEditor().getLayers();
 layers.fireModified();
 
-// Debrief.Wrappers.Track.LightweightTrackWrapper
