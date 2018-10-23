2794e73d652cc76c621c395bd63f05208764b437
Ian Mayo <ian@planetmayo.com>
2018-10-23 23:23:17 +0200

    more (beautiful) shapes

--- org.mwc.cmap.combined.feature/root_installs/sample_data/scripts/geometry.js
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/scripts/geometry.js
@@ -27,15 +27,22 @@ if (editor == null) {
 	var layers = editor.getLayers();
 	var shapes = layers.createLayer("Shapes");
 	var centre = editor.getCentre();
-	for(i=0;i<10;i++)
+	for(i=0;i<12;i++)
 	{
-		for(j=0;j<20;j++)
+		for(j=0;j<12;j++)
 		{
+			var ctr = i * 12 + j;
 			var color = getColor(20 * i, 255 - 10 * j, 200); 
 			var startP = centre.add(createVector(i * 5000, (j + 6) * 10));
 			var endP = centre.add(createVector(j * 5000, (i + 2) * 10));
-			var shape = createLine(startP, endP, "name" + (i * 10 + j), color);
-			shapes.add(shape);
+			var line = createLine(startP, endP, "name" + ctr, color);
+			shapes.add(line);
+
+			var centre = centre.add(createVector((j + 4) * 1000, (j + 2) * 100));
+			var distance = createDistance(2 + i * 6);
+			var circle = createCircle(centre, distance, "circle:"+ctr, color);
+			shapes.add(circle);
+			
 			wait(50);
 			layers.fireModified();
 		}
