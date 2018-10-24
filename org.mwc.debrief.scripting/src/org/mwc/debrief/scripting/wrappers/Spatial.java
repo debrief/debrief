225f0e242940c91a749ecf420b05856a3261ba10
Ian Mayo <ian@planetmayo.com>
2018-10-24 22:05:22 +0200

    Allow units to be specified for distance

--- org.mwc.debrief.scripting/src/org/mwc/debrief/scripting/wrappers/Spatial.java
+++ org.mwc.debrief.scripting/src/org/mwc/debrief/scripting/wrappers/Spatial.java
@@ -1,5 +1,7 @@
 package org.mwc.debrief.scripting.wrappers;
 
+import org.eclipse.ease.modules.WrapToScript;
+
 import MWC.GenericData.WorldDistance;
 import MWC.GenericData.WorldLocation;
 import MWC.GenericData.WorldVector;
@@ -7,23 +9,49 @@ import MWC.GenericData.WorldVector;
 public class Spatial
 {
 
+  /**
+   * the units for distances. Note to provide these to ease, we have to add
+   * a @WrapToScript annotation.  Once we've added this annotation to one public
+   * field/method, we have to apply it for all methods
+   */
+  @WrapToScript
+  static public final int METRES = 0;
+  @WrapToScript
+  static public final int YARDS = 1;
+  @WrapToScript
+  static public final int KM = 2;
+  @WrapToScript
+  static public final int NM = 3;
+  @WrapToScript
+  static public final int MINUTES = 4;
+  @WrapToScript
+  static public final int DEGS = 5;
+  @WrapToScript
+  static public final int KYDS = 6;
+  @WrapToScript
+  static public final int FT = 7;
+  
+  @WrapToScript
   public static WorldLocation createLocation(double dLat, double dLong,
       double depth)
   {
     return new WorldLocation(dLat, dLong, depth);
   }
 
-  public WorldDistance createDistance(double km)
+  @WrapToScript
+  public WorldDistance createDistance(double value, int units)
   {
-    return new WorldDistance(km, WorldDistance.KM);
+    return new WorldDistance(value, units);
   }
 
+  @WrapToScript
   public static WorldVector createVector(double distM, double bearingDegs)
   {
     return new WorldVector(MWC.Algorithms.Conversions.Degs2Rads(bearingDegs),
         new WorldDistance(distM, WorldDistance.METRES), null);
   }
 
+  @WrapToScript
   public static WorldVector createVectorKm(double distKm, double bearingDegs)
   {
     return new WorldVector(MWC.Algorithms.Conversions.Degs2Rads(bearingDegs),
