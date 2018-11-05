372968ec11d0e41b0df7645a70a02ff0f76d5c90
Ian Mayo <ian@planetmayo.com>
2018-11-05 15:23:43 +0000

    use local (relative) path to sources, using other version of icon

--- build-x64.js
+++ build-x64.js
@@ -1,22 +1,22 @@
 var createMsi = require('msi-packager')
- 
+
 var options = {
- 
+
   // required
-  source: '/home/travis/build/debrief/debrief/org.mwc.debrief.product/target/products/target/products/DebriefNG/win32/win32/x86_64/DebriefNG',
-  output: '/home/travis/build/debrief/debrief/org.mwc.debrief.product/target/products/DebriefNG-Windows64Bit.msi',
+  source: 'org.mwc.debrief.product/target/products/target/products/DebriefNG/win32/win32/x86_64/DebriefNG',
+  output: 'org.mwc.debrief.product/target/products/DebriefNG-Windows64Bit.msi',
   name: 'DebriefNG',
   upgradeCode: '6d8fb213-e072-4f38-a184-bfbf7505307e',
   version: 'versionReplacement',
   manufacturer: 'Deep Blue C Technology Ltd',
-  iconPath: '/home/travis/build/debrief/debrief/org.mwc.debrief.product/target/products/icon.ico',
+  iconPath: 'org.mwc.debrief.product/icon.ico',
   executable: 'DebriefNG.exe',
- 
+
   // optional
   description: "The Debrief Maritime Analysis Tool accelerates maritime data analysis and reporting. Over twenty years of visionary development make us the first choice of effective analysts the world over.",
   arch: 'x64',
   localInstall: true
- 
+
 }
 
 createMsi(options, function (err) {
