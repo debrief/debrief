8b6f5ab3dd34b53486b3f0090ba208283d1d06f0
Ian Mayo <ian@planetmayo.com>
2018-11-05 15:23:16 +0000

    use local (relative) path, using MACOS compliant sed command.

--- build-msi.sh
+++ build-msi.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 set -ex
-npm i -g npm
-npm uninstall npm
-npm install
 rm -rf node_modules/msi-packager
 git clone https://github.com/saulhidalgoaular/msi-packager.git
 mv msi-packager node_modules/
-version=$(grep "product.*version"  /home/travis/build/debrief/debrief/org.mwc.debrief.product/debriefng.product  | sed 's/^.*version="\([^"]*\)".*$/\1/')
-sed -i "s/versionReplacement/$version/g" build-x86.js
-sed -i "s/versionReplacement/$version/g" build-x64.js
+version=$(grep "product.*version"  org.mwc.debrief.product/debriefng.product  | sed 's/^.*version="\([^"]*\)".*$/\1/')
+sed -i '.bak' "s/versionReplacement/$version/g" build-x86.js
+sed -i '.bak' "s/versionReplacement/$version/g" build-x64.js
 node build-x86.js
 node build-x64.js
