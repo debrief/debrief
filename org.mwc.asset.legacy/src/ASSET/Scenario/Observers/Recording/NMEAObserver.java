d9db07d89804313560d650e449998466e0ccc6a3
Ian Mayo <ian@planetmayo.com>
2018-11-27 08:33:36 +0000

    apply code cleanup

--- org.mwc.asset.legacy/src/ASSET/Scenario/Observers/Recording/NMEAObserver.java
+++ org.mwc.asset.legacy/src/ASSET/Scenario/Observers/Recording/NMEAObserver.java
@@ -10,7 +10,7 @@
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 package ASSET.Scenario.Observers.Recording;
 
@@ -19,13 +19,11 @@ import java.beans.PropertyDescriptor;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 
 import ASSET.NetworkParticipant;
 import ASSET.ParticipantType;
-import ASSET.ScenarioType;
 import ASSET.Models.SensorType;
 import ASSET.Models.Decision.TargetType;
 import ASSET.Models.Detection.DetectionEvent;
@@ -40,6 +38,43 @@ import junit.framework.TestCase;
 
 public class NMEAObserver extends RecordStatusToFileObserverType
 {
+
+  static public class NMEAObserverInfo extends EditorType
+  {
+
+    /**
+     * constructor for editable details of a set of Layers
+     *
+     * @param data
+     *          the Layers themselves
+     */
+    public NMEAObserverInfo(final NMEAObserver data)
+    {
+      super(data, data.getName(), "Edit");
+    }
+
+    /**
+     * editable GUI properties for our participant
+     *
+     * @return property descriptions
+     */
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors()
+    {
+      try
+      {
+        final PropertyDescriptor[] res =
+        {prop("Directory", "The directory to place Debrief data-files"), prop(
+            "Active", "Whether this observer is active"),};
+
+        return res;
+      }
+      catch (final IntrospectionException e)
+      {
+        return super.getPropertyDescriptors();
+      }
+    }
+  }
 
   public static class TestConvert extends TestCase
   {
@@ -59,69 +94,19 @@ public class NMEAObserver extends RecordStatusToFileObserverType
     }
   }
 
-  protected boolean _haveOutputPositions = false;
-
-  private ArrayList<Integer> _recordedDetections = new ArrayList<Integer>();
-
-  /**
-   * the (optional) type of sensor we listen to
-   * 
-   */
-  private final String _subjectSensor;
-
-  /***************************************************************
-   * constructor
-   ***************************************************************/
-
-  /**
-   * create a new monitor
-   * 
-   * @param directoryName
-   *          the directory to output the plots to
-   * @param recordDetections
-   *          whether to record detections
-   * @param subjectSensor 
-   * @param formatHelpers
-   */
-  public NMEAObserver(final String directoryName, final String fileName,
-      final boolean recordDetections, final boolean recordPositions,
-      final TargetType subjectToTrack, final String observerName,
-      final boolean isActive, final String subjectSensor)
+  private final static String pack(final double val, final boolean isLat)
   {
-    super(directoryName, fileName, recordDetections, false, recordPositions,
-        subjectToTrack, observerName, isActive);
-    _subjectSensor = subjectSensor;
-  }
-   /**
-   * 
-   * @param loc
-   * @param stat
-   * @param pt
-   * @param newTime
-   * @return
-   */
-  private String writeStatus(final WorldLocation loc)
-  {
-    final double dLat = loc.getLat();
-    final double dLong = loc.getLong();
-    String res = "$POSL,POS,GPS," + pack(dLat, true) + "," + pack(dLong, false)
-        + ",a,b,c,d" + LB;
-    return res;
-  }
-
-  private final static String pack(double val, boolean isLat)
-  {
-    boolean isPos = val >= 0;
-    double aVal = Math.abs(val);
+    final boolean isPos = val >= 0;
+    final double aVal = Math.abs(val);
 
     final int intPart = (int) Math.floor(aVal);
     final double floatPart = (aVal - intPart) * 60d;
-    DecimalFormat p1 = new DecimalFormat("00");
-    DecimalFormat p2 = new DecimalFormat("000");
-    DecimalFormat p3 = new DecimalFormat("00.0000");
+    final DecimalFormat p1 = new DecimalFormat("00");
+    final DecimalFormat p2 = new DecimalFormat("000");
+    final DecimalFormat p3 = new DecimalFormat("00.0000");
 
-    String degs = isLat ? p1.format(intPart) : p2.format(intPart);
-    String other = p3.format(floatPart);
+    final String degs = isLat ? p1.format(intPart) : p2.format(intPart);
+    final String other = p3.format(floatPart);
 
     final String hemi;
     if (isLat)
@@ -136,99 +121,141 @@ public class NMEAObserver extends RecordStatusToFileObserverType
     return degs + other + "," + hemi;
   }
 
-  @Override
-  public void restart(ScenarioType scenario)
-  {
-    super.restart(scenario);
+  /***************************************************************
+   * constructor
+   ***************************************************************/
 
-    // and clear the stored detections
-    _recordedDetections.clear();
-  }
-
-  public void writeThesePositionDetails(final WorldLocation loc,
-      final Status stat, final ParticipantType pt,
-      long newTime)
-  {
-    // make a note that we've output some track positions now
-    // (and are now happy to output sensor data)
-    _haveOutputPositions = true;
-
-    // ok, we output DTG for all entries
-    writeToFile(writeDTG(newTime));
-
-    // course
-    if (Math.random() <= 0.6)
-    {
-      String res = writeCourse(stat.getCourse());
-      writeToFile(res);
-    }
-
-    // speed
-    if (Math.random() <= 0.5)
-    {
-      String res = writeSpeed(stat.getSpeed().getValueIn(WorldSpeed.M_sec));
-      writeToFile(res);
-    }
-
-    // position
-    if (Math.random() <= 0.3)
-    {
-      String res = writeStatus(loc);
-      writeToFile(res);
-    }
-  }
+  protected boolean _haveOutputPositions = false;
+  /**
+   * the (optional) type of sensor we listen to
+   *
+   */
+  private final String _subjectSensor;
 
   final private SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
+
   final private SimpleDateFormat time = new SimpleDateFormat("HHmmss.SSS");
+
   final private String LB = System.getProperty("line.separator");
 
-  private String writeDTG(final long newTime)
+  /**
+   * create a new monitor
+   *
+   * @param directoryName
+   *          the directory to output the plots to
+   * @param recordDetections
+   *          whether to record detections
+   * @param subjectSensor
+   * @param formatHelpers
+   */
+  public NMEAObserver(final String directoryName, final String fileName,
+      final boolean recordDetections, final boolean recordPositions,
+      final TargetType subjectToTrack, final String observerName,
+      final boolean isActive, final String subjectSensor)
   {
-    String res = "$POSL,DZA," + date.format(new Date(newTime)) + "," + time
-        .format(new Date(newTime)) + ",a,b,c,d" + LB;
+    super(directoryName, fileName, recordDetections, false, recordPositions,
+        subjectToTrack, observerName, isActive);
+    _subjectSensor = subjectSensor;
+  }
+
+  /**
+   * ok, create the property editor for this class
+   *
+   * @return the custom editor
+   */
+  @Override
+  protected Editable.EditorType createEditor()
+  {
+    return new NMEAObserver.NMEAObserverInfo(this);
+  }
+
+  /**
+   * determine the normal suffix for this file type
+   */
+  @Override
+  protected String getMySuffix()
+  {
+    return "log";
+  }
+
+  @Override
+  protected String newName(final String name)
+  {
+    return name + "_" + MWC.Utilities.TextFormatting.DebriefFormatDateTime
+        .toString(System.currentTimeMillis()) + ".log";
+  }
+
+  private String writeAISContact(final WorldLocation loc, final int target)
+  {
+    final int MMSI = 100000 + target;
+    final String res = "$POSL,AIS," + MMSI + "," + pack(loc.getLat(), true)
+        + "," + pack(loc.getLong(), false) + ",a,b,c,a,b,AIS1,c,d" + LB;
     return res;
+  }
+
+  /**
+   * output the build details to file
+   */
+  @Override
+  protected void writeBuildDate(final String theBuildDate) throws IOException
+  {
+    // ok, skip.
   }
 
   private String writeCourse(final double courseDegs)
   {
-    String res = "$POSL,HDG," + courseDegs + ",a,b,c,d" + LB;
+    final String res = "$POSL,HDG," + courseDegs + ",a,b,c,d" + LB;
     return res;
+  }
+
+  private String writeDTG(final long newTime)
+  {
+    final String res = "$POSL,DZA," + date.format(new Date(newTime)) + ","
+        + time.format(new Date(newTime)) + ",a,b,c,d" + LB;
+    return res;
+  }
+
+  /**
+   * write out the file header details for this scenario
+   *
+   * @param title
+   *          the scenario we're describing
+   * @throws IOException
+   */
+
+  @Override
+  protected void writeFileHeaderDetails(final String title,
+      final long currentDTG) throws IOException
+  {
+    _os.write("$POSL" + LB);
   }
 
   private String writeSpeed(final double speedMs)
   {
-    String res = "$POSL,VEL,SPL,a,b,c," + speedMs + ",a,b,c,d" + LB;
+    final String res = "$POSL,VEL,SPL,a,b,c," + speedMs + ",a,b,c,d" + LB;
     return res;
   }
 
   /**
-   * write this text to our stream
-   * 
-   * @param msg
-   *          the string to write
+   *
+   * @param loc
+   * @param stat
+   * @param pt
+   * @param newTime
+   * @return
    */
-  private void writeToFile(String msg)
+  private String writeStatus(final WorldLocation loc)
   {
-    if (msg != null)
-    {
-      try
-      {
-        if (_os == null)
-          super.createOutputFile();
-
-        _os.write(msg);
-        _os.flush();
-      }
-      catch (IOException e)
-      {
-        e.printStackTrace();
-      }
-    }
+    final double dLat = loc.getLat();
+    final double dLong = loc.getLong();
+    final String res = "$POSL,POS,GPS," + pack(dLat, true) + "," + pack(dLong,
+        false) + ",a,b,c,d" + LB;
+    return res;
   }
 
   /**
    * write these detections to file
-   * 
+   *
    * @param pt
    *          the participant we're on about
    * @param detections
@@ -236,13 +263,14 @@ public class NMEAObserver extends RecordStatusToFileObserverType
    * @param dtg
    *          the dtg at which the detections were observed
    */
-  protected void writeTheseDetectionDetails(ParticipantType pt,
-      DetectionList detections, long dtg)
+  @Override
+  protected void writeTheseDetectionDetails(final ParticipantType pt,
+      final DetectionList detections, final long dtg)
   {
-    Iterator<DetectionEvent> iter = detections.iterator();
+    final Iterator<DetectionEvent> iter = detections.iterator();
     while (iter.hasNext())
     {
-      DetectionEvent de = (DetectionEvent) iter.next();
+      final DetectionEvent de = iter.next();
 
       final SensorType sensor = pt.getSensorFit().getSensorWithId(de
           .getSensor());
@@ -254,109 +282,76 @@ public class NMEAObserver extends RecordStatusToFileObserverType
       {
         final double brg = de.getBearing();
         final double rng = de.getRange().getValueIn(WorldDistance.DEGS);
-        WorldVector offset = new WorldVector(brg, rng, 0d);
-        WorldLocation loc = de.getSensorLocation().add(offset);
+        final WorldVector offset = new WorldVector(brg, rng, 0d);
+        final WorldLocation loc = de.getSensorLocation().add(offset);
         writeToFile(writeAISContact(loc, de.getTarget()));
       }
     }
   }
 
-  private String writeAISContact(WorldLocation loc, int target)
+  @Override
+  protected void writeThesePositionDetails(final WorldLocation loc,
+      final Status stat, final ParticipantType pt, final long newTime)
   {
-    int MMSI = 100000 + target;
-    String res = "$POSL,AIS," + MMSI + "," + pack(loc.getLat(), true) + ","
-        + pack(loc.getLong(), false) + ",a,b,c,a,b,AIS1,c,d" + LB;
-    return res;
-  }
+    // make a note that we've output some track positions now
+    // (and are now happy to output sensor data)
+    _haveOutputPositions = true;
 
-  /**
-   * ok, create the property editor for this class
-   * 
-   * @return the custom editor
-   */
-  protected Editable.EditorType createEditor()
-  {
-    return new NMEAObserver.NMEAObserverInfo(this);
-  }
+    // ok, we output DTG for all entries
+    writeToFile(writeDTG(newTime));
 
-  protected String newName(final String name)
-  {
-    return name + "_" + MWC.Utilities.TextFormatting.DebriefFormatDateTime
-        .toString(System.currentTimeMillis()) + ".log";
-  }
-
-  /**
-   * determine the normal suffix for this file type
-   */
-  protected String getMySuffix()
-  {
-    return "log";
-  }
-
-  /**
-   * write out the file header details for this scenario
-   * 
-   * @param title
-   *          the scenario we're describing
-   * @throws IOException
-   */
-
-  protected void writeFileHeaderDetails(final String title, long currentDTG)
-      throws IOException
-  {
-    _os.write("$POSL" + LB);
-  }
-
-  /**
-   * output the build details to file
-   */
-  protected void writeBuildDate(String theBuildDate) throws IOException
-  {
-    // ok, skip.
-  }
-
-
-  static public class NMEAObserverInfo extends EditorType
-  {
-
-    /**
-     * constructor for editable details of a set of Layers
-     * 
-     * @param data
-     *          the Layers themselves
-     */
-    public NMEAObserverInfo(final NMEAObserver data)
+    // course
+    if (Math.random() <= 0.6)
     {
-      super(data, data.getName(), "Edit");
+      final String res = writeCourse(stat.getCourse());
+      writeToFile(res);
     }
 
-    /**
-     * editable GUI properties for our participant
-     * 
-     * @return property descriptions
-     */
-    public PropertyDescriptor[] getPropertyDescriptors()
+    // speed
+    if (Math.random() <= 0.5)
+    {
+      final String res = writeSpeed(stat.getSpeed().getValueIn(
+          WorldSpeed.M_sec));
+      writeToFile(res);
+    }
+
+    // position
+    if (Math.random() <= 0.3)
+    {
+      final String res = writeStatus(loc);
+      writeToFile(res);
+    }
+  }
+
+  @Override
+  protected void writeThisDecisionDetail(final NetworkParticipant pt,
+      final String activity, final long dtg)
+  {
+    throw new IllegalArgumentException("Not implemented for NMEA export");
+  }
+
+  /**
+   * write this text to our stream
+   *
+   * @param msg
+   *          the string to write
+   */
+  private void writeToFile(final String msg)
+  {
+    if (msg != null)
     {
       try
       {
-        final PropertyDescriptor[] res =
-        {prop("Directory", "The directory to place Debrief data-files"), prop(
-            "Active", "Whether this observer is active"),};
+        if (_os == null)
+          super.createOutputFile();
 
-        return res;
+        _os.write(msg);
+        _os.flush();
       }
-      catch (IntrospectionException e)
+      catch (final IOException e)
       {
-        return super.getPropertyDescriptors();
+        e.printStackTrace();
       }
     }
-  }
-
-
-  @Override
-  protected void writeThisDecisionDetail(NetworkParticipant pt, String activity,
-      long dtg)
-  {
-    throw new IllegalArgumentException("Not implemented for NMEA export");
   }
 }
