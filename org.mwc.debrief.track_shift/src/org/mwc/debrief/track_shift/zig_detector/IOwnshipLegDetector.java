9e5a0df0bbfa282a1749e3b73184956f0ad501bb
Ian Mayo <ian@planetmayo.com>
2018-11-30 11:32:50 +0000

    javadoc for method

--- org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/zig_detector/IOwnshipLegDetector.java
+++ org.mwc.debrief.track_shift/src/org/mwc/debrief/track_shift/zig_detector/IOwnshipLegDetector.java
@@ -6,6 +6,15 @@ import org.mwc.debrief.track_shift.zig_detector.ownship.LegOfData;
 
 public interface IOwnshipLegDetector
 {
+  /** produce a list of sliced ownship legs
+   * 
+   * @param times timestamps of measurements
+   * @param rawSpeeds speed measurements
+   * @param rawCourses course measurements
+   * @param minsOfAverage factor for lenght of moving average
+   * @param precision degree of precision required
+   * @return list of legs
+   */
   List<LegOfData> identifyOwnshipLegs(long[] times, double[] rawSpeeds,
       double[] rawCourses, int minsOfAverage, Precision precision);
 }
