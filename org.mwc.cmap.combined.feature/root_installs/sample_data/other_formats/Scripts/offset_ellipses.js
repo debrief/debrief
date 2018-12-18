751ac4ff64c3ca41d12294e8671964b0c0771eb8
Ian Mayo <ian@planetmayo.com>
2018-12-18 17:01:15 +0000

    introduce script to allow the duration of ellipses to be updated

---
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/Scripts/offset_ellipses.js
@@ -0,0 +1,33 @@
+loadModule("/System/UI", false); // for the input dialog
+loadModule('/Debrief/Core'); // for plot, duration, date
+var plot = getActivePlot();
+if (plot != null)
+{
+	var layers = plot.getLayers();
+	let anns = layers.findLayer("TUAs");
+	// did we find it the annotations layer?
+	if (anns != null)
+	{
+		// ok, sort out the duration
+		let mins = showInputDialog("How many minutes?", "10",
+				"Set ellipse duration");
+		let duration = createDuration(mins, DUR_MINUTES);
+
+		// now loop through the layer
+		let numer = anns.elements();
+		while (numer.hasMoreElements())
+		{
+			let item = numer.nextElement();
+			// @type java.lang.String
+			let name = item.toString();
+			if (name.startsWith("Ellipse"))
+			{
+				let start = item.getStartDTG();
+				let end = createDate((start.getDate().getTime() + duration
+						.getValueIn(DUR_MILLISECONDS)));
+				item.setEndDTG(end);
+			}
+		}
+	}
+	layers.fireModified();
+}
