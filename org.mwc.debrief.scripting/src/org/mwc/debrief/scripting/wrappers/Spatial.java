3f514a4f085f6c8ac18ded8a37428d477269dd49
Ian Mayo <ian@planetmayo.com>
2018-12-18 12:57:50 +0000

    Spatial class javadoc done.

--- org.mwc.debrief.scripting/src/org/mwc/debrief/scripting/wrappers/Spatial.java
+++ org.mwc.debrief.scripting/src/org/mwc/debrief/scripting/wrappers/Spatial.java
@@ -9,11 +9,30 @@ import MWC.GenericData.WorldSpeed;
 import MWC.GenericData.WorldVector;
 import junit.framework.TestCase;
 
+/** generic position related elements
+ * 
+ * @author ian
+ *
+ */
 public class Spatial
 {
 
   public static class TestSpatial extends TestCase
   {
+
+    static final double testDouble = 4.5;
+
+    static final double testDouble2 = 7.3;
+
+    static final double testDouble3 = 2.0;
+
+    static final int testInt = WorldSpeed.Kts;
+
+    static final WorldLocation w1 = new WorldLocation(12, 12, 0);
+
+    static final WorldLocation w2 = new WorldLocation(10, 10, 100);
+    static final WorldArea wa1 = new WorldArea(w1, w2);
+    
     public void testCreateArea()
     {
       final WorldArea ww1 = new WorldArea(w1, w2);
@@ -68,54 +87,76 @@ public class Spatial
   }
 
   /**
-   * the units for distances. Note to provide these to ease, we have to add a 
+   * the units for distances. Note to provide these to ease, we have to add a @WrapToScript
    * annotation. Once we've added this annotation to one public field/method, we have to apply it
    * for all methods
    */
-  
+  /** Metres type
+   * 
+   */
+  @WrapToScript
   static public final int METRES = 0;
-  
+  /** Yards type
+   * 
+   */
+  @WrapToScript
   static public final int YARDS = 1;
-  
+  /** Kilometres type
+   * 
+   */
+  @WrapToScript
   static public final int KM = 2;
-  
+  /** Nautical Miles type
+   * 
+   */
+  @WrapToScript
   static public final int NM = 3;
-  
+  /** Arc Minutes type
+   * 
+   */
+  @WrapToScript
   static public final int MINUTES = 4;
-  
+  /** Arc Degrees type
+   * 
+   */
+  @WrapToScript
   static public final int DEGS = 5;
-  
+  /** Kiloyards type
+   * 
+   */
+  @WrapToScript
   static public final int KYDS = 6;
 
-  
+  /** Feet type
+   * 
+   */
+  @WrapToScript
   static public final int FT = 7;
   /**
    * Units for world speed.
    */
-  
+  /**
+   * Metres/sec for speed object.
+   */
+  @WrapToScript
   static public final int M_SEC = 0;
-  
+  /**
+   * Knots for speed object.
+   */
+  @WrapToScript
   static public final int KTS = 1;
-  
+  /**
+   * Feet/sec for speed object.
+   */
+  @WrapToScript
   static public final int FT_SEC = 2;
 
-  
+  /**
+   * Feet/Min for speed object.
+   */
+  @WrapToScript
   static public final int FT_MIN = 3;
 
-  static final double testDouble = 4.5;
-
-  static final double testDouble2 = 7.3;
-
-  static final double testDouble3 = 2.0;
-
-  static final int testInt = WorldSpeed.Kts;
-
-  static final WorldLocation w1 = new WorldLocation(12, 12, 0);
-
-  static final WorldLocation w2 = new WorldLocation(10, 10, 100);
-  static final WorldArea wa1 = new WorldArea(w1, w2);
-
-  
   /**
    * produce an area object using the two corners
    *
@@ -125,27 +166,47 @@ public class Spatial
    *          bottom-right corner
    * @return MWC.GenericData.WorldArea
    */
+  @WrapToScript
   public static WorldArea createArea(final WorldLocation tl,
       final WorldLocation br)
   {
     return new WorldArea(tl, br);
   }
 
-  
+  /** create a distance object
+   * 
+   * @param value how many units of distance
+   * @param units the units for the measurement
+   * @return a distance object
+   */
+  @WrapToScript
   public static WorldDistance createDistance(final double value,
       final int units)
   {
     return new WorldDistance(value, units);
   }
 
-  
+  /** create a location
+   * 
+   * @param dLat degrees of latitude
+   * @param dLong degrees of longitude
+   * @param depth depth in metres
+   * @return a location object
+   */
+  @WrapToScript
   public static WorldLocation createLocation(final double dLat,
       final double dLong, final double depth)
   {
     return new WorldLocation(dLat, dLong, depth);
   }
 
-  
+  /** create a measured vector
+   * 
+   * @param distM distance in metres
+   * @param bearingDegs bearing in degrees
+   * @return a vector object
+   */
+  @WrapToScript
   public static WorldVector createVector(final double distM,
       final double bearingDegs)
   {
@@ -153,7 +214,13 @@ public class Spatial
         new WorldDistance(distM, WorldDistance.METRES), null);
   }
 
-  
+  /** create a vector object using km
+   * 
+   * @param distKm distance in Km
+   * @param bearingDegs bearing in degrees
+   * @return a vector object
+   */
+  @WrapToScript
   public static WorldVector createVectorKm(final double distKm,
       final double bearingDegs)
   {
@@ -161,7 +228,13 @@ public class Spatial
         new WorldDistance(distKm, WorldDistance.KM), null);
   }
 
-  
+  /** create a speed object
+   * 
+   * @param value how many units of speed to use
+   * @param units the units for this measurement
+   * @return a speed object
+   */
+  @WrapToScript
   public static WorldSpeed createWorldSpeed(final double value, final int units)
   {
     return new WorldSpeed(value, units);
