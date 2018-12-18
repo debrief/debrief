8057d8231750668485a2efce2e85d0ee3114b639
Ian Mayo <ian@planetmayo.com>
2018-12-18 21:26:03 +0000

    Switch to exporting calculated turn rate

--- org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/Scripts/write_CSV.js
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/Scripts/write_CSV.js
@@ -1,7 +1,5 @@
 loadModule('/System/Resources', false);
 loadModule('/Debrief/Core'); // date
-loadModule('/Debrief/Spatial'); // location
-loadModule('/Debrief/Tactical'); // for plot, duration, date
 loadModule("/System/UI", false); // for the input dialogs
 
 // get project instance
@@ -13,7 +11,6 @@ if (!project.exists())
 	exit(0);
 }
 
-
 let plot = getActivePlot(); // get the current plot
 if (plot != null) // did we find a Debrief plot?
 {
@@ -23,9 +20,9 @@ if (plot != null) // did we find a Debrief plot?
 			"Choose track to export"); // let the user select a name
 	if (choice != null) // did user select something?
 	{
-	// create a file within the project
-		var file = openFile("workspace://sample_data/other_formats/tutorial_" + choice + "_export.csv",
-				WRITE, true);
+		// create a file within the project
+		var file = openFile("workspace://sample_data/other_formats/tutorial_"
+				+ choice + "_export.csv", WRITE, true);
 
 		if (!file.exists())
 		{
@@ -33,23 +30,42 @@ if (plot != null) // did we find a Debrief plot?
 			exit(0);
 		}
 
-		writeLine(file, ";; dtg, lat, long");
+		writeLine(file, ";; elapsed (secs), course rate (degs/sec)");
 
-		
 		let track = layers.findTrack(choice); // try to get the selected layer
 		if (track != null) // did we find it the annotations layer?
 		{
 			var ctr = 0;
-			let fixes = track.getPositionIterator();
-			while (fixes.hasMoreElements())
+			let fixes = track.getPositionIterator(); // to loop through positions
+			var lastCourse = null;
+			var lastTime = null;
+			var firstTime = null;
+			while (fixes.hasMoreElements()) // while there are more elements
 			{
 				// @type Debrief.Wrappers.FixWrapper
-				let fix = fixes.nextElement();
-				// @type MWC.GenericData.WorldLocation
-				let loc = fix.getLocation();
-				
-				let str = fix.getDTG().getDate() + ", " + loc.getLat() + ", " + loc.getLong();
-				writeLine(file, str);
+				let fix = fixes.nextElement(); // get the next element
+
+				let thisCourse = fix.getCourseDegs();
+				let thisTime = fix.getDTG().getDate().getTime();
+
+				if (firstTime == null)
+				{
+					firstTime = thisTime; // remember the start time
+				}
+
+				if (lastCourse != null)
+				{
+					let courseDelta = thisCourse - lastCourse;
+					let timeDelta = (thisTime - lastTime) / 1000.0;
+					let courseRate = courseDelta / timeDelta;
+					let elapsed = (thisTime - firstTime) / 1000.0;
+					let str = elapsed + ", " + courseRate;
+					writeLine(file, str);
+				}
+
+				lastCourse = thisCourse;
+				lastTime = thisTime;
+
 				ctr++;
 			}
 		}
