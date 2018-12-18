c221ba733b6cf2675bfe17e3f63dea750d76d5e8
Ian Mayo <ian@planetmayo.com>
2018-12-18 22:05:20 +0000

    tidying

--- org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/Scripts/filter_matching_symbol.js
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/Scripts/filter_matching_symbol.js
@@ -5,13 +5,16 @@
 loadModule("/System/UI", false); // for the input dialogs
 loadModule('/Debrief/Core'); // for plot, duration, date
 
-/** function to return the unique entries in the provided list
- * from here: https://coderwall.com/p/nilaba/simple-pure-javascript-array-unique-method-with-5-lines-of-code
+/**
+ * function to return the unique entries in the provided list from here:
+ * https://coderwall.com/p/nilaba/simple-pure-javascript-array-unique-method-with-5-lines-of-code
  */
-Array.prototype.unique = function() {
-  return this.filter(function (value, index, self) { 
-    return self.indexOf(value) === index;
-  });
+Array.prototype.unique = function()
+{
+	return this.filter(function(value, index, self)
+	{
+		return self.indexOf(value) === index;
+	});
 }
 
 let plot = getActivePlot(); // get the current plot
@@ -20,38 +23,37 @@ if (plot != null) // did we find a Debrief plot?
 	let layers = plot.getLayers(); // get the layers for the plot
 	let tracks = layers.getTracks();
 
-	// collate a unique list of symbols
+	// collate a list of symbols
 	var symbols =
 	[];
-	let len = tracks.length;
-	for (var i = 0; i < len; i++)
+	let len = tracks.length; // find number of tracks
+	for (var i = 0; i < len; i++) // loop through tracks
 	{
-		let track = tracks[i];  // get this track
-		let symbol = track.getSymbolType();  // get the symbol type
-
-		// add this symbol to the list
-		symbols[i] = symbol;
+		let track = tracks[i]; // get this track
+		let symbol = track.getSymbolType(); // get the symbol type
+		symbols[i] = symbol; // add this symbol to the list
 	}
 
 	// find the unique list of symbols
 	symbols = symbols.unique();
-	
-	// ask the user which one to filter
-	let chosenSym = showSelectionDialog(symbols, "Choose symbol to match", "Filter to matching symbol");
 
-	if (chosenSym == null)
+	// ask the user which one to filter
+	let chosenSym = showSelectionDialog(symbols, "Choose symbol to match",
+			"Filter to matching symbol");
+
+	if (chosenSym == null) // did one get chosen?
 	{
 		exit(0); // ok, drop out.
 	}
-	
+
 	// now loop through, and hide any that don't match
 	for (var i = 0; i < len; i++)
 	{
-		let track = tracks[i];
-		let symbol = track.getSymbolType();
-		
-		let isVis = symbol == chosenSym;
-		track.setVisible(isVis);
+		let track = tracks[i]; // get the next track
+		let symbol = track.getSymbolType(); // get its symbol
+
+		let isVis = symbol == chosenSym; // does this match the chosen one?
+		track.setVisible(isVis);  // set visibility accordingly
 	}
 
 	layers.fireModified();
