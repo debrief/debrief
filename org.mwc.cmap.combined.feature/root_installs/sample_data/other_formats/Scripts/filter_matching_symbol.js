65ce72d046256ef32c40bd69f6c1f76e3b7adcbe
Ian Mayo <ian@planetmayo.com>
2018-12-18 22:03:24 +0000

    Generate unique list of matching items

--- org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/Scripts/filter_matching_symbol.js
+++ org.mwc.cmap.combined.feature/root_installs/sample_data/other_formats/Scripts/filter_matching_symbol.js
@@ -4,6 +4,16 @@
  */
 loadModule("/System/UI", false); // for the input dialogs
 loadModule('/Debrief/Core'); // for plot, duration, date
+
+/** function to return the unique entries in the provided list
+ * from here: https://coderwall.com/p/nilaba/simple-pure-javascript-array-unique-method-with-5-lines-of-code
+ */
+Array.prototype.unique = function() {
+  return this.filter(function (value, index, self) { 
+    return self.indexOf(value) === index;
+  });
+}
+
 let plot = getActivePlot(); // get the current plot
 if (plot != null) // did we find a Debrief plot?
 {
@@ -23,6 +33,9 @@ if (plot != null) // did we find a Debrief plot?
 		symbols[i] = symbol;
 	}
 
+	// find the unique list of symbols
+	symbols = symbols.unique();
+	
 	// ask the user which one to filter
 	let chosenSym = showSelectionDialog(symbols, "Choose symbol to match", "Filter to matching symbol");
 
