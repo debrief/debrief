2655d0fc2a67805ab102b3291a0afab3b4ab194c
Ian Mayo <ian@planetmayo.com>
2019-02-02 16:23:19 +0000

    Implement fit to window

--- org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/menu/DebriefRibbonView.java
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/menu/DebriefRibbonView.java
@@ -1,14 +1,15 @@
 package org.mwc.debrief.lite.menu;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.geotools.swing.JMapPane;
 import org.geotools.swing.action.PanAction;
-import org.geotools.swing.action.ResetAction;
 import org.geotools.swing.action.ZoomInAction;
 import org.geotools.swing.action.ZoomOutAction;
+import org.mwc.debrief.lite.gui.GeoToolMapProjection;
 import org.mwc.debrief.lite.map.GeoToolMapRenderer;
 import org.pushingpixels.flamingo.api.ribbon.JRibbon;
 import org.pushingpixels.flamingo.api.ribbon.JRibbonBand;
@@ -18,13 +19,15 @@ import org.pushingpixels.flamingo.api.ribbon.resize.CoreRibbonResizePolicies;
 import org.pushingpixels.flamingo.api.ribbon.resize.CoreRibbonResizePolicies.IconRibbonBandResizePolicy;
 import org.pushingpixels.flamingo.api.ribbon.resize.RibbonBandResizePolicy;
 
+import MWC.GenericData.WorldArea;
+
 public class DebriefRibbonView
 {
   protected static void addViewTab(final JRibbon ribbon,
-      final GeoToolMapRenderer _geoMapRenderer)
+      final GeoToolMapRenderer geoMapRenderer, final GeoToolMapProjection projection)
   {
     final JRibbonBand viewBand = new JRibbonBand("View", null);
-    final JMapPane mapPane = (JMapPane) _geoMapRenderer.getMap();
+    final JMapPane mapPane = (JMapPane) geoMapRenderer.getMap();
     viewBand.startGroup();
     MenuUtils.addCommand("Pan", "images/16/hand.png", new PanAction(mapPane),
         viewBand, RibbonElementPriority.TOP);
@@ -34,8 +37,23 @@ public class DebriefRibbonView
     MenuUtils.addCommand("Zoom Out", "images/16/zoomout.png", new ZoomOutAction(
         mapPane), viewBand, RibbonElementPriority.TOP);
     viewBand.startGroup();
+      
+    
+    ActionListener fitToWin = new ActionListener()
+    {
+      @Override
+      public void actionPerformed(ActionEvent e)
+      {
+        System.out.println("doing reset");
+        WorldArea area = projection.getDataArea();
+        projection.setDataArea(area);
+        geoMapRenderer.getMap().invalidate();
+        geoMapRenderer.getMap().repaint();
+//        new ResetAction(mapPane);
+      }
+    };
     MenuUtils.addCommand("Fit to Window", "images/16/fit_to_win.png",
-        new ResetAction(mapPane), viewBand, null);
+        fitToWin, viewBand, null);
     final List<RibbonBandResizePolicy> policies = new ArrayList<>();
     policies.add(new CoreRibbonResizePolicies.Mirror(viewBand));
     policies.add(new CoreRibbonResizePolicies.Mid2Low(viewBand));
