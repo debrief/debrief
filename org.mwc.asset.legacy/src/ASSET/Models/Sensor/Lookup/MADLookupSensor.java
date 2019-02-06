331adf5e61bda8806add4f0184361c7cfc793575
Ian Mayo <ian@planetmayo.com>
2019-02-06 20:53:07 +0000

    imrpove static analysis

--- org.mwc.asset.legacy/src/ASSET/Models/Sensor/Lookup/MADLookupSensor.java
+++ org.mwc.asset.legacy/src/ASSET/Models/Sensor/Lookup/MADLookupSensor.java
@@ -172,7 +172,7 @@ public class MADLookupSensor extends LookupSensor
    *
    * @return yes/no
    */
-  protected boolean canProduceRange()
+  public boolean canProduceRange()
   {
     return false;
   }
