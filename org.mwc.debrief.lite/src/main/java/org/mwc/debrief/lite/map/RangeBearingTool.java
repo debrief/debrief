8937e92b53e6960146ac175f8bd1cb00416ef534
Ian Mayo <ian@planetmayo.com>
2019-03-13 18:13:50 +0000

    Improve static analysis - avoid using unicode symbol

--- org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/map/RangeBearingTool.java
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/map/RangeBearingTool.java
@@ -128,7 +128,7 @@ public class RangeBearingTool extends AbstractZoomTool
       bearing += 360d;
     }
     final String msg = "Range:" + (int) distance.getValueIn(
-        WorldDistance.METRES) + "m Brg:" + (int) bearing + "\u00b0";
+        WorldDistance.METRES) + "m Brg:" + (int) bearing + "°";
     if (_statusBar != null)
     {
       _statusBar.setText(msg);
