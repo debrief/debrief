7c1be265826c5af27c38daea3c4ae43bd1c81cca
Ian Mayo <ian@planetmayo.com>
2019-03-21 13:22:38 +0000

    Code tidying

--- org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/outline/LogicHelpers.java
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/outline/LogicHelpers.java
@@ -6,63 +6,15 @@ import MWC.GUI.Plottable;
 
 public class LogicHelpers
 {
-  public static interface Helper
-  {
-    ArrayList<Plottable> getSelection();
-    ArrayList<Plottable> getClipboardContents();
-  }
-  
-  public abstract static class EnabledTest
-  {
-    private final String _name;
-    public EnabledTest(final String name)
-    {
-      _name = name;
-    }
-    @Override
-    public String toString()
-    {
-      return _name;
-    }
-    abstract boolean isEnabled(final Helper helper);
-  }
-
-  public abstract static class EnabledTestGroup extends EnabledTest
-  {
-    protected EnabledTest[] _tests;
-
-    public EnabledTestGroup(final String name, EnabledTest... tests)
-    {
-      super(name);
-      _tests = tests;
-    }
-  }
-  
-  public static class Or extends EnabledTestGroup
-  {
-
-    public Or(EnabledTest... tests)
-    {
-      super("Or", tests);
-    }
-
-    @Override
-    boolean isEnabled(final Helper helper)
-    {
-      for (EnabledTest t : _tests)
-      {
-        if (t.isEnabled(helper))
-          return true;
-      }
-      return false;
-    }
-  }
-  
-  
+  /** logical AND operation
+   * 
+   * @author ian
+   *
+   */
   public static class And extends EnabledTestGroup
   {
 
-    public And(EnabledTest... tests)
+    public And(final EnabledTest... tests)
     {
       super("And", tests);
     }
@@ -70,12 +22,96 @@ public class LogicHelpers
     @Override
     boolean isEnabled(final Helper helper)
     {
-      for (EnabledTest t : _tests)
+      for (final EnabledTest t : _tests)
       {
         if (!t.isEnabled(helper))
           return false;
       }
       return true;
+    }
+  }
+
+  /** parent of all tests
+   * 
+   * @author ian
+   *
+   */
+  public abstract static class EnabledTest
+  {
+    private final String _name;
+
+    public EnabledTest(final String name)
+    {
+      _name = name;
+    }
+
+    abstract boolean isEnabled(final Helper helper);
+
+    @Override
+    public String toString()
+    {
+      return _name;
+    }
+  }
+
+  /** series of tests
+   * 
+   * @author ian
+   *
+   */
+  public abstract static class EnabledTestGroup extends EnabledTest
+  {
+    protected EnabledTest[] _tests;
+
+    public EnabledTestGroup(final String name, final EnabledTest... tests)
+    {
+      super(name);
+      _tests = tests;
+    }
+  }
+
+  /** helper that can provide selection and clipboard contents
+   * 
+   * @author ian
+   *
+   */
+  public static interface Helper
+  {
+    /** what's on the clipboard?
+     * 
+     * @return
+     */
+    ArrayList<Plottable> getClipboardContents();
+
+    /** what's currently selected?
+     * 
+     * @return
+     */
+    ArrayList<Plottable> getSelection();
+  }
+
+  /** logical OR method
+   * 
+   * @author ian
+   *
+   */
+  public static class Or extends EnabledTestGroup
+  {
+
+    public Or(final EnabledTest... tests)
+    {
+      super("Or", tests);
+    }
+
+    @Override
+    boolean isEnabled(final Helper helper)
+    {
+      for (final EnabledTest t : _tests)
+      {
+        if (t.isEnabled(helper))
+          return true;
+      }
+      return false;
     }
   }
 }
