b37fc5c02e2e0ff316fa993a0571f1dd9c940145
Saul Hidalgo <saulhidalgoaular@gmaill.com>
2019-04-29 14:05:56 +0000

    Fix https://github.com/debrief/debrief/issues/4011

--- org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/menu/DebriefRibbonTimeController.java
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/menu/DebriefRibbonTimeController.java
@@ -159,7 +159,7 @@ public class DebriefRibbonTimeController
 
     public int getCurrentAt(final long now)
     {
-      return (int) ((now - origin) / step);
+      return (int) Math.round((double)(now - origin) / step);
     }
 
     public int getEnd()
