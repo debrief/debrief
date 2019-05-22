400065d50bb99b8784decc265974929ddf68e727
Ian Mayo <ian@planetmayo.com>
2019-05-22 20:47:23 +0100

    lots more functionality

--- org.mwc.debrief.legacy/src/Debrief/ReaderWriter/Word/ImportASWDataDocument.java
+++ org.mwc.debrief.legacy/src/Debrief/ReaderWriter/Word/ImportASWDataDocument.java
@@ -15,14 +15,22 @@
 package Debrief.ReaderWriter.Word;
 
 import java.awt.Color;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import Debrief.GUI.Frames.Application;
+import Debrief.Wrappers.FixWrapper;
 import Debrief.Wrappers.TrackWrapper;
 import MWC.GUI.Layer;
 import MWC.GUI.Layers;
+import MWC.GUI.ToolParent;
 import MWC.GUI.Properties.DebriefColors;
+import MWC.GenericData.HiResDate;
+import MWC.GenericData.WorldLocation;
+import MWC.Utilities.TextFormatting.GMTDateFormat;
 import junit.framework.TestCase;
 
 public class ImportASWDataDocument
@@ -38,24 +46,106 @@ public class ImportASWDataDocument
 
   public static class TestImportWord extends TestCase
   {
+    public void testParseDates()
+    {
+      final String d1 = "TIMPD/290000Z/300200Z/APR/2016/APR2016";
+      final String d2 = "TIMPD//230001Z//232359Z//APR//2017//APR//2017//";
+      final String d3 = "TIMPD/220000Z/222400Z/APR/2018";
+      final String d4 = "TIMPD/261200ZAPR/IN/262359ZAPR/IN/APR/2019/APR/2019/";
+
+      // start off with good ones
+      assertEquals("date 1", 2016, yearFor(d1).intValue());
+      assertEquals("date 2", 2017, yearFor(d2).intValue());
+      assertEquals("date 3", 2018, yearFor(d3).intValue());
+      assertEquals("date 4", 2019, yearFor(d4).intValue());
+
+      assertNull("wrong format", yearFor(
+          "TMPOS/290000Z/300200Z/APR/2016/APR2016"));
+      assertNull("no year", yearFor("TMPOS/290000Z/300200Z/APR/201A6/APR2016"));
+      assertNull("too early", yearFor("TMPOS/290000Z/300200Z/APR/16/APR2016"));
+      assertNull("too late", yearFor(
+          "TMPOS/290000Z/300200Z/APR/124316/APR2016"));
+    }
+
+    @SuppressWarnings("unused")
+    public void testParseFixes()
+    {
+      final String d1 =
+          "TMPOS/261200ZAPR/IN/12.23.34N-121.12.1E/057T/04KTS/00.0M//";
+      final String d2 =
+          "TMPOS/262327ZAPR/INS/13.14.15S-011.22.33W/180T/13KTS/000FT";
+      final String d3 = "TMPOS/220000ZAPR/GPS/1230N/01215W";
+      final String d4 =
+          "TMPOS/230001ZAPR/GPS/22.33.44N/020.11.22W/006T/10KTS//";
+      final String d5 = "TMPOS/290000ZAPR/GPS/04 .02. 01N/111.22. 11W/89T/9KTS";
+      // repeat of last line, but with spaces removed
+      final String d6 = "TMPOS/290000ZAPR/GPS/04.02.01N/111.22.11W/89T/9KTS";
+    }
+    
+    @SuppressWarnings("unused")
+    public void testParseLocations()
+    {
+      assertNull("fails for empty", locationFor(null));
+
+      final String d1 =
+          "TMPOS/261200ZAPR/IN/12.23.34N-121.12.1E/057T/04KTS/00.0M//";
+      final String d2 =
+          "TMPOS/262327ZAPR/INS/13.14.15S-011.22.33W/180T/13KTS/000FT";
+      final String d3 = "TMPOS/220000ZAPR/GPS/1230N/01215W";
+      final String d4 =
+          "TMPOS/230001ZAPR/GPS/22.33.44N/020.11.22W/006T/10KTS//";
+      final String d5 = "TMPOS/290000ZAPR/GPS/04 .02. 01N/111.22. 11W/89T/9KTS";
+      // repeat of last line, but with spaces removed
+      final String d6 = "TMPOS/290000ZAPR/GPS/04.02.01N/111.22.11W/89T/9KTS";
+
+  //    assertEquals("d1", loc(1, 2, 3), locationFor(d1));
+
+    }
+
+    public void testDates()
+    {
+      final String d1 = "261200ZAPR";
+      final String d2 = "262327ZAPR";
+      final String d3 = "220000ZMAY";
+      final String d4 = "012345ZAPR";
+      final String d5 = "292359ZAPR";
+
+      DateFormat df = new GMTDateFormat("yy-MM-dd HH:mm:ss");
+      assertEquals("aaa", "70-04-26 12:00:00", df.format(dateFor(d1)
+          .getDate()));
+      assertEquals("aaa", "70-04-26 23:27:00", df.format(dateFor(d2)
+          .getDate()));
+      assertEquals("aaa", "70-05-22 00:00:00", df.format(dateFor(d3)
+          .getDate()));
+      assertEquals("aaa", "70-04-01 23:45:00", df.format(dateFor(d4)
+          .getDate()));
+      assertEquals("aaa", "70-04-29 23:59:00", df.format(dateFor(d5)
+          .getDate()));
+    }
+
+    @SuppressWarnings("unused")
+    private static WorldLocation loc(final double lat, final double lon,
+        final double dep)
+    {
+      return new WorldLocation(lat, lon, dep);
+    }
+
     public void testIsValid()
     {
       List<String> input = new ArrayList<String>();
-      for(int i=0;i<20;i++)
+      for (int i = 0; i < 20; i++)
       {
         input.add("Some old duff information");
       }
       assertFalse("not suitable for import", canImport(input));
-      for(int i=0;i<10;i++)
+      for (int i = 0; i < 10; i++)
       {
         input.add("TMPOS/Here we go!");
       }
       assertTrue("now suitable for import", canImport(input));
-      
+
     }
   }
-
-
 
   /**
    * helper class that can ask the user a question populated via Dependency Injection
@@ -146,7 +236,7 @@ public class ImportASWDataDocument
   {
     _layers = destination;
   }
-  
+
   /**
    * repeatably find a color for the specified track id The color will be RED if it's a master track
    * ("M01"). Shades of gray nor ownship blue are returned.
@@ -172,8 +262,8 @@ public class ImportASWDataDocument
     return res;
   }
 
-
-  public static void logError(final int status, final String msg, final Exception e)
+  public static void logError(final int status, final String msg,
+      final Exception e)
   {
     logThisError(status, msg, e);
   }
@@ -199,7 +289,6 @@ public class ImportASWDataDocument
     // ok, now we can loop through the strings
     if (proceed)
     {
-      @SuppressWarnings("unused")
       int ctr = 0;
       TrackWrapper track = null;
       for (final String raw_text : strings)
@@ -209,7 +298,7 @@ public class ImportASWDataDocument
 
         // also remove any other control chars that may throw MS Word
         final String text = removeBadChars(raw_text);
-        
+
         // process it
       }
 
@@ -221,7 +310,107 @@ public class ImportASWDataDocument
   }
 
   final private static String marker = "TMPOS";
+
+  private static HiResDate dateFor(final String str)
+  {
+    DateFormat df = new GMTDateFormat("ddHHmm'Z'MMM");
+    HiResDate res = null;
+    try
+    {
+      Date date = df.parse(str);
+      res = new HiResDate(date);
+    }
+    catch (ParseException e)
+    {
+      logError(ToolParent.ERROR, "While Parsing:" + str, e);
+    }
+    return res;
+  }
+
+  private static WorldLocation locationFor(final String str)
+  {
+    // TMPOS/261200ZAPR/IN/00.00.0N-000.00.0W/057T/04KTS/00.0M//
+    // TMPOS/262327ZAPR/INS/00.00.00N-000.00.00W/180T/13KTS/000FT
+    // TMPOS/220000ZAPR/GPS/ddmmN/dddmmW
+    // TMPOS/230001ZAPR/GPS/00.00.00N/000.00.00W/006T/10KTS//
+    // TMPOS/290000ZAPR/GPS/00 .00. 00N/000.00. 00W/89T/9KTS
+    
+    return null;
+  }
   
+  @SuppressWarnings({"unused"})
+  private static FixWrapper fixFor(final String str)
+    {
+
+
+    if (str == null)
+    {
+      return null;
+    }
+    else if (!str.startsWith("TMPOS"))
+    {
+      return null;
+    }
+    else
+    {
+      String[] tokens = str.split("/");
+
+      // start off with the date
+      HiResDate date = null;
+      Double dLat = null;
+      Double dLon = null;
+      for (final String s : tokens)
+      {
+        if (date == null)
+        {
+          int zIndex = s.indexOf("Z");
+          if (zIndex != -1)
+          {
+            date = dateFor(s);
+          }
+        }
+        else
+        {
+          // ok on with lat/lon
+        }
+      }
+    }
+
+    return null;
+  }
+
+  private static Integer yearFor(String str)
+  {
+    // formats encountered in the wild
+    // "TIMPD/290000Z/300200Z/APR/2016/APR2019";
+    // "TIMPD//230001Z//232359Z//APR//2017//APR//2019//";
+    // "TIMPD/220000Z/222400Z/APR/2017";
+    // "TIMPD/261200ZAPR/IN/262359ZAPR/IN/APR/2019/APR/2019/";
+
+    // double-check it's one of ours
+    if (!str.startsWith("TIMPD"))
+    {
+      return null;
+    }
+    else
+    {
+      // ok, tokenise
+      String[] tokens = str.split("/");
+      for (final String s : tokens)
+      {
+        if (s.length() == 4 && s.matches("^-?\\d+$"))
+        {
+          final int year = Integer.parseInt(s);
+          if (year > 1000 && year < 3000)
+          {
+            return year;
+          }
+        }
+      }
+      return null;
+    }
+  }
+
   public static boolean canImport(List<String> strings)
   {
     // run through strings, see if we have TMPOS in more than 5 lines in the first 50
@@ -229,27 +418,27 @@ public class ImportASWDataDocument
     final int requiredLines = 5;
     int matches = 0;
     int lines = 0;
-    for(final String l: strings)
+    for (final String l : strings)
     {
-      lines ++;
-      
-      if(l.startsWith(marker))
+      lines++;
+
+      if (l.startsWith(marker))
       {
         matches++;
       }
-      
-      if(matches >= requiredLines)
+
+      if (matches >= requiredLines)
       {
         return true;
       }
-      
-      if(lines > numLines)
+
+      if (lines > numLines)
       {
         break;
       }
-      
+
     }
     return false;
   }
-  
+
 }
