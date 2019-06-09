42047c32b962701a768b470b622d8295ff10119c
Saul Hidalgo <saulhidalgoaular@gmail.com>
2019-06-09 08:25:03 +0200

    Code Cleaning

--- org.mwc.debrief.legacy/src/Debrief/GUI/Tote/AWT/AWTStepControl.java
+++ org.mwc.debrief.legacy/src/Debrief/GUI/Tote/AWT/AWTStepControl.java
@@ -10,7 +10,7 @@
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 package Debrief.GUI.Tote.AWT;
 
@@ -82,16 +82,27 @@ package Debrief.GUI.Tote.AWT;
 // Revision 1.1  1999-08-04 10:53:01+01  administrator
 // Initial revision
 //
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Checkbox;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-import java.awt.*;
-import java.awt.event.*;
-import Debrief.GUI.Tote.*;
-import MWC.GUI.Properties.*;
-import MWC.GUI.*;
-import MWC.Utilities.TextFormatting.*;
+import Debrief.GUI.Tote.StepControl;
+import MWC.GUI.Editable;
+import MWC.GUI.ToolParent;
+import MWC.GUI.Properties.PropertiesPanel;
 import MWC.GenericData.HiResDate;
+import MWC.Utilities.TextFormatting.DebriefFormatDateTime;
 
-public final class AWTStepControl extends StepControl implements java.awt.event.ActionListener
+public final class AWTStepControl extends StepControl implements
+    java.awt.event.ActionListener
 {
   /////////////////////////////////////////////////////////////
   // member variables
@@ -108,12 +119,11 @@ public final class AWTStepControl extends StepControl implements java.awt.event.
   private final PropertiesPanel _theEditor;
   Checkbox _autoBtn;
 
-
-
   /////////////////////////////////////////////////////////////
   // constructor
   ////////////////////////////////////////////////////////////
-  public AWTStepControl(final PropertiesPanel theEditor, final ToolParent theParent)
+  public AWTStepControl(final PropertiesPanel theEditor,
+      final ToolParent theParent)
   {
     super(theParent, Color.white);
 
@@ -122,9 +132,138 @@ public final class AWTStepControl extends StepControl implements java.awt.event.
     initForm();
   }
 
+  /**
+   * one of our edit buttons has been pressed
+   */
+  @Override
+  public final void actionPerformed(final java.awt.event.ActionEvent p1)
+  {
+    // get the name of the control
+    boolean fwd = true;
+    boolean large = true;
+    final Button b = (Button) p1.getSource();
+
+    // first sort out which set it is
+    if ((b == _startBtn) || (b == _endBtn))
+    {
+      if (b == _startBtn)
+      {
+        super.gotoStart();
+      }
+      else
+        super.gotoEnd();
+    }
+    else
+    {
+      if (b == _largeBwd)
+      {
+        fwd = false;
+        large = true;
+      }
+      if (b == _smallBwd)
+      {
+        fwd = false;
+        large = false;
+      }
+      if (b == _smallFwd)
+      {
+        fwd = true;
+        large = false;
+      }
+      if (b == _largeFwd)
+      {
+        fwd = true;
+        large = true;
+      }
+
+      _goingForward = fwd;
+      _largeSteps = large;
+
+      super.doStep(fwd, large);
+    }
+  }
+
+  /**
+   * set the automatic mode as indicated
+   * 
+   * @param go
+   *          boolean whether to go auto or not
+   */
+  void doAuto(final boolean go)
+  {
+
+    if (go)
+      startTimer();
+    else
+      stopTimer();
+  }
+
+  /**
+   * edit button has been pressed
+   */
+  void doEdit()
+  {
+    // has the editor been assigned?
+    if (_thePanel != null)
+    {
+      // get our edit info
+      final Editable.EditorType et2 = getInfo();
+      // and open it in the panel
+      _theEditor.addEditor(et2, null);
+    }
+  }
+
+  // convenience method called from parent class
+  @Override
+  protected void doEditPainter()
+  {
+    doEdit();
+  }
+
+  @Override
+  protected final void formatTimeText()
+  {
+    // don't bother
+  }
+
+  public final Panel getPanel()
+  {
+    return _thePanel;
+  }
+
+  /**
+   * accessor method from the parent class
+   */
+  @Override
+  protected final PropertiesPanel getPropertiesPanel()
+  {
+    return _theEditor;
+  }
+
+  /**
+   * get the time in the finish slider in the toolbox
+   *
+   */
+  @Override
+  public final HiResDate getToolboxEndTime()
+  {
+    return null;
+  }
+
+  /**
+   * get the time in the start slider in the toolbox
+   *
+   */
+  @Override
+  public final HiResDate getToolboxStartTime()
+  {
+    return null;
+  }
+
   /////////////////////////////////////////////////////////////
   // member functions
   ////////////////////////////////////////////////////////////
+  @Override
   protected final void initForm()
   {
     _thePanel = new Panel();
@@ -134,28 +273,32 @@ public final class AWTStepControl extends StepControl implements java.awt.event.
     final Panel lhh = new Panel();
     lhh.setLayout(new BorderLayout());
     final Panel lh = new Panel();
-    lh.setLayout(new GridLayout(1,0));
+    lh.setLayout(new GridLayout(1, 0));
     final Panel rh = new Panel();
-    rh.setLayout(new GridLayout(1,0));
+    rh.setLayout(new GridLayout(1, 0));
 
     // create the edit button
     _editBtn = new Button("?");
-    _editBtn.addActionListener(new ActionListener(){
+    _editBtn.addActionListener(new ActionListener()
+    {
+      @Override
       public void actionPerformed(final ActionEvent e)
       {
         doEdit();
       }
-      });
+    });
 
     // create the manual/auto button
     _autoBtn = new Checkbox("Auto");
     _autoBtn.setState(false);
-    _autoBtn.addItemListener(new ItemListener(){
+    _autoBtn.addItemListener(new ItemListener()
+    {
+      @Override
       public void itemStateChanged(final ItemEvent e)
       {
         doAuto(_autoBtn.getState());
       }
-      });
+    });
 
     // create the simple buttons
     _startBtn = new Button("<-");
@@ -193,128 +336,45 @@ public final class AWTStepControl extends StepControl implements java.awt.event.
     _endBtn.addActionListener(this);
   }
 
-  public final Panel getPanel()
-  {
-    return _thePanel;
-  }
-
-  /** register with the painter manager
+  /***************************************************
+   * set of methods to control the time displayed in the toolbox (of particular use in remembering
+   * the T-Zero time)
    */
+
+  /**
+   * register with the painter manager
+   */
+  @Override
   protected final void painterIsDefined()
   {
   }
 
-
-  // convenience method called from parent class
-  protected void doEditPainter()
+  /**
+   * set the time in the start slider in the toolbox
+   *
+   */
+  @Override
+  public final void setToolboxEndTime(final HiResDate val)
   {
-    doEdit();
   }
 
-  /** respond to update event as triggered by GUI-independent parent
+  /**
+   * set the time in the start slider in the toolbox
+   *
    */
+  @Override
+  public final void setToolboxStartTime(final HiResDate val)
+  {
+  }
+
+  /**
+   * respond to update event as triggered by GUI-independent parent
+   */
+  @Override
   public final void updateForm(final HiResDate DTG)
   {
     final String val = DebriefFormatDateTime.toStringHiRes(DTG);
     _timeTxt.setText(val);
   }
-
-  /** one of our edit buttons has been pressed
-   */
-  public final void actionPerformed(final java.awt.event.ActionEvent p1)
-  {
-    // get the name of the control
-    boolean fwd=true;
-    boolean large=true;
-    final Button b =(Button) p1.getSource();
-
-    // first sort out which set it is
-    if((b == _startBtn) || (b == _endBtn))
-    {
-      if(b == _startBtn)
-      {
-        super.gotoStart();
-      }
-      else
-        super.gotoEnd();
-    }
-    else
-    {
-      if(b == _largeBwd)
-         {  fwd = false; large = true;  }
-      if(b == _smallBwd)
-         {  fwd = false; large = false; }
-      if(b == _smallFwd)
-         {  fwd = true;  large = false; }
-      if(b == _largeFwd)
-         {  fwd = true;  large = true;  }
-
-      _goingForward = fwd;
-      _largeSteps = large;
-
-      super.doStep(fwd, large);
-    }
-  }
-
-  /** edit button has been pressed
-   */
-  void doEdit()
-  {
-    // has the editor been assigned?
-    if(_thePanel != null)
-    {
-      // get our edit info
-      final Editable.EditorType et2 = getInfo();
-      // and open it in the panel
-      _theEditor.addEditor(et2, null);
-    }
-  }
-
-  /** set the automatic mode as indicated
-   * @param go boolean whether to go auto or not
-   */
-  void doAuto(final boolean go)
-  {
-
-    if(go)
-      startTimer();
-    else
-      stopTimer();
-  }
-
-	protected final void formatTimeText()
-	{
-		// don't bother
-	}
-
-  /** accessor method from the parent class
-   */
-  protected final PropertiesPanel getPropertiesPanel()
-  {
-    return _theEditor;
-  }
-
-  /***************************************************
-   * set of methods to control the time displayed in the toolbox (of particular
-   * use in remembering the T-Zero time)
-   */
-
-  /** get the time in the start slider in the toolbox
-   *
-   */
-  public final HiResDate getToolboxStartTime(){return null;}
-  /** get the time in the finish slider in the toolbox
-   *
-   */
-  public final HiResDate getToolboxEndTime(){return null;}
-  /** set the time in the start slider in the toolbox
-   *
-   */
-  public final void setToolboxStartTime(final HiResDate val){}
-  /** set the time in the start slider in the toolbox
-   *
-   */
-  public final void setToolboxEndTime(final HiResDate val){}
-
 
 }
