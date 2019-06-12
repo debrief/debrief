14a438964f2fac746ee1e96ab56f677867403be2
Ian Mayo <ian@planetmayo.com>
2019-06-12 13:01:41 +0100

    code cleanup

--- org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/map/AdvancedZoomInAction.java
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/map/AdvancedZoomInAction.java
@@ -5,30 +5,28 @@ import java.awt.event.ActionEvent;
 import org.geotools.swing.MapPane;
 import org.geotools.swing.action.ZoomInAction;
 
-/** use our advanced zoom in tool, that also support zoom out
- * 
+/**
+ * use our advanced zoom in tool, that also support zoom out
+ *
  * @author ian
  *
  */
 public class AdvancedZoomInAction extends ZoomInAction
 {
 
-  public AdvancedZoomInAction(MapPane mapPane)
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+
+  public AdvancedZoomInAction(final MapPane mapPane)
   {
     super(mapPane);
   }
 
-  /**
-   * 
-   */
-  private static final long serialVersionUID = 1L;
-
   @Override
-  public void actionPerformed(ActionEvent ev)
+  public void actionPerformed(final ActionEvent ev)
   {
     getMapPane().setCursorTool(new AdvancedZoomInTool());
   }
-
-  
-  
 }
