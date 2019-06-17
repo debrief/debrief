4de1bad1da8b140d378adba5ed0b217a823393d9
Ian Mayo <ian@planetmayo.com>
2019-06-17 11:44:32 +0100

    reduce debug information

--- org.mwc.debrief.legacy/src/Debrief/ReaderWriter/NMEA/ImportNMEA.java
+++ org.mwc.debrief.legacy/src/Debrief/ReaderWriter/NMEA/ImportNMEA.java
@@ -797,7 +797,7 @@ public class ImportNMEA
     // from different messages
     Double drCourse = null;
 
-    int ctr = 0;
+ //   int ctr = 0;
 
     // loop through the lines
     while ((nmea_sentence = br.readLine()) != null)
@@ -805,17 +805,17 @@ public class ImportNMEA
 
       final MsgType msg = parseType(nmea_sentence);
 
-      ctr++;
-
-      if (ctr % 10000 == 0)
-      {
-        System.out.print(".");
-      }
-      if (ctr % 50000 == 0)
-      {
-        System.out.println("");
-        System.out.print(ctr);
-      }
+//      ctr++;
+//
+//      if (ctr % 10000 == 0)
+//      {
+//        System.out.print(".");
+//      }
+//      if (ctr % 50000 == 0)
+//      {
+//        System.out.println("");
+//        System.out.print(ctr);
+//      }
 
       switch (msg)
       {
@@ -974,7 +974,7 @@ public class ImportNMEA
       tr.setName(trackName);
       tr.setColor(colors.get(trackName));
 
-      System.out.println("storing " + track.size() + " for " + trackName);
+//      System.out.println("storing " + track.size() + " for " + trackName);
 
       // SPECIAL HANDLING - we filter DR tracks at this stage
       Long lastTime = null;
@@ -1036,6 +1036,8 @@ public class ImportNMEA
     if(contactHolder!=null) {
       _layers.addThisLayer(contactHolder);
     }
+    
+    _layers.fireExtended();
   }
 
   private void storeDRFix(final WorldLocation origin, final double myCourseDegs,
