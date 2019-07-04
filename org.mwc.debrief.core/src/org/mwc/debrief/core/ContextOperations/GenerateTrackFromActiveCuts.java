65e7a18c1d425240683255e5e850d0ba7c65f1a2
Ian Mayo <ian@planetmayo.com>
2019-07-04 13:53:44 +0100

    start to generate data for test

--- org.mwc.debrief.core/src/org/mwc/debrief/core/ContextOperations/GenerateTrackFromActiveCuts.java
+++ org.mwc.debrief.core/src/org/mwc/debrief/core/ContextOperations/GenerateTrackFromActiveCuts.java
@@ -14,6 +14,7 @@
  */
 package org.mwc.debrief.core.ContextOperations;
 
+import java.awt.Color;
 import java.util.Enumeration;
 import java.util.Vector;
 
@@ -36,6 +37,7 @@ import Debrief.Wrappers.TrackWrapper;
 import MWC.GUI.Editable;
 import MWC.GUI.Layer;
 import MWC.GUI.Layers;
+import MWC.GenericData.HiResDate;
 import MWC.GenericData.WorldDistance;
 import MWC.GenericData.WorldLocation;
 import MWC.GenericData.WorldVector;
@@ -45,84 +47,133 @@ import MWC.TacticalData.Fix;
  * @author ian.mayo
  * 
  */
-public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerator
-{
+public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerator {
 
 	// ////////////////////////////////////////////////////////////////////////////////////////////////
 	// testing for this class
 	// ////////////////////////////////////////////////////////////////////////////////////////////////
-	static public final class testMe extends junit.framework.TestCase
-	{
+	static public final class testMe extends junit.framework.TestCase {
 		static public final String TEST_ALL_TEST_TYPE = "UNIT";
 
-		public testMe(final String val)
-		{
+		public testMe(final String val) {
 			super(val);
 		}
 
 		public final void testIWork()
 		{
+			// /////////////////////////////////////////////////////////
+			// test the position related stuff
+			final TrackWrapper track = new TrackWrapper();
+
+			// and add the fixes
+			final java.util.Calendar cal = new java.util.GregorianCalendar(2001, 10,
+					4, 4, 4, 0);
+
+			cal.set(2001, 10, 4, 4, 4, 0);
+			track.addFix(new FixWrapper(new MWC.TacticalData.Fix(new HiResDate(cal
+					.getTime().getTime(), 0), new WorldLocation(2.0, 2.0, 0.0), 12, 12)));
+
+			cal.set(2001, 10, 4, 4, 4, 01);
+			track
+					.addFix(new FixWrapper(new MWC.TacticalData.Fix(new HiResDate(cal
+							.getTime().getTime(), 0), new WorldLocation(2.0, 2.25, 0.0), 12,
+							12)));
+
+			cal.set(2001, 10, 4, 4, 4, 02);
+			track.addFix(new FixWrapper(new MWC.TacticalData.Fix(new HiResDate(cal
+					.getTime().getTime(), 0), new WorldLocation(2.0, 2.5, 0.0), 12, 12)));
+			cal.set(2001, 10, 4, 4, 4, 05);
+			track
+					.addFix(new FixWrapper(new MWC.TacticalData.Fix(new HiResDate(cal
+							.getTime().getTime(), 0), new WorldLocation(2.0, 2.75, 0.0), 12,
+							12)));
+			cal.set(2001, 10, 4, 4, 4, 23);
+			track
+					.addFix(new FixWrapper(new MWC.TacticalData.Fix(new HiResDate(cal
+							.getTime().getTime(), 0), new WorldLocation(2.25, 2.0, 0.0), 12,
+							12)));
+			cal.set(2001, 10, 4, 4, 4, 25);
+			track.addFix(new FixWrapper(new MWC.TacticalData.Fix(new HiResDate(cal
+					.getTime().getTime(), 0), new WorldLocation(2.5, 2.0, 0.0), 12, 12)));
+			cal.set(2001, 10, 4, 4, 4, 28);
+			final WorldLocation theLoc = new WorldLocation(2.75d, 2.0, 0.0);
+			track.addFix(new FixWrapper(new MWC.TacticalData.Fix(new HiResDate(cal
+					.getTime().getTime(), 0), theLoc, 12, 12)));
+			cal.set(2001, 10, 4, 4, 4, 55);
+			track
+					.addFix(new FixWrapper(new MWC.TacticalData.Fix(new HiResDate(cal
+							.getTime().getTime(), 0), new WorldLocation(2.25, 2.25, 0.0), 12,
+							12)));
+
+			// and some sensor data, which goes past the end of O/S track
+			SensorWrapper sensor = new SensorWrapper("SensorName");
+			
+			cal.set(2001, 10, 4, 4, 4, 25);
+			sensor.add(new SensorContactWrapper("track", new HiResDate(cal.getTime().getTime()),
+					new WorldDistance(1000, WorldDistance.YARDS), 23.3d, new WorldLocation(0d, 0d, 0d), Color.RED,
+					"Some lable", 1, sensor.getName()));
+
+			cal.set(2001, 10, 4, 4, 4, 27);
+			sensor.add(new SensorContactWrapper("track", new HiResDate(cal.getTime().getTime()),
+					new WorldDistance(1000, WorldDistance.YARDS), 23.3d, new WorldLocation(0d, 0d, 0d), Color.RED,
+					"Some lable", 1, sensor.getName()));
+
+			cal.set(2001, 10, 4, 4, 4, 55);
+			sensor.add(new SensorContactWrapper("track", new HiResDate(cal.getTime().getTime()),
+					new WorldDistance(1000, WorldDistance.YARDS), 23.3d, new WorldLocation(0d, 0d, 0d), Color.RED,
+					"Some lable", 1, sensor.getName()));
+
+			cal.set(2001, 10, 4, 4, 5, 05);
+			sensor.add(new SensorContactWrapper("track", new HiResDate(cal.getTime().getTime()),
+					new WorldDistance(1000, WorldDistance.YARDS), 23.3d, new WorldLocation(0d, 0d, 0d), Color.RED,
+					"Some lable", 1, sensor.getName()));
 
 		}
 	}
 
-	private static class TrackfromSensorCuts extends TrackfromSensorData
-	{
+	private static class TrackfromSensorCuts extends TrackfromSensorData {
 		private final SensorContactWrapper[] _items;
 
-		public TrackfromSensorCuts(final SensorContactWrapper[] items,
-				final Layers theLayers)
-		{
+		public TrackfromSensorCuts(final SensorContactWrapper[] items, final Layers theLayers) {
 			super("Create Track from Active cuts", theLayers);
 			_items = items;
 		}
 
-		protected SensorContactWrapper[] getCuts()
-		{
+		protected SensorContactWrapper[] getCuts() {
 			return _items;
 		}
 	}
 
-	private static class TrackfromSensorWrappers extends TrackfromSensorData
-	{
+	private static class TrackfromSensorWrappers extends TrackfromSensorData {
 		private final SensorWrapper _wrapper;
 
-		public TrackfromSensorWrappers(final SensorWrapper wrapper,
-				final Layers theLayers)
-		{
+		public TrackfromSensorWrappers(final SensorWrapper wrapper, final Layers theLayers) {
 			super("Create Track from Active cuts", theLayers);
 			_wrapper = wrapper;
 		}
 
-		protected SensorContactWrapper[] getCuts()
-		{
+		protected SensorContactWrapper[] getCuts() {
 
 			final Vector<SensorContactWrapper> wraps = new Vector<SensorContactWrapper>();
 
-			if (_wrapper.size() > 0)
-			{
+			if (_wrapper.size() > 0) {
 				// ok, now check for range
 				Editable first = _wrapper.elements().nextElement();
 				SensorContactWrapper scw = (SensorContactWrapper) first;
-				if (scw.getHasBearing() && scw.getRange() != null)
-				{
+				if (scw.getHasBearing() && scw.getRange() != null) {
 					// ok, it's a goer.
 					final Enumeration<Editable> numer = _wrapper.elements();
-					while (numer.hasMoreElements())
-					{
+					while (numer.hasMoreElements()) {
 						wraps.add((SensorContactWrapper) numer.nextElement());
 					}
 				}
 			}
 
 			final SensorContactWrapper[] _items;
-			if (wraps.size() > 0)
-			{
+			if (wraps.size() > 0) {
 				SensorContactWrapper[] sample = new SensorContactWrapper[wraps.size()];
 				_items = wraps.toArray(sample);
-			}
-			else
-			{
+			} else {
 				_items = null;
 			}
 
@@ -130,14 +181,12 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 		}
 	}
 
-	private abstract static class TrackfromSensorData extends CMAPOperation
-	{
+	private abstract static class TrackfromSensorData extends CMAPOperation {
 
 		private final Layers _layers;
 		private TrackWrapper _newTrack;
 
-		public TrackfromSensorData(String title, final Layers theLayers)
-		{
+		public TrackfromSensorData(String title, final Layers theLayers) {
 			super(title);
 			_layers = theLayers;
 		}
@@ -145,9 +194,7 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 		abstract protected SensorContactWrapper[] getCuts();
 
 		@Override
-		public IStatus execute(final IProgressMonitor monitor, final IAdaptable info)
-				throws ExecutionException
-		{
+		public IStatus execute(final IProgressMonitor monitor, final IAdaptable info) throws ExecutionException {
 
 			SensorContactWrapper[] cuts = getCuts();
 
@@ -158,13 +205,11 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 			boolean fixesAdded = false;
 
 			// ok, now loop through and assign the cuts
-			for (int cnt = 0; cnt < cuts.length; cnt++)
-			{
+			for (int cnt = 0; cnt < cuts.length; cnt++) {
 				SensorContactWrapper cut = cuts[cnt];
-				
+
 				// double-check this is suitable
-				if (cut.getHasBearing() && cut.getRange() != null && !cut.getHasAmbiguousBearing())
-				{
+				if (cut.getHasBearing() && cut.getRange() != null && !cut.getHasAmbiguousBearing()) {
 					// represent rng/brg as a vector
 					WorldVector vec = new WorldVector(Math.toRadians(cut.getBearing()),
 							cut.getRange().getValueIn(WorldDistance.DEGS), 0);
@@ -174,10 +219,10 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 
 					Fix fix = new Fix(cut.getDTG(), loc, 0d, 0d);
 					FixWrapper fw = new FixWrapper(fix);
-					
+
 					// give it a sensible label
 					fw.resetName();
-					
+
 					_newTrack.add(fw);
 
 					fixesAdded = true;
@@ -185,8 +230,7 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 				}
 			}
 
-			if (fixesAdded)
-			{
+			if (fixesAdded) {
 				// and auto-generate courses and speeds
 				_newTrack.calcCourseSpeed();
 
@@ -195,21 +239,16 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 
 				// sorted, do the update
 				_layers.fireExtended();
-			}
-			else
-			{
+			} else {
 				CorePlugin.logError(Status.WARNING,
-						"Failed to find fixes with range and bearing - "
-								+ "track not generated", null);
+						"Failed to find fixes with range and bearing - " + "track not generated", null);
 			}
 
 			return Status.OK_STATUS;
 		}
 
 		@Override
-		public IStatus undo(final IProgressMonitor monitor, final IAdaptable info)
-				throws ExecutionException
-		{
+		public IStatus undo(final IProgressMonitor monitor, final IAdaptable info) throws ExecutionException {
 			// forget about the new tracks
 			_layers.removeThisLayer(_newTrack);
 			_layers.fireExtended();
@@ -225,39 +264,31 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 	 * @param parentLayers
 	 * @param subjects
 	 */
-	public void generate(final IMenuManager parent, final Layers theLayers,
-			final Layer[] parentLayers, final Editable[] subjects)
-	{
+	public void generate(final IMenuManager parent, final Layers theLayers, final Layer[] parentLayers,
+			final Editable[] subjects) {
 		Action _myAction = null;
 
 		// so, see if it's something we can do business with
-		if (subjects.length == 1)
-		{
+		if (subjects.length == 1) {
 			// ok, do I know how to create a TMA segment from this?
 			final Editable onlyOne = subjects[0];
-			if (onlyOne instanceof SensorWrapper)
-			{
+			if (onlyOne instanceof SensorWrapper) {
 				final SensorWrapper sw = (SensorWrapper) onlyOne;
 
-				if (sw.size() > 0)
-				{
+				if (sw.size() > 0) {
 					// ok, now check for range
 					Editable first = sw.elements().nextElement();
 					SensorContactWrapper scw = (SensorContactWrapper) first;
-					
-					if (scw.getHasBearing() && scw.getRange() != null && !scw.getHasAmbiguousBearing())
-					{
+
+					if (scw.getHasBearing() && scw.getRange() != null && !scw.getHasAmbiguousBearing()) {
 
 						// cool wrap it in an action.
-						_myAction = new Action("Generate Track from Active Sensor Data")
-						{
+						_myAction = new Action("Generate Track from Active Sensor Data") {
 							@Override
-							public void run()
-							{
+							public void run() {
 								// ok, go for it.
 								// sort it out as an operation
-								final IUndoableOperation convertToTrack1 = new TrackfromSensorWrappers(
-										sw, theLayers);
+								final IUndoableOperation convertToTrack1 = new TrackfromSensorWrappers(sw, theLayers);
 
 								// ok, stick it on the buffer
 								runIt(convertToTrack1);
@@ -266,11 +297,9 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 					}
 				}
 			}
-		}
-		else if(subjects.length > 1)
-		{
+		} else if (subjects.length > 1) {
 			// more than one item = maybe it's a series of sensor cuts
-			
+
 			//
 			SensorContactWrapper[] sonarCuts = null;
 
@@ -278,50 +307,37 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 			// so, it's a number of items, Are they all sensor contact wrappers
 			boolean allGood = true;
 			sonarCuts = new SensorContactWrapper[subjects.length];
-			for (int i = 0; i < subjects.length; i++)
-			{
+			for (int i = 0; i < subjects.length; i++) {
 				final Editable editable = subjects[i];
-				if (editable instanceof SensorContactWrapper)
-				{
+				if (editable instanceof SensorContactWrapper) {
 					SensorContactWrapper scw = (SensorContactWrapper) editable;
-					if (scw.getHasBearing() && scw.getRange() != null && !scw.getHasAmbiguousBearing())
-					{
+					if (scw.getHasBearing() && scw.getRange() != null && !scw.getHasAmbiguousBearing()) {
 						// cool, stick with it
 						sonarCuts[i] = (SensorContactWrapper) editable;
-					}
-					else
-					{
+					} else {
 						allGood = false;
 					}
-				}
-				else
-				{
+				} else {
 					allGood = false;
 					break;
 				}
 
 			}
 			// are we good to go?
-			if (!allGood)
-			{
+			if (!allGood) {
 				// nope, clear the items list
 				sonarCuts = null;
-			}
-			else
-			{
+			} else {
 				// cool, go for it
 				final SensorContactWrapper[] finalItems = sonarCuts;
 
 				// cool wrap it in an action.
-				_myAction = new Action("Generate Track from Active Sensor Data")
-				{
+				_myAction = new Action("Generate Track from Active Sensor Data") {
 					@Override
-					public void run()
-					{
+					public void run() {
 						// ok, go for it.
 						// sort it out as an operation
-						final IUndoableOperation convertToTrack1 = new TrackfromSensorCuts(
-								finalItems, theLayers);
+						final IUndoableOperation convertToTrack1 = new TrackfromSensorCuts(finalItems, theLayers);
 
 						// ok, stick it on the buffer
 						runIt(convertToTrack1);
@@ -330,8 +346,7 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 			}
 		}
 
-		if (_myAction != null)
-		{
+		if (_myAction != null) {
 			parent.add(_myAction);
 		}
 	}
@@ -342,8 +357,7 @@ public class GenerateTrackFromActiveCuts implements RightClickContextItemGenerat
 	 * 
 	 * @param operation
 	 */
-	protected void runIt(final IUndoableOperation operation)
-	{
+	protected void runIt(final IUndoableOperation operation) {
 		CorePlugin.run(operation);
 	}
 }
