75b177c380b752b18a6c49dacd29766946fdd3a6
Ian Mayo <ian@planetmayo.com>
2019-07-11 20:26:56 +0100

    Improve static analysis

--- org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/menu/DebriefRibbonLite.java
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/menu/DebriefRibbonLite.java
@@ -18,7 +18,6 @@ import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
 
 import javax.swing.AbstractAction;
 import javax.swing.JOptionPane;
