811fbe61da79e726dd102115a1751ab80434cd7b
Ian Mayo <ian@planetmayo.com>
2019-08-07 11:59:40 +0100

    apply code formatting

--- org.mwc.debrief.lite/src/test/java/org/mwc/debrief/lite/utils/TestUtils.java
+++ org.mwc.debrief.lite/src/test/java/org/mwc/debrief/lite/utils/TestUtils.java
@@ -22,78 +22,102 @@ package org.mwc.debrief.lite.utils;
 import java.awt.*;
 import javax.swing.*;
 
-public class TestUtils {
+public class TestUtils
+{
 
   static int counter;
 
-  public static Component getChildNamed(Component parent, String name) {
+  public static Component getChildNamed(Component parent, String name)
+  {
 
     // Debug line
-    //System.out.println("Class: " + parent.getClass() +
-    //    " Name: " + parent.getName());
+    // System.out.println("Class: " + parent.getClass() +
+    // " Name: " + parent.getName());
 
-    if (name.equals(parent.getName())) { return parent; }
+    if (name.equals(parent.getName()))
+    {
+      return parent;
+    }
 
-    if (parent instanceof Container) {
-      Component[] children = (parent instanceof JMenu) ?
-          ((JMenu)parent).getMenuComponents() :
-          ((Container)parent).getComponents();
+    if (parent instanceof Container)
+    {
+      Component[] children = (parent instanceof JMenu) ? ((JMenu) parent)
+          .getMenuComponents() : ((Container) parent).getComponents();
 
-      for (int i = 0; i < children.length; ++i) {
+      for (int i = 0; i < children.length; ++i)
+      {
         Component child = getChildNamed(children[i], name);
-        if (child != null) { return child; }
+        if (child != null)
+        {
+          return child;
+        }
       }
     }
-    
+
     return null;
   }
 
-  public static Component getChildIndexed(
-      Component parent, String klass, int index) {
+  public static Component getChildIndexed(Component parent, String klass,
+      int index)
+  {
     counter = 0;
 
     // Step in only owned windows and ignore its components in JFrame
-    if (parent instanceof Window) {
-      Component[] children = ((Window)parent).getOwnedWindows();
+    if (parent instanceof Window)
+    {
+      Component[] children = ((Window) parent).getOwnedWindows();
 
-      for (int i = 0; i < children.length; ++i) {
+      for (int i = 0; i < children.length; ++i)
+      {
         // take only active windows
-        if (children[i] instanceof Window &&
-            !((Window)children[i]).isActive()) { continue; }
+        if (children[i] instanceof Window && !((Window) children[i]).isActive())
+        {
+          continue;
+        }
 
-        Component child = getChildIndexedInternal(
-            children[i], klass, index);
-        if (child != null) { return child; }
+        Component child = getChildIndexedInternal(children[i], klass, index);
+        if (child != null)
+        {
+          return child;
+        }
       }
     }
 
     return null;
   }
 
-  private static Component getChildIndexedInternal(
-      Component parent, String klass, int index) {
+  private static Component getChildIndexedInternal(Component parent,
+      String klass, int index)
+  {
 
     // Debug line
-    //System.out.println("Class: " + parent.getClass() +
-    //    " Name: " + parent.getName());
+    // System.out.println("Class: " + parent.getClass() +
+    // " Name: " + parent.getName());
 
-    if (parent.getClass().toString().endsWith(klass)) {
-      if (counter == index) { return parent; }
+    if (parent.getClass().toString().endsWith(klass))
+    {
+      if (counter == index)
+      {
+        return parent;
+      }
       ++counter;
     }
 
-    if (parent instanceof Container) {
-      Component[] children = (parent instanceof JMenu) ?
-          ((JMenu)parent).getMenuComponents() :
-          ((Container)parent).getComponents();
+    if (parent instanceof Container)
+    {
+      Component[] children = (parent instanceof JMenu) ? ((JMenu) parent)
+          .getMenuComponents() : ((Container) parent).getComponents();
 
-      for (int i = 0; i < children.length; ++i) {
-        Component child = getChildIndexedInternal(
-            children[i], klass, index);
-        if (child != null) { return child; }
+      for (int i = 0; i < children.length; ++i)
+      {
+        Component child = getChildIndexedInternal(children[i], klass, index);
+        if (child != null)
+        {
+          return child;
+        }
       }
     }
-    
+
     return null;
   }
 }
