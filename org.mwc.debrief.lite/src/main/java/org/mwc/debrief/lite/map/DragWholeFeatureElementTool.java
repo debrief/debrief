3f644388445e5ccfc604ff2f266d35e412c89510
Saul Hidalgo <saulhidalgoaular@gmail.com>
2019-08-27 00:10:00 +0200

    Revert "Code Formatting."
    This reverts commit 01aa232143f5605fa6fa23d404563b9c4ddb6c38.

--- org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/map/DragWholeFeatureElementTool.java
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/map/DragWholeFeatureElementTool.java
@@ -16,7 +16,7 @@ import MWC.GenericData.WorldVector;
 
 public class DragWholeFeatureElementTool extends GenericDragTool
 {
-
+  
   /**
    * the thing we're currently hovering over
    */
@@ -27,6 +27,80 @@ public class DragWholeFeatureElementTool extends GenericDragTool
   {
     super(layers, projection, mapPane);
   }
+
+
+  @Override
+  public void onMouseMoved(MapMouseEvent ev)
+  {
+    super.onMouseMoved(ev);
+    
+
+    // try to determine if we're going over an item, to
+    // change the cursor
+    
+    // don't bother if we're already in a pan operation
+    if (!panning && !lastCursor.equals(draggingCursor))
+    {
+      panePos = mouseDelta(ev.getPoint());
+
+      final WorldLocation cursorLoc = _projection.toWorld(panePos);
+      // find the nearest editable item
+      final LocationConstruct currentNearest = new LocationConstruct();
+      final int num = layers.size();
+      for (int i = 0; i < num; i++)
+      {
+        final Layer thisL = layers.elementAt(i);
+        if (thisL.getVisible())
+        {
+          // find the nearest items, this method call will recursively pass down
+          // through
+          // the layers
+          // final Layer thisLayer,
+          FindNearest.findNearest(thisL, cursorLoc, panePos, currentNearest,
+              null, layers);
+        }
+      }
+
+      // Note - the following test does a distance check using world distance,
+      // which is quite unreliable,
+      
+      // did we find anything?
+      if (currentNearest.populated())
+      {
+        // generate a screen point from the cursor pos plus our distnace
+        // NOTE: we're not basing this on the target location - we may not have
+        // a
+        // target location as such for a strangely shaped object
+        final WorldLocation tgtPt =
+            cursorLoc.add(new WorldVector(Math.PI / 2,
+                currentNearest._distance, null));
+
+        // is it close enough
+        final Point tPoint = _projection.toScreen(tgtPt);
+
+        // get click point
+        Point cursorPos = ev.getPoint();
+        
+        // get distance of click point from nearest object, in screen coords
+        final double scrDist = tPoint.distance(cursorPos);
+
+        if (scrDist <= SCREEN_JITTER && !lastCursor.equals(greenCursor))
+        {
+          lastCursor = greenCursor;
+          _mapPane.setCursor(greenCursor);
+        }
+        else if ( scrDist > SCREEN_JITTER && !lastCursor.equals(normalCursor) )
+        {
+          lastCursor = normalCursor;
+          _mapPane.setCursor(normalCursor);
+        }
+      }
+    }
+  }
+
+
+
+
 
   /**
    * Respond to a mouse dragged event. Calls {@link org.geotools.swing.MapPane#moveImage()}
@@ -58,73 +132,6 @@ public class DragWholeFeatureElementTool extends GenericDragTool
           _mapPane.repaint();
         }
         panePos = pos;
-      }
-    }
-  }
-
-  @Override
-  public void onMouseMoved(final MapMouseEvent ev)
-  {
-    super.onMouseMoved(ev);
-
-    // try to determine if we're going over an item, to
-    // change the cursor
-
-    // don't bother if we're already in a pan operation
-    if (!panning && !lastCursor.equals(draggingCursor))
-    {
-      panePos = mouseDelta(ev.getPoint());
-
-      final WorldLocation cursorLoc = _projection.toWorld(panePos);
-      // find the nearest editable item
-      final LocationConstruct currentNearest = new LocationConstruct();
-      final int num = layers.size();
-      for (int i = 0; i < num; i++)
-      {
-        final Layer thisL = layers.elementAt(i);
-        if (thisL.getVisible())
-        {
-          // find the nearest items, this method call will recursively pass down
-          // through
-          // the layers
-          // final Layer thisLayer,
-          FindNearest.findNearest(thisL, cursorLoc, panePos, currentNearest,
-              null, layers);
-        }
-      }
-
-      // Note - the following test does a distance check using world distance,
-      // which is quite unreliable,
-
-      // did we find anything?
-      if (currentNearest.populated())
-      {
-        // generate a screen point from the cursor pos plus our distnace
-        // NOTE: we're not basing this on the target location - we may not have
-        // a
-        // target location as such for a strangely shaped object
-        final WorldLocation tgtPt = cursorLoc.add(new WorldVector(Math.PI / 2,
-            currentNearest._distance, null));
-
-        // is it close enough
-        final Point tPoint = _projection.toScreen(tgtPt);
-
-        // get click point
-        final Point cursorPos = ev.getPoint();
-
-        // get distance of click point from nearest object, in screen coords
-        final double scrDist = tPoint.distance(cursorPos);
-
-        if (scrDist <= SCREEN_JITTER && !lastCursor.equals(greenCursor))
-        {
-          lastCursor = greenCursor;
-          _mapPane.setCursor(greenCursor);
-        }
-        else if (scrDist > SCREEN_JITTER && !lastCursor.equals(normalCursor))
-        {
-          lastCursor = normalCursor;
-          _mapPane.setCursor(normalCursor);
-        }
       }
     }
   }
@@ -165,19 +172,20 @@ public class DragWholeFeatureElementTool extends GenericDragTool
       // did we find anything?
       if (currentNearest.populated())
       {
-        // generate a screen point from the cursor pos plus our distnace
+     // generate a screen point from the cursor pos plus our distnace
         // NOTE: we're not basing this on the target location - we may not have
         // a
         // target location as such for a strangely shaped object
-        final WorldLocation tgtPt = cursorLoc.add(new WorldVector(Math.PI / 2,
-            currentNearest._distance, null));
+        final WorldLocation tgtPt =
+            cursorLoc.add(new WorldVector(Math.PI / 2,
+                currentNearest._distance, null));
 
         // is it close enough
         final Point tPoint = _projection.toScreen(tgtPt);
 
         // get click point
-        final Point cursorPos = ev.getPoint();
-
+        Point cursorPos = ev.getPoint();
+        
         // get distance of click point from nearest object, in screen coords
         final double distance = tPoint.distance(cursorPos);
         if (distance < JITTER)
