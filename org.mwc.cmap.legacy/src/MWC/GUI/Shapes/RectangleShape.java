6ed18feed89fe67ae2218d75edbeb5fc294bd709
Saul Hidalgo <saulhidalgoaular@gmail.com>
2019-08-29 01:03:18 +0200

    Code Cleaning

--- org.mwc.cmap.legacy/src/MWC/GUI/Shapes/RectangleShape.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/Shapes/RectangleShape.java
@@ -10,7 +10,7 @@
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 // $RCSfile: RectangleShape.java,v $
 // @author $Author: ian.mayo $
@@ -172,63 +172,259 @@ import MWC.GenericData.WorldLocation;
 import MWC.GenericData.WorldVector;
 
 public class RectangleShape extends PlainShape implements Editable,
-		HasDraggableComponents
+    HasDraggableComponents
 {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+  // ////////////////////////////////////////////////////
+  // bean info for this class
+  // ///////////////////////////////////////////////////
+  public class RectangleInfo extends Editable.EditorType
+  {
 
-	// ////////////////////////////////////////////////
-	// member variables
-	// ////////////////////////////////////////////////
-	protected WorldArea _myArea;
+    public RectangleInfo(final RectangleShape data, final String theName)
+    {
+      super(data, theName, "");
+    }
 
-	/**
-	 * our editor
-	 */
-	transient private Editable.EditorType _myEditor;
-
-	// ////////////////////////////////////////////////
-	// constructor
-	// ////////////////////////////////////////////////
-	public RectangleShape(final WorldLocation TL, final WorldLocation BR)
-	{
-		super(0, "Rectangle");
-
-		_myArea = new WorldArea(TL, BR);
-		_myArea.normalise();
-	}
-
-	// ////////////////////////////////////////////////
-	// member functions
-	// ////////////////////////////////////////////////
-
-	/**
-	 * paint this shape to the destination canvas
-	 */
-	public void paint(final CanvasType dest)
-	{
-		// are we visible?
-		if (!getVisible())
-			return;
-
-		// create a transparent colour
-		final Color newcol = getColor();
-		dest.setColor(new Color(newcol.getRed(), newcol.getGreen(), newcol
-				.getBlue(), TRANSPARENCY_SHADE));
-		final Collection<WorldLocation> pts = getDataPoints();
-		final Iterator<WorldLocation> iter = pts.iterator();
-		final int STEPS = pts.size();
-		final int[] xP = new int[STEPS];
-		final int[] yP = new int[STEPS];
-		int ctr = 0;
-		while (iter.hasNext())
-		{
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors()
+    {
       try
       {
-        WorldLocation loc = iter.next();
+        final PropertyDescriptor[] res =
+        {displayProp("Corner_TopLeft", "Top left corner", "the top left corner",
+            SPATIAL), displayProp("CornerBottomRight", "Bottom right corner",
+                "the bottom right corner", SPATIAL), prop("Filled",
+                    "whether this shape is filled", FORMAT), displayProp(
+                        "SemiTransparent", "Semi transparent",
+                        "whether the filled rect is semi-transparent",
+                        FORMAT),};
+
+        return res;
+
+      }
+      catch (final IntrospectionException e)
+      {
+        return super.getPropertyDescriptors();
+      }
+    }
+  }
+
+  // ////////////////////////////////////////////////////////////////////////////////////////////////
+  // testing for this class
+  // ////////////////////////////////////////////////////////////////////////////////////////////////
+  static public class RectangleTest extends junit.framework.TestCase
+  {
+    static public final String TEST_ALL_TEST_TYPE = "UNIT";
+
+    public RectangleTest(final String val)
+    {
+      super(val);
+    }
+
+    public void testMyParams()
+    {
+      final WorldLocation scrap = new WorldLocation(2d, 2d, 2d);
+      MWC.GUI.Editable ed = new RectangleShape(scrap, scrap);
+      MWC.GUI.Editable.editableTesterSupport.testParams(ed, this);
+      ed = null;
+    }
+  }
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  // ////////////////////////////////////////////////
+  // member variables
+  // ////////////////////////////////////////////////
+  protected WorldArea _myArea;
+
+  // ////////////////////////////////////////////////
+  // member functions
+  // ////////////////////////////////////////////////
+
+  /**
+   * our editor
+   */
+  transient private Editable.EditorType _myEditor;
+
+  // ////////////////////////////////////////////////
+  // constructor
+  // ////////////////////////////////////////////////
+  public RectangleShape(final WorldLocation TL, final WorldLocation BR)
+  {
+    super(0, "Rectangle");
+
+    _myArea = new WorldArea(TL, BR);
+    _myArea.normalise();
+  }
+
+  @Override
+  public void findNearestHotSpotIn(final Point cursorPos,
+      final WorldLocation cursorLoc, final ComponentConstruct currentNearest,
+      final Layer parentLayer)
+  {
+
+    // right - the first two points are easy, we just pass the location directly
+    // to the caller
+    checkThisOne(_myArea.getTopLeft(), cursorLoc, currentNearest, this,
+        parentLayer);
+    checkThisOne(_myArea.getBottomRight(), cursorLoc, currentNearest, this,
+        parentLayer);
+
+    // now for our 'special' corners, that we can't just shift over...
+    // we've got to wrap the locations in an object that actually updates the TL
+    // & BR corners of the
+    // rectangle
+    final WorldLocation bl = new WorldLocation(_myArea.getBottomLeft())
+    {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public void addToMe(final WorldVector delta)
+      {
+        final WorldLocation newBL = _myArea.getBottomLeft().add(delta);
+        final WorldLocation newTR = _myArea.getTopRight();
+        _myArea = new WorldArea(newBL, newTR);
+        _myArea.normalise();
+      }
+    };
+    final WorldLocation tr = new WorldLocation(_myArea.getTopRight())
+    {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public void addToMe(final WorldVector delta)
+      {
+        final WorldLocation newBL = _myArea.getBottomLeft();
+        final WorldLocation newTR = _myArea.getTopRight().add(delta);
+        _myArea = new WorldArea(newBL, newTR);
+        _myArea.normalise();
+      }
+    };
+
+    // now check the ranges...
+    checkThisOne(bl, cursorLoc, currentNearest, this, parentLayer);
+    checkThisOne(tr, cursorLoc, currentNearest, this, parentLayer);
+
+    final WorldLocation center = new WorldLocation(_myArea.getCentre())
+    {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public void addToMe(final WorldVector delta)
+      {
+        final WorldLocation newBL = _myArea.getBottomLeft().add(delta);
+        final WorldLocation newTR = _myArea.getTopRight().add(delta);
+        _myArea = new WorldArea(newBL, newTR);
+        _myArea.normalise();
+      }
+    };
+    checkThisOne(center, cursorLoc, currentNearest, this, parentLayer);
+  }
+
+  /**
+   * get the 'anchor point' for any labels attached to this shape
+   */
+  public MWC.GenericData.WorldLocation getAnchor()
+  {
+    return _myArea.getCentre();
+  }
+
+  /**
+   * return the area covered by this shape
+   * 
+   * @return WorldArea the area covered
+   */
+  @Override
+  public MWC.GenericData.WorldArea getBounds()
+  {
+    return _myArea;
+  }
+
+  /**
+   * get the TopLeft corner
+   * 
+   * @return WorldLocation for the TopLeft corner
+   */
+  public WorldLocation getCorner_TopLeft()
+  {
+    return _myArea.getTopLeft();
+  }
+
+  /**
+   * get the BottomRight corner
+   * 
+   * @return WorldLocation for the BottomRight corner
+   */
+  public WorldLocation getCornerBottomRight()
+  {
+    return _myArea.getBottomRight();
+  }
+
+  /**
+   * get the shape as a series of WorldLocation points. Joined up, these form a representation of
+   * the shape
+   */
+  private Collection<WorldLocation> getDataPoints()
+  {
+    final Vector<WorldLocation> res = new Vector<WorldLocation>(0, 1);
+
+    res.add(_myArea.getTopLeft());
+    res.add(_myArea.getTopRight());
+    res.add(_myArea.getBottomRight());
+    res.add(_myArea.getBottomLeft());
+
+    return res;
+  }
+
+  @Override
+  public Editable.EditorType getInfo()
+  {
+    if (_myEditor == null)
+      _myEditor = new RectangleInfo(this, this.getName());
+
+    return _myEditor;
+  }
+
+  public Color getRectangleColor()
+  {
+    return super.getColor();
+  }
+
+  @Override
+  public boolean hasEditor()
+  {
+    return true;
+  }
+
+  /**
+   * paint this shape to the destination canvas
+   */
+  @Override
+  public void paint(final CanvasType dest)
+  {
+    // are we visible?
+    if (!getVisible())
+      return;
+
+    // create a transparent colour
+    final Color newcol = getColor();
+    dest.setColor(new Color(newcol.getRed(), newcol.getGreen(), newcol
+        .getBlue(), TRANSPARENCY_SHADE));
+    final Collection<WorldLocation> pts = getDataPoints();
+    final Iterator<WorldLocation> iter = pts.iterator();
+    final int STEPS = pts.size();
+    final int[] xP = new int[STEPS];
+    final int[] yP = new int[STEPS];
+    int ctr = 0;
+    while (iter.hasNext())
+    {
+      try
+      {
+        final WorldLocation loc = iter.next();
         final Point pt = dest.toScreen(loc);
         // Rotate Rectangle around its center for degrees, specified in
         // Orientation
@@ -238,295 +434,113 @@ public class RectangleShape extends PlainShape implements Editable,
           yP[ctr++] = pt.y;
         }
       }
-      catch (Exception e)
+      catch (final Exception e)
       {
         e.printStackTrace();
       }
-		}
+    }
 
-		// is it to be filled?
-		// and plot the polygon
-		if (getFilled())
-		{
-			if (getSemiTransparent() && dest instanceof ExtendedCanvasType)
-			{
-				ExtendedCanvasType ext = (ExtendedCanvasType) dest;
-				ext.semiFillPolygon(xP, yP, STEPS);
-			}
-			else
-				dest.fillPolygon(xP, yP, STEPS);
-		}
-		else
-		{
-			dest.drawPolygon(xP, yP, STEPS);
-		}
-
-	}
-
-	/**
-	 * return the area covered by this shape
-	 * 
-	 * @return WorldArea the area covered
-	 */
-	public MWC.GenericData.WorldArea getBounds()
-	{
-		return _myArea;
-	}
-
-	public void setRectangleColor(final Color val)
-	{
-		super.setColor(val);
-	}
-
-	public Color getRectangleColor()
-	{
-		return super.getColor();
-	}
-
-	/**
-	 * get the shape as a series of WorldLocation points. Joined up, these form a
-	 * representation of the shape
-	 */
-	private Collection<WorldLocation> getDataPoints()
-	{
-		final Vector<WorldLocation> res = new Vector<WorldLocation>(0, 1);
-
-		res.add(_myArea.getTopLeft());
-		res.add(_myArea.getTopRight());
-		res.add(_myArea.getBottomRight());
-		res.add(_myArea.getBottomLeft());
-
-		return res;
-	}
-
-	/**
-	 * get the range from the indicated world location - making this abstract
-	 * allows for individual shapes to have 'hit-spots' in various locations.
-	 */
-	public double rangeFrom(final WorldLocation point)
-	{
-		return _myArea.rangeFrom(point);
-	}
-
-	public boolean hasEditor()
-	{
-		return true;
-	}
-
-	public Editable.EditorType getInfo()
-	{
-		if (_myEditor == null)
-			_myEditor = new RectangleInfo(this, this.getName());
-
-		return _myEditor;
-	}
-
-	/**
-	 * get the 'anchor point' for any labels attached to this shape
-	 */
-	public MWC.GenericData.WorldLocation getAnchor()
-	{
-		return _myArea.getCentre();
-	}
-
-	/**
-	 * get the TopLeft corner
-	 * 
-	 * @return WorldLocation for the TopLeft corner
-	 */
-	public WorldLocation getCorner_TopLeft()
-	{
-		return _myArea.getTopLeft();
-	}
-
-	/**
-	 * set the TopLeft corner
-	 * 
-	 * @param loc
-	 *          WorldLocation for the corner
-	 */
-	public void setCorner_TopLeft(final WorldLocation loc)
-	{
-		final WorldLocation br = _myArea.getBottomRight();
-		_myArea = new WorldArea(loc, br);
-
-		firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
-	}
-
-	/**
-	 * get the BottomRight corner
-	 * 
-	 * @return WorldLocation for the BottomRight corner
-	 */
-	public WorldLocation getCornerBottomRight()
-	{
-		return _myArea.getBottomRight();
-	}
-
-	/**
-	 * set the BottomRight corner
-	 * 
-	 * @param loc
-	 *          WorldLocation for the corner
-	 */
-	public void setCornerBottomRight(final WorldLocation loc)
-	{
-		final WorldLocation tl = _myArea.getTopLeft();
-		_myArea = new WorldArea(tl, loc);
-
-		firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
-	}
-
-	// ////////////////////////////////////////////////////
-	// bean info for this class
-	// ///////////////////////////////////////////////////
-	public class RectangleInfo extends Editable.EditorType
-	{
-
-		public RectangleInfo(final RectangleShape data, final String theName)
-		{
-			super(data, theName, "");
-		}
-
-		public PropertyDescriptor[] getPropertyDescriptors()
-		{
-			try
-			{
-				final PropertyDescriptor[] res =
-				{
-						displayProp("Corner_TopLeft", "Top left corner", "the top left corner", SPATIAL),
-						displayProp("CornerBottomRight",  "Bottom right corner", "the bottom right corner", SPATIAL),
-						prop("Filled", "whether this shape is filled", FORMAT),
-						displayProp("SemiTransparent", "Semi transparent",
-								"whether the filled rect is semi-transparent", FORMAT), };
-
-				return res;
-
-			}
-			catch (final IntrospectionException e)
-			{
-				return super.getPropertyDescriptors();
-			}
-		}
-	}
-
-	// ////////////////////////////////////////////////////////////////////////////////////////////////
-	// testing for this class
-	// ////////////////////////////////////////////////////////////////////////////////////////////////
-	static public class RectangleTest extends junit.framework.TestCase
-	{
-		static public final String TEST_ALL_TEST_TYPE = "UNIT";
-
-		public RectangleTest(final String val)
-		{
-			super(val);
-		}
-
-		public void testMyParams()
-		{
-			final WorldLocation scrap = new WorldLocation(2d, 2d, 2d);
-			MWC.GUI.Editable ed = new RectangleShape(scrap, scrap);
-			MWC.GUI.Editable.editableTesterSupport.testParams(ed, this);
-			ed = null;
-		}
-	}
-
-	/**
-	 * move the whole shape by the specified distance
-	 * 
-	 */
-	public void shift(final WorldVector vector)
-	{
-
-		// get the old centre
-		final WorldLocation oldCentre = _myArea.getCentre();
-
-		// apply the offset
-		final WorldLocation newCentre = oldCentre.add(vector);
-
-		// ok, apply the offset to each corner
-		_myArea.setCentre(newCentre);
-
-		// and make it square again
-		_myArea.normalise();
-
-		// and inform the parent, so we can shift the label location
-		firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
-	}
-
-	/**
-	 * move one of the corners of the shape
-	 * 
-	 */
-	public void shift(final WorldLocation feature, final WorldVector vector)
-	{
-		// ok, just shift it...
-		feature.addToMe(vector);
-
-		// better normalise the shape now...
-		_myArea.normalise();
-
-		// and inform the parent, so we can shift the label location
-		firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
-	}
-
-	public void findNearestHotSpotIn(final Point cursorPos,
-			final WorldLocation cursorLoc, final ComponentConstruct currentNearest,
-			final Layer parentLayer)
-	{
-
-		// right - the first two points are easy, we just pass the location directly
-		// to the caller
-		checkThisOne(_myArea.getTopLeft(), cursorLoc, currentNearest, this,
-				parentLayer);
-		checkThisOne(_myArea.getBottomRight(), cursorLoc, currentNearest, this,
-				parentLayer);
-
-		// now for our 'special' corners, that we can't just shift over...
-		// we've got to wrap the locations in an object that actually updates the TL
-		// & BR corners of the
-		// rectangle
-		final WorldLocation bl = new WorldLocation(_myArea.getBottomLeft())
-		{
-			private static final long serialVersionUID = 1L;
-
-			public void addToMe(final WorldVector delta)
-			{
-				final WorldLocation newBL = _myArea.getBottomLeft().add(delta);
-				final WorldLocation newTR = _myArea.getTopRight();
-				_myArea = new WorldArea(newBL, newTR);
-				_myArea.normalise();
-			}
-		};
-		final WorldLocation tr = new WorldLocation(_myArea.getTopRight())
-		{
-			private static final long serialVersionUID = 1L;
-
-			public void addToMe(final WorldVector delta)
-			{
-				final WorldLocation newBL = _myArea.getBottomLeft();
-				final WorldLocation newTR = _myArea.getTopRight().add(delta);
-				_myArea = new WorldArea(newBL, newTR);
-				_myArea.normalise();
-			}
-		};
-
-		// now check the ranges...
-		checkThisOne(bl, cursorLoc, currentNearest, this, parentLayer);
-		checkThisOne(tr, cursorLoc, currentNearest, this, parentLayer);
-
-    final WorldLocation center = new WorldLocation(_myArea.getCentre()) {
-      private static final long serialVersionUID = 1L;
-
-      public void addToMe(final WorldVector delta)
+    // is it to be filled?
+    // and plot the polygon
+    if (getFilled())
+    {
+      if (getSemiTransparent() && dest instanceof ExtendedCanvasType)
       {
-        final WorldLocation newBL = _myArea.getBottomLeft().add(delta);
-        final WorldLocation newTR = _myArea.getTopRight().add(delta);
-        _myArea = new WorldArea(newBL, newTR);
-        _myArea.normalise();
+        final ExtendedCanvasType ext = (ExtendedCanvasType) dest;
+        ext.semiFillPolygon(xP, yP, STEPS);
       }
-    };
-		checkThisOne(center, cursorLoc, currentNearest, this, parentLayer);
-	}
+      else
+        dest.fillPolygon(xP, yP, STEPS);
+    }
+    else
+    {
+      dest.drawPolygon(xP, yP, STEPS);
+    }
+
+  }
+
+  /**
+   * get the range from the indicated world location - making this abstract allows for individual
+   * shapes to have 'hit-spots' in various locations.
+   */
+  @Override
+  public double rangeFrom(final WorldLocation point)
+  {
+    return _myArea.rangeFrom(point);
+  }
+
+  /**
+   * set the TopLeft corner
+   * 
+   * @param loc
+   *          WorldLocation for the corner
+   */
+  public void setCorner_TopLeft(final WorldLocation loc)
+  {
+    final WorldLocation br = _myArea.getBottomRight();
+    _myArea = new WorldArea(loc, br);
+
+    firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
+  }
+
+  /**
+   * set the BottomRight corner
+   * 
+   * @param loc
+   *          WorldLocation for the corner
+   */
+  public void setCornerBottomRight(final WorldLocation loc)
+  {
+    final WorldLocation tl = _myArea.getTopLeft();
+    _myArea = new WorldArea(tl, loc);
+
+    firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
+  }
+
+  public void setRectangleColor(final Color val)
+  {
+    super.setColor(val);
+  }
+
+  /**
+   * move one of the corners of the shape
+   * 
+   */
+  @Override
+  public void shift(final WorldLocation feature, final WorldVector vector)
+  {
+    // ok, just shift it...
+    feature.addToMe(vector);
+
+    // better normalise the shape now...
+    _myArea.normalise();
+
+    // and inform the parent, so we can shift the label location
+    firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
+  }
+
+  /**
+   * move the whole shape by the specified distance
+   * 
+   */
+  @Override
+  public void shift(final WorldVector vector)
+  {
+
+    // get the old centre
+    final WorldLocation oldCentre = _myArea.getCentre();
+
+    // apply the offset
+    final WorldLocation newCentre = oldCentre.add(vector);
+
+    // ok, apply the offset to each corner
+    _myArea.setCentre(newCentre);
+
+    // and make it square again
+    _myArea.normalise();
+
+    // and inform the parent, so we can shift the label location
+    firePropertyChange(PlainWrapper.LOCATION_CHANGED, null, null);
+  }
 
 }
