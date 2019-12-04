2e3c377f3cfd1cff3d30f1ca76c5b11f94c39059
Ayesha <Ayesha@DESKTOP-C5J4B96.Dlink>
2019-12-04 20:52:19 +0530

    suppressed warnings

--- org.mwc.cmap.legacy/src/MWC/GUI/Tools/PlainTool.java
+++ org.mwc.cmap.legacy/src/MWC/GUI/Tools/PlainTool.java
@@ -108,6 +108,7 @@ import MWC.GenericData.WorldArea;
  * which are 'operations', ie do a single GUI-independent process
  * immediately the button is pressed
  */
+@SuppressWarnings("serial")
 abstract public class PlainTool extends AbstractAction implements Tool {
   
   public static interface BoundsProvider
