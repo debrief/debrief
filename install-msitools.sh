f9b3822c2d944e0aaf5df446277c16f426ec4c1b
Saul Hidalgo <saulhidalgoaular@gmail.com>
2020-07-09 15:14:46 +0200

    msitools 0.100

--- install-msitools.sh
+++ install-msitools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-wget http://ftp.gnome.org/pub/GNOME/sources/msitools/0.99/msitools-0.99.tar.xz
-tar xvf msitools-0.99.tar.xz
-cd msitools-0.99 && ./configure --prefix=/usr && make -j10 && sudo make install
+wget http://ftp.gnome.org/pub/GNOME/sources/msitools/0.100/msitools-0.100.tar.xz
+tar xvf msitools-0.100.tar.xz
+cd msitools-0.100 && ./configure --prefix=/usr && make -j10 && sudo make install
 cd ..
