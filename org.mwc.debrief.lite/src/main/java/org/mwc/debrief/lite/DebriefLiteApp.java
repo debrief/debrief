c8d61b2ae24f5fdc58f7950bac82c4a248b7d4b8
Saul Hidalgo <saulhidalgoaular@gmail.com>
2020-08-10 09:04:22 +0200

    Moving Antares import management to a different class in Debrief Lite - https://github.com/debrief/debrief/pull/4945#discussion_r467040947

--- org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/DebriefLiteApp.java
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/DebriefLiteApp.java
@@ -87,6 +87,7 @@ import org.mwc.debrief.lite.gui.custom.graph.GraphPanelView;
 import org.mwc.debrief.lite.gui.custom.narratives.NarrativeConfigurationModel;
 import org.mwc.debrief.lite.gui.custom.narratives.NarrativePanelToolbar;
 import org.mwc.debrief.lite.gui.custom.narratives.NarrativePanelView;
+import org.mwc.debrief.lite.loader.AntaresLoaderDebriefLite;
 import org.mwc.debrief.lite.map.ContextMenu;
 import org.mwc.debrief.lite.map.GeoToolMapRenderer;
 import org.mwc.debrief.lite.map.GeoToolMapRenderer.MapRenderer;
@@ -1083,7 +1084,8 @@ public class DebriefLiteApp implements FileDropListener {
 						// Maybe it is an antares file
 						final ImportAntares antaresImporter = new ImportAntares();
 						if (antaresImporter.canImportThisFile(file.getAbsolutePath())) {
-							handleImportAntares(file, antaresImporter);
+							AntaresLoaderDebriefLite.handleImportAntares(file, antaresImporter, _theLayers,
+									getApplicationFrame());
 						}
 					} else {
 						// try nisida format - we can't rely on filename
@@ -1104,98 +1106,6 @@ public class DebriefLiteApp implements FileDropListener {
 		}
 
 		restoreCursor();
-	}
-
-	private void handleImportAntares(final File file, final ImportAntares antaresImporter) {
-		// We need to ask to the user the trackname, month and year.
-		
-		antaresImporter.setLayers(_theLayers);
-		
-		final JPanel mainPanel = new JPanel(new BorderLayout());
-		
-		final JLabel nameOfTheTrackLabel = new JLabel("Please, choose the name of the track");
-		nameOfTheTrackLabel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		final JLabel dateLabel = new JLabel("Please, choose the month and year");
-		dateLabel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		final JTextField nameOfTheTrackTextField = new JTextField();
-		nameOfTheTrackTextField.setToolTipText("Name of the track");
-		
-		
-		final JComboBox<String> monthComboBox = new JComboBox<String>(
-				IntStream.range(1, 13).mapToObj(i -> String.format("%01d", i)).toArray(String[]::new));
-		monthComboBox.setSelectedIndex(Calendar.getInstance().get(Calendar.MONTH));
-		final JTextField yearTextField = new JTextField();
-		yearTextField.setText(Calendar.getInstance().get(Calendar.YEAR) + "");
-		
-		final JButton cancelButton = new JButton("Cancel");
-		final JButton acceptButton = new JButton("Accept");
-		
-		final JPanel centerPanel = new JPanel();
-		
-		centerPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
-		centerPanel.add(nameOfTheTrackLabel);
-		centerPanel.add(nameOfTheTrackTextField);
-		centerPanel.add(dateLabel);
-		
-		
-		
-		final JPanel datePanel = new JPanel();
-		datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.X_AXIS));
-		datePanel.add(monthComboBox);
-		datePanel.add(yearTextField);
-		
-		centerPanel.add(datePanel);
-		
-		mainPanel.add(centerPanel, BorderLayout.CENTER);
-		
-		final JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		buttonsPanel.add(cancelButton);
-		buttonsPanel.add(acceptButton);
-		
-		mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
-
-		final JDialog frame = new JDialog(getApplicationFrame(), "Antares Import Configuration", true) {
-
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = -4759126543816346899L;
-
-			@Override
-			public Dimension getPreferredSize() {
-				return new Dimension(300, 200);
-			}
-			
-		};
-		cancelButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-			}
-		});
-		
-		acceptButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				antaresImporter.setMonth(monthComboBox.getSelectedIndex());
-				antaresImporter.setYear(Integer.parseInt(yearTextField.getText()));
-				antaresImporter.setTrackName(nameOfTheTrackTextField.getText());
-				try {
-					antaresImporter.importThis(file.getName(), new FileInputStream(file));
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				frame.dispose();
-			}
-		});
-		frame.setLocationRelativeTo(null);
-		frame.getContentPane().add(mainPanel);
-		frame.pack();
-		frame.setVisible(true);
 	}
 
 	public JRibbonFrame getApplicationFrame() {
---
+++ org.mwc.debrief.lite/src/main/java/org/mwc/debrief/lite/loader/AntaresLoaderDebriefLite.java
@@ -0,0 +1,118 @@
+package org.mwc.debrief.lite.loader;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Calendar;
+import java.util.stream.IntStream;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import Debrief.ReaderWriter.Antares.ImportAntares;
+import MWC.GUI.Layers;
+
+public class AntaresLoaderDebriefLite {
+
+	public static void handleImportAntares(final File file, final ImportAntares antaresImporter,
+			final Layers theLayers, final JFrame owner) {
+		// We need to ask to the user the trackname, month and year.
+
+		antaresImporter.setLayers(theLayers);
+
+		final JPanel mainPanel = new JPanel(new BorderLayout());
+
+		final JLabel nameOfTheTrackLabel = new JLabel("Please, choose the name of the track");
+		nameOfTheTrackLabel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		final JLabel dateLabel = new JLabel("Please, choose the month and year");
+		dateLabel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		final JTextField nameOfTheTrackTextField = new JTextField();
+		nameOfTheTrackTextField.setToolTipText("Name of the track");
+
+		final JComboBox<String> monthComboBox = new JComboBox<String>(
+				IntStream.range(1, 13).mapToObj(i -> String.format("%01d", i)).toArray(String[]::new));
+		monthComboBox.setSelectedIndex(Calendar.getInstance().get(Calendar.MONTH));
+		final JTextField yearTextField = new JTextField();
+		yearTextField.setText(Calendar.getInstance().get(Calendar.YEAR) + "");
+
+		final JButton cancelButton = new JButton("Cancel");
+		final JButton acceptButton = new JButton("Accept");
+
+		final JPanel centerPanel = new JPanel();
+
+		centerPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+		centerPanel.add(nameOfTheTrackLabel);
+		centerPanel.add(nameOfTheTrackTextField);
+		centerPanel.add(dateLabel);
+
+		final JPanel datePanel = new JPanel();
+		datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.X_AXIS));
+		datePanel.add(monthComboBox);
+		datePanel.add(yearTextField);
+
+		centerPanel.add(datePanel);
+
+		mainPanel.add(centerPanel, BorderLayout.CENTER);
+
+		final JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		buttonsPanel.add(cancelButton);
+		buttonsPanel.add(acceptButton);
+
+		mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
+		final JDialog frame = new JDialog(owner, "Antares Import Configuration", true) {
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = -4759126543816346899L;
+
+			@Override
+			public Dimension getPreferredSize() {
+				return new Dimension(300, 200);
+			}
+
+		};
+		cancelButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+
+		acceptButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				antaresImporter.setMonth(monthComboBox.getSelectedIndex());
+				antaresImporter.setYear(Integer.parseInt(yearTextField.getText()));
+				antaresImporter.setTrackName(nameOfTheTrackTextField.getText());
+				try {
+					antaresImporter.importThis(file.getName(), new FileInputStream(file));
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				frame.dispose();
+			}
+		});
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().add(mainPanel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
