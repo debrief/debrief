6ad31e060387f1c0d0dacc16b6c76953e02d8d86
Saul Hidalgo <saulhidalgoaular@gmail.com>
2020-08-20 15:00:14 +0200

    Create of the FeatureCollection object from a TrackWrapper.

--- org.mwc.debrief.legacy/src/Debrief/ReaderWriter/GeoPDF/GenerateGeoJSON.java
+++ org.mwc.debrief.legacy/src/Debrief/ReaderWriter/GeoPDF/GenerateGeoJSON.java
@@ -5,11 +5,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 
 import Debrief.GUI.Frames.Session;
+import Debrief.ReaderWriter.Replay.ImportReplay;
 import Debrief.ReaderWriter.XML.DebriefXMLReaderWriter;
 import Debrief.Wrappers.FixWrapper;
 import Debrief.Wrappers.TrackWrapper;
@@ -19,8 +22,13 @@ import MWC.GUI.Editable;
 import MWC.GUI.Layers;
 import MWC.GUI.ToolParent;
 import junit.framework.TestCase;
+import mil.nga.sf.Geometry;
+import mil.nga.sf.LineString;
+import mil.nga.sf.MultiLineString;
+import mil.nga.sf.Point;
+import mil.nga.sf.geojson.Feature;
+import mil.nga.sf.geojson.FeatureCollection;
 import mil.nga.sf.geojson.FeatureConverter;
-import mil.nga.sf.geojson.MultiLineString;
 import mil.nga.sf.geojson.Position;
 
 public class GenerateGeoJSON {
@@ -57,7 +65,7 @@ public class GenerateGeoJSON {
 						 * Let's use a MultiLineString because some tracks have multiples
 						 * segments, so we will have 1 line per segment :)
 						 */
-						final List<List<Position>> positionForTrackGeometry = new ArrayList<>();
+						final List<LineString> positionForTrackGeometry = new ArrayList<>();
 						
 						final SegmentList segmentList = currentTrack.getSegments();
 						final Enumeration<Editable> segmentEnumeration = segmentList.elements();
@@ -80,7 +88,7 @@ public class GenerateGeoJSON {
 										 * LineString then.
 										 */
 										
-										final ArrayList<Position> coordinates = new ArrayList<Position>();
+										final ArrayList<Point> coordinates = new ArrayList<Point>();
 										if (fixWrapperEditable instanceof FixWrapper) {
 											final FixWrapper currentFixWrapper = (FixWrapper) fixWrapperEditable;
 											
@@ -89,7 +97,7 @@ public class GenerateGeoJSON {
 											 */
 											if (currentFixWrapper.getVisible()) {
 												// Ok, so now let's add this position to the position list.
-												final Position newPosition = new Position(currentFixWrapper.getLocation().getLong(), currentFixWrapper.getLocation().getLat());
+												final Point newPosition = new Point(currentFixWrapper.getLocation().getLong(), currentFixWrapper.getLocation().getLat());
 												coordinates.add(newPosition);
 											}
 										}
@@ -98,25 +106,39 @@ public class GenerateGeoJSON {
 										 * In case that we have any coordinate to add, let's add it to the geometry.
 										 */
 										if (!coordinates.isEmpty()) {
-											positionForTrackGeometry.add(coordinates);
+											positionForTrackGeometry.add(new LineString(coordinates));
 										}
 									}
 								}
 							}
 						}
-						
 	
 						final MultiLineString trackAsGeometry = new MultiLineString(positionForTrackGeometry);
+						
+						
 						
 						/**
 						 * Let's create the features
 						 */
-						final String featureString = FeatureConverter.toStringValue(trackAsGeometry);
+						final Feature feature = FeatureConverter.toFeature(trackAsGeometry);
+						final HashMap<String, Object> properties = new HashMap<>();
+						
+						final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ssZ");
+						properties.put("begin", simpleDateFormat.format(currentTrack.getStartDTG().getDate()));
+						properties.put("end", simpleDateFormat.format(currentTrack.getEndDTG().getDate()));
+						feature.setProperties(properties);
+						final FeatureCollection featureCollection = new FeatureCollection(feature);
+						
+						final String featureCollectionContent = FeatureConverter.toStringValue(featureCollection);
+						
+						/*
+						
+						final String featureString = FeatureConverter.toStringValue(feature);*/
 						
 						/**
 						 * Let's write the new feature to the output stream.
 						 */
-						printWriter.println(featureString);
+						printWriter.println(featureCollectionContent);
 					}
 					
 				}
@@ -132,6 +154,7 @@ public class GenerateGeoJSON {
 	public static class GenerateGeoJSONTest extends TestCase {
 		
 		private final static String sampledpf = "../org.mwc.cmap.combined.feature/root_installs/sample_data/sample.dpf";
+		private final static String boat1rep = "../org.mwc.cmap.combined.feature/root_installs/sample_data/boat2.rep";
 		
 		private static class MockSession extends Session {
 
@@ -175,7 +198,7 @@ public class GenerateGeoJSON {
 				
 				@Override
 				public void write(int b) throws IOException {
-					builder.append(b);
+					builder.append((char)b);
 				}
 				
 				@Override
@@ -186,6 +209,34 @@ public class GenerateGeoJSON {
 			GenerateGeoJSON.createGeoJSON(layers, toStringOutputStream);
 			
 			// Test the JSON generated.
+			
+			System.out.println(toStringOutputStream.toString());
+		}
+		
+		public void testCreateGeoJSON2() throws FileNotFoundException {
+			final Layers layers = new Layers();
+			final ImportReplay replayImporter = new ImportReplay();
+			replayImporter.importThis("boat1.rep", new FileInputStream(boat1rep), layers);
+
+			final OutputStream toStringOutputStream = new OutputStream() {
+				
+				private final StringBuilder builder =  new StringBuilder();
+				
+				@Override
+				public void write(int b) throws IOException {
+					builder.append((char)b);
+				}
+				
+				@Override
+				public String toString() {
+					return builder.toString();
+				}
+			};
+			GenerateGeoJSON.createGeoJSON(layers, toStringOutputStream);
+			
+			// Test the JSON generated.
+			
+			System.out.println(toStringOutputStream.toString());
 		}
 	}
 }
