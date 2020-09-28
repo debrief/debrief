eaa0d1b396e1689b049027c3540de65b723b823b
Ayesha <Ayesha@DESKTOP-C5J4B96>
2020-09-28 14:33:57 +0530

    fixed a bug: the path was appending the incorrect absolute path
     instead of just the file name

--- org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/editors/CorePlotEditor.java
+++ org.mwc.cmap.plotViewer/src/org/mwc/cmap/plotViewer/editors/CorePlotEditor.java
@@ -795,9 +795,11 @@ public abstract class CorePlotEditor extends EditorPart implements IResourceProv
 				// if the file is dragged from outside workspace, get the location of the plot
 				// file
 				String tmpFilePath;
+				
 				try {
 					final File localFile = new File(((FileStoreEditorInput) input).getURI().toURL().getFile());
-					tmpFilePath = localFile.getParentFile().getAbsolutePath() + File.separator + tifFile;
+					
+					tmpFilePath = localFile.getParentFile().getAbsolutePath() + File.separator + getFileNameFromAbsoluteFile(tifFile.getAbsolutePath());
 				} catch (final MalformedURLException e) {
 					MWC.Utilities.Errors.Trace.trace(e, fileName + "File doesnt exist and couldnt be loaded");
 					tmpFilePath = null;
@@ -814,6 +816,18 @@ public abstract class CorePlotEditor extends EditorPart implements IResourceProv
 			return fileName;
 		}
 		return tiffFilePath;
+	}
+	
+	private String getFileNameFromAbsoluteFile(String fileName) {
+		final String retVal;
+		if(fileName.contains(File.separator)) {
+			int lastIndex = fileName.lastIndexOf(File.separator);
+			retVal =fileName.substring(lastIndex+1); 
+		}
+		else {
+			retVal = fileName;
+		}
+		return retVal;
 	}
 
 	@Override
