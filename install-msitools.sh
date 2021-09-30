b6686fb6c505e5cae5ecacde171a0ba57f7b38b8
Ian Mayo <ian@planetmayo.com>
2021-09-30 17:05:10 +0100

    Step back to previous version

--- install-msitools.sh
+++ install-msitools.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
-wget http://ftp.gnome.org/pub/GNOME/sources/msitools/0.101/msitools-0.101.tar.xz
-tar xvf msitools-0.101.tar.xz
-cd msitools-0.101 && ./configure --prefix=/usr && make -j10 && sudo make install
+wget http://ftp.gnome.org/pub/GNOME/sources/msitools/0.100/msitools-0.100.tar.xz
+tar xvf msitools-0.100.tar.xz
+cd msitools-0.100 && ./configure --prefix=/usr && make -j10 && sudo make install
 cd ..
 ls /usr/bin/wixl
 ls /usr/bin/wixl-heat
