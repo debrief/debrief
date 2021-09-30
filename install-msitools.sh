91b1d5f1effa718c1ae5642ee18b8b9a4fda8df5
Ian Mayo <ian@planetmayo.com>
2021-09-30 13:16:38 +0100

    Insert missing version number

--- install-msitools.sh
+++ install-msitools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-wget http://ftp.gnome.org/pub/GNOME/sources/msitools/0.101/msitools-0.100.tar.xz
+wget http://ftp.gnome.org/pub/GNOME/sources/msitools/0.101/msitools-0.101.tar.xz
 tar xvf msitools-0.101.tar.xz
 cd msitools-0.101 && ./configure --prefix=/usr && make -j10 && sudo make install
 cd ..
