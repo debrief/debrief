d4958c33200ad66b6f4587db3984f190116fd7c2
Saul Hidalgo <saulhidalgoaular@gmail.com>
2022-02-24 14:16:41 +0100

    New implementation of IPlatformRunnable

--- org.mwc.cmap.NarrativeViewer/src/org/mwc/cmap/NarrativeViewer/app/NarrativeViewerApplication.java
+++ org.mwc.cmap.NarrativeViewer/src/org/mwc/cmap/NarrativeViewer/app/NarrativeViewerApplication.java
@@ -15,27 +15,37 @@
 
 package org.mwc.cmap.NarrativeViewer.app;
 
-import org.eclipse.core.runtime.IPlatformRunnable;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 
 /**
  * This class controls all aspects of the application's execution
  */
-@SuppressWarnings("deprecation")
-public class NarrativeViewerApplication implements IPlatformRunnable {
+public class NarrativeViewerApplication implements IApplication {
 
 	@Override
-	public Object run(final Object args) throws Exception {
+	public Object start(IApplicationContext args) throws Exception {
 		final Display display = PlatformUI.createDisplay();
 		try {
 			final int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
 			if (returnCode == PlatformUI.RETURN_RESTART) {
-				return IPlatformRunnable.EXIT_RESTART;
+				return PlatformUI.RETURN_RESTART;
 			}
-			return IPlatformRunnable.EXIT_OK;
+			return PlatformUI.RETURN_OK;
 		} finally {
 			display.dispose();
+		}
+	}
+
+	@Override
+	public void stop() {
+		final Display display = PlatformUI.createDisplay();
+		try {
+			display.dispose();
+		}catch (Exception e) {
+			
 		}
 	}
 }
