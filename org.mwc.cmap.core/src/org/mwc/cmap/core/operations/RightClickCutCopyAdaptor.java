1398ae0f67ae23f854eefda1492b4289f6493765
Ian Mayo <ian@planetmayo.com>
2024-08-29 14:29:30 +0100

    Remove duplicate method

--- org.mwc.cmap.core/src/org/mwc/cmap/core/operations/RightClickCutCopyAdaptor.java
+++ org.mwc.cmap.core/src/org/mwc/cmap/core/operations/RightClickCutCopyAdaptor.java
@@ -1014,34 +1014,6 @@ public class RightClickCutCopyAdaptor {
 		return false;
 	}
 	
-	static boolean isTryingToDeleteAllLegItems(final Editable[] selection, final HasEditables[] parents) {
-		boolean sameParent = true;
-		HasEditables firstParent = parents[0];
-		for (int i = 1; i < parents.length; i++) {
-			if (parents[i] != firstParent) {
-				sameParent = false;
-				break;
-			}
-		}
-
-		final HasEditables[] firstArray = {firstParent};
-		final HasEditables[] safeParents = (sameParent) ? firstArray : parents;		
-		
-		// if it's a single parent, check we're not trying to delete
-		// all children
-		if(safeParents.length == 1) {
-			HasEditables parent = safeParents[0];
-			if (parent instanceof CoreTMASegment) {
-				CoreTMASegment segment = (CoreTMASegment) parent;
-				if (segment.size() == selection.length ) {
-					System.err.println("Cannot delete all items from TMA Leg");
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
 	static public void getDropdownListFor(final IMenuManager manager, final Editable[] editables,
 			final Layer[] updateLayers, final HasEditables[] parentLayers, final Layers theLayers,
 			final Clipboard _clipboard) {
